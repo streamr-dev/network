96f8267d5aa032f4bd8abcf4314458d3a32d05d6
Henri Pihkala <henri.pihkala@streamr.com>
2016-08-08 19:04:32 +0300

    restful endpoints for historical data

--- socketio-server.js
+++ socketio-server.js
@@ -1,10 +1,27 @@
-var SocketIoServer = require('./lib/socketio-server').SocketIoServer
+var app = require('express')();
+var http = require('http').Server(app);
+var cors = require('cors')
+
+var SocketIoServer = require('./lib/socketio-server')
+var RedisHelper = require('./lib/redis-helper')
+var CassandraHelper = require('./lib/cassandra-helper')
 
 var argv = require('optimist')
-	.usage('Usage: $0 --zookeeper <conn_string> --port <port>')
-	.demand(['zookeeper', 'port'])
+	.usage('Usage: $0 --redis <redis_hosts_separated_by_commas> --cassandra <cassandra_hosts_separated_by_commas> --keyspace <cassandra_keyspace> --port <port>')
+	.demand(['redis', 'cassandra', 'keyspace', 'port'])
 	.argv;
 
-var server = new SocketIoServer(argv.zookeeper, parseInt(argv.port))
+var app = require('express')();
+var http = require('http').Server(app);
 
-console.log("Server started on port "+argv.port)
+var redis = new RedisHelper(argv.redis.split(","))
+var cassandra = new CassandraHelper(argv.cassandra.split(","), argv.keyspace)
+var server = new SocketIoServer(http, redis)
+
+app.use(cors());
+
+require('./lib/cassandra-rest-endpoints')(app, cassandra)
+
+http.listen(argv.port, function() {
+	console.log("Server started on port "+argv.port)
+});
