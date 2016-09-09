b177527e2153754485b83bd941ecbc0b235067bd
Henri Pihkala <henri.pihkala@streamr.com>
2016-09-09 15:58:12 +0300

    redis decoding

--- test/test.decoder.js
+++ test/test.decoder.js
@@ -8,17 +8,19 @@ describe('decoder', function () {
 		it('should encode/decode as expected', function() {
 			var version = 28
 			var offset = 100
+			var previousOffset = 99
 			var streamId = "streamId"
 			var msg = {foo: "bar"}
 			var timestamp = Date.now()
 			var buf = decoder.encode(version, timestamp, streamId, decoder.CONTENT_TYPE_JSON, msg)
 
-			var result = decoder.decode(buf, 100)
+			var result = decoder.decode(buf, offset, previousOffset)
 
 			assert.equal(decoder.get('version', result), version)
 			assert.equal(decoder.get('streamId', result), streamId)
 			assert.equal(decoder.get('timestamp', result), timestamp)
 			assert.equal(decoder.get('offset', result), offset)
+			assert.equal(decoder.get('previousOffset', result), previousOffset)
 			assert.equal(decoder.get('contentType', result), decoder.CONTENT_TYPE_JSON)
 			assert.deepEqual(decoder.get('content', result), msg)
 		})
