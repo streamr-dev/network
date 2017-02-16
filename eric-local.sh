2453a3c77dcff3b6949284455f730af2d9937a77
Eric Andrews <eric.andrews@streamr.com>
2017-02-16 14:03:22 +0200

    update eric-local.sh

--- eric-local.sh
+++ eric-local.sh
@@ -1,1 +1,1 @@
-node socketio-server.js --port 7777 --redis 127.0.0.1 --redis-pwd notneeded --cassandra 127.0.0.1 --keyspace streamr_dev
+node socketio-server.js --port 7777 --redis 127.0.0.1 --redis-pwd kakka --cassandra 127.0.0.1 --keyspace streamr_dev
