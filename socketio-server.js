09ce1c6f73202e879ddb1a0d004fda1dbfe5c566
Eric Andrews <eric.andrews@streamr.com>
2017-02-21 22:33:54 +0200

    maybe working redis synchornization

--- socketio-server.js
+++ socketio-server.js
@@ -6,7 +6,6 @@ var SocketIoServer = require('./lib/socketio-server')
 var RedisHelper = require('./lib/redis-helper')
 var RedisOffsetFetcher = require('./lib/redis-offset-fetcher')
 var CassandraHelper = require('./lib/cassandra-helper')
-var LatestOffsetBackedCassandraHelper = require('./lib/latest-offset-backed-cassandra-helper')
 
 var argv = require('optimist')
 	.usage('Usage: $0 --redis <redis_hosts_separated_by_commas> --redis-pwd <password> --cassandra <cassandra_hosts_separated_by_commas> --keyspace <cassandra_keyspace> --port <port>')
@@ -19,12 +18,11 @@ var http = require('http').Server(app);
 var redis = new RedisHelper(argv.redis.split(","), argv['redis-pwd'])
 var cassandra = new CassandraHelper(argv.cassandra.split(","), argv.keyspace)
 var redisOffsetFetcher = new RedisOffsetFetcher(argv.redis.split(",")[0], argv['redis-pwd'])
-var redisBackedCassandra = new LatestOffsetBackedCassandraHelper(cassandra, redisOffsetFetcher)
-var server = new SocketIoServer(http, redis, redisBackedCassandra)
+var server = new SocketIoServer(http, redis, cassandra, redisOffsetFetcher)
 
 app.use(cors());
 
-require('./lib/cassandra-rest-endpoints')(app, redisBackedCassandra)
+require('./lib/cassandra-rest-endpoints')(app, cassandra)
 
 http.listen(argv.port, function() {
 	console.log("Server started on port "+argv.port)
