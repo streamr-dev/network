a372b9fef0abc26b7fa07f7b93557c1061c8f5f0
Eric Andrews <eric.andrews@streamr.com>
2017-02-21 18:14:22 +0200

    LatestOffsetBackedCassandraHelper (wip)

--- socketio-server.js
+++ socketio-server.js
@@ -4,7 +4,9 @@ var cors = require('cors')
 
 var SocketIoServer = require('./lib/socketio-server')
 var RedisHelper = require('./lib/redis-helper')
+var RedisOffsetFetcher = require('./lib/redis-offset-fetcher')
 var CassandraHelper = require('./lib/cassandra-helper')
+var LatestOffsetBackedCassandraHelper = require('./lib/latest-offset-backed-cassandra-helper')
 
 var argv = require('optimist')
 	.usage('Usage: $0 --redis <redis_hosts_separated_by_commas> --redis-pwd <password> --cassandra <cassandra_hosts_separated_by_commas> --keyspace <cassandra_keyspace> --port <port>')
@@ -16,11 +18,13 @@ var http = require('http').Server(app);
 
 var redis = new RedisHelper(argv.redis.split(","), argv['redis-pwd'])
 var cassandra = new CassandraHelper(argv.cassandra.split(","), argv.keyspace)
-var server = new SocketIoServer(http, redis, cassandra)
+var redisOffsetFetcher = new RedisOffsetFetcher(argv.redis.split(",")[0], argv['redis-pwd'])
+var redisBackedCassandra = new LatestOffsetBackedCassandraHelper(cassandra, redisOffsetFetcher)
+var server = new SocketIoServer(http, redis, redisBackedCassandra)
 
 app.use(cors());
 
-require('./lib/cassandra-rest-endpoints')(app, cassandra)
+require('./lib/cassandra-rest-endpoints')(app, redisBackedCassandra)
 
 http.listen(argv.port, function() {
 	console.log("Server started on port "+argv.port)
--- test/test.redis-helper.js
+++ test/test.redis-helper.js
@@ -2,19 +2,19 @@ const assert = require('assert')
 const sinon = require('sinon')
 const redis = require('redis')
 
-const RedisHelper = require('../lib/redis-helper')
+const RedisOffsetFetcher = require('../lib/redis-offset-fetcher')
 
-describe('RedisHelper', function() {
+describe('RedisOffsetFetcher', function() {
 
 	const REDIS_HOST = "127.0.0.1"
 	const REDIS_PASS = "kakka"
 
 	var testRedisClient
-	var redisHelper
+	var redisOffsetFetcher
 
 	beforeEach(function() {
 		testRedisClient = redis.createClient({ host: REDIS_HOST, password: REDIS_PASS })
-		redisHelper = new RedisHelper([REDIS_HOST], REDIS_PASS)
+		redisOffsetFetcher = new RedisOffsetFetcher(REDIS_HOST, REDIS_PASS)
 	})
 
 	afterEach(function() {
@@ -23,18 +23,16 @@ describe('RedisHelper', function() {
 	})
 
 	describe('fetchOffset', function() {
-		it("returns null if key doesn't exist", function(done) {
-			redisHelper.fetchOffset("stream-1", 0, function(result) {
-				assert.equal(result, null)
-				done()
-			});
+		it("returns null if key doesn't exist", function() {
+			return redisOffsetFetcher.fetchOffset("stream-1", 0).then(function(value) {
+				assert.equal(value, null)
+			})
 		})
 
-		it("returns value if key exists", function(done) {
+		it("returns value if key exists", function() {
 			testRedisClient.setex("stream-1-0", 10, "2487679201527")
-			redisHelper.fetchOffset("stream-1", 0, function(result) {
-				assert.equal(result, 2487679201527)
-				done()
+			return redisOffsetFetcher.fetchOffset("stream-1", 0).then(function(value) {
+				assert.equal(value, 2487679201527)
 			});
 		})
 	})
