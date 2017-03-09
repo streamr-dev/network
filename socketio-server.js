7e73a1c4e95ed397a8e53750a3460b90fb63fbcf
Eric Andrews <eric.andrews@streamr.com>
2017-03-09 18:30:49 +0200

    encode streamr URL into environments

--- socketio-server.js
+++ socketio-server.js
@@ -9,18 +9,18 @@ var RedisOffsetFetcher = require('./lib/redis-offset-fetcher')
 var CassandraHelper = require('./lib/cassandra-helper')
 
 var argv = require('optimist')
-	.usage('Usage: $0 --redis <redis_hosts_separated_by_commas> --redis-pwd <password> --cassandra <cassandra_hosts_separated_by_commas> --keyspace <cassandra_keyspace> --port <port>')
-	.demand(['redis', 'redis-pwd', 'cassandra', 'keyspace', 'port'])
+	.usage('Usage: $0 --redis <redis_hosts_separated_by_commas> --redis-pwd <password> --cassandra <cassandra_hosts_separated_by_commas> --keyspace <cassandra_keyspace> --streamr <streamr> --port <port>')
+	.demand(['redis', 'redis-pwd', 'cassandra', 'keyspace', 'streamr', 'port'])
 	.argv;
 
 var app = require('express')();
 var http = require('http').Server(app);
 
-var authenticator = new Authenticator('http://dev.streamr')
+var authenticator = new Authenticator(argv['streamr'])
 var redis = new RedisHelper(argv.redis.split(","), argv['redis-pwd'])
 var cassandra = new CassandraHelper(argv.cassandra.split(","), argv.keyspace)
 var redisOffsetFetcher = new RedisOffsetFetcher(argv.redis.split(",")[0], argv['redis-pwd'])
-var server = new SocketIoServer(http, redis, cassandra, redisOffsetFetcher, authenticator)
+var server = new SocketIoServer(http, redis, cassandra, redisOffsetFetcher, null, authenticator)
 
 app.use(cors());
 
