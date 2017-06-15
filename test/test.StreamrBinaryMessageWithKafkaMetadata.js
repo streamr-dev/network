87225a2d1f90030dc51cd6159e93c60696f109bb
Eric Andrews <eric.andrews@streamr.com>
2017-06-15 15:46:02 +0300

    Unit tests  StreamrBinaryMessageWithKafkaMetadata

--- test/test.StreamrBinaryMessageWithKafkaMetadata.js
+++ test/test.StreamrBinaryMessageWithKafkaMetadata.js
@@ -1,82 +1,136 @@
-var assert = require('assert')
-var sinon = require('sinon')
-var StreamrBinaryMessage = require('../lib/protocol/StreamrBinaryMessage')
-var StreamrBinaryMessageWithKafkaMetadata = require('../lib/protocol/StreamrBinaryMessageWithKafkaMetadata')
+const assert = require('assert')
+const sinon = require('sinon')
+const StreamrBinaryMessage = require('../lib/protocol/StreamrBinaryMessage')
+const StreamrBinaryMessageWithKafkaMetadata = require('../lib/protocol/StreamrBinaryMessageWithKafkaMetadata')
 
 describe('StreamrBinaryMessageWithKafkaMetadata', function () {
 
-	var offset = 100
-	var previousOffset = 99
-	var kafkaPartition = 0
-
-	var streamId = "streamId"
-	var streamPartition = 0
-	var content = JSON.stringify({foo: "bar"})
-	var timestamp = Date.now()
-	var ttl = 100
-
-	var version
-
 	describe('version 0', function() {
 
-		var streamrBinaryMessage
+		let msg
+		let msgWithMetadata
+
+		before(function() {
+			sinon.test = require('sinon-test').configureTest(sinon)
+		})
 
 		beforeEach(function() {
-			version = 0
-			streamrBinaryMessage = new StreamrBinaryMessage(streamId, streamPartition, timestamp, ttl, StreamrBinaryMessage.CONTENT_TYPE_JSON, content)
+			const streamId = "streamId"
+			const streamPartition = 0
+			const timestamp = 1497529459457
+			const ttl = 100
+			const content = '{"foo":"bar"}'
+			const offset = 100
+			const previousOffset = 99
+			const kafkaPartition = 0
+
+			msg = new StreamrBinaryMessage(
+				streamId,
+				streamPartition,
+				timestamp,
+				ttl,
+				StreamrBinaryMessage.CONTENT_TYPE_JSON,
+				content
+			)
+
+			msgWithMetadata = new StreamrBinaryMessageWithKafkaMetadata(msg, offset, previousOffset, kafkaPartition)
 		})
 
 		describe('toBytes/fromBytes', function() {
 
-			it('must encode/decode the values properly', function() {
-				var bytes = new StreamrBinaryMessageWithKafkaMetadata(streamrBinaryMessage, offset, previousOffset, kafkaPartition).toBytes()
-				var m = StreamrBinaryMessageWithKafkaMetadata.fromBytes(bytes)
+			it('encodes/decodes message fields properly', function() {
+				const msgAsBytes = msgWithMetadata.toBytes()
+				const rebuiltMsg = StreamrBinaryMessageWithKafkaMetadata.fromBytes(msgAsBytes)
 
-				assert.equal(m.version, version)
-				assert.equal(m.offset, offset)
-				assert.equal(m.previousOffset, previousOffset)
+				assert.equal(rebuiltMsg.version, 0)
+				assert.equal(rebuiltMsg.offset, 100)
+				assert.equal(rebuiltMsg.previousOffset, 99)
 			})
 
-			it('must support undefined previousOffset', function() {
-				previousOffset = undefined
-				var bytes = new StreamrBinaryMessageWithKafkaMetadata(streamrBinaryMessage, offset, previousOffset, kafkaPartition).toBytes()
-				var m = StreamrBinaryMessageWithKafkaMetadata.fromBytes(bytes)
+			it('support undefined previousOffset', function() {
+				const msgAsBytes = new StreamrBinaryMessageWithKafkaMetadata(msg, 100, undefined, 0).toBytes()
+				const rebuiltMsg = StreamrBinaryMessageWithKafkaMetadata.fromBytes(msgAsBytes)
 
-				assert.equal(m.previousOffset, previousOffset)
+				assert.equal(rebuiltMsg.previousOffset, undefined)
 			})
 
-			it('must keep the wrapped StreamrBinaryMessage untouched', function() {
-				var bytes = new StreamrBinaryMessageWithKafkaMetadata(streamrBinaryMessage, offset, previousOffset, kafkaPartition).toBytes()
-				var m = StreamrBinaryMessageWithKafkaMetadata.fromBytes(bytes).getStreamrBinaryMessage()
+			it('keeps wrapped StreamrBinaryMessage untouched', function() {
+				const msgAsBytes = msgWithMetadata.toBytes()
+				const rebuiltMsg = StreamrBinaryMessageWithKafkaMetadata.fromBytes(msgAsBytes).getStreamrBinaryMessage()
 
-				assert.equal(m.streamId, streamId)
-				assert.equal(m.streamPartition, streamPartition)
-				assert.equal(m.timestamp, timestamp)
-				assert.equal(m.ttl, ttl)
-				assert.equal(m.contentType, StreamrBinaryMessage.CONTENT_TYPE_JSON)
-				assert.deepEqual(m.content, content)
+				assert.deepEqual(rebuiltMsg, msg)
 			})
 
-			it('will not call StreamrBinaryMessage.fromBytes() when StreamrBinaryMessage passed as buffer', function() {
+			it('does not call StreamrBinaryMessage.fromBytes() when StreamrBinaryMessage passed as buffer', function() {
 				// sinon.test() sandbox removes the spy when done
 				sinon.test(function() {
 					this.spy(StreamrBinaryMessage, "fromBytes")
 
-					var bytes = new StreamrBinaryMessageWithKafkaMetadata(streamrBinaryMessage.toBytes(), offset, previousOffset, kafkaPartition).toBytes()
-					StreamrBinaryMessageWithKafkaMetadata.fromBytes(bytes).getStreamrBinaryMessage()
+					const msgAsBytes = new StreamrBinaryMessageWithKafkaMetadata(msg.toBytes(), 100, 99, 0).toBytes()
+					StreamrBinaryMessageWithKafkaMetadata.fromBytes(msgAsBytes).getStreamrBinaryMessage()
 
 					assert.equal(StreamrBinaryMessage.fromBytes.callCount, 0)
 				})
 			})
 		})
 
-		describe('toArray()', function() {
-			it('must produce the correct result', function() {
-				var msg = new StreamrBinaryMessageWithKafkaMetadata(streamrBinaryMessage, offset, previousOffset, kafkaPartition)
-				var arr = msg.toArray()
-				var s = streamrBinaryMessage
+		describe('toArray(contentAsBuffer)', function() {
+			it('returns data in array format given contentAsBuffer=true', function() {
+				assert.deepEqual(msgWithMetadata.toArray(true), [
+					28,
+					"streamId",
+					0,
+					1497529459457,
+					100,
+					100,
+					99,
+					StreamrBinaryMessage.CONTENT_TYPE_JSON,
+					'{"foo":"bar"}'
+				])
+			})
 
-				assert.deepEqual(arr, [s.version, s.streamId, s.streamPartition, s.timestamp, s.ttl, msg.offset, msg.previousOffset, s.contentType, s.content])
+			it('returns data in array format with pre-parsed content contentAsBuffer=false', function() {
+				assert.deepEqual(msgWithMetadata.toArray(false), [
+					28,
+					"streamId",
+					0,
+					1497529459457,
+					100,
+					100,
+					99,
+					StreamrBinaryMessage.CONTENT_TYPE_JSON,
+					{ foo: "bar" }
+				])
+			})
+		})
+
+		describe('toObject(contentAsBuffer)', function() {
+			it('returns data in object format given contentAsBuffer=true', function() {
+				assert.deepEqual(msgWithMetadata.toObject(true), {
+					version: 28,
+					streamId: "streamId",
+					partition: 0,
+					timestamp: 1497529459457,
+					ttl: 100,
+					offset: 100,
+					previousOffset: 99,
+					contentType: StreamrBinaryMessage.CONTENT_TYPE_JSON,
+					content: '{"foo":"bar"}'
+				})
+			})
+
+			it('returns data in object format with pre-parsed content contentAsBuffer=false', function() {
+				assert.deepEqual(msgWithMetadata.toObject(false), {
+					version: 28,
+					streamId: "streamId",
+					partition: 0,
+					timestamp: 1497529459457,
+					ttl: 100,
+					offset: 100,
+					previousOffset: 99,
+					contentType: StreamrBinaryMessage.CONTENT_TYPE_JSON,
+					content: { foo: "bar" }
+				})
 			})
 		})
 
