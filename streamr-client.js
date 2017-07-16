9274db9d031e379dde843a16510908bd39600d0e
Aapeli Haanpuu <aapzu@iki.fi>
2017-07-16 12:20:57 +0200

    Add a hack to make streamr-client work with Webpack
    * client has its own ws now. examples fixed
    
    * think (again) that I fixed the modularization issues
    
    * added comments to describe changes

--- streamr-client.js
+++ streamr-client.js
@@ -1,24 +1,21 @@
 "use strict";
 
 (function() {
-
-	var debug
-	if (typeof module !== 'undefined') {
-		debug = require('debug')('StreamrClient')
-	} else {
-		debug = (window.debug ? window.debug('StreamrClient') : function() {
-			if (window.consoleLoggingEnabled)
-				console.log.apply(console, arguments)
-		})
-	}
 	
-	var WebSocket
-	if (typeof module !== 'undefined') {
-		WebSocket = require('ws')
-	} else {
-		WebSocket = window.WebSocket
-	}
-
+	/* <---
+	Checking of the environment must be done with ternary operator instead of if-clause because of (possible) usage in Webpack.
+	With if-clause, Webpack forces the code to use require, also in browser.
+	Now it just works, in browser, in Node.js and in browser with Webpack. */
+	var debug = (typeof window !== 'undefined' && window.debug) ? window.debug('StreamrClient') : // If in browser and window.debug exists
+		(typeof require !== 'undefined' ? require('debug')('StreamrClient') : // Else if in node
+			function() { // Else use mock
+				if (window.consoleLoggingEnabled)
+					console.log.apply(console, arguments)
+			})
+	
+	var WebSocket = typeof window !== 'undefined' ? window.WebSocket : require('ws')
+	/* ---> */
+	
 	var BYE_KEY = "_bye"
 
 	function extend() {
