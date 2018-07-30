f689a0e8237016155f0b9804e1ef7de52e40ea6c
Henri Pihkala <henri.pihkala@unifina.com>
2018-07-30 15:53:15 +0300

    fix code style

--- data-api.js
+++ data-api.js
@@ -1,32 +1,32 @@
-var argv = require('optimist')
-	.usage('Usage: $0 --data-topic <topic> --zookeeper <conn_string> --redis <redis_hosts_separated_by_commas> --redis-pwd <password> --cassandra <cassandra_hosts_separated_by_commas> --keyspace <cassandra_keyspace> --streamr <streamr> --port <port>')
-	.demand(['data-topic', 'zookeeper', 'redis', 'redis-pwd', 'cassandra', 'keyspace', 'streamr', 'port'])
-	.argv
+const argv = require('optimist')
+    .usage('Usage: $0 --data-topic <topic> --zookeeper <conn_string> --redis <redis_hosts_separated_by_commas> --redis-pwd <password> --cassandra <cassandra_hosts_separated_by_commas> --keyspace <cassandra_keyspace> --streamr <streamr> --port <port>')
+    .demand(['data-topic', 'zookeeper', 'redis', 'redis-pwd', 'cassandra', 'keyspace', 'streamr', 'port'])
+    .argv
 
-var StreamFetcher = require('./src/StreamFetcher')
-var WebsocketServer = require('./src/WebsocketServer')
-var RedisHelper = require('./src/RedisUtil')
-var RedisOffsetFetcher = require('./src/RedisOffsetFetcher')
-var CassandraHelper = require('./src/CassandraUtil')
+const cors = require('cors')
+const StreamFetcher = require('./src/StreamFetcher')
+const WebsocketServer = require('./src/WebsocketServer')
+const RedisHelper = require('./src/RedisUtil')
+const RedisOffsetFetcher = require('./src/RedisOffsetFetcher')
+const CassandraHelper = require('./src/CassandraUtil')
 const StreamrKafkaProducer = require('./src/KafkaUtil')
 const partitioner = require('./src/Partitioner')
 
-var cors = require('cors')
-var app = require('express')()
-var http = require('http').Server(app)
+const app = require('express')()
+const http = require('http').Server(app)
 
-var streamFetcher = new StreamFetcher(argv['streamr'])
-var redis = new RedisHelper(argv.redis.split(","), argv['redis-pwd'])
-var cassandra = new CassandraHelper(argv.cassandra.split(","), argv.keyspace)
-var redisOffsetFetcher = new RedisOffsetFetcher(argv.redis.split(",")[0], argv['redis-pwd'])
-const kafka = new StreamrKafkaProducer(argv['data-topic'], partitioner, argv['zookeeper'])
+const streamFetcher = new StreamFetcher(argv.streamr)
+const redis = new RedisHelper(argv.redis.split(','), argv['redis-pwd'])
+const cassandra = new CassandraHelper(argv.cassandra.split(','), argv.keyspace)
+const redisOffsetFetcher = new RedisOffsetFetcher(argv.redis.split(',')[0], argv['redis-pwd'])
+const kafka = new StreamrKafkaProducer(argv['data-topic'], partitioner, argv.zookeeper)
 
 app.use(cors())
 
 /**
  * Streaming endpoints
  */
-var server = new WebsocketServer(http, redis, cassandra, redisOffsetFetcher, null, streamFetcher)
+const server = new WebsocketServer(http, redis, cassandra, redisOffsetFetcher, null, streamFetcher)
 
 /**
  * REST endpoints
@@ -34,10 +34,10 @@ var server = new WebsocketServer(http, redis, cassandra, redisOffsetFetcher, nul
 app.use('/api/v1', require('./src/rest/DataQueryEndpoints')(cassandra, streamFetcher))
 app.use('/api/v1', require('./src/rest/DataProduceEndpoints')(streamFetcher, kafka, partitioner))
 
-http.listen(argv.port, function() {
-	console.log("Configured with Redis: " + argv.redis)
-	console.log("Configured with Cassandra: " + argv.cassandra)
-	console.log("Configured with Kafka: " + argv['zookeeper'] + " and topic '" + argv['data-topic'] + "'")
-	console.log("Configured with Streamr: " + argv['streamr'])
-	console.log("Listening on port "+argv.port)
+http.listen(argv.port, () => {
+    console.log(`Configured with Redis: ${argv.redis}`)
+    console.log(`Configured with Cassandra: ${argv.cassandra}`)
+    console.log(`Configured with Kafka: ${argv.zookeeper} and topic '${argv['data-topic']}'`)
+    console.log(`Configured with Streamr: ${argv.streamr}`)
+    console.log(`Listening on port ${argv.port}`)
 })
