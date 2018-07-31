ea4b0ae2a133eb0f9ee4c29bc5e64fbdb5d5d798
Henri Pihkala <henri.pihkala@unifina.com>
2018-07-31 17:41:27 +0200

    fix jest coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -18,10 +18,10 @@ module.exports = {
     clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
-    // collectCoverage: false,
+    collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: null,
+    collectCoverageFrom: ['data-api.js', 'src/**'],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
