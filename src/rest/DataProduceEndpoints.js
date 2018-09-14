ec5837bd5885ca274e3aa7f5a23af80424030ac2
Eric Andrews <eric.andrews@streamr.com>
2018-09-14 16:11:23 +0300

    rm StreamrBinaryMessage stuff for now

--- src/rest/DataProduceEndpoints.js
+++ src/rest/DataProduceEndpoints.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const StreamrBinaryMessage = require('../protocol/StreamrBinaryMessage')
 const InvalidMessageContentError = require('../errors/InvalidMessageContentError')
 const FailedToPublishError = require('../errors/FailedToPublishError')
 const NotReadyError = require('../errors/NotReadyError')
