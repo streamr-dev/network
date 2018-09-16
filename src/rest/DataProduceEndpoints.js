fcc7c290c86732582efd6f5cab4cfb40a0bcefb1
Eric Andrews <eric.andrews@streamr.com>
2018-09-16 17:58:38 +0300

    Revert "rm StreamrBinaryMessage stuff for now"
    This reverts commit ec5837bd5885ca274e3aa7f5a23af80424030ac2.

--- src/rest/DataProduceEndpoints.js
+++ src/rest/DataProduceEndpoints.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const StreamrBinaryMessage = require('../protocol/StreamrBinaryMessage')
 const InvalidMessageContentError = require('../errors/InvalidMessageContentError')
 const FailedToPublishError = require('../errors/FailedToPublishError')
 const NotReadyError = require('../errors/NotReadyError')
---
+++ test/unit/protocol/StreamrBinaryMessage.test.js
@@ -0,0 +1,96 @@
+const assert = require('assert')
+const sinon = require('sinon')
+const StreamrBinaryMessage = require('../../../src/protocol/StreamrBinaryMessage')
+
+describe('StreamrBinaryMessage', () => {
+    let version
+    const streamId = 'streamId'
+    const streamPartition = 0
+    const msg = {
+        foo: 'bar',
+    }
+    const timestamp = Date.now()
+    const ttl = 100
+
+    describe('version 28', () => {
+        let bytes
+
+        beforeEach(() => {
+            version = 28
+            bytes = new StreamrBinaryMessage(
+                streamId, streamPartition, timestamp, ttl,
+                StreamrBinaryMessage.CONTENT_TYPE_JSON, Buffer.from(JSON.stringify(msg), 'utf8'),
+            ).toBytes()
+        })
+
+        describe('toBytes/fromBytes', () => {
+            it('must not alter the field content', () => {
+                const m = StreamrBinaryMessage.fromBytes(bytes)
+
+                assert.equal(m.version, version)
+                assert.equal(m.streamId, streamId)
+                assert.equal(m.streamPartition, streamPartition)
+                assert.equal(m.timestamp, timestamp)
+                assert.equal(m.ttl, ttl)
+                assert.equal(m.contentType, StreamrBinaryMessage.CONTENT_TYPE_JSON)
+                assert.deepEqual(m.getContentParsed(), msg)
+            })
+
+            describe('with sinon spys on JSON object', () => {
+                beforeEach(() => {
+                    sinon.spy(JSON, 'parse')
+                    sinon.spy(JSON, 'stringify')
+                })
+
+                afterEach(() => {
+                    JSON.parse.restore()
+                    JSON.stringify.restore()
+                })
+
+                it('must not parse the content with contentAsBuffer=true', () => {
+                    const m = StreamrBinaryMessage.fromBytes(bytes, true)
+
+                    assert.equal(m.version, version)
+                    assert.equal(m.streamId, streamId)
+                    assert.equal(m.streamPartition, streamPartition)
+                    assert.equal(m.timestamp, timestamp)
+                    assert.equal(m.ttl, ttl)
+                    assert.equal(m.contentType, StreamrBinaryMessage.CONTENT_TYPE_JSON)
+                    assert(Buffer.isBuffer(m.content))
+
+                    // Since the content was passed as a buffer, it should remain as is on toBytes()
+                    m.toBytes()
+                    assert.equal(JSON.parse.callCount, 0)
+                    assert.equal(JSON.parse.callCount, 0)
+                })
+            })
+        })
+
+        describe('constructor', () => {
+            it('must accept a buffer content', () => {
+                new StreamrBinaryMessage(
+                    streamId, streamPartition, timestamp, ttl,
+                    StreamrBinaryMessage.CONTENT_TYPE_JSON, Buffer.from(JSON.stringify(msg), 'utf8'),
+                )
+            })
+
+            it('must accept a string content', () => {
+                new StreamrBinaryMessage(
+                    streamId, streamPartition, timestamp, ttl,
+                    StreamrBinaryMessage.CONTENT_TYPE_JSON, 'I AM A STRING',
+                )
+            })
+
+            it('must throw if content is not a buffer or a string', () => {
+                assert.throws(() => {
+                    new StreamrBinaryMessage(
+                        streamId, streamPartition, timestamp, ttl,
+                        StreamrBinaryMessage.CONTENT_TYPE_JSON, {
+                            iam: 'an object',
+                        },
+                    )
+                })
+            })
+        })
+    })
+})
