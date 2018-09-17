df0de5e8354a26d24c370b7d4368583e5de653fe
Eric Andrews <eric.andrews@streamr.com>
2018-09-17 17:12:14 +0300

    fix bug #64

--- node.js
+++ node.js
@@ -1,10 +1,12 @@
 const { createConnection } = require('./src/connection/Connection')
+const NodeToNode = require('./src/protocol/NodeToNode')
+const TrackerNode = require('./src/protocol/TrackerNode')
 const Node = require('./src/logic/Node')
 
 const port = process.argv[2] || 30301
 
 createConnection('127.0.0.1', port, '', true).then((connection) => {
-    return new Node(connection)
+    return new Node(new TrackerNode(connection), new NodeToNode(connection))
 }).catch((err) => {
     throw err
 })
