d11bd3d59b81021596daec07606c500265315f6e
Eric Andrews <eric.andrews@streamr.com>
2019-02-07 15:58:45 +0200

    Revert "Storage: fetch from starting timestamp"
    This reverts commit f35e1ed1b3be81977faf13ddd13a2f863ad455c4.

--- src/Storage.js
+++ src/Storage.js
@@ -1,4 +1,3 @@
-const { Transform } = require('stream')
 const cassandra = require('cassandra-driver')
 
 const callbackToPromise = (method, ...args) => {
@@ -9,25 +8,10 @@ const callbackToPromise = (method, ...args) => {
     })
 }
 
-const transformRowToMessage = new Transform({
-    objectMode: true,
-    transform: (row, _, done) => {
-        done(null, {
-            streamId: row.id,
-            streamPartition: row.partition,
-            ts: row.ts.getTime(),
-            sequenceNo: row.sequence_no,
-            publisherId: row.publisher_id,
-            payload: row.payload.toString(),
-        })
-    },
-})
-
 class Storage {
     constructor(cassandraClient) {
         this.execute = cassandraClient.execute.bind(cassandraClient)
         this.shutdown = cassandraClient.shutdown.bind(cassandraClient)
-        this.stream = cassandraClient.stream.bind(cassandraClient)
     }
 
     store(streamId, streamPartition, timestamp, sequenceNo, publisherId, payload) {
@@ -44,20 +28,6 @@ class Storage {
         ], {
             prepare: true,
         })
-    }
-
-    fetchFromTimestamp(streamId, streamPartition, from) {
-        if (!Number.isInteger(from)) {
-            throw new Error('from is not an integer')
-        }
-
-        const query = 'SELECT * FROM stream_data WHERE id = ? AND partition = ? AND ts >= ? ORDER BY ts ASC'
-        const queryParams = [streamId, streamPartition, from]
-
-        return this.stream(query, queryParams, {
-            prepare: true,
-            autoPage: true,
-        }).pipe(transformRowToMessage)
     }
 
     close() {
