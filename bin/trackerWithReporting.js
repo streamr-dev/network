e65cc2435ab95b1efc4553b4ba133fb2929343f3
Miroslav Pokrovskii <7105861+mirpo@users.noreply.github.com>
2019-07-23 11:10:50 +0300

    Configure sentry (#303)

--- bin/trackerWithReporting.js
+++ bin/trackerWithReporting.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const StreamrClient = require('streamr-client')
+const Sentry = require('@sentry/node')
 
 const { startTracker } = require('../src/composition')
 
@@ -10,6 +11,16 @@ const maxNeighborsPerNode = process.argv[4] || 4
 const apiKey = process.argv[5] || 'EmqyPJBAR-26T60BbxLazQhN8GKqhOQQe2rbEqRwECCQ'
 const streamId = process.argv[6] || 'cueeTiqTQUmHjZJhv4rOhA'
 const id = `tracker-${port}`
+
+Sentry.init({
+    dsn: 'https://0fcf3b8f6b254caa9a7fadd77bcc37a4@sentry.io/1510389',
+    integrations: [
+        new Sentry.Integrations.Console({
+            levels: ['error']
+        })
+    ],
+    environment: 'tracker'
+})
 
 startTracker(ip, port, id, maxNeighborsPerNode)
     .then((tracker) => {
