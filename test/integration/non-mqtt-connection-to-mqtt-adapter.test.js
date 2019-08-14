e9147c95ec5554e492fee9b10e6ecb41e0f5895d
Eric Andrews <eric.andrews@streamr.com>
2019-08-14 17:21:47 +0300

    Integration test for when non-mqtt connection is established to MQTT adapter (#27)
    * test for mqtt-bad-connection
    
    * rename test
    
    * rm unused require

---
+++ test/integration/non-mqtt-connection-to-mqtt-adapter.test.js
@@ -0,0 +1,67 @@
+const net = require('net')
+
+const { startTracker } = require('@streamr/streamr-p2p-network')
+
+const createBroker = require('../../src/broker')
+
+const trackerPort = 12411
+const brokerPort = 12412
+const mqttPort = 12413
+
+describe('non-mqtt connection to MQTT adapter', () => {
+    let tracker
+    let broker
+
+    beforeEach(async () => {
+        tracker = await startTracker('127.0.0.1', trackerPort, 'tracker')
+        broker = await createBroker({
+            network: {
+                id: 'broker',
+                hostname: '127.0.0.1',
+                port: brokerPort,
+                advertisedWsUrl: null,
+                tracker: `ws://127.0.0.1:${trackerPort}`,
+                isStorageNode: false
+            },
+            cassandra: false,
+            reporting: false,
+            streamrUrl: 'http://localhost:8081/streamr-core',
+            adapters: [
+                {
+                    name: 'mqtt',
+                    port: mqttPort,
+                    streamsTimeout: 300000
+                }
+            ],
+        })
+    })
+
+    afterEach(async () => {
+        broker.close()
+        tracker.stop()
+    })
+
+    test('sending unrecognized packets causes client to be dropped without server crashing', (done) => {
+        const socket = new net.Socket()
+
+        socket.connect(mqttPort, '127.0.0.1', () => {
+            for (let i = 0; i < 100; ++i) {
+                socket.write('nonsensepackage\r\n')
+            }
+        })
+
+        socket.on('close', (hadError) => {
+            // Make sure we didn't close with error
+            expect(hadError).toEqual(false)
+
+            // Ensure that server is indeed still up
+            const newSocket = new net.Socket()
+            newSocket.on('error', (err) => {
+                done(err)
+            })
+            newSocket.connect(mqttPort, '127.0.0.1', () => {
+                done()
+            })
+        })
+    })
+})
