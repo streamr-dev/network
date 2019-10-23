48a242dfe2b0f953532617222307a2a1a4052fa6
Henri Pihkala <hpihkala@users.noreply.github.com>
2019-10-23 14:57:09 +0300

    stream.publish(...) equals client.publish(stream, ...)

--- src/rest/domain/Stream.js
+++ src/rest/domain/Stream.js
@@ -88,7 +88,7 @@ export default class Stream {
         )
     }
 
-    publish(data, timestamp = Date.now()) {
-        return this._client.publish(this.id, data, timestamp)
+    publish(...theArgs) {
+        return this._client.publish(this.id, ...theArgs)
     }
 }
