695da21defddad8d9f91e54a5e9c310272193aa2
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-10-23 10:41:27 +0300

    Fix DATA token address

--- src/StreamrClient.js
+++ src/StreamrClient.js
@@ -61,7 +61,7 @@ export default class StreamrClient extends EventEmitter {
             subscriberGroupKeys: {}, // {streamId: {publisherId: groupKey}}
             streamrNodeAddress: '0xf3E5A65851C3779f468c9EcB32E6f25D9D68601a',
             streamrOperatorAddress: '0xc0aa4dC0763550161a6B59fa430361b5a26df28C',
-            tokenAddress: '0xc0aa4dC0763550161a6B59fa430361b5a26df28C',
+            tokenAddress: '0x0Cf0Ee63788A0849fE5297F3407f701E122cC023',
         }
         this.subscribedStreams = {}
 
