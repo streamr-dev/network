e05ea4e314adfc41a4bf303c170e00a4cb2ca87f
mikhaelsantos <mikhaelsantos@gmail.com>
2019-10-26 16:48:59 +0300

    fix: static start services

--- .codedeploy/start_server.sh
+++ .codedeploy/start_server.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-service network-node* start
+service network-node start
+service network-node-2 start
+service network-node-3 start
+service network-node-4 start
+service network-node-5 start
+service network-node-6 start
