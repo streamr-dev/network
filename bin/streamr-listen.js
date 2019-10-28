8cc9579bc15a5728fc3cc96d07133ee3ac97a9aa
Eric Andrews <eric.andrews@streamr.com>
2019-10-28 08:14:18 +0200

    add --partition support to listen command (#18)

--- bin/streamr-listen.js
+++ bin/streamr-listen.js
@@ -6,6 +6,7 @@ const { envOptions, exitWitHelpIfArgsNotBetween, formStreamrOptionsWithEnv } = r
 program
     .usage('<streamId> [apiKey]')
     .description('subscribe and listen to a stream, prints JSON messages to stdout line-by-line')
+    .option('-p, --partition [partition]', 'partition', (s) => parseInt(s), 0)
 envOptions(program)
     .version(require('../package.json').version)
     .parse(process.argv)
@@ -13,4 +14,4 @@ envOptions(program)
 exitWitHelpIfArgsNotBetween(program, 1, 2)
 
 const options = formStreamrOptionsWithEnv(program)
-listen(program.args[0], program.args[1], options)
+listen(program.args[0], program.partition, program.args[1], options)
--- src/listen.js
+++ src/listen.js
@@ -1,12 +1,13 @@
 const StreamrClient = require('streamr-client')
 
-module.exports = function listen(stream, apiKey, streamrOptions) {
+module.exports = function listen(stream, partition, apiKey, streamrOptions) {
     const options = { ...streamrOptions }
     if (apiKey != null) {
         options.auth = { apiKey }
     }
     new StreamrClient(options).subscribe({
         stream,
+        partition,
         apiKey
     }, (message, metadata) => console.info(JSON.stringify(message)))
 }
