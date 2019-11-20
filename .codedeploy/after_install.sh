f23b3795a34699a4139ca5715bcda30e5a557e1f
mikhaelsantos <mikhaelsantos@gmail.com>
2019-11-20 08:55:23 +0200

    fix: Package versioning:
    * Package version is different from tag name. Must remove the initial v.

--- .codedeploy/after_install.sh
+++ .codedeploy/after_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /srv/broker
 mkdir /srv/broker/.npm-global
-NPM_CONFIG_PREFIX=/srv/broker/.npm-global npm install -g streamr-broker@TAG --unsafe-perm
+NPM_CONFIG_PREFIX=/srv/broker/.npm-global npm install -g streamr-broker@PACKAGE_VERSION --unsafe-perm
