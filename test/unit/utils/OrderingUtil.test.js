39e2a382879d3e607669a160b360d7a505c3177d
Melchior Thambipillai <melkiorthambipillai@gmail.com>
2020-03-18 12:26:00 +0100

    add e2e test for OrderingUtil (#30)

--- test/unit/utils/OrderingUtil.test.js
+++ test/unit/utils/OrderingUtil.test.js
@@ -1,4 +1,5 @@
 import assert from 'assert'
+import shuffle from 'array-shuffle'
 
 import OrderingUtil from '../../../src/utils/OrderingUtil'
 import StreamMessage from '../../../src/protocol/message_layer/StreamMessage'
@@ -61,5 +62,71 @@ describe('OrderingUtil', () => {
             util.add(msg3)
             done()
         }, 500)
+    })
+    it('handles unordered messages in order (large randomized test)', () => {
+        const msg1Pub1 = createMsg(1, 0, null, 0, {}, 'publisherId1')
+        const msg1Pub2 = createMsg(1, 0, null, 0, {}, 'publisherId2')
+        const msg1Pub3 = createMsg(1, 0, null, 0, {}, 'publisherId3')
+        const expected1 = [msg1Pub1]
+        const expected2 = [msg1Pub2]
+        const expected3 = [msg1Pub3]
+        let i
+        for (i = 2; i <= 100000; i++) {
+            expected1.push(createMsg(i, 0, i - 1, 0, {}, 'publisherId1'))
+        }
+        for (i = 2; i <= 100000; i++) {
+            expected2.push(createMsg(i, 0, i - 1, 0, {}, 'publisherId2'))
+        }
+        for (i = 2; i <= 100000; i++) {
+            expected3.push(createMsg(i, 0, i - 1, 0, {}, 'publisherId3'))
+        }
+        const shuffled = shuffle(expected1.concat(expected2).concat(expected3))
+        const received1 = []
+        const received2 = []
+        const received3 = []
+        util = new OrderingUtil('streamId', 0, (m) => {
+            if (m.getPublisherId() === 'publisherId1') {
+                received1.push(m)
+            } else if (m.getPublisherId() === 'publisherId2') {
+                received2.push(m)
+            } else if (m.getPublisherId() === 'publisherId3') {
+                received3.push(m)
+            }
+        }, () => {}, 50)
+        util.add(msg1Pub1)
+        util.add(msg1Pub2)
+        util.add(msg1Pub3)
+        shuffled.forEach((m) => {
+            if (m.getTimestamp() !== 1) {
+                util.add(m)
+            }
+        })
+        try {
+            assert.deepStrictEqual(received1, expected1)
+            assert.deepStrictEqual(received2, expected2)
+            assert.deepStrictEqual(received3, expected3)
+        } catch (e) {
+            const shuffledTimestamps = []
+            shuffled.forEach((streamMessage) => {
+                shuffledTimestamps.push(streamMessage.getTimestamp())
+            })
+            const receivedTimestamps1 = []
+            received1.forEach((streamMessage) => {
+                receivedTimestamps1.push(streamMessage.getTimestamp())
+            })
+            const receivedTimestamps2 = []
+            received2.forEach((streamMessage) => {
+                receivedTimestamps2.push(streamMessage.getTimestamp())
+            })
+            const receivedTimestamps3 = []
+            received3.forEach((streamMessage) => {
+                receivedTimestamps3.push(streamMessage.getTimestamp())
+            })
+            throw new Error('Was expecting to receive messages ordered per timestamp but instead received timestamps in this order:\n'
+                + `publisher 1: ${receivedTimestamps1}\n`
+                + `publisher 2: ${receivedTimestamps2}\n`
+                + `publisher 3: ${receivedTimestamps3}\n`
+                + `The unordered messages were processed in the following timestamp order:\n${shuffledTimestamps}`)
+        }
     })
 })
