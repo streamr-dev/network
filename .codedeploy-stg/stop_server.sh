9d02ea6c55b1faa85237c7d295fbcdaccd9ab3cf
Mikhael <mikhael.santos@streamr.com>
2020-06-25 14:11:41 +0300

    Fix codedeploy (#148)

--- .codedeploy-stg/stop_server.sh
+++ .codedeploy-stg/stop_server.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-service network-node stop
-service network-node-2 stop
-service network-node-3 stop
-service network-node-4 stop
-service network-node-5 stop
-service network-node-6 stop
+service network-node-1 stop
