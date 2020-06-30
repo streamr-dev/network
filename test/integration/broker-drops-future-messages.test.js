7d7f8c2bb47759bebd01c7c4bf43bdfbea7bc6eb
Miroslav Pokrovskii <7105861+mirpo@users.noreply.github.com>
2020-06-30 18:21:29 +0300

    Add back drop future messages v2 (#149)

---
+++ test/integration/broker-drops-future-messages.test.js
@@ -0,0 +1,134 @@
+const url = require('url')
+
+const WebSocket = require('ws')
+const fetch = require('node-fetch')
+const { startTracker } = require('streamr-network')
+const { StreamMessage, MessageIDStrict } = require('streamr-network').Protocol.MessageLayer
+const { ControlLayer } = require('streamr-client-protocol')
+
+const { startBroker, createClient } = require('../utils')
+
+const trackerPort = 19420
+const networkPort = 19421
+const httpPort = 19422
+const wsPort = 19423
+const mqttPort = 19424
+
+// default thresholdForFutureMessageSeconds is 300 seconds = 5 minutes
+const thresholdForFutureMessageSeconds = 5 * 60
+
+function buildMsg(
+    streamId,
+    streamPartition,
+    timestamp,
+    sequenceNumber,
+    publisherId = 'publisher',
+    msgChainId = '1',
+    content = {}
+) {
+    return new StreamMessage({
+        messageId: new MessageIDStrict(streamId, streamPartition, timestamp, sequenceNumber, publisherId, msgChainId),
+        content: JSON.stringify(content)
+    })
+}
+
+describe('broker drops future messages', () => {
+    let tracker
+    let broker
+    let streamId
+    let client
+    let token
+
+    beforeEach(async () => {
+        tracker = await startTracker('127.0.0.1', trackerPort, 'tracker')
+        broker = await startBroker('broker', networkPort, trackerPort, httpPort, wsPort, mqttPort, false)
+
+        console.log(1)
+        client = createClient(wsPort)
+        console.log(2)
+        const freshStream = await client.createStream({
+            name: 'broker-drops-future-messages' + Date.now()
+        })
+        streamId = freshStream.id
+        token = await client.session.getSessionToken()
+    })
+
+    afterEach(async () => {
+        await broker.close()
+        await tracker.stop()
+
+        await client.ensureDisconnected()
+    })
+
+    test('pushing message with too future timestamp to HTTP adapter returns 400 error & does not crash broker', (done) => {
+        const streamMessage = buildMsg(
+            streamId, 10, Date.now() + (thresholdForFutureMessageSeconds + 5) * 1000,
+            0, 'publisher', '1', {}
+        )
+
+        const query = {
+            ts: streamMessage.getTimestamp(),
+            address: streamMessage.getPublisherId(),
+            msgChainId: streamMessage.messageId.msgChainId,
+            signatureType: streamMessage.signatureType,
+            signature: streamMessage.signature,
+        }
+
+        const streamUrl = url.format({
+            protocol: 'http',
+            hostname: '127.0.0.1',
+            port: httpPort,
+            pathname: `/api/v1/streams/${streamId}/data`,
+            query
+        })
+
+        const settings = {
+            method: 'POST',
+            body: streamMessage.serialize(),
+            headers: {
+                Authorization: 'token tester1-api-key',
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+
+        fetch(streamUrl, settings)
+            .then((res) => {
+                expect(res.status).toEqual(400)
+                return res.json()
+            })
+            .then((json) => {
+                expect(json.error).toContain('future timestamps are not allowed')
+                done()
+            })
+    })
+
+    test('pushing message with too future timestamp to Websocket adapter returns error & does not crash broker', (done) => {
+        const streamMessage = buildMsg(
+            streamId, 10, Date.now() + (thresholdForFutureMessageSeconds + 5) * 1000,
+            0, 'publisher', '1', {}
+        )
+
+        const publishRequest = new ControlLayer.PublishRequest({
+            streamMessage,
+            requestId: '',
+            sessionToken: token,
+        })
+
+        const ws = new WebSocket(`ws://127.0.0.1:${wsPort}/api/v1/ws?messageLayerVersion=31&controlLayerVersion=2`, {
+            rejectUnauthorized: false // needed to accept self-signed certificate
+        })
+
+        ws.on('open', () => {
+            ws.send(publishRequest.serialize())
+        })
+
+        ws.on('message', (msg) => {
+            expect(msg).toContain('future timestamps are not allowed')
+            ws.close()
+            done()
+        })
+
+        ws.on('error', (err) => done(err))
+    })
+})
--- test/integration/helpers/getTrackers.test.js
+++ test/integration/helpers/getTrackers.test.js
@@ -18,7 +18,7 @@ describe('getTrackers', () => {
         try {
             await getTrackers('address', config, jsonRpcProvider)
         } catch (e) {
-            expect(e.toString()).toMatch('Error: network does not support ENS (operation="ENS", network="unknown", code=UNSUPPORTED_OPERATION, version=providers/5.0.2)')
+            expect(e.toString()).toContain('Error: network does not support ENS')
             done()
         }
     })
@@ -27,7 +27,7 @@ describe('getTrackers', () => {
         try {
             await getTrackers('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', config, jsonRpcProvider)
         } catch (e) {
-            expect(e.toString()).toMatch('Error: call revert exception (method="getNodes()", errorSignature=null, errorArgs=[null], reason=null, code=CALL_EXCEPTION, version=abi/5.0.1)')
+            expect(e.toString()).toContain('Error: call revert exception')
             done()
         }
     })
@@ -36,7 +36,7 @@ describe('getTrackers', () => {
         try {
             await getTrackers(address, 'config', jsonRpcProvider)
         } catch (e) {
-            expect(e.toString()).toMatch("Error: ENOENT: no such file or directory, open './configs/config'")
+            expect(e.toString()).toContain("Error: ENOENT: no such file or directory, open './configs/config'")
             done()
         }
     })
@@ -45,7 +45,7 @@ describe('getTrackers', () => {
         try {
             await getTrackers(address, config, 'jsonRpcProvider')
         } catch (e) {
-            expect(e.toString()).toMatch('Error: could not detect network (event="noNetwork", code=NETWORK_ERROR, version=providers/5.0.2)')
+            expect(e.toString()).toContain('Error: could not detect network')
             done()
         }
     })
