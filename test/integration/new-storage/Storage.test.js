ef70611bcd66e78cd44eb7a000ba1cf0da5cc427
Miroslav Pokrovskii <7105861+mirpo@users.noreply.github.com>
2020-08-04 11:01:27 +0300

    Update Cassandra test (#165)

--- test/integration/new-storage/Storage.test.js
+++ test/integration/new-storage/Storage.test.js
@@ -243,30 +243,30 @@ describe('Storage', () => {
     })
 
     test('requestLast fast big stream', async () => {
-        for (let i = 0; i < 10000; i++) {
+        for (let i = 0; i < 1000; i++) {
             const msg = buildMsg(streamId, 0, (i + 1) * 1000, i, 'publisher1')
             storage.store(msg)
         }
 
         await wait(10000)
 
-        const streamingResults = storage.requestLast(streamId, 0, 10000)
+        const streamingResults = storage.requestLast(streamId, 0, 1000)
         const results = await toArray(streamingResults)
 
-        expect(results.length).toEqual(10000)
+        expect(results.length).toEqual(1000)
     }, 20000)
 
     test('requestFrom fast big stream', async () => {
-        for (let i = 0; i < 10000; i++) {
+        for (let i = 0; i < 1000; i++) {
             const msg = buildMsg(streamId, 0, (i + 1) * 1000, i, 'publisher1')
             storage.store(msg)
         }
 
-        await wait(20000)
+        await wait(10000)
 
         const streamingResults = storage.requestFrom(streamId, 0, 1000)
         const results = await toArray(streamingResults)
 
-        expect(results.length).toEqual(10000)
-    }, 30000)
+        expect(results.length).toEqual(1000)
+    }, 20000)
 })
