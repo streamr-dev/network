5ef3571b38aadc1ac94c562c2ad55432e46a3285
Santeri Juslenius <santeri.juslenius@gmail.com>
2020-08-11 11:27:27 +0300

    tracker.js ethereum auth (#169)
    * tracker.js ethereum auth
    
    * update nodejs

--- tracker.js
+++ tracker.js
@@ -2,7 +2,28 @@
 const { spawn } = require('child_process')
 const path = require('path')
 
-spawn(path.resolve(__dirname, './node_modules/streamr-network/bin/tracker.js'), process.argv, {
+const program = require('commander')
+
+const { authenticateFromConfig } = require('./src/helpers/ethereumAuthenticate')
+
+program
+    .usage('<ethereumPrivateKey> <trackerName>')
+    .parse(process.argv)
+
+if (program.args.length < 2) {
+    program.help()
+}
+const privateKey = program.args[0]
+const trackerName = program.args[1]
+
+const address = authenticateFromConfig({
+    privateKey
+})
+
+process.argv.splice(2, 2)
+const argv = process.argv.concat(['--trackerName', trackerName, '--id', address])
+
+spawn(path.resolve(__dirname, './node_modules/streamr-network/bin/tracker.js'), argv, {
     cwd: process.cwd(),
     detached: false,
     stdio: 'inherit'
