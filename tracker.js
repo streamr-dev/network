d9ccd661f6b56c1c3f0f2eacf896c4422fe460a1
Santeri Juslenius <santeri.juslenius@gmail.com>
2020-08-11 14:45:21 +0300

    add options from network/bin/tracker.js (#175)

--- tracker.js
+++ tracker.js
@@ -8,6 +8,15 @@ const { authenticateFromConfig } = require('./src/helpers/ethereumAuthenticate')
 
 program
     .usage('<ethereumPrivateKey> <trackerName>')
+    .option('--port <port>', 'port', 30300)
+    .option('--ip <ip>', 'ip', '0.0.0.0')
+    .option('--maxNeighborsPerNode <maxNeighborsPerNode>', 'maxNeighborsPerNode', 4)
+    .option('--apiKey <apiKey>', 'apiKey for StreamrClient', undefined)
+    .option('--streamId <streamId>', 'streamId for StreamrClient', undefined)
+    .option('--sentryDns <sentryDns>', 'sentryDns', undefined)
+    .option('--metrics <metrics>', 'output metrics to console', false)
+    .option('--metricsInterval <metricsInterval>', 'metrics output interval (ms)', 5000)
+    .option('--endpointServerPort <endpointServerPort>', 'port for endpoint server', undefined)
     .parse(process.argv)
 
 if (program.args.length < 2) {
@@ -22,7 +31,6 @@ const address = authenticateFromConfig({
 
 process.argv.splice(2, 2)
 const argv = process.argv.concat(['--trackerName', trackerName, '--id', address])
-
 spawn(path.resolve(__dirname, './node_modules/streamr-network/bin/tracker.js'), argv, {
     cwd: process.cwd(),
     detached: false,
