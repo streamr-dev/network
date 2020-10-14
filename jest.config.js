49109f3392605980c96f5044f3e8bf3dd7b29eb0
Eric Andrews <eric.andrews@streamr.com>
2020-10-14 09:47:01 +0300

    disable automatic coverage file creation

--- jest.config.js
+++ jest.config.js
@@ -17,11 +17,8 @@ module.exports = {
     // Automatically clear mock calls and instances between every test
     clearMocks: true,
 
-    // Indicates whether the coverage information should be collected while executing the test
-    collectCoverage: true,
-
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ['data-api.js', 'src/**'],
+    collectCoverageFrom: ['src/**'],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
