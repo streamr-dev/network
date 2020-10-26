8674a2de785591b82ff1a224286bd38679e01209
Eric Andrews <eric.andrews@streamr.com>
2020-10-26 17:16:48 +0200

    simplify tracker & remove deps (#470)

--- bin/tracker.js
+++ bin/tracker.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
-
 const program = require('commander')
-const StreamrClient = require('streamr-client')
-const Sentry = require('@sentry/node')
 const pino = require('pino')
 
 const CURRENT_VERSION = require('../package.json').version
@@ -16,38 +13,13 @@ program
     .option('--port <port>', 'port', 30300)
     .option('--ip <ip>', 'ip', '0.0.0.0')
     .option('--maxNeighborsPerNode <maxNeighborsPerNode>', 'maxNeighborsPerNode', 4)
-    .option('--attachHttpEndpoints', 'attach http endpoints')
-    .option('--apiKey <apiKey>', 'apiKey for StreamrClient', undefined)
-    .option('--streamId <streamId>', 'streamId for StreamrClient', undefined)
-    .option('--sentryDns <sentryDns>', 'sentryDns', undefined)
     .option('--metrics <metrics>', 'output metrics to console', false)
     .option('--metricsInterval <metricsInterval>', 'metrics output interval (ms)', 5000)
-    .option('--privateKeyFileName <privateKeyFileName>', 'private key filename', undefined)
-    .option('--certFileName <certFileName>', 'cert filename', undefined)
     .description('Run tracker with reporting')
     .parse(process.argv)
 
 const id = program.id || `tracker-${program.port}`
 const name = program.trackerName || id
-
-if (program.sentryDns) {
-    logger.info('Configuring Sentry with dns: %s', program.sentryDns)
-    Sentry.init({
-        dsn: program.sentryDns,
-        integrations: [
-            new Sentry.Integrations.Console({
-                levels: ['error']
-            })
-        ],
-        environment: id
-    })
-
-    Sentry.configureScope((scope) => {
-        scope.setUser({
-            id
-        })
-    })
-}
 
 async function main() {
     try {
@@ -55,17 +27,12 @@ async function main() {
             host: program.ip,
             port: Number.parseInt(program.port, 10),
             id,
-            maxNeighborsPerNode: Number.parseInt(program.maxNeighborsPerNode, 10),
             name,
-            attachHttpEndpoints: program.attachHttpEndpoints,
-            privateKeyFileName: program.privateKeyFileName,
-            certFileName: program.certFileName
+            maxNeighborsPerNode: Number.parseInt(program.maxNeighborsPerNode, 10),
         })
 
         const trackerObj = {}
-        const fields = [
-            'ip', 'port', 'maxNeighborsPerNode', 'privateKeyFileName', 'certFileName', 'metrics',
-            'metricsInterval', 'apiKey', 'streamId', 'sentryDns', 'attachHttpEndpoints']
+        const fields = ['ip', 'port', 'maxNeighborsPerNode', 'metrics', 'metricsInterval']
         fields.forEach((prop) => {
             trackerObj[prop] = program[prop]
         })
@@ -76,21 +43,9 @@ async function main() {
             ...trackerObj
         })
 
-        if (program.metrics && program.apiKey && program.streamId) {
-            const client = new StreamrClient({
-                auth: {
-                    apiKey: program.apiKey
-                },
-                autoConnect: false
-            })
+        if (program.metrics) {
             setInterval(async () => {
                 const metrics = await tracker.getMetrics()
-
-                // send metrics to streamr.network
-                if (client) {
-                    client.publishHttp(program.streamId, metrics)
-                }
-
                 // output to console
                 if (program.metrics) {
                     logger.info(JSON.stringify(metrics, null, 4))
