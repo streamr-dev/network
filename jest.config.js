98be6eb891673fda313daf399eab14b536fe8387
Tim Oxley <secoif@gmail.com>
2020-11-23 16:27:43 -0500

    Use jest-circus runner.

--- jest.config.js
+++ jest.config.js
@@ -112,7 +112,7 @@ module.exports = {
     // ],
 
     // Allows you to use a custom runner instead of Jest's default test runner
-    // runner: "jest-runner",
+    testRunner: 'jest-circus/runner',
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
     // setupFiles: [],
