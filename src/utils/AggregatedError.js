d762f3c69f21d04cb95fdb0d8cd61dd27680161c
Tim Oxley <secoif@gmail.com>
2020-12-03 22:57:14 -0500

    Add comments to utils/AggregatedError

--- src/utils/AggregatedError.js
+++ src/utils/AggregatedError.js
@@ -1,3 +1,11 @@
+/**
+ * An Error of Errors
+ * Pass an array of errors + message to create
+ * Single error without throwing away other errors
+ *
+ * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AggregateError
+ */
+
 export default class AggregatedError extends Error {
     // specifically not using AggregateError name as this has slightly different API
     constructor(errors = [], errorMessage = '') {
@@ -8,9 +16,14 @@ export default class AggregatedError extends Error {
         }
     }
 
-    static from(err, newErr, msg) {
+    /**
+     * Handles 'upgrading' an existing error to an AggregatedError when necesary.
+     */
+
+    static from(oldErr, newErr, msg) {
         switch (true) {
-            case !err: {
+            // When no oldErr, just return newErr
+            case !oldErr: {
                 if (msg) {
                     // copy message
                     newErr.message = msg // eslint-disable-line no-param-reassign
@@ -18,14 +31,20 @@ export default class AggregatedError extends Error {
 
                 return newErr
             }
-            case typeof err.extend === 'function': {
-                return err.extend(newErr, msg || newErr.message)
+            // When oldErr is an AggregatedError, extend it
+            case typeof oldErr.extend === 'function': {
+                return oldErr.extend(newErr, msg || newErr.message)
             }
+            // Otherwise create new AggregatedError from oldErr and newErr
             default: {
-                return new AggregatedError([err, newErr], msg || newErr.message)
+                return new AggregatedError([oldErr, newErr], msg || newErr.message)
             }
         }
     }
+
+    /**
+     * Create a new error that adds err to list of errors
+     */
 
     extend(err, message = '') {
         if (err === this || this.errors.has(err)) {
