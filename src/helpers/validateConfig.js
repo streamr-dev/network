20ceacf1a055351cd28bc4c76db5d6d05a897696
Eric Andrews <eric.andrews@streamr.com>
2020-12-23 12:40:27 +0200

    add missing ajv format

--- src/helpers/validateConfig.js
+++ src/helpers/validateConfig.js
@@ -1,9 +1,11 @@
 const Ajv = require('ajv').default
+const addFormats = require('ajv-formats')
 
 const BROKER_CONFIG_SCHEMA = require('./config.schema.json')
 
 module.exports = function validateConfig(config) {
     const ajv = new Ajv()
+    addFormats(ajv)
     if (!ajv.validate(BROKER_CONFIG_SCHEMA, config)) {
         throw new Error(ajv.errors.map((e) => {
             let text = ajv.errorsText([e])
