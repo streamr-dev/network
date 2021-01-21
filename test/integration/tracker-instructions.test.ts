9c5361de22f90923d19160f3d689d241c2af2b3b
Eric Andrews <eric.andrews@streamr.com>
2021-01-21 12:11:20 +0200

    address flakayness of test/integration/tracker-instructions.test.ts

--- test/integration/tracker-instructions.test.ts
+++ test/integration/tracker-instructions.test.ts
@@ -1,6 +1,6 @@
 import { Tracker } from '../../src/logic/Tracker'
 import { NetworkNode } from '../../src/NetworkNode'
-import { waitForEvent } from 'streamr-test-utils'
+import { waitForCondition, waitForEvent } from 'streamr-test-utils'
 import { TrackerLayer } from 'streamr-client-protocol'
 
 import { startNetworkNode, startTracker } from '../../src/composition'
@@ -97,6 +97,9 @@ describe('check tracker, nodes and statuses from nodes', () => {
             // @ts-expect-error private variable
             waitForEvent(tracker.trackerServer, TrackerServerEvent.NODE_STATUS_RECEIVED)
         ])
+
+        await waitForCondition(() => node1.getNeighbors().length > 0)
+        await waitForCondition(() => node2.getNeighbors().length > 0)
 
         expect(getTopology(tracker.getOverlayPerStream())).toEqual({
             'stream-1::0': {
