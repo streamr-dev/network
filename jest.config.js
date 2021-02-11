58b145f83a2d7c0198dae231d1a9e8c27c166a30
Tim Oxley <secoif@gmail.com>
2021-02-11 13:38:08 -0500

    Add jest.setup.js file for DEBUG_CONSOLE env var support.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
     preset: 'ts-jest',
     clearMocks: true,
     testEnvironment: 'node',
+    // A path to a module which exports an async function that is triggered once before all test suites
+    globalSetup: './jest.setup.js',
+    // Allows you to use a custom runner instead of Jest's default test runner
+    // testRunner: 'jest-circus/runner',
 }
