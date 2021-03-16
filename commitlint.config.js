c4518eb8d92f3485bbc09584f60d2a648a31dd1d
Tim Oxley <secoif@gmail.com>
2021-03-16 14:06:33 -0400

    style(git): Add valid conventional-commit types + descriptions. Add deploy & deps types.

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,6 +12,22 @@ module.exports = {
         'body-case': [0, 'always', 'lower-case'],
         // don't care about trailing full-stop.
         'subject-full-stop': [0, 'never', '.'],
-        'header-full-stop': [0, 'never', '.']
+        'header-full-stop': [0, 'never', '.'],
+        // valid types + descriptions
+        // feel free to add more types as necessary
+        'type-enum': [2, 'always', [
+            'build', // Changes that affect the build system
+            'ci', // Changes to our CI configuration files and scripts.
+            'docs', // Documentation only changes
+            'feat', // A new feature
+            'fix', // A bug fix
+            'perf', // A code change that improves performance
+            'refactor', // A code change that neither fixes a bug nor adds a feature
+            'style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+            'test', // Adding missing tests or correcting existing tests
+            'revert', // git revert
+            'deps', // Changes that affect external dependencies e.g. refreshing package-lock, updating deps.
+            'deploy', // for gh-pages
+        ]],
     }
 }
