7acfe43e302efce64099aabf7519fdfebd7e83ad
Teo Gebhard <teo@streamr.com>
2021-03-22 11:33:57 +0200

    refactor: use supported parameter type when calling hexZeroPad()

--- src/dataunion/DataUnion.ts
+++ src/dataunion/DataUnion.ts
@@ -234,8 +234,7 @@ export class DataUnion {
 
     /** @internal */
     async _createWithdrawSignature(amountTokenWei: BigNumber|number|string, to: EthereumAddress, withdrawn: BigNumber, signer: JsonRpcSigner) {
-        // @ts-expect-error
-        const message = to + hexZeroPad(BigNumber.from(amountTokenWei), 32).slice(2) + this.getSidechainAddress().slice(2) + hexZeroPad(withdrawn, 32).slice(2)
+        const message = to + hexZeroPad(BigNumber.from(amountTokenWei).toHexString(), 32).slice(2) + this.getSidechainAddress().slice(2) + hexZeroPad(withdrawn.toHexString(), 32).slice(2)
         const signature = await signer.signMessage(arrayify(message))
         return signature
     }
