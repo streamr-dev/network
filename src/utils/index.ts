2638f2b164455b6c45f3c6e9d3d6b297669ebc7a
Tim Oxley <secoif@gmail.com>
2021-03-25 16:14:50 -0400

    perf(util/defer): Rejig exposed functions so resolve/reject can be gc'ed.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -220,13 +220,32 @@ export function CacheFn(fn: Parameters<typeof mem>[0], {
 type PromiseResolve = L.Compulsory<Parameters<Promise<any>['then']>>[0]
 type PromiseReject = L.Compulsory<Parameters<Promise<any>['then']>>[1]
 
-export function Defer<T>(executor: (...args: Parameters<Promise<T>['then']>) => void = () => {}) {
-    let resolve: PromiseResolve = () => {}
-    let reject: PromiseReject = () => {}
+const noop = () => {}
+
+export function Defer<T>(executor: (...args: Parameters<Promise<T>['then']>) => void = noop) {
+    let resolveFn: PromiseResolve | undefined
+    let rejectFn: PromiseResolve | undefined
+    const resolve: PromiseReject = (value) => {
+        if (resolveFn) {
+            const r = resolveFn
+            resolveFn = undefined
+            rejectFn = undefined
+            r(value)
+        }
+    }
+    const reject: PromiseReject = (error) => {
+        if (rejectFn) {
+            const r = rejectFn
+            resolveFn = undefined
+            rejectFn = undefined
+            r(error)
+        }
+    }
+
     // eslint-disable-next-line promise/param-names
     const p = new Promise((_resolve, _reject) => {
-        resolve = _resolve
-        reject = _reject
+        resolveFn = _resolve
+        rejectFn = _reject
         executor(resolve, reject)
     })
     p.catch(() => {}) // prevent unhandledrejection
