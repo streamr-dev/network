d76cea9f7fbaa21d98551ad373e91901e02ea210
Kare Nuorteva <kare.nuorteva@me.com>
2021-04-06 16:54:08 +0300

    Remote Travis CI

--- .travis-scripts/run-test-scenarios.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Run all test scenarios
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -s stream-cleartext-unsigned -c config/default-ci.conf && \
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -s stream-cleartext-signed -c config/default-ci.conf && \
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -s stream-encrypted-shared-signed -c config/default-ci.conf && \
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -s stream-encrypted-shared-rotating-signed -c config/default-ci.conf && \
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -s stream-encrypted-exchanged-rotating-signed -c config/default-ci.conf && \
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -s stream-encrypted-exchanged-rotating-revoking-signed -c config/default-ci.conf
