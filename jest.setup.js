829591ad540dafeb85954246dc27edeed9438ac0
Tim Oxley <secoif@gmail.com>
2021-04-08 16:02:54 -0400

    test: Remove DEBUG_CONSOLE env var handling, no longer works in latest Jest.

--- jest.setup.js
+++ jest.setup.js
@@ -1,16 +1,8 @@
 import GitRevisionPlugin from 'git-revision-webpack-plugin'
-import Debug from 'debug'
 
 const pkg = require('./package.json')
 
 export default async () => {
-    if (process.env.DEBUG_CONSOLE) {
-        // Use debug as console log
-        // This prevents jest messing with console output
-        // Ensuring debug messages are printed alongside console messages, in the correct order
-        console.log = Debug('Streamr::CONSOLE') // eslint-disable-line no-console
-    }
-
     if (!process.env.GIT_VERSION) {
         const gitRevisionPlugin = new GitRevisionPlugin()
         const [GIT_VERSION, GIT_COMMITHASH, GIT_BRANCH] = await Promise.all([
