06f7c7afd5ca1fdca3314a74986027e92ff9c430
Tim Oxley <secoif@gmail.com>
2021-04-20 15:03:41 -0400

    Disable autoconnect/disconnect for js publisher.

--- publisher.js
+++ publisher.js
@@ -63,6 +63,7 @@ if (publishFunctionName === 'default') {
 
 let Counter = 0
 const publishMessage = async () => {
+    await client.connect() // disable autoconnect/disconnect
     const counter = Counter++
     const msg = {
         "counter": counter,
