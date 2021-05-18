0649320d484a52dea9f80954f2d75ec3ee1b1a9b
Tim Kevin Oxley <secoif@gmail.com>
2021-05-18 06:02:04 -0400

    docs: Add autogenerated docs. (#58)

---
+++ typedoc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entryPoints: [
+        'src/index.ts',
+    ],
+    tsconfig: 'tsconfig-build.json',
+    readme: true,
+    excludeInternal: true,
+    includeVersion: true,
+}
+
