552806edfaed396903ae93151d068667130ca953
Tim Oxley <secoif@gmail.com>
2021-05-26 09:52:41 -0400

    feat(protocol): Expose SPID to top-level index + utils exports.

--- packages/protocol/src/utils/index.ts
+++ packages/protocol/src/utils/index.ts
@@ -3,9 +3,12 @@ import OrderingUtil from "./OrderingUtil"
 import StreamMessageValidator from "./StreamMessageValidator"
 import CachingStreamMessageValidator from "./CachingStreamMessageValidator"
 import SigningUtil from "./SigningUtil"
+import StreamPartitionID, { SPIDish } from "./StreamPartitionID"
 import { createTrackerRegistry, getTrackerRegistryFromContract, TrackerRegistry } from "./TrackerRegistry"
 
 export {
+    SPIDish,
+    StreamPartitionID,
     TimestampUtil,
     OrderingUtil,
     StreamMessageValidator,
