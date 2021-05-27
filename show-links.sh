23ba23c6e2eb338fb2f3bc5cd7f7f14b718c28e7
Eric Andrews <harbu88@hotmail.com>
2021-05-27 11:37:36 +0300

    fix: add show-links.sh and update README

---
+++ show-links.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Go through packages and for each enumrate the node_modules that are symlinked
+
+TITLE_COLOR='\033[0;36m'
+NO_COLOR='\033[0m'
+
+PACKAGES="broker
+cli-tools
+client
+network
+protocol
+test-utils
+"
+for p in $PACKAGES
+do
+    RES=`(cd packages/$p && find node_modules -maxdepth 1 -type l) | sed 's/^/\t/'`
+    printf "${TITLE_COLOR}$p${NO_COLOR}\n${RES}\n"
+done
