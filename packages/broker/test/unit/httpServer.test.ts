67a6deda0438cc4f587d8291e9304f20925b7dfe
Teo Gebhard <teo@streamr.com>
2021-05-31 18:30:01 +0300

    test: cleanup

--- packages/broker/test/unit/httpServer.test.ts
+++ packages/broker/test/unit/httpServer.test.ts
@@ -42,32 +42,41 @@ describe('HttpServer', () => {
             const server = await startTestServer({
                 keys: [MOCK_API_KEY]
             })
-            const response = await createRequest({
-                Authorization: `Bearer ${MOCK_API_KEY}`
-            })
-            const body = await response.text()
-            expect(body).toBe('FOO')
-            await stopServer(server)
+            try {
+                const response = await createRequest({
+                    Authorization: `Bearer ${MOCK_API_KEY}`
+                })
+                const body = await response.text()
+                expect(body).toBe('FOO')    
+            } finally {
+                await stopServer(server)
+            }
         })
 
         it('forbidden', async () => {
             const server = await startTestServer({
                 keys: [MOCK_API_KEY]
             })
-            const response = await createRequest({
-                Authorization: 'Bearer invalid-api-key'
-            })
-            expect(response.status).toBe(403)
-            await stopServer(server)
+            try {
+                const response = await createRequest({
+                    Authorization: 'Bearer invalid-api-key'
+                })
+                expect(response.status).toBe(403)
+            } finally {
+                await stopServer(server)
+            }
         })
 
         it('unauthorized', async () => {
             const server = await startTestServer({
                 keys: [MOCK_API_KEY]
-            })
-            const response = await createRequest()
-            expect(response.status).toBe(401)
-            await stopServer(server)
+            })           
+            try {
+                const response = await createRequest()
+                expect(response.status).toBe(401)
+            } finally {
+                await stopServer(server)
+            }
         })
 
     })
