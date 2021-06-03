4d19116432f01ea49769824c0eb9be9c88551d35
bearni95 <bernatcanal@gmail.com>
2021-06-03 14:06:49 +0200

    test: eslint

--- packages/broker/src/broker.ts
+++ packages/broker/src/broker.ts
@@ -33,11 +33,11 @@ export interface Broker {
 // https://gist.github.com/erikvullings/b71a0be49e5e79945805bd209e22c7d2
 export const uuidv4 = () => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      // tslint:disable-next-line:no-bitwise
-      const r = (Math.random() * 16) | 0;
-      // tslint:disable-next-line:no-bitwise
-      const v = c === 'x' ? r : (r & 0x3) | 0x8
-      return v.toString(16)
+        // tslint:disable-next-line:no-bitwise
+        const r = (Math.random() * 16) | 0
+        // tslint:disable-next-line:no-bitwise
+        const v = c === 'x' ? r : (r & 0x3) | 0x8
+        return v.toString(16)
     })
 }
 
