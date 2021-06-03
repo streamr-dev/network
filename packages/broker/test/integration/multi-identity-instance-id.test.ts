8a280a5004dd1565aa8ec2f461db9ad4638eb5f3
bearni95 <bernatcanal@gmail.com>
2021-06-03 14:06:49 +0200

    test: eslint

--- packages/broker/test/integration/multi-identity-instance-id.test.ts
+++ packages/broker/test/integration/multi-identity-instance-id.test.ts
@@ -1,31 +1,23 @@
-import { startTracker, startNetworkNode, MetricsContext, NetworkNode, Tracker } from 'streamr-network'
-
+import { startTracker, Tracker } from 'streamr-network'
 
 import { Wallet } from 'ethers'
 import { Broker } from '../../src/broker'
 
-import { startBroker, createMockUser, createClient, StorageAssignmentEventManager, waitForStreamPersistedInStorageNode } from '../utils'
-
-
-const wsPort = 29351
-const networkNodePort = 29361
+import { startBroker } from '../utils'
 
 const httpPort1 = 29341
 const httpPort2 = 29342
-const httpPort3 = 29343
 const wsPort1 = 29351
 const wsPort2 = 29352
-const wsPort3 = 29353
 const networkPort1 = 29361
 const networkPort2 = 29362
-const networkPort3 = 29363
 const trackerPort = 29370
 
 describe('Multi-identity brokers', () => {
 
-    let broker1:Broker 
-    let broker2:Broker
-    let tracker:Tracker
+    let broker1: Broker 
+    let broker2: Broker
+    let tracker: Tracker
 
     beforeEach(async () => {
         const identity = Wallet.createRandom()
@@ -56,7 +48,6 @@ describe('Multi-identity brokers', () => {
             enableCassandra: true
         })
     })
-
    
     afterEach(async () => {
         await Promise.all([
