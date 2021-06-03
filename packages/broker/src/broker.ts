23686876b69ebd3de2bf5469d43ea458cca562d1
bearni95 <bernatcanal@gmail.com>
2021-06-03 13:43:51 +0200

    uuid fn

--- packages/broker/src/broker.ts
+++ packages/broker/src/broker.ts
@@ -27,10 +27,18 @@ export interface Broker {
     close: () => Promise<unknown>
 }
 
-export function uuidv4(): string {
-    return (`${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`).replace(/[018]/g, (c: any) =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
+// replace with an actual uuid function in it's final location 
+// using this placeholder for scaffolding purposes 
+// source:
+// https://gist.github.com/erikvullings/b71a0be49e5e79945805bd209e22c7d2
+export const uuid4 = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      // tslint:disable-next-line:no-bitwise
+      const r = (Math.random() * 16) | 0;
+      // tslint:disable-next-line:no-bitwise
+      const v = c === 'x' ? r : (r & 0x3) | 0x8
+      return v.toString(16)
+    })
 }
 
 export const startBroker = async (config: Config): Promise<Broker> => {
