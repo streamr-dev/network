aed4a19d251957ce26f6427d466559efa5b3902a
Tim Oxley <secoif@gmail.com>
2021-06-04 09:40:53 -0400

    Revert "types(cli-tools): Address tsc complaints."
    This reverts commit 10bfd91a9d7c27e2008cde259f856be86885d184.

--- packages/cli-tools/src/show.ts
+++ packages/cli-tools/src/show.ts
@@ -8,9 +8,9 @@ export const show = (
     const options = { ...streamrOptions }
     const client = new StreamrClient(options)
     client.getStream(streamId).then(async (stream) => {
+        // @ts-expect-error accessing internal method, should be made public perhaps?
         const obj = stream.toObject()
         if (includePermissions) {
-            // @ts-expect-error permissions does not exist on {}
             obj.permissions = await stream.getPermissions()
         }
         console.info(JSON.stringify(obj, null, 2))
