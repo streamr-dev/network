69aa59ef7eeb51ce71a0465c21ba2388483a88e6
juslesan <santeri.juslenius@gmail.com>
2021-06-04 18:02:28 +0300

    fix(docker in ci): run parity-sidechain-node0 in CI for protocol

--- packages/protocol/test/integration/StorageNodeRegistry.test.ts
+++ packages/protocol/test/integration/StorageNodeRegistry.test.ts
@@ -1,4 +1,4 @@
-import { createStorageNodeRegistry, getStorageNodeRegistryFromContract } from '../../src/utils/StorageNodeRegistry'
+import {createStorageNodeRegistry, getStorageNodeRegistryFromContract} from '../../src/utils/StorageNodeRegistry'
 
 const contractAddress = '0xEAA002f7Dc60178B6103f8617Be45a9D3df659B6'
 const jsonRpcProvider = `http://${process.env.STREAMR_DOCKER_DEV_HOST || 'localhost'}:8546`
