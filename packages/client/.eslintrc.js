0fce75fda2dbcf4f08033e07cd8e6252f60b80a8
Tim Oxley <secoif@gmail.com>
2021-06-25 13:46:28 -0400

    style(client): Allow eslint to ignore unused _prefixed args.

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -52,7 +52,9 @@ module.exports = {
         'no-shadow': 'off',
         '@typescript-eslint/no-shadow': 'error',
         'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-unused-vars': ['error', {
+            argsIgnorePattern: '^_',
+        }],
     },
     settings: {
         'import/resolver': {
