3fdbbdd3e64ffe279452783c7445434c60477f4b
Teo Gebhard <teo@streamr.com>
2021-06-28 14:25:24 +0300

    style: parameter types and return types

--- packages/broker/src/helpers/PayloadFormat.ts
+++ packages/broker/src/helpers/PayloadFormat.ts
@@ -1,6 +1,6 @@
 export interface PayloadFormat {
-    createMessage: (payload: string) => Message
-    createPayload: (content: any, metadata?: Metadata) => string
+    createMessage: (payload: string) => Message|never
+    createPayload: (content: any, metadata?: Metadata) => string|never
 }
 
 export interface Message {
@@ -15,13 +15,15 @@ export interface Metadata {
     msgChainId?: string
 }
 
-const pickProperties = (fields: string[], from: any) => {
+const METADATA_FIELDS = [ 'timestamp', 'sequenceNumber', 'publisherId', 'msgChainId' ]
+
+const pickProperties = (fields: string[], from: Record<string,unknown>): Record<string,unknown> => {
     const result: any = {}
     fields.forEach((field) => result[field] = from[field])
     return result
 }
 
-const assertContent = (content: any) => {
+const assertContent = (content: any): void|never => {
     if ((content === undefined) || (content === null)) {
         throw new Error('Content missing')
     }
@@ -30,7 +32,7 @@ const assertContent = (content: any) => {
     }
 }
 
-const assertMetadata = (metadata: any) => {
+const assertMetadata = (metadata: any): void|never => {
     if (!(metadata instanceof Object)) {
         throw new Error('Metadata is not an object')
     }
@@ -45,14 +47,14 @@ const parsePayloadJson = (payload: string) => {
 }
 
 export class PlainPayloadFormat implements PayloadFormat {
-    createMessage(payload: string): Message {
+    createMessage(payload: string): Message|never {
         return {
             content: parsePayloadJson(payload),
             metadata: {}
         }
     }
 
-    createPayload(content: any): string {
+    createPayload(content: any): string|never {
         assertContent(content)
         return JSON.stringify(content)
     }
@@ -60,30 +62,28 @@ export class PlainPayloadFormat implements PayloadFormat {
 
 export class MetadataPayloadFormat implements PayloadFormat {
 
-    private static FIELDS = [ 'timestamp', 'sequenceNumber', 'publisherId', 'msgChainId' ]
-
-    createMessage(payload: string): Message {
+    createMessage(payload: string): Message|never {
         const json = parsePayloadJson(payload)
         const content = json.content
         assertContent(content)
         let metadata
         if (json.metadata !== undefined) {
             assertMetadata(json.metadata)
-            metadata = pickProperties(MetadataPayloadFormat.FIELDS, json.metadata)
+            metadata = pickProperties(METADATA_FIELDS, json.metadata)
         } else {
             metadata = {}
         }
         return { content, metadata }
     }
 
-    createPayload(content: any, metadata?: Metadata): string {
+    createPayload(content: any, metadata?: Metadata): string|never {
         assertContent(content)
         const payload: any = {
             content
         }
         if (metadata !== undefined) {
             assertMetadata(metadata)
-            payload.metadata = pickProperties(MetadataPayloadFormat.FIELDS, metadata)
+            payload.metadata = pickProperties(METADATA_FIELDS, metadata as Record<string,unknown>)
         }
         return JSON.stringify(payload)
     }
