e301283a14f3da49719a1c9c37245cb25908b575
Teo Gebhard <teo@streamr.com>
2021-07-01 14:15:49 +0300

    fix: revert examples as we can't update the package.json dependency yet

--- packages/network/examples/system-metrics-pubsub/subscriber.ts
+++ packages/network/examples/system-metrics-pubsub/subscriber.ts
@@ -7,8 +7,10 @@ import {
 /**
  * Run a subscriber node that subscribes to stream "system-report" and logs all received messages in stdout.
  */
-async function runSubscriber(id: string): Promise<NetworkNode> {
+async function runSubscriber(id: string, port: number): Promise<NetworkNode> {
     const subscriberNode: NetworkNode = await startNetworkNode({
+        host: '127.0.0.1',
+        port: port,
         name: id,
         trackers: ['ws://127.0.0.1:30300']
     })
@@ -22,15 +24,17 @@ async function runSubscriber(id: string): Promise<NetworkNode> {
 }
 
 async function main(): Promise<void> {
+    let SUB_PORT = 30304
     let count = 0
     let maxTries = 20
     let nodeRunning = false
 
     while(!nodeRunning) {
         try {
-            await runSubscriber(`subscriberNode ${count}`)
+            await runSubscriber(`subscriberNode ${SUB_PORT}`, SUB_PORT)
             nodeRunning = true
         } catch (e) {
+            ++SUB_PORT
             if (++count == maxTries) throw e;
         }
     }
