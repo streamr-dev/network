6b7f1555ab30f7b6c933bdf9f67ccd1f94152123
Eric Andrews <harbu88@hotmail.com>
2021-07-21 11:39:11 +0300

    fix: NET-373 rename broker executables

--- packages/broker/bin/broker.js
+++ packages/broker/bin/broker.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const os = require('os')
+const path = require('path')
 
 const program = require('commander')
 
@@ -10,12 +12,19 @@ program
     .version(CURRENT_VERSION)
     .name('broker')
     .description('Run broker under environment specified by given configuration file.')
-    .arguments('<configFile>')
+    .arguments('[configFile]')
     .option('--streamrUrl <url>', 'override streamrUrl with given value')
     .option('--streamrAddress <address>', 'override streamrAddress with given value')
     .option('--networkId <id>', 'override networkId with given value')
     .option('--test', 'test the configuration (does not start the broker)')
     .action(async (configFile) => {
+        if (configFile == null) {
+            configFile = path.join(os.homedir(), '/.streamr/broker-config.json')
+            if (!fs.existsSync(configFile)) {
+                console.error('Config file not found in the default location ~/.streamr/broker-config.json. You can run "streamr-broker-init" to generate a config file interactively, or specify the config file as argument: "streamr-broker path-to-config/file.json"')
+            }
+            process.exit(1)
+        }
         const config = JSON.parse(fs.readFileSync(configFile, 'utf8'))
 
         if (program.opts().streamrUrl) {
