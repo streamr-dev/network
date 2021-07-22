92c7c1a9eaf8dd2cd96dae427d7885ea905145d1
Tim Oxley <secoif@gmail.com>
2021-07-22 11:26:56 -0400

    feat(protocol): Add optional StreamMessage & code to ValidationError.

--- packages/protocol/src/errors/ValidationError.ts
+++ packages/protocol/src/errors/ValidationError.ts
@@ -1,6 +1,12 @@
+import {StreamMessage} from "../protocol/message_layer"
+
 export default class ValidationError extends Error {
-    constructor(msg: string) {
+    streamMessage?: StreamMessage
+    code?: string
+    constructor(msg: string, streamMessage?: StreamMessage, code?: string) {
         super(msg)
+        this.streamMessage = streamMessage
+        this.code = code
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, this.constructor)
         }
