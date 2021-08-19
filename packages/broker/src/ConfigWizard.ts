9129d5ba13904cb522e76f83ed89aee829764370
Eric Andrews <harbu88@hotmail.com>
2021-08-19 12:26:44 +0300

    broker: fix broken configwizard stuff

--- packages/broker/src/ConfigWizard.ts
+++ packages/broker/src/ConfigWizard.ts
@@ -58,6 +58,7 @@ export const DEFAULT_CONFIG: any = {
         stun: "stun:turn.streamr.network:5349",
         turn: null
     },
+    generateSessionId: false,
     streamrUrl: 'https://streamr.network',
     streamrAddress: '0xf3E5A65851C3779f468c9EcB32E6f25D9D68601a',
     storageNodeConfig: {
@@ -67,6 +68,7 @@ export const DEFAULT_CONFIG: any = {
         }]
     },
     plugins: {
+        legacyWebsocket: {},
         testnetMiner: {
             rewardStreamId: "streamr.eth/brubeck-testnet/rewards",
             claimServerUrl: "http://testnet1.streamr.network:3011",
--- packages/broker/src/localStreamrClient.ts
+++ packages/broker/src/localStreamrClient.ts
@@ -1,5 +1,8 @@
 import { StreamrClient } from 'streamr-client'
 import { Config } from './config'
+import * as LegacyWebsocketConfigSchema from './plugins/legacyWebsocket/config.schema.json'
+
+const DEFAULT_LEGACY_WS_PORT = LegacyWebsocketConfigSchema.properties.port.default
 
 // Plugins can communicate with this node via StreamrClient if WS plugin is enabled
 // This is a temporary solution to prototype Brubeck-plugins
@@ -8,7 +11,7 @@ export const createLocalStreamrClient = (config: Config): StreamrClient|undefine
     if (!config.plugins.legacyWebsocket) {
         return undefined
     }
-    const wsPort = config.plugins.legacyWebsocket.port
+    const wsPort = config.plugins.legacyWebsocket.port ?? DEFAULT_LEGACY_WS_PORT
     return new StreamrClient({
         auth:{
             privateKey: config.ethereumPrivateKey
