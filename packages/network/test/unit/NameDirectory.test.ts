55def7dfe8fd50980ac3702b4f07847f64489ab5
Tim Oxley <secoif@gmail.com>
2021-09-06 10:58:45 -0400

    fix(broker, network): Update dev/test tracker ids.

--- packages/network/test/unit/NameDirectory.test.ts
+++ packages/network/test/unit/NameDirectory.test.ts
@@ -2,7 +2,7 @@ import { NameDirectory } from '../../src/NameDirectory'
 
 describe('NameDirectory', () => {
     test('known', () => {
-        expect(NameDirectory.getName('0xDE33390cC85aBf61d9c27715Fa61d8E5efC61e75')).toBe('T3')
+        expect(NameDirectory.getName('0xf2C195bE194a2C91e93Eacb1d6d55a00552a85E2')).toBe('T3')
     })
     test('unknown', () => {
         expect(NameDirectory.getName('0x1234567890123456789012345678901234567890')).toBe('0x123456')
