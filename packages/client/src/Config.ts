80214187f8eaa34a412e2c4eb4df3d1115f8167a
Tim Oxley <secoif@gmail.com>
2021-09-09 08:59:59 -0400

    fix(client): Add production (testnet) tracker config.

--- packages/client/src/Config.ts
+++ packages/client/src/Config.ts
@@ -73,11 +73,58 @@ const BRUBECK_CLIENT_DEFAULTS = {
         url: 'https://corea1.streamr.network:8001',
     }],
     network: {
-        trackers: [{
-            ws: 'wss://testnet1.streamr.network:30300',
-            http: 'https://testnet1.streamr.network:30300',
-            id: '0x49D45c17bCA1Caf692001D21c38aDECCB4c08504',
-        }],
+        trackers: [
+            {
+                id: '0xFBB6066c44bc8132bA794C73f58F391273E3bdA1',
+                ws: 'wss://testnet3.streamr.network:30401',
+                http: 'https://testnet3.streamr.network:30401'
+            },
+            {
+                id: '0x3D61bFeFA09CEAC1AFceAA50c7d79BE409E1ec24',
+                ws: 'wss://testnet3.streamr.network:30402',
+                http: 'https://testnet3.streamr.network:30402'
+            },
+            {
+                id: '0xE80FB5322231cBC1e761A0F896Da8E0CA2952A66',
+                ws: 'wss://testnet3.streamr.network:30403',
+                http: 'https://testnet3.streamr.network:30403'
+            },
+            {
+                id: '0xf626285C6AACDE39ae969B9Be90b1D9855F186e0',
+                ws: 'wss://testnet3.streamr.network:30404',
+                http: 'https://testnet3.streamr.network:30404'
+            },
+            {
+                id: '0xce88Da7FE0165C8b8586aA0c7C4B26d880068219',
+                ws: 'wss://testnet3.streamr.network:30405',
+                http: 'https://testnet3.streamr.network:30405'
+            },
+            {
+                id: '0x05e7a0A64f88F84fB1945a225eE48fFC2c48C38E',
+                ws: 'wss://testnet4.streamr.network:30401',
+                http: 'https://testnet4.streamr.network:30401'
+            },
+            {
+                id: '0xF15784106ACd35b0542309CDF2b35cb5BA642C4F',
+                ws: 'wss://testnet4.streamr.network:30402',
+                http: 'https://testnet4.streamr.network:30402'
+            },
+            {
+                id: '0x77FA7Af34108abdf8e92B8f4C4AeC7CbfD1d6B09',
+                ws: 'wss://testnet4.streamr.network:30403',
+                http: 'https://testnet4.streamr.network:30403'
+            },
+            {
+                id: '0x7E83e0bdAF1eF06F31A02f35A07aFB48179E536B',
+                ws: 'wss://testnet4.streamr.network:30404',
+                http: 'https://testnet4.streamr.network:30404'
+            },
+            {
+                id: '0x2EeF37180691c75858Bf1e781D13ae96943Dd388',
+                ws: 'wss://testnet4.streamr.network:30405',
+                http: 'https://testnet4.streamr.network:30405'
+            }
+        ]
     },
 }
 
