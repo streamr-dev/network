73867f342fadcebb950a68654cc583e053a38ee2
Tim Oxley <secoif@gmail.com>
2021-09-17 10:03:29 -0400

    refactor(broker): Report storage refresh failure correctly.

--- packages/broker/src/plugins/storage/StorageConfig.ts
+++ packages/broker/src/plugins/storage/StorageConfig.ts
@@ -97,7 +97,14 @@ export class StorageConfig {
 
     async refresh(): Promise<void> {
         const res = await fetch(`${this.apiUrl}/storageNodes/${this.nodeId}/streams`)
+        if (!res.ok) {
+            throw new Error(`Refresh failed: ${res.status} ${await res.text()}`)
+        }
         const json = await res.json()
+        if (!Array.isArray(json)) {
+            throw new Error(`Invalid response. Refresh failed: ${json}`)
+        }
+
         const streamKeys = new Set<StreamKey>(json.flatMap((stream: { id: string, partitions: number }) => ([
             ...getKeysFromStream(stream.id, stream.partitions)
         ])))
