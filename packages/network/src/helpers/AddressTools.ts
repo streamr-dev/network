4934a868e77e19f47acd859e52092172acec613f
Paul-Louis Ageneau <paul-louis@ageneau.org>
2021-10-21 15:03:34 +0200

    feat(network): add AddressTools to extract and match IP addresses

---
+++ packages/network/src/helpers/AddressTools.ts
@@ -0,0 +1,27 @@
+import ipaddr from 'ipaddr.js'
+
+// IPv4 private address ranges as specified by RFC 1918
+const IPv4PrivateRanges = [
+    '10.0.0.0/8',
+    '172.16.0.0/12',
+    '192.168.0.0/16',
+].map((a: string) => ipaddr.parseCIDR(a))
+
+export function isPrivateIPv4(address: string): boolean {
+    if (ipaddr.IPv4.isValid(address)) {
+        const ip = ipaddr.IPv4.parse(address)
+        for (const range of IPv4PrivateRanges) {
+            if (ip.match(range)) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
+
+export function getAddressFromIceCandidate(candidate: string) : string | undefined {
+    const fields = candidate.split(' ').filter((field: string) => field.length > 0)
+    return fields.length >= 5 && ipaddr.isValid(fields[4]) ? fields[4] : undefined
+}
+
