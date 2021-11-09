3585cbcd711795b401f2212fbc4bb1f153efb131
Tim Oxley <secoif@gmail.com>
2021-11-09 09:32:06 -0500

    fix(broker): Fix merge issues.

--- packages/broker/test/unit/RequestAuthenticatorMiddleware.test.ts
+++ packages/broker/test/unit/RequestAuthenticatorMiddleware.test.ts
@@ -2,7 +2,6 @@ import assert from 'assert'
 import sinon from 'sinon'
 import { authenticator } from '../../src/RequestAuthenticatorMiddleware'
 import { HttpError } from '../../src/errors/HttpError'
-import { Todo } from '../../src/types'
 
 describe('AuthenticationMiddleware', () => {
     let request: any
