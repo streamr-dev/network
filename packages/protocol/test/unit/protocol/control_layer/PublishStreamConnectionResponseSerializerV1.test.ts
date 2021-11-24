4b4fd1b7b08b6890e47497fd6aff86a55c729c27
juslesan <santeri.juslenius@gmail.com>
2021-11-24 12:58:02 +0200

    feat: new protocol messages

---
+++ packages/protocol/test/unit/protocol/control_layer/PublishStreamConnectionResponseSerializerV1.test.ts
@@ -0,0 +1,34 @@
+import assert from 'assert'
+
+import { PublishStreamConnectionResponse, ControlMessage } from '../../../../src/index'
+import { PLACEHOLDER_REQUEST_ID_PROTOCOL_V1 } from '../../../../src/protocol/control_layer/ControlMessage'
+
+const VERSION = 1
+const streamId = 'stream'
+const streamPartition = 0
+const senderId = 'node'
+const accepted = true
+
+// Message definitions
+const message = new PublishStreamConnectionResponse({
+    version: VERSION,
+    streamId,
+    streamPartition,
+    senderId,
+    accepted,
+    requestId: PLACEHOLDER_REQUEST_ID_PROTOCOL_V1,
+})
+const serializedMessage = JSON.stringify([VERSION, ControlMessage.TYPES.PublishStreamConnectionResponse, streamId, streamPartition, senderId, accepted])
+
+describe('PublishStreamConnectionResponseSerializerV1', () => {
+    describe('deserialize', () => {
+        it('correctly parses messages', () => {
+            assert.deepStrictEqual(ControlMessage.deserialize(serializedMessage), message)
+        })
+    })
+    describe('serialize', () => {
+        it('correctly serializes messages', () => {
+            assert.deepStrictEqual(message.serialize(VERSION, 30), serializedMessage)
+        })
+    })
+})
