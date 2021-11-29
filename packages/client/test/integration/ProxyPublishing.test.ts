15e4a29af76c0383ff23c908f33c9ebe4e116bf6
juslesan <santeri.juslenius@gmail.com>
2021-11-29 13:03:34 +0200

    fix: tests for list functions

--- packages/client/test/integration/ProxyPublishing.test.ts
+++ packages/client/test/integration/ProxyPublishing.test.ts
@@ -6,15 +6,19 @@ import { SPID } from 'streamr-client-protocol'
 describe('PubSub with proxy connections', () => {
     let stream: Stream
     let publishingClient: StreamrClient
-    let proxyClient: StreamrClient
+    let proxyClient1: StreamrClient
+    let proxyClient2: StreamrClient
     let pubPrivateKey: string
-    let proxyPrivateKey: string
-    let proxyNodeId: string
+    let proxyPrivateKey1: string
+    let proxyPrivateKey2: string
+    let proxyNodeId1: string
+    let proxyNodeId2: string
     const createClient = getCreateClient()
 
     beforeEach(async () => {
         pubPrivateKey = fakePrivateKey()
-        proxyPrivateKey = fakePrivateKey()
+        proxyPrivateKey1 = fakePrivateKey()
+        proxyPrivateKey2 = fakePrivateKey()
 
         publishingClient = createClient({
             id: 'publisher',
@@ -22,20 +26,32 @@ describe('PubSub with proxy connections', () => {
                 privateKey: pubPrivateKey
             }
         })
-        proxyClient = createClient({
+        proxyClient1 = createClient({
             id: 'proxy',
             auth: {
-                privateKey: proxyPrivateKey
+                privateKey: proxyPrivateKey1
             },
             network: {
                 acceptProxyConnections: true,
                 trackers: ConfigTest.network.trackers
             }
         })
-        proxyNodeId = await proxyClient.node.getNodeId()
+        proxyClient2 = createClient({
+            id: 'proxy',
+            auth: {
+                privateKey: proxyPrivateKey2
+            },
+            network: {
+                acceptProxyConnections: true,
+                trackers: ConfigTest.network.trackers
+            }
+        })
+        proxyNodeId1 = await proxyClient1.node.getNodeId()
+        proxyNodeId2 = await proxyClient2.node.getNodeId()
         stream = await createTestStream(publishingClient, module)
         const pubUser = await publishingClient.getUserInfo()
-        const proxyUser = await proxyClient.getUserInfo()
+        const proxyUser = await proxyClient1.getUserInfo()
+        const proxyUser2 = await proxyClient2.getUserInfo()
 
         await stream.grantPermission(StreamOperation.STREAM_GET, pubUser.username)
         await stream.grantPermission(StreamOperation.STREAM_PUBLISH, pubUser.username)
@@ -44,15 +60,18 @@ describe('PubSub with proxy connections', () => {
         await stream.grantPermission(StreamOperation.STREAM_PUBLISH, proxyUser.username)
         await stream.grantPermission(StreamOperation.STREAM_SUBSCRIBE, proxyUser.username)
 
+        await stream.grantPermission(StreamOperation.STREAM_GET, proxyUser2.username)
+        await stream.grantPermission(StreamOperation.STREAM_PUBLISH, proxyUser2.username)
+        await stream.grantPermission(StreamOperation.STREAM_SUBSCRIBE, proxyUser2.username)
     })
 
     it('Publish only connections work', async () => {
         const receivedMessagesProxy: any[] = []
-        await proxyClient.subscribe(stream, (msg) => {
+        await proxyClient1.subscribe(stream, (msg) => {
             receivedMessagesProxy.push(msg)
         })
         await wait(1000)
-        await publishingClient.setPublishProxy(stream, proxyNodeId)
+        await publishingClient.setPublishProxy(stream, proxyNodeId1)
         publishingClient.publish(stream, {
             msg: 'hellow'
         })
@@ -64,25 +83,78 @@ describe('PubSub with proxy connections', () => {
         })
         await wait(2500)
         expect(receivedMessagesProxy.length).toEqual(3)
+
         // @ts-expect-error private
-        expect((await publishingClient.publisher.node.getNode()).streams.hasOutOnlyConnection(new SPID(stream.streamId, 0), proxyNodeId)).toEqual(true)
+        expect((await publishingClient.publisher.node.getNode())
+            // @ts-expect-error private
+            .streams.hasOutOnlyConnection(new SPID(stream.streamId, 0), proxyNodeId1))
+            .toEqual(true)
     }, 15000)
 
     it('removing proxy publishing node works', async () => {
         const receivedMessagesProxy: any[] = []
-        await proxyClient.subscribe(stream, (msg) => {
+        await proxyClient1.subscribe(stream, (msg) => {
             receivedMessagesProxy.push(msg)
         })
         await wait(1000)
-        await publishingClient.setPublishProxy(stream, proxyNodeId)
+        await publishingClient.setPublishProxy(stream, proxyNodeId1)
         await wait(2000)
-        // @ts-expect-error private
-        expect((await publishingClient.publisher.node.getNode()).streams.hasOutOnlyConnection(new SPID(stream.streamId, 0), proxyNodeId)).toEqual(true)
 
-        await publishingClient.removePublishProxy(stream, proxyNodeId)
+        // @ts-expect-error private
+        expect((await publishingClient.publisher.node.getNode())
+            // @ts-expect-error private
+            .streams.hasOutOnlyConnection(new SPID(stream.streamId, 0), proxyNodeId1))
+            .toEqual(true)
+
+        await publishingClient.removePublishProxy(stream, proxyNodeId1)
         await wait(2500)
 
         // @ts-expect-error private
-        expect((await publishingClient.publisher.node.getNode()).streams.isSetUp(new SPID(stream.streamId, 0))).toEqual(false)
+        expect((await publishingClient.publisher.node.getNode())
+            // @ts-expect-error private
+            .streams.isSetUp(new SPID(stream.streamId, 0)))
+            .toEqual(false)
+    }, 15000)
+
+    it('setPublishProxies, removePublishProxies', async () => {
+        const receivedMessagesProxy1: any[] = []
+        const receivedMessagesProxy2: any[] = []
+        await proxyClient1.subscribe(stream, (msg) => {
+            receivedMessagesProxy1.push(msg)
+        })
+        await proxyClient2.subscribe(stream, (msg) => {
+            receivedMessagesProxy2.push(msg)
+        })
+        await wait(1000)
+        await publishingClient.setPublishProxies(stream, [proxyNodeId1, proxyNodeId2])
+        await wait(2000)
+
+        // @ts-expect-error private
+        expect((await publishingClient.publisher.node.getNode())
+            // @ts-expect-error private
+            .streams.hasOutOnlyConnection(new SPID(stream.streamId, 0), proxyNodeId1))
+            .toEqual(true)
+
+        // @ts-expect-error private
+        expect((await publishingClient.publisher.node.getNode())
+            // @ts-expect-error private
+            .streams.hasOutOnlyConnection(new SPID(stream.streamId, 0), proxyNodeId2))
+            .toEqual(true)
+
+        await publishingClient.removePublishProxies(stream, [proxyNodeId1, proxyNodeId2])
+        await wait(2000)
+
+        // @ts-expect-error private
+        expect((await publishingClient.publisher.node.getNode())
+            // @ts-expect-error private
+            .streams.isSetUp(new SPID(stream.streamId, 0)))
+            .toEqual(false)
+
+        // @ts-expect-error private
+        expect((await publishingClient.publisher.node.getNode())
+            // @ts-expect-error private
+            .streams.isSetUp(new SPID(stream.streamId, 0)))
+            .toEqual(false)
+
     }, 15000)
 })
