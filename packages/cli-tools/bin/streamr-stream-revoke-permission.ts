9112e79f8fff4a3dc61c68b701cf838bcc1eb193
Teo Gebhard <teo@streamr.com>
2021-12-02 10:35:32 +0200

    refactor: move cli-tool permission handling to a helper method

--- packages/cli-tools/bin/streamr-stream-revoke-permission.ts
+++ packages/cli-tools/bin/streamr-stream-revoke-permission.ts
@@ -1,47 +1,10 @@
 #!/usr/bin/env node
-import { Command } from 'commander'
-import {
-    envOptions,
-    authOptions,
-    formStreamrOptionsWithEnv,
-    getStreamId
-} from './common'
-import pkg from '../package.json'
-import { StreamPermission, StreamrClient } from 'streamr-client'
 
-const PUBLIC_PERMISSION_ID = 'public'
+import { Stream, StreamPermission } from 'streamr-client'
+import { runModifyPermissionsCommand } from '../src/permission'
 
-const getTarget = (user: string): string|undefined => {
-    if (user === PUBLIC_PERMISSION_ID) {
-        return undefined
-    } else {
-        return user
-    }
-}
-
-const program = new Command()
-program
-    .arguments('<streamId> <user> <operations...>')
-    .description('revoke permission: use keyword "public" as a user to grant a public permission')
-authOptions(program)
-envOptions(program)
-    .version(pkg.version)
-    .action(async (streamIdOrPath: string, user: string, permissionIds: string[], options: any) => {
-        const target = getTarget(user)
-        const client = new StreamrClient(formStreamrOptionsWithEnv(options))
-        const streamId = getStreamId(streamIdOrPath, options)!
-        const stream = await client.getStream(streamId)
-        for await (const permissionId of permissionIds) {
-            const permission = permissionId as StreamPermission
-            if (target !== undefined) {
-                await stream.revokeUserPermission(permission, target)
-            } else {
-                await stream.revokePublicPermission(permission)
-            }
-        }
-    })
-    .parseAsync(process.argv)
-    .catch((e) => {
-        console.error(e)
-        process.exit(1)
-    })
+runModifyPermissionsCommand(
+    (stream: Stream, permission: StreamPermission, target: string) => stream.revokeUserPermission(permission, target),
+    (stream: Stream, permission: StreamPermission) => stream.revokePublicPermission(permission),
+    'revoke'
+)
