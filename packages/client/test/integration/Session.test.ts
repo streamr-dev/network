c6e8478aa12bec1cabda971ee9278f82ef75949d
Teo Gebhard <teo@streamr.com>
2021-12-14 15:50:49 +0200

    test: fix Session.test.ts

--- packages/client/test/integration/Session.test.ts
+++ packages/client/test/integration/Session.test.ts
@@ -4,38 +4,17 @@ describe('Session', () => {
     const createClient = getCreateClient()
 
     describe('Token retrievals', () => {
-        it('fails if trying to use apiKey', async () => {
-            await expect(async () => {
-                await (await createClient({
-                    auth: {
-                        apiKey: 'tester1-api-key',
-                    },
-                })).session.getSessionToken()
-            }).rejects.toThrow('no longer supported')
+        it('gets the token using private key', async () => {
+            const token = await (await createClient()).session.getSessionToken()
+            expect(token).toBeTruthy()
         })
 
-        // it('gets the token using private key', async () => {
-        //     const token = await (await createClient()).session.getSessionToken()
-        //     expect(token).toBeTruthy()
-        // })
-
-        // it('can handle multiple client instances', async () => {
-        //     const client1 = await createClient()
-        //     const client2 = await createClient()
-        //     const token1 = await client1.session.getSessionToken()
-        //     const token2 = await client2.session.getSessionToken()
-        //     expect(token1).not.toEqual(token2)
-        // })
-
-        it('fails if trying to get the token using username and password', async () => {
-            await expect(async () => {
-                await (await createClient({
-                    auth: {
-                        username: 'tester2@streamr.com',
-                        password: 'tester2',
-                    },
-                })).session.getSessionToken()
-            }).rejects.toThrow('no longer supported')
+        it('can handle multiple client instances', async () => {
+            const client1 = await createClient()
+            const client2 = await createClient()
+            const token1 = await client1.session.getSessionToken()
+            const token2 = await client2.session.getSessionToken()
+            expect(token1).not.toEqual(token2)
         })
 
         it('gets no token (undefined) when the auth object is empty', async () => {
