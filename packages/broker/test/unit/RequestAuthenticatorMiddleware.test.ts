2ab1a02347f97ed50712f7c255f979dfa9852ca2
samt1803 <christoph-samuel.pitter@streamr.com>
2021-12-17 09:52:30 +0400

    test: re-enabled tests that got disabled on OCR branch (Resends) and removed commented out tests that are now obsolete (testing mostly authentication in streamfetcher, that now works without authentication and also mostly delegates calls to client) (#322)

--- packages/broker/test/unit/RequestAuthenticatorMiddleware.test.ts
+++ packages/broker/test/unit/RequestAuthenticatorMiddleware.test.ts
@@ -26,22 +26,6 @@ describe('AuthenticationMiddleware', () => {
         middlewareInstance = authenticator(streamFetcherStub, StreamPermission.SUBSCRIBE, 'fakeaddress')
     })
 
-    // to get stream no session is needed any more, streamFetcher is getting streams from chain
-    // describe('given no authorization token', () => {
-    //     it('delegates streamId to streamFetcher#authenticate without session token', () => {
-    //         streamFetcherStub.authenticate = sinon.stub()
-    //         streamFetcherStub.authenticate.returns(Promise.resolve({}))
-
-    //         middlewareInstance(request, response, next)
-
-    //         sinon.assert.calledOnce(streamFetcherStub.authenticate)
-    //         sinon.assert.calledWithExactly(
-    //             streamFetcherStub.authenticate,
-    //             'streamId', undefined, 'stream_subscribe',
-    //         )
-    //     })
-    // })
-
     it('responds 400 and error message if authorization header malformed', () => {
         streamFetcherStub.authenticate = sinon.stub()
         request.headers.authorization = 'foobar 90rjsdojg9823jtopsdjglsd'
@@ -66,80 +50,6 @@ describe('AuthenticationMiddleware', () => {
             }
         })
 
-        // to get stream no session is needed any more, streamFetcher is getting streams from chain
-        // it('delegates streamId and session token to streamFetcher#authenticate', () => {
-        //     streamFetcherStub.authenticate = sinon.stub()
-        //     streamFetcherStub.authenticate.returns(Promise.resolve({}))
-
-        //     middlewareInstance(request, response, next)
-
-        //     sinon.assert.calledOnce(streamFetcherStub.authenticate)
-        //     sinon.assert.calledWithExactly(
-        //         streamFetcherStub.authenticate,
-        //         'streamId', 'session-token', 'stream_subscribe',
-        //     )
-        // })
-
-        // it('authenticates with an explicitly given permission', () => {
-        //     streamFetcherStub.authenticate = sinon.stub()
-        //     streamFetcherStub.authenticate.returns(Promise.resolve({}))
-
-        //     middlewareInstance = authenticator(streamFetcherStub, 'stream_publish')
-        //     middlewareInstance(request, response, next)
-
-        //     sinon.assert.calledOnce(streamFetcherStub.authenticate)
-        //     sinon.assert.calledWithExactly(
-        //         streamFetcherStub.authenticate,
-        //         'streamId', 'session-token', 'stream_publish',
-        //     )
-        // })
-
-        // it('responds 403 and error message if streamFetcher#authenticate results in 403', (done) => {
-        //     streamFetcherStub.authenticate = () => Promise.reject(new HttpError(403, 'GET', ''))
-
-        //     middlewareInstance(request, response, next)
-
-        //     setTimeout(() => {
-        //         sinon.assert.notCalled(next)
-        //         sinon.assert.calledOnce(response.status)
-        //         sinon.assert.calledOnce(response.send)
-        //         sinon.assert.calledWithExactly(response.status, 403)
-        //         sinon.assert.calledWithExactly(response.send, {
-        //             error: 'Authentication failed.',
-        //         })
-        //         done()
-        //     })
-        // })
-
-        // it('responds with 404 if the stream is not found', (done) => {
-        //     streamFetcherStub.authenticate = () => Promise.reject(new HttpError(404, 'GET', ''))
-
-        //     middlewareInstance(request, response, next)
-
-        //     setTimeout(() => {
-        //         sinon.assert.notCalled(next)
-        //         sinon.assert.calledOnce(response.status)
-        //         sinon.assert.calledOnce(response.send)
-        //         sinon.assert.calledWithExactly(response.status, 404)
-        //         sinon.assert.calledWithExactly(response.send, {
-        //             error: 'Stream streamId not found.',
-        //         })
-        //         done()
-        //     })
-        // })
-
-        // it('responds with whatever status code the backend returns', (done) => {
-        //     streamFetcherStub.authenticate = () => Promise.reject(new HttpError(123, 'GET', ''))
-
-        //     middlewareInstance(request, response, next)
-
-        //     setTimeout(() => {
-        //         sinon.assert.notCalled(next)
-        //         sinon.assert.calledWithExactly(response.status, 123)
-        //         done()
-        //     })
-        // })
-
         describe('given streamFetcher#authenticate authenticates successfully', () => {
             beforeEach(() => {
                 streamFetcherStub.authenticate = (streamId: string) => Promise.resolve({
@@ -160,22 +70,6 @@ describe('AuthenticationMiddleware', () => {
                     done()
                 })
             })
-
-            // it('puts stream JSON in request object', (done) => {
-            //     middlewareInstance(request, response, next)
-            //     setTimeout(() => {
-            //         assert.deepEqual(request.stream, {
-            //             id: 'streamId',
-            //             partitions: 5,
-            //             name: 'my stream',
-            //             feed: {},
-            //             config: {},
-            //             description: 'description',
-            //             uiChannel: null,
-            //         })
-            //         done()
-            //     })
-            // })
         })
     })
 })
