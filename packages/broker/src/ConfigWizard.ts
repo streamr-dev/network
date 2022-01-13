d3077e06facea0bc10effe12b2dd0a1396c9380a
Teo Gebhard <teo@streamr.com>
2022-01-13 15:44:56 +0200

    NET-629: Broker config file location (#339)
    Broker's default config file location moved from .streamr/broker-config.json to .streamr/config/default.json. The new location is compatible with CLI-tools, see #324.
    
    Improved also the parent directory handling in ConfigWizard. Now it creates all missing parent directories (e.g. both ~/.streamr and ~/.streamr/config). Previosly it supported only one level of missing directories.
    
    Refactored the ConfigWizard storagePath prompt:
    - the overwrite confirmation is now implemented as a conditional prompt
    - existence of files/directories are analyzed outside of that prompt
    - removed the validator implementation (there were no use case where it returned a validation error)
    
    Possible improvements for storagePath:
    - We could validate the input of the storagePath prompt somehow. E.g. that the path doesn't contain invalid characters. Or that the user is able to create a file (or parent directory) to given location.
    - We could support paths which are relative to the home directory, e.g. ~/foo.json. Currently it creates a directory ~ in the working directory if that kind of relative path is provided in storagePath input.

--- packages/broker/src/ConfigWizard.ts
+++ packages/broker/src/ConfigWizard.ts
@@ -2,7 +2,6 @@ import inquirer from 'inquirer'
 import { Wallet } from 'ethers'
 import path from 'path'
 import { writeFileSync, existsSync, mkdirSync, chmodSync } from 'fs'
-import * as os from 'os'
 import chalk from 'chalk'
 import { v4 as uuid } from 'uuid'
 import * as Protocol from 'streamr-client-protocol'
@@ -10,6 +9,7 @@ import * as Protocol from 'streamr-client-protocol'
 import * as WebsocketConfigSchema from './plugins/websocket/config.schema.json'
 import * as MqttConfigSchema from './plugins/mqtt/config.schema.json'
 import * as BrokerConfigSchema from './helpers/config.schema.json'
+import { getDefaultFile } from './config'
 
 const createLogger = () => {
     return {
@@ -221,25 +221,19 @@ export const PROMPTS = {
     plugins: createPluginPrompts(),
 }
 
-export const selectStoragePathPrompt = {
+export const storagePathPrompts = [{
     type: 'input',
-    name: 'selectStoragePath',
-    message: `Select a path to store the generated config in `,
-    default: path.join(os.homedir(), '.streamr/broker-config.json'),
-    validate: (input: string, answers: inquirer.Answers = {}): string | boolean => {
-        try {
-            const parentDirPath = path.dirname(input)
-
-            answers.parentDirPath = parentDirPath
-            answers.parentDirExists = existsSync(parentDirPath)
-            answers.fileExists = existsSync(input)
-
-            return true
-        } catch (e: any) {
-            return e.message
-        }
-    }
-}
+    name: 'storagePath',
+    message: 'Select a path to store the generated config in',
+    default: getDefaultFile()
+},
+{
+    type: 'confirm',
+    name: 'overwrite',
+    message: (answers: inquirer.Answers): string => `The selected destination ${answers.storagePath} already exists, do you want to overwrite it?`,
+    default: false,
+    when: (answers: inquirer.Answers): boolean => existsSync(answers.storagePath)
+}]
 
 export const getConfig = (privateKey: string, pluginsAnswers: inquirer.Answers): any => {
     const config = { ... CONFIG_TEMPLATE, plugins: { ... CONFIG_TEMPLATE.plugins } }
@@ -271,35 +265,25 @@ export const getConfig = (privateKey: string, pluginsAnswers: inquirer.Answers):
 }
 
 const selectStoragePath = async (): Promise<inquirer.Answers> => {
-    const answers = await inquirer.prompt([selectStoragePathPrompt])
-
-    if (answers.fileExists) {
-        const overwriteAnswers = await inquirer.prompt([
-            {
-                type: 'confirm',
-                name: 'confirmOverwrite',
-                message: `The selected destination ${answers.selectStoragePath} already exists, do you want to overwrite it?`,
-                default: false,
-            }
-        ])
-
-        if (!overwriteAnswers.confirmOverwrite) {
-            return selectStoragePath()
-        }
-    }
-
+    let answers
+    do {
+        answers = await inquirer.prompt(storagePathPrompts)
+    } while (answers.overwrite === false)
     return answers
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const createStorageFile = async (config: any, answers: inquirer.Answers): Promise<string> => {
-    if (!answers.parentDirExists) {
-        mkdirSync(answers.parentDirPath)
+    const dirPath = path.dirname(answers.storagePath)
+    const dirExists = existsSync(dirPath)
+    if (!dirExists) {
+        mkdirSync(dirPath, {
+            recursive: true
+        })
     }
-
-    writeFileSync(answers.selectStoragePath, JSON.stringify(config, null, 2))
-    chmodSync(answers.selectStoragePath, '0600')
-    return answers.selectStoragePath
+    writeFileSync(answers.storagePath, JSON.stringify(config, null, 2))
+    chmodSync(answers.storagePath, '0600')
+    return answers.storagePath
 }
 
 export const getPrivateKey = (answers: inquirer.Answers): string => {
