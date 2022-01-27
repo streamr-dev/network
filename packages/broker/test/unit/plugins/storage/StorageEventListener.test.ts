3fa76893f8c4908aecb5ca8ba7c95ec8eda5a41a
Eric Andrews <harbu88@hotmail.com>
2022-01-27 19:03:31 +0200

    feat(broker): StorageEventListener
    Extract class `StorageEventListener` from `StorageConfig` that is
    responsible for handling real-time storage node assignment events
    learned about via streamr-client (which in turn learns about them via
    smart contracts).
    
    Removing the duplicate functionality from `StorageConfig` follows in a
    later commit.

---
+++ packages/broker/test/unit/plugins/storage/StorageEventListener.test.ts
@@ -0,0 +1,75 @@
+import { StorageEventListener } from '../../../../src/plugins/storage/StorageEventListener'
+import { EthereumStorageEvent, Stream, StreamrClient } from 'streamr-client'
+import { afterEach } from 'jest-circus'
+import { wait } from 'streamr-test-utils'
+
+describe(StorageEventListener, () => {
+    let stubClient: Pick<StreamrClient, 'getStream'
+        | 'registerStorageEventListener'
+        | 'unRegisterStorageEventListeners'>
+    let emitStorageAssignmentEvent: ((event: EthereumStorageEvent) => any) | undefined
+    let handleEvent: jest.Mock<void, [stream: Stream, type: 'added' | 'removed', block: number]>
+    let listener: StorageEventListener
+
+    beforeEach(() => {
+        stubClient = {
+            async getStream() {
+                return {
+                    id: 'streamId',
+                    partitions: 0
+                } as Stream
+            },
+            async registerStorageEventListener(cb: (arg0: EthereumStorageEvent) => any): Promise<void> {
+                emitStorageAssignmentEvent = cb
+            },
+            unRegisterStorageEventListeners: jest.fn()
+        }
+        emitStorageAssignmentEvent = undefined
+        handleEvent = jest.fn()
+        listener = new StorageEventListener('clusterId', stubClient as StreamrClient, handleEvent)
+    })
+
+    afterEach(() => {
+        listener?.destroy()
+    })
+
+    it('start() registers storage event listener on client', async () => {
+        expect(emitStorageAssignmentEvent).toBeUndefined()
+        await listener.start()
+        expect(emitStorageAssignmentEvent).toBeDefined()
+    })
+
+    it('destroy() unregisters storage event listener on client', async () => {
+        expect(stubClient.unRegisterStorageEventListeners).toHaveBeenCalledTimes(0)
+        await listener.destroy()
+        expect(stubClient.unRegisterStorageEventListeners).toHaveBeenCalledTimes(1)
+    })
+
+    function assignStorageNode(recipient: string) {
+        emitStorageAssignmentEvent!({
+            nodeAddress: recipient,
+            streamId: 'streamId',
+            type: 'added',
+            blockNumber: 8694
+        })
+    }
+
+    it('storage node assignment event gets passed to handleEvent', async () => {
+        await listener.start()
+        assignStorageNode('clusterId')
+        await wait(0)
+        expect(handleEvent).toHaveBeenCalledTimes(1)
+        expect(handleEvent).toHaveBeenCalledWith(
+            { id: 'streamId', partitions: 0 },
+            'added',
+            8694
+        )
+    })
+
+    it('storage node assignment events meant for another recipient are ignored', async () => {
+        await listener.start()
+        assignStorageNode('otherClusterId')
+        await wait(0)
+        expect(handleEvent).toHaveBeenCalledTimes(0)
+    })
+})
