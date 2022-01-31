dbe9b15000690ec7a36c21a50dad0a01b1aa411c
Teo Gebhard <teo@streamr.com>
2022-01-31 10:34:54 +0200

    feat: permission filter in cli-tools

--- packages/cli-tools/bin/streamr-stream-search.ts
+++ packages/cli-tools/bin/streamr-stream-search.ts
@@ -1,14 +1,54 @@
 #!/usr/bin/env node
 import '../src/logLevel'
-import StreamrClient from 'streamr-client'
+import StreamrClient, { SearchStreamsPermissionFilter, StreamPermission } from 'streamr-client'
 import { createClientCommand } from '../src/command'
+import { Option } from 'commander'
+import { getPermission, PERMISSIONS } from '../src/permission'
+import { getOptionType, OptionType } from '../src/common'
 
-createClientCommand(async (client: StreamrClient, term: string) => {
-    const streams = client.searchStreams(term)
+const createPermissionFilter = async (
+    user: string | boolean | undefined,
+    allowPublic: boolean | undefined,
+    allOf: StreamPermission[] | undefined,
+    anyOf: StreamPermission[] | undefined,
+    client: StreamrClient
+): Promise<SearchStreamsPermissionFilter| undefined> => {
+    if (user !== undefined) {
+        return {
+            user: (getOptionType(user) === OptionType.ARGUMENT) ? user as string : await client.getAddress(),
+            allowPublic: allowPublic ?? false,
+            allOf,
+            anyOf
+        }
+    } else if ((allowPublic !== undefined) || (allOf !== undefined) || (anyOf !== undefined)) {
+        console.error('specify a user with "--user" when using "--public", "--all" or "--any"')
+        process.exit(1)
+    }
+}
+
+const createPermissionListOption = (id: string) => {
+    return new Option(`--${id} <permissions>`, 'comma-separated list of permissions')
+        .choices(Array.from(PERMISSIONS.keys()))
+        .argParser((value: string) => value.split(',').map((id) => getPermission(id)))
+}
+
+createClientCommand(async (client: StreamrClient, term: string, options: any ) => {
+    const permissionFilter = await createPermissionFilter(
+        options.user,
+        options.public,
+        options.all,
+        options.any,
+        client
+    )
+    const streams = client.searchStreams(term, permissionFilter)
     for await (const stream of streams) {
         console.log(stream.id)
     }
 })
-    .arguments('<term>')
+    .arguments('[term]')
     .description('search streams')
+    .option('--user [user]', 'a stream must have permissions for the given user, defaults to the authenticated user')
+    .option('--public', 'the permission can be implicit (a public permission to the stream)')
+    .addOption(createPermissionListOption('all'))
+    .addOption(createPermissionListOption('any'))
     .parseAsync()
--- packages/cli-tools/src/common.ts
+++ packages/cli-tools/src/common.ts
@@ -4,6 +4,21 @@ export interface GlobalCommandLineArgs {
     privateKey?: string
 }
 
+export enum OptionType {
+    FLAG, // e.g. "--enable"
+    ARGUMENT  // e.g. "--private-key 0x1234"
+}
+
+export const getOptionType = (value: string | boolean): OptionType | never => {
+    if (typeof value === 'boolean') {
+        return OptionType.FLAG
+    } else if (typeof value === 'string') {
+        return OptionType.ARGUMENT
+    } else {
+        throw new Error(`unknown option type (value: ${value})`)
+    }
+}
+
 export function createFnParseInt(name: string): (s: string) => number {
     return (str: string) => {
         const n = parseInt(str, 10)
