34c57863d60b6f120b76f61d0c63bd46af5dab8b
Teo Gebhard <teo@streamr.com>
2022-01-31 10:34:49 +0200

    refactor: use new searchStreams in cli-tools

--- packages/cli-tools/bin/streamr-stream-search.ts
+++ packages/cli-tools/bin/streamr-stream-search.ts
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 import '../src/logLevel'
-import EasyTable from 'easy-table'
 import StreamrClient from 'streamr-client'
 import { createClientCommand } from '../src/command'
 
 createClientCommand(async (client: StreamrClient, term: string) => {
-    const streams = await client.searchStreams(term)
-    if (streams.length > 0) {
-        console.info(EasyTable.print(streams.map(({id}) => ({
-            id
-        }))))
+    const streams = client.searchStreams(term)
+    for await (const stream of streams) {
+        console.log(stream.id)
     }
 })
     .arguments('<term>')
