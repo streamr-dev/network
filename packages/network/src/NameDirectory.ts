a74b38535e38de38bf09b671a2fedc1e72c3ea1f
Teo Gebhard <teo@streamr.com>
2022-02-04 13:44:45 +0200

    feat: do not include production addresses

--- packages/network/src/NameDirectory.ts
+++ packages/network/src/NameDirectory.ts
@@ -1,6 +1,9 @@
 /*
- * Get human-readable names for some Ethereum addresses in Streamr prod/test environments
- * Currently contains hardcoded names for all streamr-docker-dev entities
+ * Get human-readable names for some Ethereum addresses in Streamr test environment
+ * The list doesn't contain all known contract addresses or nodes, but we can add more
+ * addresses if needed.
+ * 
+ * Currently contains hardcoded names for all streamr-docker-dev Broker/Tracker entities
  * -> in the future each node receives the peer names from Tracker
  *    and we can remove the hardcoded values
  */
@@ -14,9 +17,6 @@ const NAMES: Record<EthereumAddress,string> = {
     '0xde1112f631486cfc759a50196853011528bc5fa0': 'S1',
     '0xde222e8603fcf641f928e5f66a0cbf4de70d5352': 'B1',
     '0xde3331ca6b8b636e0b82bf08e941f727b8927442': 'B2',
-    '0x0d483e10612f327fc11965fc82e90dc19b141641': 'PROD_STREAM_REGISTRY',
-    '0xe8e2660cedf2a59c917a5ed05b72df4146b58399': 'PROD_STREAM_STORAGE_REGISTRY',
-    '0x080f34fec2bc33928999ea9e39adc798bef3e0d6': 'PROD_STORAGE_NODE_REGISTRY',
     '0x6ccdd5d866ea766f6df5965aa98deccd629ff222': 'DOCKER_DEV_STREAM_REGISTRY',
     '0xd04af489677001444280366dd0885b03daade71d': 'DOCKER_DEV_STREAM_STORAGE_REGISTRY',
     '0x231b810d98702782963472e1d60a25496999e75d': 'DOCKER_DEV_STORAGE_NODE_REGISTRY'
