2f614d7defcf30c7a428f0bc5d2092124668c7b2
Eric Andrews <harbu88@hotmail.com>
2022-02-04 13:12:13 +0200

    make fetch work in browser

--- packages/client/src/utils/fetch.ts
+++ packages/client/src/utils/fetch.ts
@@ -1,16 +1,17 @@
 import fetch from 'node-fetch'
 import debug from 'debug'
-import type { RequestInfo, RequestInit, Response } from 'node-fetch'
+import type { Response } from 'node-fetch'
 
 const DEFAULT_TIMEOUT = 30 * 1000
 
 const log = debug('Streamr:fetch')
 
-export default function fetchWithTimeoutAndLogging(url: RequestInfo, init?: RequestInit): Promise<Response> {
+// Cannot use more accurate types, like "{ RequestInfo, RequestInit, Response } from 'node-fetch'" due to shimming
+export default function fetchWithTimeoutAndLogging(url: string, init?: Record<string, unknown>): Promise<Response> {
     log('fetching %s', url)
     return fetch(url, {
         timeout: DEFAULT_TIMEOUT,
         ...init
-    })
+    } as any)
 
 }
