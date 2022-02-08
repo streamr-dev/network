3074cc72e113f82f32cf1122d0bb344eccea414c
juslesan <santeri.juslenius@gmail.com>
2022-02-08 12:51:56 +0200

    fix: collect all published messages

--- packages/client/test/integration/GroupKeyPersistence.test.ts
+++ packages/client/test/integration/GroupKeyPersistence.test.ts
@@ -148,7 +148,7 @@ describe('Group Key Persistence', () => {
             await wait(2000)
 
             await Promise.all([
-                sub2.collect(2), // either one old and one new or 2 new messages
+                sub2.collect(3), // either one old and one new or 2 new messages
                 published.push(...await publishTestMessages(3)),
             ])
 
