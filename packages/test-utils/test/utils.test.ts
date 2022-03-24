ea382ce8a45a9fc525bea9b510c64b9310984bfb
Eric Andrews <harbu88@hotmail.com>
2022-03-24 15:11:02 +0200

    hotfix(test-utils): add jitter to wait() test

--- packages/test-utils/test/utils.test.ts
+++ packages/test-utils/test/utils.test.ts
@@ -159,11 +159,14 @@ describe(waitForCondition, () => {
 })
 
 describe(wait, () => {
+    // https://stackoverflow.com/questions/21097421/what-is-the-reason-javascript-settimeout-is-so-inaccurate
+    const JITTER_FACTOR = 4
+
     it("waits at least the predetermined time", async () => {
         const start = Date.now()
         await wait(20)
         const end = Date.now()
-        expect(end - start).toBeGreaterThanOrEqual(20)
+        expect(end - start).toBeGreaterThanOrEqual(20 - JITTER_FACTOR)
     })
 })
 
