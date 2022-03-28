2225bba24b273b5fa8af75f232166adf7be34b32
Eric Andrews <harbu88@hotmail.com>
2022-03-28 17:11:59 +0300

    flaky.sh

--- packages/broker/flaky.sh
+++ packages/broker/flaky.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {1..100}; do
-    npx jest --silent test/integration/plugins/storage/Storage.test.ts -t 'multiple buckets'
+    npx jest --silent test/integration/plugins/storage/Storage.test.ts
     if [[ "$?" != '0' ]]; then
         echo "Failed after $i attempts"
         break
