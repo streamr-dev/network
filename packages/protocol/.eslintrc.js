cced44a84da7a73f27c4dbbce6c0218d426cefdd
Eric Andrews <eric.andrews@streamr.com>
2022-04-05 20:03:58 +0300

    monorepo shared eslint-config (#549)
    Extract shared monorepo eslint rules into new package `eslint-config-monorepo` that itself "inherits" from [eslint-config-streamr-ts](https://github.com/streamr-dev/eslint-config-streamr/tree/master/ts). 
    
    The idea is that we can easily add or try-out eslint rules monorepo-wide without them becoming necessarily streamr-wide rules.
    
    Notice: looks like package `streamr-client` wasn't originally using `eslint-config-streamr-ts` rules so I left it as is. Thus in a further PR I will make client package use the shared monorepo eslint rules as well.

--- packages/protocol/.eslintrc.js
+++ packages/protocol/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    extends: 'eslint-config-streamr-ts',
+    extends: 'eslint-config-monorepo',
     rules: { 'no-console': [ 'error', { allow: [ 'warn', 'error', 'info' ] } ] }
 }
