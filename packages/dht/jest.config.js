591523fe01313c720f1ac8fe0fcc0263afbad665
juslesan <santeri.juslenius@gmail.com>
2022-04-07 17:49:50 +0300

    feat: base package

---
+++ packages/dht/jest.config.js
@@ -0,0 +1,36 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+module.exports = {
+
+    // Preset ts-jest
+    preset: 'ts-jest',
+
+    globals: {
+        'ts-jest': {
+            babelConfig: false,
+        }
+    },
+
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
+
+    // An array of glob patterns indicating a set of files for which coverage information should be collected
+    collectCoverageFrom: ['src/**'],
+
+    // The directory where Jest should output its coverage files
+    coverageDirectory: 'coverage',
+
+    // The test environment that will be used for testing
+    testEnvironment: 'node',
+
+    // Default timeout of a test in milliseconds
+    testTimeout: 15000,
+    maxWorkers: 3,
+
+    // This option allows use of a custom test runner
+    testRunner: 'jest-circus/runner',
+
+    testPathIgnorePatterns: ['/node_modules/'],
+
+    setupFilesAfterEnv: ['jest-extended'],
+}
