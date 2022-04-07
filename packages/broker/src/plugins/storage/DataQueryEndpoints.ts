7086206afd643ea36eb9ba3d6944e9de8b63ed86
Teo Gebhard <teo@streamr.com>
2022-04-07 14:13:07 +0300

    refactor: remove obsolete metrics (no data produced)

--- packages/broker/src/plugins/storage/DataQueryEndpoints.ts
+++ packages/broker/src/plugins/storage/DataQueryEndpoints.ts
@@ -131,8 +131,6 @@ type RangeRequest = BaseRequest<{
 export const router = (storage: Storage, metricsContext: MetricsContext): Router => {
     const router = express.Router()
     const metrics = metricsContext.create('broker/storage/query')
-        .addRecordedMetric('outBytes')
-        .addRecordedMetric('outMessages')
         .addRecordedMetric('lastRequests')
         .addRecordedMetric('fromRequests')
         .addRecordedMetric('rangeRequests')
