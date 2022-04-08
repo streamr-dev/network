029e7d87e2626f0636447cb8f49da334ed673cb7
juslesan <santeri.juslenius@gmail.com>
2022-04-08 15:09:17 +0300

    refactor: directories

--- packages/dht/proto.sh
+++ packages/dht/proto.sh
@@ -1,2 +1,3 @@
+mkdir ./src/proto
 npx protoc --ts_out ./src/proto --proto_path protos protos/ClosestPeers.proto
 npx protoc --ts_out ./src/proto --ts_opt generate_dependencies --proto_path protos protos/RouteMessage.proto
