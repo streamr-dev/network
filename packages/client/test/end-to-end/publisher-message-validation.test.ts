8e60b53a777523274b466d8134be3c0084200613
Eric Andrews <harbu88@hotmail.com>
2022-04-11 14:16:42 +0300

    feat(test): publisher-message-validation

---
+++ packages/client/test/end-to-end/publisher-message-validation.test.ts
@@ -0,0 +1,44 @@
+import { StreamrClient } from '../../src'
+import { createTestStream, fetchPrivateKeyWithGas, getCreateClient } from '../test-utils/utils'
+import { fastPrivateKey, wait } from 'streamr-test-utils'
+import { StreamID } from 'streamr-client-protocol'
+
+const createClient = getCreateClient()
+
+const TIMEOUT = 10 * 1000
+
+describe('publisher message validation', () => {
+    let publisherClient: StreamrClient
+    let subscriberClient: StreamrClient
+    let streamId: StreamID
+
+    beforeEach(async () => {
+        publisherClient = await createClient({
+            auth: {
+                privateKey: fastPrivateKey()
+            }
+        })
+        subscriberClient = await createClient({
+            auth: {
+                privateKey: await fetchPrivateKeyWithGas()
+            }
+        })
+        const stream = await createTestStream(subscriberClient, module)
+        streamId = stream.id
+    }, TIMEOUT)
+
+    it('invalid message does not get published', async () => {
+        let subscriberReceivedMsgs = 0
+        const subscription = await subscriberClient.subscribe(streamId, () => {
+            subscriberReceivedMsgs += 1
+        })
+        subscription.onError((_err) => {
+            subscriberReceivedMsgs += 1
+        })
+        await expect(publisherClient.publish(streamId, { not: 'allowed' }))
+            .rejects
+            .toThrow(/is not a publisher on stream/)
+        await wait(2000)
+        expect(subscriberReceivedMsgs).toEqual(0)
+    }, TIMEOUT)
+})
