dde9b3f1d688c049ede35fb0f9ee7bcf2d6b3c87
Eric Andrews <harbu88@hotmail.com>
2022-04-22 15:53:40 +0300

    refactor test

--- packages/client/test/end-to-end/publish-subscribe.test.ts
+++ packages/client/test/end-to-end/publish-subscribe.test.ts
@@ -1,66 +1,100 @@
-import { fastPrivateKey, waitForCondition } from 'streamr-test-utils'
+import { waitForCondition } from 'streamr-test-utils'
 import { createTestStream, fetchPrivateKeyWithGas } from '../test-utils/utils'
-import { ConfigTest, Stream, StreamPermission, StreamrClient } from '../../src'
-import { createNetworkNode, NetworkNode } from 'streamr-network'
-import { toStreamPartID } from 'streamr-client-protocol'
+import { ConfigTest, PermissionAssignment, Stream, StreamPermission, StreamrClient } from '../../src'
+import { createNetworkNode } from 'streamr-network'
+import { StreamID, toStreamPartID } from 'streamr-client-protocol'
+import { Wallet } from 'ethers'
 
-const TIMEOUT = 10 * 1000
+const TIMEOUT = 20 * 1000
 
 const PAYLOAD = { hello: 'world' }
 
 const ENCRYPTED_MESSSAGE_FORMAT = /^[0-9A-Fa-f]+$/
 
+async function startNetworkNodeAndListenForAtLeastOneMessage(streamId: StreamID): Promise<unknown[]> {
+    const networkNode = await createNetworkNode({
+        ...ConfigTest.network,
+        id: 'networkNode',
+    })
+    try {
+        networkNode.subscribe(toStreamPartID(streamId, 0))
+        const messages: unknown[] = []
+        networkNode.addMessageListener((msg) => {
+            messages.push(msg.getContent())
+        })
+        await waitForCondition(() => messages.length > 0, TIMEOUT - 100)
+        return messages
+    } finally {
+        await networkNode.stop()
+    }
+}
+
+async function createStreamWithPermissions(
+    privateKey: string,
+    ...assignments: PermissionAssignment[]
+): Promise<Stream> {
+    const creatorClient = new StreamrClient({
+        ...ConfigTest,
+        auth: {
+            privateKey
+        }
+    })
+    try {
+        const stream = await createTestStream(creatorClient, module)
+        await stream.grantPermissions(...assignments)
+        return stream
+    } finally {
+        await creatorClient.destroy()
+    }
+}
+
 describe('publish-subscribe', () => {
+    let subscriberWallet: Wallet
+    let publisherPk: string
     let publisherClient: StreamrClient
     let subscriberClient: StreamrClient
-    let networkNode: NetworkNode
+
+    beforeAll(async () => {
+        subscriberWallet = Wallet.createRandom()
+        publisherPk = await fetchPrivateKeyWithGas()
+    })
 
     beforeEach(async () => {
         publisherClient = new StreamrClient({
             ...ConfigTest,
             auth: {
-                privateKey: await fetchPrivateKeyWithGas()
+                privateKey: publisherPk
             }
         })
         subscriberClient = new StreamrClient({
             ...ConfigTest,
             auth: {
-                privateKey: fastPrivateKey()
+                privateKey: subscriberWallet.privateKey
             }
         })
-        networkNode = await createNetworkNode({
-            ...ConfigTest.network,
-            id: 'networkNode',
-        })
+
     }, TIMEOUT)
 
     afterEach(async () => {
         await Promise.allSettled([
             publisherClient?.destroy(),
             subscriberClient?.destroy(),
-            networkNode?.stop()
         ])
     }, TIMEOUT)
 
     describe('non-public stream', () => {
         let stream: Stream
 
-        beforeEach(async () => {
-            stream = await createTestStream(publisherClient, module)
-            await stream.grantPermissions({
+        beforeAll(async () => {
+            stream = await createStreamWithPermissions(publisherPk, {
                 permissions: [StreamPermission.SUBSCRIBE],
-                user: await subscriberClient.getAddress()
+                user: subscriberWallet.address
             })
         }, TIMEOUT)
 
         it('messages are published encrypted', async () => {
-            const messages: unknown[] = []
-            await networkNode.subscribeAndWaitForJoin(toStreamPartID(stream.id, 0), 4999)
-            networkNode.addMessageListener((msg) => {
-                messages.push(msg.getContent())
-            })
             await publisherClient.publish(stream.id, PAYLOAD)
-            await waitForCondition(() => messages.length > 0)
+            const messages = await startNetworkNodeAndListenForAtLeastOneMessage(stream.id)
             expect(messages).toHaveLength(1)
             expect(messages[0]).toMatch(ENCRYPTED_MESSSAGE_FORMAT)
         }, TIMEOUT)
@@ -79,22 +113,16 @@ describe('publish-subscribe', () => {
     describe('public stream', () => {
         let stream: Stream
 
-        beforeEach(async () => {
-            stream = await createTestStream(publisherClient, module)
-            await stream.grantPermissions({
+        beforeAll(async () => {
+            stream = await createStreamWithPermissions(publisherPk, {
                 permissions: [StreamPermission.SUBSCRIBE],
                 public: true
             })
         }, TIMEOUT)
 
         it('messages are published unencrypted', async () => {
-            const messages: unknown[] = []
-            await networkNode.subscribeAndWaitForJoin(toStreamPartID(stream.id, 0), 4999)
-            networkNode.addMessageListener((msg) => {
-                messages.push(msg.getContent())
-            })
             await publisherClient.publish(stream.id, PAYLOAD)
-            await waitForCondition(() => messages.length > 0)
+            const messages = await startNetworkNodeAndListenForAtLeastOneMessage(stream.id)
             expect(messages).toEqual([PAYLOAD])
         }, TIMEOUT)
 
