eac2c0c5a99204ca3d006b90565ac35701776582
juslesan <santeri.juslenius@gmail.com>
2022-04-29 15:21:04 +0300

    refactor: clean

--- packages/dht/test/unit/RouterDuplicateDetector.test.ts
+++ packages/dht/test/unit/RouterDuplicateDetector.test.ts
@@ -2,9 +2,9 @@ import { RouterDuplicateDetector } from '../../src/dht/RouterDuplicateDetector'
 
 describe('Route Message With Mock Connections', () => {
     let detector: RouterDuplicateDetector
-
+    const maxLimit = 10
     beforeEach(async () => {
-        detector = new RouterDuplicateDetector(2**15, 16, 5, 10)
+        detector = new RouterDuplicateDetector(2**15, 16, 5, maxLimit)
     })
 
     it('detects duplicates', async () => {
@@ -14,26 +14,12 @@ describe('Route Message With Mock Connections', () => {
     })
 
     it('resets on resetLimit', () => {
-        detector.add('test0')
-        detector.add('test1')
-        detector.add('test2')
-        detector.add('test3')
-        detector.add('test4')
-        detector.add('test5')
-        detector.add('test6')
-        detector.add('test7')
-        detector.add('test8')
-        detector.add('test9')
-        expect(detector.test('test0')).toEqual(true)
-        expect(detector.test('test1')).toEqual(true)
-        expect(detector.test('test2')).toEqual(true)
-        expect(detector.test('test3')).toEqual(true)
-        expect(detector.test('test4')).toEqual(true)
-        expect(detector.test('test5')).toEqual(true)
-        expect(detector.test('test6')).toEqual(true)
-        expect(detector.test('test7')).toEqual(true)
-        expect(detector.test('test8')).toEqual(true)
-        expect(detector.test('test9')).toEqual(true)
+        for (let i = 0; i < maxLimit; i++) {
+            detector.add(`test${i}`)
+        }
+        for (let i = 0; i < maxLimit; i++) {
+            expect(detector.test(`test${i}`)).toEqual(true)
+        }
         detector.add('test10')
         expect(detector.counter).toEqual(1)
         expect(detector.test('test1')).toEqual(false)
