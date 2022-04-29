a4731792639d04187916c267bb2c8acfb2e1f95e
juslesan <santeri.juslenius@gmail.com>
2022-04-29 14:04:15 +0300

    fix: add test

---
+++ packages/dht/test/unit/RouterDuplicateDetector.test.ts
@@ -0,0 +1,36 @@
+import { RouterDuplicateDetector } from '../../src/dht/RouterDuplicateDetector'
+
+describe('Route Message With Mock Connections', () => {
+    let detector: RouterDuplicateDetector
+
+    beforeEach(async () => {
+        detector = new RouterDuplicateDetector(2**15, 16, 5, 10)
+    })
+
+    afterEach(async () => {
+        await node.stop()
+    })
+
+    it('detects duplicates', async () => {
+        detector.add('test')
+        expect(detector.counter).toEqual(1)
+        expect(detector.test('test')).toEqual(true)
+    })
+
+    it('resets on resetLimit', () => {
+        detector.add('test0')
+        detector.add('test1')
+        detector.add('test2')
+        detector.add('test3')
+        detector.add('test4')
+        detector.add('test5')
+        detector.add('test6')
+        detector.add('test7')
+        detector.add('test8')
+        detector.add('test9')
+        expect(detector.test('test0')).toEqual(true)
+        detector.add('test10')
+        expect(detector.counter).toEqual(0)
+        expect(detector.test('test1')).toEqual(false)
+    })
+})
