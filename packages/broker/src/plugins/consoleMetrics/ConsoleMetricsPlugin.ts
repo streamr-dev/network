4875ed7aa276a49b1f3d188c612def75b1bcc5c0
Teo Gebhard <teo@streamr.com>
2022-05-03 10:23:59 +0300

    feat: print all metrics to console

--- packages/broker/src/plugins/consoleMetrics/ConsoleMetricsPlugin.ts
+++ packages/broker/src/plugins/consoleMetrics/ConsoleMetricsPlugin.ts
@@ -2,6 +2,7 @@ import { Schema } from 'ajv'
 import { Plugin, PluginOptions } from '../../Plugin'
 import PLUGIN_CONFIG_SCHEMA from './config.schema.json'
 import { Logger, MetricsReport } from 'streamr-network'
+import { omit } from 'lodash'
 
 const logger = new Logger(module)
 
@@ -24,67 +25,16 @@ export class ConsoleMetricsPlugin extends Plugin<ConsoleMetricsPluginConfig> {
     async start(): Promise<void> {
         const metricsContext = (await (this.streamrClient!.getNode())).getMetricsContext()
         this.producer = metricsContext.createReportProducer((report: MetricsReport) => {
-            this.printReport(report)
+            // omit timestamp info as that is printed by the logger
+            const data = omit(report, 'period')
+            // remove quote chars to improve readability
+            const output = JSON.stringify(data, undefined, 4).replace(/"/g, '')
+            logger.info(`Report\n${output}`)
         }, this.pluginConfig.interval * 1000, formatNumber)
     }
     
     async stop(): Promise<void> {
         this.producer?.stop()
-    }
-        
-    printReport(report: MetricsReport): void {
-        let storageReadMessagesPerSecond = 0
-        let storageWriteMessagesPerSecond = 0
-        let storageReadKilobytesPerSecond = 0
-        let storageWriteKilobytesPerSecond = 0
-        let resendRate = {
-            last: 0,
-            from: 0,
-            range: 0
-        }
-        const storageMetrics = report.broker?.plugin?.storage
-        if (storageMetrics !== undefined) {
-            storageReadMessagesPerSecond = storageMetrics.readMessagesPerSecond
-            storageWriteMessagesPerSecond = storageMetrics.writeMessagesPerSecond
-            storageReadKilobytesPerSecond = storageMetrics.readBytesPerSecond / 1000
-            storageWriteKilobytesPerSecond = storageMetrics.writeBytesPerSecond / 1000
-            resendRate = {
-                last: storageMetrics.resendLastQueriesPerSecond,
-                from: storageMetrics.resendFromQueriesPerSecond,
-                range: storageMetrics.resendRangeQueriesPerSecond
-            }    
-        }
-
-        const connectionAverageCount = report.node.connectionAverageCount
-        const receiveMessagesPerSecond = report.node.receiveMessagesPerSecond
-        const sendMessagesPerSecond = report.node.sendMessagesPerSecond
-        const receiveKilobytesPerSecond = report.node.receiveBytesPerSecond / 1000
-        const sendKilobytesPerSecond = report.node.sendBytesPerSecond / 1000
-
-        logger.info(
-            'Report\n'
-            + '\tNetwork connections: %s\n'
-            + '\tNetwork in: %s events/s, %s kb/s\n'
-            + '\tNetwork out: %s events/s, %s kb/s\n'
-            + '\tStorage read: %s events/s, %s kb/s\n'
-            + '\tStorage write: %s events/s, %s kb/s\n'
-            + '\tResends:\n'
-            + '\t- last: %s requests/s\n'
-            + '\t- from: %s requests/s\n'
-            + '\t- range: %s requests/s\n',
-            connectionAverageCount,
-            receiveMessagesPerSecond,
-            receiveKilobytesPerSecond,
-            sendMessagesPerSecond,
-            sendKilobytesPerSecond,
-            storageReadMessagesPerSecond,
-            storageReadKilobytesPerSecond,
-            storageWriteMessagesPerSecond,
-            storageWriteKilobytesPerSecond,
-            resendRate.last,
-            resendRate.from,
-            resendRate.range,
-        )
     }
 
     getConfigSchema(): Schema {
