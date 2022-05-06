088e4cc1516a2fa305a7fa6a7ae02d97aa256dc8
Kare Nuorteva <kare.nuorteva@me.com>
2022-05-06 20:30:12 +0300

    refactor(network): remove deprecated pino options

--- packages/network/src/helpers/logger/LoggerNode.ts
+++ packages/network/src/helpers/logger/LoggerNode.ts
@@ -1,20 +1,5 @@
 import pino from 'pino'
-import pinoPretty from 'pino-pretty'
-
 import { LoggerCommon } from './LoggerCommon'
-
-const parseBoolean = (value: string|undefined) => {
-    switch (value) {
-        case 'true':
-            return true
-        case 'false':
-            return false
-        case undefined:
-            return undefined
-        default:
-            throw new Error('Invalid boolean value: ${value}')
-    }
-}
 
 export class LoggerNode extends LoggerCommon {
     constructor(module: NodeJS.Module, context?: string, destinationStream?: { write(msg: string): void }) {
@@ -22,17 +7,6 @@ export class LoggerNode extends LoggerCommon {
             name: LoggerNode.createName(module, context),
             enabled: !process.env.NOLOG,
             level: process.env.LOG_LEVEL || 'info',
-            // explicitly pass prettifier, otherwise pino may try to lazy require it,
-            // which can fail when under jest+typescript, due to some CJS/ESM
-            // incompatibility leading to throwing an error like:
-            // "prettyFactory is not a function"
-            prettifier: process.env.NODE_ENV === 'production' ? undefined : pinoPretty,
-            prettyPrint: process.env.NODE_ENV === 'production' ? false : {
-                colorize: parseBoolean(process.env.LOG_COLORS) ?? true,
-                translateTime: 'yyyy-mm-dd"T"HH:MM:ss.l',
-                ignore: 'pid,hostname',
-                levelFirst: true,
-            }
         }
         super(options, destinationStream)
     }
