7fff2694feec2f9487115ef86822f5cb0bb2bd74
Teo Gebhard <teo@streamr.com>
2022-05-09 13:36:06 +0300

    feat: update simulator

--- packages/network/src/simulator/NodeWebRtcConnection_simulator.ts
+++ packages/network/src/simulator/NodeWebRtcConnection_simulator.ts
@@ -6,13 +6,15 @@ import { WebRtcConnectionFactory } from "../connection/webrtc/WebRtcEndpoint"
 import { Simulator } from "./Simulator"
 import { DescriptionType } from 'node-datachannel'
 
-const NodeWebRtcConnectionFactory: WebRtcConnectionFactory = Object.freeze({
+export const webRtcConnectionFactory = new class implements WebRtcConnectionFactory {
     createConnection(opts: ConstructorOptions): WebRtcConnection {
         return new NodeWebRtcConnection(opts)
     }
-})
-
-export default NodeWebRtcConnectionFactory
+    registerWebRtcEndpoint(): void {
+    }
+    unregisterWebRtcEndpoint(): void {
+    }
+}
 
 export class NodeWebRtcConnection extends WebRtcConnection {
     private readonly logger: Logger
