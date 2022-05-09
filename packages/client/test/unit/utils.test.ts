ba0d73edba66da7167bcc883878067dc0c15eb75
Eric Andrews <harbu88@hotmail.com>
2022-05-09 10:55:14 +0300

    fix(client): remove unneeded test

--- packages/client/test/unit/utils.test.ts
+++ packages/client/test/unit/utils.test.ts
@@ -1,59 +1,7 @@
-import { Server } from 'http'
-import express, { Application } from 'express'
-import { wait } from 'streamr-test-utils'
-
 import * as utils from '../../src/utils'
 import { inspect, format, DEFAULT_INSPECT_OPTS } from '../../src/utils/log'
 
-import { describeRepeats, Debug } from '../test-utils/utils'
-
-const debug = Debug('StreamrClient::test::utils')
-
-interface TestResponse {
-    test: string
-}
-
-describeRepeats('utils', () => {
-    describe('with mock server', () => {
-        let expressApp: Application
-        let server: Server
-        const baseUrl = 'http://127.0.0.1:30000'
-        const testUrl = '/some-test-url'
-
-        beforeEach((done) => {
-            expressApp = express()
-
-            function handle(req: any, res: any) {
-                res.status(200).send({
-                    test: 'test',
-                })
-            }
-
-            expressApp.get(testUrl, (req: any, res: any) => handle(req, res))
-
-            server = expressApp.listen(30000, () => {
-                debug('Mock server started on port 30000\n')
-                done()
-            })
-        })
-
-        afterEach(async () => {
-            await new Promise((resolve, reject) => {
-                server.close((err) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve(undefined)
-                    }
-                })
-            })
-        })
-
-        afterAll(async () => {
-            await wait(1000) // wait a moment for server to truly close
-        })
-    })
-
+describe('utils', () => {
     describe('uuid', () => {
         it('generates different ids', () => {
             expect(utils.uuid('test')).not.toEqual(utils.uuid('test'))
