bece171702fd33463a687c3f81353dcc30c8fb79
Santeri Juslenius <santeri.juslenius@gmail.com>
2022-05-16 16:18:32 +0300

    NET-830: WebRTC RPC stack (#650)
    * feat: WebRTC remote connetor, RPC stack
    
    * fix: flaky browser test

---
+++ packages/dht/src/connection/WebRTC/RemoteWebrtcConnector.ts
@@ -0,0 +1,162 @@
+import {
+    IceCandidate,
+    NotificationResponse,
+    PeerDescriptor,
+    RtcAnswer,
+    RtcOffer,
+    WebRtcConnectionRequest
+} from '../../proto/DhtRpc'
+import { IWebRtcConnectorClient } from '../../proto/DhtRpc.client'
+import { PeerID } from '../../PeerID'
+import { DhtRpcOptions } from '../../rpc-protocol/ClientTransport'
+import { TODO } from '../../types'
+import { IWebRtcConnector } from '../../proto/DhtRpc.server'
+import { ServerCallContext } from '@protobuf-ts/runtime-rpc'
+import { DummyServerCallContext } from '../../rpc-protocol/ServerTransport'
+
+export class RemoteWebrtcConnector {
+    private peerId: PeerID
+    constructor(private peerDescriptor: PeerDescriptor, private client: IWebRtcConnectorClient) {
+        this.peerId = PeerID.fromValue(peerDescriptor.peerId)
+    }
+
+    async requestConnection(sourceDescriptor: PeerDescriptor, connectionId: string): Promise<boolean> {
+        const request: WebRtcConnectionRequest = {
+            target: this.peerDescriptor,
+            requester: sourceDescriptor,
+            connectionId
+        }
+        const options: DhtRpcOptions = {
+            sourceDescriptor: sourceDescriptor as PeerDescriptor,
+            targetDescriptor: this.peerDescriptor as PeerDescriptor,
+            notification: true
+        }
+        try {
+            const response = await this.client.requestConnection(request, options)
+            const res = await response.response
+            return res.sent
+        } catch (err) {
+            console.error(err)
+            return false
+        }
+    }
+
+    async sendRtcOffer(sourceDescriptor: PeerDescriptor, description: string, connectionId: string): Promise<boolean> {
+        const request: RtcOffer = {
+            target: this.peerDescriptor,
+            requester: sourceDescriptor,
+            connectionId,
+            description
+        }
+        const options: DhtRpcOptions = {
+            sourceDescriptor: sourceDescriptor as PeerDescriptor,
+            targetDescriptor: this.peerDescriptor as PeerDescriptor,
+            notification: true
+        }
+        try {
+            const response = await this.client.rtcOffer(request, options)
+            const res = await response.response
+            return res.sent
+        } catch (err) {
+            console.error(err)
+            return false
+        }
+    }
+
+    async sendRtcAnswer(sourceDescriptor: PeerDescriptor, description: string, connectionId: string): Promise<boolean> {
+        const request: RtcOffer = {
+            target: this.peerDescriptor,
+            requester: sourceDescriptor,
+            connectionId,
+            description
+        }
+        const options: DhtRpcOptions = {
+            sourceDescriptor: sourceDescriptor as PeerDescriptor,
+            targetDescriptor: this.peerDescriptor as PeerDescriptor,
+            notification: true
+        }
+        try {
+            const response = await this.client.rtcAnswer(request, options)
+            const res = await response.response
+            return res.sent
+        } catch (err) {
+            console.error(err)
+            return false
+        }
+    }
+
+    async sendIceCandidate(sourceDescriptor: PeerDescriptor, candidate: string, mid: string, connectionId: string): Promise<boolean> {
+        const request: IceCandidate = {
+            target: this.peerDescriptor,
+            requester: sourceDescriptor,
+            connectionId,
+            mid,
+            candidate
+        }
+        const options: DhtRpcOptions = {
+            sourceDescriptor: sourceDescriptor as PeerDescriptor,
+            targetDescriptor: this.peerDescriptor as PeerDescriptor,
+            notification: true
+        }
+        try {
+            const response = await this.client.iceCandidate(request, options)
+            const res = await response.response
+            return res.sent
+        } catch (err) {
+            console.error(err)
+            return false
+        }
+    }
+}
+
+export const createRemoteWebRtcConnectorServer = (onRtcOffer: TODO, onRtcAnswer: TODO, onIceCandidate: TODO, connectFn: TODO): any => {
+    const rpc: IWebRtcConnector = {
+        async requestConnection(request: WebRtcConnectionRequest, _context: ServerCallContext): Promise<NotificationResponse> {
+            setImmediate(() => connectFn(request.requester, request.target, request.connectionId))
+            const res: NotificationResponse = {
+                sent: true
+            }
+            return res
+        },
+        async rtcOffer(request: RtcOffer, _context: ServerCallContext): Promise<NotificationResponse> {
+            setImmediate(() => onRtcOffer(request.requester, request.target, request.description, request.connectionId))
+            const res: NotificationResponse = {
+                sent: true
+            }
+            return res
+        },
+        async rtcAnswer(request: RtcAnswer, _context: ServerCallContext): Promise<NotificationResponse> {
+            setImmediate(() => onRtcAnswer(request.requester, request.target, request.description, request.connectionId))
+            const res: NotificationResponse = {
+                sent: true
+            }
+            return res
+        },
+        async iceCandidate(request: IceCandidate, _context: ServerCallContext): Promise<NotificationResponse> {
+            setImmediate(() => onIceCandidate(request.requester, request.target, request.candidate, request.mid, request.connectionId))
+            const res: NotificationResponse = {
+                sent: true
+            }
+            return res
+        }
+    }
+    const register = {
+        async requestConnection(bytes: Uint8Array): Promise<void> {
+            const request = WebRtcConnectionRequest.fromBinary(bytes)
+            await rpc.requestConnection(request, new DummyServerCallContext())
+        },
+        async rtcOffer(bytes: Uint8Array): Promise<void> {
+            const request = RtcOffer.fromBinary(bytes)
+            await rpc.rtcOffer(request, new DummyServerCallContext())
+        },
+        async rtcAnswer(bytes: Uint8Array): Promise<void> {
+            const request = RtcAnswer.fromBinary(bytes)
+            await rpc.rtcAnswer(request, new DummyServerCallContext())
+        },
+        async iceCandidate(bytes: Uint8Array): Promise<void> {
+            const request = IceCandidate.fromBinary(bytes)
+            await rpc.iceCandidate(request, new DummyServerCallContext())
+        }
+    }
+    return register
+}
---
+++ packages/dht/test/integration/WebRtcConnectorRpc.test.ts
@@ -0,0 +1,134 @@
+import { ITransport } from '../../src/transport/ITransport'
+import { RpcCommunicator } from '../../src/transport/RpcCommunicator'
+import { WebRtcConnectorClient } from '../../src/proto/DhtRpc.client'
+import { Simulator } from '../../src/connection/Simulator'
+import { Message, PeerDescriptor } from '../../src/proto/DhtRpc'
+import { generateId } from '../../src/dht/helpers'
+import { MockConnectionManager } from '../../src/connection/MockConnectionManager'
+import { createRemoteWebRtcConnectorServer } from '../../src/connection/WebRTC/RemoteWebrtcConnector'
+import { waitForCondition } from 'streamr-test-utils'
+
+describe('WebRTC rpc messages', () => {
+    let mockConnectionLayer1: ITransport,
+        mockConnectionLayer2: ITransport,
+        rpcCommunicator1: RpcCommunicator,
+        rpcCommunicator2: RpcCommunicator,
+        client: WebRtcConnectorClient
+
+    const simulator = new Simulator()
+
+    let requestConnectionCounter: number
+    let rtcOfferCounter: number
+    let rtcAnswerCounter: number
+    let iceCandidateCounter: number
+
+    const peerDescriptor1: PeerDescriptor = {
+        peerId: generateId('peer1'),
+        type: 0
+    }
+
+    const peerDescriptor2: PeerDescriptor = {
+        peerId: generateId('peer2'),
+        type: 0
+    }
+
+    beforeEach(() => {
+        requestConnectionCounter = 0
+        rtcOfferCounter = 0
+        rtcAnswerCounter = 0
+        iceCandidateCounter = 0
+
+        mockConnectionLayer1 = new MockConnectionManager(peerDescriptor1, simulator)
+        rpcCommunicator1 = new RpcCommunicator({
+            connectionLayer: mockConnectionLayer1,
+            appId: "webrtc"
+        })
+        const serverFunctions = createRemoteWebRtcConnectorServer(
+            () => { rtcOfferCounter += 1 },
+            () => { rtcAnswerCounter += 1 },
+            () => { iceCandidateCounter += 1 },
+            () => { requestConnectionCounter += 1 }
+        )
+
+        mockConnectionLayer2 = new MockConnectionManager(peerDescriptor2, simulator)
+        rpcCommunicator2 = new RpcCommunicator({
+            connectionLayer: mockConnectionLayer2,
+            appId: "webrtc"
+        })
+        rpcCommunicator2.registerServerMethod('rtcOffer', serverFunctions.rtcOffer)
+        rpcCommunicator2.registerServerMethod('rtcAnswer', serverFunctions.rtcAnswer)
+        rpcCommunicator2.registerServerMethod('iceCandidate', serverFunctions.iceCandidate)
+        rpcCommunicator2.registerServerMethod('requestConnection', serverFunctions.requestConnection)
+
+        rpcCommunicator1.setSendFn((peerDescriptor: PeerDescriptor, message: Message) => {
+            rpcCommunicator2.onIncomingMessage(peerDescriptor, message)
+        })
+
+        rpcCommunicator2.setSendFn((peerDescriptor: PeerDescriptor, message: Message) => {
+            rpcCommunicator1.onIncomingMessage(peerDescriptor, message)
+        })
+
+        client = new WebRtcConnectorClient(rpcCommunicator1.getRpcClientTransport())
+    })
+
+    afterEach(async () => {
+        await rpcCommunicator1.stop()
+        await rpcCommunicator2.stop()
+    })
+
+    it('send connectionRequest', async () => {
+        const response = client.requestConnection({
+            requester: peerDescriptor1,
+            target: peerDescriptor2,
+            connectionId: 'connectionRequest'
+        },
+        { targetDescriptor: peerDescriptor2, notification: true }
+        )
+        const res = await response.response
+        await (expect(res.sent)).toEqual(true)
+        await waitForCondition(() => requestConnectionCounter === 1)
+    })
+
+    it('send rtcOffer', async () => {
+        const response = client.rtcOffer({
+            requester: peerDescriptor1,
+            target: peerDescriptor2,
+            connectionId: 'rtcOffer',
+            description: 'aaaaaa'
+        },
+        { targetDescriptor: peerDescriptor2, notification: true }
+        )
+        const res = await response.response
+        await (expect(res.sent)).toEqual(true)
+        await waitForCondition(() => rtcOfferCounter === 1)
+    })
+
+    it('send rtcAnswer', async () => {
+        const response = client.rtcAnswer({
+            requester: peerDescriptor1,
+            target: peerDescriptor2,
+            connectionId: 'rtcOffer',
+            description: 'aaaaaa'
+        },
+        { targetDescriptor: peerDescriptor2, notification: true }
+        )
+        const res = await response.response
+        await (expect(res.sent)).toEqual(true)
+        await waitForCondition(() => rtcAnswerCounter === 1)
+    })
+
+    it('send iceCandidate', async () => {
+        const response = client.iceCandidate({
+            requester: peerDescriptor1,
+            target: peerDescriptor2,
+            connectionId: 'rtcOffer',
+            candidate: 'aaaaaa',
+            mid: 'asdasdasdasdasd'
+        },
+        { targetDescriptor: peerDescriptor2, notification: true }
+        )
+        const res = await response.response
+        await (expect(res.sent)).toEqual(true)
+        await waitForCondition(() => iceCandidateCounter === 1)
+    })
+})
