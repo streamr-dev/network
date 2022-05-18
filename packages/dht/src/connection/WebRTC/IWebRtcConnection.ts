bdd3122d0b4b6f5c40c4f9c30b87ab55b87d7677
Petri Savolainen <petri.savolainen@spaceify.net>
2022-05-18 11:08:51 +0300

    feat: add IWebRtcConnection

---
+++ packages/dht/src/connection/WebRTC/IWebRtcConnection.ts
@@ -0,0 +1,16 @@
+export enum Event {
+    LOCAL_DESCRIPTION = 'streamr:dht:webrtcconnection:localdescription',
+    LOCAL_CANDIDATE = 'streamr:dht:webrtcconnection:localcandidate',
+}
+
+export interface IWebRtcConnection {
+    start(isOffering: boolean): void
+
+    on(event: Event.LOCAL_DESCRIPTION, listener: (description: string, type: string) => void): this
+    on(event: Event.LOCAL_CANDIDATE, listener: (candidate: string, mid: string) => void): this
+    once(event: Event.LOCAL_CANDIDATE, listener: (candidate: string, mid: string) => void): this
+    once(event: Event.LOCAL_DESCRIPTION, listener: (description: string, type: string) => void): this
+
+    setRemoteDescription(description: string, type: string): void
+    addRemoteCandidate(candidate: string, mid: string): void
+}
