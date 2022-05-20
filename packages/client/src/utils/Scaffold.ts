15cc79a492e2ed3a4442b994e9594f0452206258
Teo Gebhard <teo@streamr.com>
2022-05-20 15:04:21 +0300

    refactor: return type of Scaffold

--- packages/client/src/utils/Scaffold.ts
+++ packages/client/src/utils/Scaffold.ts
@@ -26,11 +26,23 @@ type ScaffoldOptions = {
 
 const noop = () => {}
 
+type ScaffoldReturnType = (() => Promise<void>) & 
+{ 
+    readonly activeCount: number
+    readonly pendingCount: number
+    next: () => Promise<void>
+    isActive: () => boolean
+    getCurrentStep(): Promise<void>
+    setError(err: Error): void
+    getError(): Error | undefined
+    clearError(): Error | undefined
+}
+
 export function Scaffold(
     sequence: Step[] = [],
     _checkFn: (() => Promise<boolean>) | (() => boolean),
     { id = '', onError, onDone, onChange }: ScaffoldOptions = {}
-): any {
+): ScaffoldReturnType {
     let error: Error | undefined
     // ignore error if check fails
 
@@ -52,7 +64,7 @@ export function Scaffold(
     let isDone = false
     let didStart = false
 
-    function collectErrors(err: Error) {
+    function collectErrors(err: Error): void {
         try {
             if (typeof onError === 'function') {
                 onError(err) // give option to suppress error
@@ -132,7 +144,7 @@ export function Scaffold(
         return Promise.resolve()
     }
 
-    function isActive() {
+    function isActive(): boolean {
         return !(
             didStart
             && isDone
@@ -169,7 +181,7 @@ export function Scaffold(
     return Object.assign(queuedNext, {
         next: nextDone,
         isActive,
-        getCurrentStep() {
+        getCurrentStep(): Promise<void> {
             return currentStep
         },
         get activeCount() {
@@ -178,13 +190,13 @@ export function Scaffold(
         get pendingCount() {
             return queue.pendingCount
         },
-        setError(err: Error) {
+        setError(err: Error): void {
             error = AggregatedError.from(error, err)
         },
-        getError() {
+        getError(): Error | undefined {
             return error
         },
-        clearError() {
+        clearError(): Error | undefined {
             const err = error
             error = undefined
             return err
