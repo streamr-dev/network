3f635399b3f4a0c1843fa428305f0c00625877c0
Eric Andrews <harbu88@hotmail.com>
2022-05-30 12:59:47 +0300

    Revert "fix: add listening to Node?"
    This reverts commit 40c7c93eb8f7170c5503cabfa6c229803dbdfe71.

--- packages/network/src/logic/Node.ts
+++ packages/network/src/logic/Node.ts
@@ -1,6 +1,11 @@
 import { EventEmitter } from 'events'
-import { MessageLayer, ProxyDirection, StreamMessage, StreamPartID } from 'streamr-client-protocol'
-import { Event as NodeToNodeEvent, NodeToNode } from '../protocol/NodeToNode'
+import {
+    MessageLayer,
+    StreamPartID,
+    StreamMessage,
+    ProxyDirection
+} from 'streamr-client-protocol'
+import { NodeToNode, Event as NodeToNodeEvent } from '../protocol/NodeToNode'
 import { NodeToTracker } from '../protocol/NodeToTracker'
 import { Metric, MetricsContext, MetricsDefinition, RateMetric } from '../helpers/Metric'
 import { promiseTimeout } from '../helpers/PromiseTools'
@@ -192,11 +197,6 @@ export class Node extends EventEmitter {
 
         this.nodeToNode.on(NodeToNodeEvent.LEAVE_REQUEST_RECEIVED, (message, nodeId) => {
             this.proxyStreamConnectionManager.processLeaveRequest(message, nodeId)
-        })
-        this.on(Event.PROXY_CONNECTION_ACCEPTED, (node: string, stream: StreamPartID) => {
-            // I as publish-proxy join a stream OR
-            // I as normal node receive subscribe-only connection
-            this.propagation.onNeighborJoined(node, stream)
         })
     }
 
