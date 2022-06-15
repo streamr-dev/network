e13246c9b381b64103b75fb82408d89eef1005ea
Eric Andrews <harbu88@hotmail.com>
2022-06-15 15:19:14 +0300

    refactors

--- packages/protocol/test/integration/OrderingUtil.test.ts
+++ packages/protocol/test/integration/OrderingUtil.test.ts
@@ -5,43 +5,67 @@ import MessageRef from '../../src/protocol/message_layer/MessageRef'
 import { wait, waitForCondition } from 'streamr-test-utils'
 
 const MESSAGES_PER_PUBLISHER = 1000
-const MAX_MESSAGE_ARRIVAL_LATENCY = 20
-const MESSAGE_FAILURE_RATE = 1/10
-const NON_EXISTING_MESSAGE_RATE = 1/100
+const NUM_OF_DUPLICATE_MESSAGES = 500
+const MAX_GAP_FILL_MESSAGE_LATENCY = 20 // latency ~ [0, 20]
+const GAP_FILLED_RATE = 1/10
+const UNAVAILABLE_RATE = 1/100
 
-const PROPAGATION_TIMEOUT = MAX_MESSAGE_ARRIVAL_LATENCY * 3
-const RESEND_TIMEOUT = MAX_MESSAGE_ARRIVAL_LATENCY * 5
-const MAX_GAP_REQUESTS = 3
+const PROPAGATION_TIMEOUT = 200
+const RESEND_TIMEOUT = 100
+const MAX_GAP_REQUESTS = 5
 
 const PUBLISHER_IDS = ['publisherOne', 'publisherTwo', 'publisherThree']
+
+enum Delivery {
+    REAL_TIME,
+    GAP_FILL,
+    UNAVAILABLE
+}
 
 interface MessageInfo {
     publisherId: string
     timestamp: number
-    missing: boolean
-    initiallyMissing: boolean
+    delivery: Delivery
+}
+
+function duplicateElements<T>(arr: readonly T[], numOfDuplicates: number): T[] {
+    const newArr = Array.from(arr)
+    for (let i = 0; i < numOfDuplicates; ++i) {
+        newArr.push(arr[i % arr.length])
+    }
+    return newArr
+}
+
+function intoChunks<T>(arr: readonly T[], chunkSize: number): T[][] {
+    const chunks: T[][] = []
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        chunks.push(arr.slice(i, i + chunkSize))
+    }
+    return chunks
 }
 
 function formChainOfMessages(publisherId: string): Array<MessageInfo> {
     const chainOfMessages: MessageInfo[] = [{
         publisherId,
         timestamp: 1,
-        missing: false,
-        initiallyMissing: false
+        delivery: Delivery.REAL_TIME
     }]
     for (let i = 2; i < MESSAGES_PER_PUBLISHER; i++) {
         chainOfMessages.push({
             publisherId,
             timestamp: i,
-            missing: Math.random() < NON_EXISTING_MESSAGE_RATE,
-            initiallyMissing: Math.random() < MESSAGE_FAILURE_RATE
+            delivery: Math.random() < UNAVAILABLE_RATE
+                ? Delivery.UNAVAILABLE
+                : (Math.random() < GAP_FILLED_RATE
+                    ? Delivery.GAP_FILL
+                    : Delivery.REAL_TIME
+                )
         })
     }
     chainOfMessages.push({
         publisherId,
         timestamp: MESSAGES_PER_PUBLISHER,
-        missing: false,
-        initiallyMissing: false
+        delivery: Delivery.REAL_TIME
     })
     return chainOfMessages
 }
@@ -56,53 +80,50 @@ function createMsg({ publisherId, timestamp }: MessageInfo): StreamMessage {
     })
 }
 
-function calculateNumberOfGaps(messageInfosInOrder: MessageInfo[]): number {
-    let lastMissing = false
+function calculateNumberOfUnfillableGaps(messageInfosInOrder: MessageInfo[]): number {
+    let lastMessageUnavailable = false
     let gaps = 0
-    messageInfosInOrder.forEach((mi) => {
-        if (!lastMissing && mi.missing) {
-            lastMissing = true
-        } else if (lastMissing && !mi.missing) {
+    messageInfosInOrder.forEach((messageInfo) => {
+        if (!lastMessageUnavailable && messageInfo.delivery === Delivery.UNAVAILABLE) {
+            lastMessageUnavailable = true
+        } else if (lastMessageUnavailable && messageInfo.delivery !== Delivery.UNAVAILABLE) {
             gaps += 1
-            lastMissing = false
+            lastMessageUnavailable = false
         }
     })
     return gaps
 }
 
 describe(OrderingUtil, () => {
-    it('randomized "worst-case" scenario with missing messages and gap fill needs', async () => {
+    it('randomized "worst-case" scenario with unavailable messages and gap fill needs', async () => {
         const groundTruthMessages: Record<string, MessageInfo[]> = {}
         const actual: Record<string, number[]> = {}
         const expected: Record<string, number[]> = {}
 
         for (const publisherId of PUBLISHER_IDS) {
-            actual[publisherId] = []
             groundTruthMessages[publisherId] = formChainOfMessages(publisherId)
+            actual[publisherId] = []
             expected[publisherId] = groundTruthMessages[publisherId]
-                .filter((mi) => !mi.missing)
-                .map((mi) => mi.timestamp)
+                .filter(({ delivery }) => delivery !== Delivery.UNAVAILABLE)
+                .map(({ timestamp }) => timestamp)
         }
 
-        const totalGaps = PUBLISHER_IDS.reduce((sum, publisherId) => (
-            sum + calculateNumberOfGaps(groundTruthMessages[publisherId])
+        const totalUnfillableGaps = PUBLISHER_IDS.reduce((sum, publisherId) => (
+            sum + calculateNumberOfUnfillableGaps(groundTruthMessages[publisherId])
         ), 0)
 
         const inOrderHandler = (msg: StreamMessage) => {
             actual[msg.getPublisherId()].push(msg.getTimestamp())
         }
 
-        const addMessageToUtil = async (msgInfo: MessageInfo, maxLatency: number) => {
-            await wait(Math.random() * maxLatency)
-            util.add(createMsg(msgInfo))
-        }
-
         const gapHandler = async (from: MessageRef, to: MessageRef, publisherId: string) => {
-            const requestedMessages = groundTruthMessages[publisherId].filter(({ missing, timestamp }) => {
-                return !missing && (timestamp > from.timestamp && timestamp <= to.timestamp)
+            //console.info(`gapHandler: ${from.serialize()} -> ${to.serialize()} (${publisherId})`)
+            const requestedMessages = groundTruthMessages[publisherId].filter(({ delivery, timestamp }) => {
+                return delivery === Delivery.GAP_FILL && (timestamp > from.timestamp && timestamp <= to.timestamp)
             })
-            for (const msg of requestedMessages) {
-                await addMessageToUtil(msg, MAX_MESSAGE_ARRIVAL_LATENCY)
+            for (const msgInfo of requestedMessages) {
+                await wait(Math.random() * MAX_GAP_FILL_MESSAGE_LATENCY)
+                util.add(createMsg(msgInfo))
             }
         }
 
@@ -112,22 +133,37 @@ describe(OrderingUtil, () => {
 
         // supply 1st message of chain always to set gap detection to work from 1st message onwards
         for (const publisherId of PUBLISHER_IDS) {
-            await addMessageToUtil(groundTruthMessages[publisherId][0], 0)
+            util.add(createMsg(groundTruthMessages[publisherId][0]))
         }
 
-        const arrivingMessages = shuffle(Object.values(groundTruthMessages).flat())
-            .filter(({ missing, initiallyMissing }) => !missing && !initiallyMissing)
-        for (const msgInfo of arrivingMessages) {
-            await addMessageToUtil(msgInfo, MAX_MESSAGE_ARRIVAL_LATENCY)
+        const realTimeMessages = Object.values(groundTruthMessages)
+            .flat()
+            .filter(({ delivery }) => delivery === Delivery.REAL_TIME)
+        const shuffledWithDuplicates = duplicateElements(shuffle(realTimeMessages), NUM_OF_DUPLICATE_MESSAGES)
+
+        const realTimeStart = Date.now()
+        for (const chunkOfMsgInfos of intoChunks(shuffledWithDuplicates, 10)) {
+            await wait(0)
+            for (const msgInfo of chunkOfMsgInfos) {
+                util.add(createMsg(msgInfo))
+            }
+        }
+        const realTimeEnd = Date.now()
+        const realTimeTook = realTimeEnd - realTimeStart
+        const firstGapFillCouldFailAfter = PROPAGATION_TIMEOUT + RESEND_TIMEOUT * MAX_GAP_REQUESTS
+        if (realTimeTook > firstGapFillCouldFailAfter) {
+            // The time it takes to push all real-time messages should not exceed the time the first gap fill could fail
+            // due to the message arriving later on...
+            throw new Error(`took too long (${realTimeTook} ms > ${firstGapFillCouldFailAfter} ms) to ` +
+            'push real-time messages, consider adding more timeout...')
         }
 
         await Promise.race([
             waitForCondition(() => PUBLISHER_IDS.every((publisherId) => (
                 expected[publisherId].length === actual[publisherId].length
-            )), 30*1000),
-            wait(29 * 1000)
+            )), 60*1000)
         ])
-        expect(expected).toStrictEqual(actual)
-        expect(errorHandler).toHaveBeenCalledTimes(totalGaps)
-    }, 60 * 1000)
+        expect(errorHandler).toHaveBeenCalledTimes(totalUnfillableGaps)
+        expect(actual).toStrictEqual(expected)
+    }, 120 * 1000)
 })
