3d76732c742ac11b7b3b05891f71c2f430332893
Eric Andrews <harbu88@hotmail.com>
2022-06-16 19:04:12 +0300

    wip

--- packages/protocol/test/unit/protocol/message_layer/GroupKeyAnnounce.test.ts
+++ packages/protocol/test/unit/protocol/message_layer/GroupKeyAnnounce.test.ts
@@ -1,4 +1,5 @@
 import assert from 'assert'
+import { foobar } from '@streamr/utils'
 
 import {
     StreamMessage,
@@ -21,6 +22,7 @@ const serializedMessage = JSON.stringify(['streamId', [['groupKeyId1', 'encrypte
 describe('GroupKeyAnnounce', () => {
     describe('deserialize', () => {
         it('correctly parses messages', () => {
+            console.info(foobar())
             assert.deepStrictEqual(GroupKeyMessage.deserialize(serializedMessage, StreamMessage.MESSAGE_TYPES.GROUP_KEY_ANNOUNCE), message)
         })
     })
