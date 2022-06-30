65eff41070f45a627bb2a8220a9916c5ee697010
Teo Gebhard <teo@streamr.com>
2022-06-30 23:55:07 +0300

    refactor: timeouts config in HttpFetcher

--- packages/client/src/utils/HttpFetcher.ts
+++ packages/client/src/utils/HttpFetcher.ts
@@ -1,7 +1,7 @@
 import { inject, Lifecycle, scoped } from 'tsyringe'
 import { Debugger } from 'debug'
 import { Context } from './Context'
-import { ConfigInjectionToken, StrictStreamrClientConfig } from '../Config'
+import { ConfigInjectionToken, TimeoutsConfig } from '../Config'
 import { instanceId } from './utils'
 import fetch, { Response } from 'node-fetch'
 
@@ -11,14 +11,13 @@ export class HttpFetcher {
 
     constructor(
         context: Context,
-        @inject(ConfigInjectionToken.Root) private config: StrictStreamrClientConfig
+        @inject(ConfigInjectionToken.Timeouts) private timeoutsConfig: TimeoutsConfig
     ) {
         this.debug = context.debug.extend(instanceId(this))
     }
 
     fetch(url: string, init?: Record<string, unknown>): Promise<Response> {
-        // eslint-disable-next-line no-underscore-dangle
-        const timeout = this.config._timeouts.httpFetchTimeout
+        const timeout = this.timeoutsConfig.httpFetchTimeout
         this.debug('fetching %s (timeout %d ms)', url, timeout)
         return fetch(url, {
             timeout,
