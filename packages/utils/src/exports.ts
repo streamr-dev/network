1b51d21cfd1165a5e2bdc4cc3ca03c11492c51f4
Eric Andrews <harbu88@hotmail.com>
2022-07-01 14:57:11 +0300

    fix(utils): remove HELLO_WORLD placeholder export

--- packages/utils/src/exports.ts
+++ packages/utils/src/exports.ts
@@ -1,9 +1,7 @@
 import { LoggerNode as Logger } from './LoggerNode'
 import { withTimeout, TimeoutError } from './withTimeout'
 
-const HELLO_WORLD = 'hello world'
 export {
-    HELLO_WORLD,
     withTimeout,
     TimeoutError,
     Logger
