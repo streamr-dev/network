dc2c8a9afeddc612c05dea11b6f93060edd145a2
Eric Andrews <harbu88@hotmail.com>
2022-07-07 10:43:43 +0300

    test(utils): test for scheduleAtFixedRate and scheduleAtInterval

---
+++ packages/utils/test/scheduleAtFixedRate.test.ts
@@ -0,0 +1,24 @@
+import { scheduleAtFixedRate } from '../src/scheduleAtFixedRate'
+import { wait } from 'streamr-test-utils'
+
+const INTERVAL = 100
+
+describe(scheduleAtFixedRate, () => {
+    let task: jest.Mock<Promise<void>, [number]>
+    let ref: { stop: () => void }
+
+    beforeEach(() => {
+        task = jest.fn()
+    })
+
+    afterEach(() => {
+        ref.stop()
+    })
+
+    it('repeats every `interval`', async () => {
+        ref = scheduleAtFixedRate(task, INTERVAL)
+        await wait(4 * INTERVAL)
+        expect(task.mock.calls.length).toBeGreaterThanOrEqual(4)
+        expect(task.mock.calls.every(([now]) => now % 100 === 0)).toEqual(true)
+    })
+})
---
+++ packages/utils/test/scheduleAtInterval.test.ts
@@ -0,0 +1,41 @@
+import { scheduleAtInterval } from '../src/scheduleAtInterval'
+import { wait } from 'streamr-test-utils'
+
+const INTERVAL = 40
+const FIVE_REPEATS_TIME = INTERVAL * 5 + INTERVAL/2
+
+describe(scheduleAtInterval, () => {
+    let task: jest.Mock<Promise<void>, []>
+    let ref: { stop: () => void }
+
+    beforeEach(() => {
+        task = jest.fn()
+    })
+
+    afterEach(() => {
+        ref.stop()
+    })
+
+    it('execute at start enabled', async () => {
+        ref = await scheduleAtInterval(task, INTERVAL, true)
+        expect(task).toHaveBeenCalledTimes(1)
+    })
+
+    it('execute at start disabled', async () => {
+        ref = await scheduleAtInterval(task, INTERVAL, false)
+        expect(task).toHaveBeenCalledTimes(0)
+    })
+
+    it('repeats every `interval`', async () => {
+        ref = await scheduleAtInterval(task, INTERVAL, false)
+        await wait(FIVE_REPEATS_TIME)
+        expect(task.mock.calls.length).toEqual(5)
+    })
+
+    it('does not take into account the time for the promise to settle', async () => {
+        task.mockImplementation(() => wait(INTERVAL))
+        ref = await scheduleAtInterval(task, INTERVAL, false)
+        await wait(FIVE_REPEATS_TIME)
+        expect(task.mock.calls.length).toBeLessThan(5)
+    })
+})
