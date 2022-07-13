556482c86778053436a0990b1acad800a764a2ef
Eric Andrews <harbu88@hotmail.com>
2022-07-13 16:53:31 +0300

    refactor: mv keyToArrayIndex to @streamr/utils

--- packages/protocol/src/utils/index.ts
+++ packages/protocol/src/utils/index.ts
@@ -3,7 +3,6 @@ import StreamMessageValidator, { StreamMetadata } from "./StreamMessageValidator
 import SigningUtil from "./SigningUtil"
 import { createTrackerRegistry, TrackerRegistry, SmartContractRecord } from "./TrackerRegistry"
 import { generateMnemonicFromAddress, parseAddressFromNodeId } from './NodeUtil'
-import { keyToArrayIndex } from "./HashUtil"
 import { StreamID, toStreamID, StreamIDUtils } from "./StreamID"
 import { StreamPartID, toStreamPartID, StreamPartIDUtils, MAX_PARTITION_COUNT } from "./StreamPartID"
 import { KeyExchangeStreamIDUtils } from "./KeyExchangeStreamID"
@@ -19,7 +18,6 @@ export {
     createTrackerRegistry,
     generateMnemonicFromAddress,
     parseAddressFromNodeId,
-    keyToArrayIndex,
     toStreamID,
     toStreamPartID,
     StreamID,
