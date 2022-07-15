fbe379dd14ba5e6b8ae236222b02f561426913bc
Eric Andrews <harbu88@hotmail.com>
2022-07-15 13:30:16 +0300

    eslint: no-non-null-asserted-nullish-coalescing

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
         '@typescript-eslint/no-confusing-non-null-assertion': ['error'],
         '@typescript-eslint/no-duplicate-enum-values': ['error'],
         '@typescript-eslint/no-extraneous-class': ['error'],
-        '@typescript-eslint/no-invalid-void-type': ['error']
+        '@typescript-eslint/no-invalid-void-type': ['error'],
+        '@typescript-eslint/no-non-null-asserted-nullish-coalescing': ['error']
         //'@typescript-eslint/no-empty-function': 'error',
     }
 }
