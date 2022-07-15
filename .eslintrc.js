11cf358e0a94988ac03086981890da3f73e5417c
Eric Andrews <harbu88@hotmail.com>
2022-07-15 18:03:38 +0300

    eslint: no-invalid-this

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,8 @@ module.exports = {
         }],
         '@typescript-eslint/default-param-last': ['error'],
         '@typescript-eslint/func-call-spacing': ['error'],
-        '@typescript-eslint/keyword-spacing': ['error']
+        '@typescript-eslint/keyword-spacing': ['error'],
+        '@typescript-eslint/no-invalid-this': ['error']
         //'@typescript-eslint/no-empty-function': 'error',
     }
 }
