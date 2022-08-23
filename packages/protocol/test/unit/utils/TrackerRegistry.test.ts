f4b348e97c39e5a06612ee780f6fe52d27dd987e
Teo Gebhard <teo@streamr.com>
2022-08-23 15:57:39 +0300

    test: mv part of TrackerRegistry tests logic, rm the end-to-end test

--- packages/protocol/test/unit/utils/TrackerRegistry.test.ts
+++ packages/protocol/test/unit/utils/TrackerRegistry.test.ts
@@ -1,3 +1,4 @@
+import { StreamPartIDUtils } from '../../../src/utils/StreamPartID'
 import { createTrackerRegistry } from '../../../src/utils/TrackerRegistry'
 
 describe('TrackerRegistry', () => {
@@ -25,6 +26,17 @@ describe('TrackerRegistry', () => {
                     ws: 'ws://10.200.10.1:30302'
                 }
             ])
+
+            expect(trackerRegistry.getTracker(StreamPartIDUtils.parse('stream-1#11'))).toEqual({
+                id: '',
+                http: 'http://10.200.10.1:30301',
+                ws: 'ws://10.200.10.1:30301'
+            })
+            expect(trackerRegistry.getTracker(StreamPartIDUtils.parse('stream-2#10'))).toEqual({
+                id: '',
+                http: 'http://10.200.10.1:30302',
+                ws: 'ws://10.200.10.1:30302'
+            })
         })
     })
 })
