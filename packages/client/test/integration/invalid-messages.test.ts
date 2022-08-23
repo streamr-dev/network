a46bff4fbe82b7931124b04039b1e569d1ed1fbf
Teo Gebhard <teo@streamr.com>
2022-08-23 14:49:18 +0300

    test: convert client-behaviour-on-invalid-messages.test.ts

--- packages/client/test/integration/invalid-messages.test.ts
+++ packages/client/test/integration/invalid-messages.test.ts
@@ -1,31 +1,23 @@
 import { StreamrClient } from '../../src/StreamrClient'
 import { StreamPermission } from '../../src/permission'
-import { ConfigTest } from '../../src/ConfigTest'
-import { GroupKey } from '../../src/encryption/GroupKey'
-import { EncryptionUtil } from '../../src/encryption/EncryptionUtil'
 import { sign } from '../../src/utils/signingUtils'
-import { createTestStream, getCreateClient } from '../test-utils/utils'
-import { fastPrivateKey, fastWallet, fetchPrivateKeyWithGas } from 'streamr-test-utils'
+import { createTestStream } from '../test-utils/utils'
+import { fastWallet } from 'streamr-test-utils'
 import { wait } from '@streamr/utils'
 import { MessageID, StreamID, StreamMessage } from 'streamr-client-protocol'
-import { createNetworkNode, NetworkNode } from 'streamr-network'
+import { FakeEnvironment } from '../test-utils/fake/FakeEnvironment'
 
-const createClient = getCreateClient()
-const TIMEOUT = 20 * 1000
 const PROPAGATION_WAIT_TIME = 2000
 
 describe('client behaviour on invalid message', () => {
     let streamId: StreamID
     let subscriberClient: StreamrClient
     let publisherClient: StreamrClient
-    let networkNode: NetworkNode | undefined
+    let environment: FakeEnvironment
 
     beforeAll(async () => {
-        const creatorClient = await createClient({
-            auth: {
-                privateKey: await fetchPrivateKeyWithGas()
-            }
-        })
+        environment = new FakeEnvironment()
+        const creatorClient = environment.createClient()
         try {
             const stream = await createTestStream(creatorClient, module)
             streamId = stream.id
@@ -36,26 +28,17 @@ describe('client behaviour on invalid message', () => {
         } finally {
             await creatorClient.destroy()
         }
-    }, TIMEOUT)
+    })
 
     beforeEach(async () => {
-        subscriberClient = await createClient({
-            auth: {
-                privateKey: fastPrivateKey()
-            }
-        })
-        publisherClient = await createClient({
-            auth: {
-                privateKey: fastPrivateKey()
-            }
-        })
-    }, TIMEOUT)
+        subscriberClient = environment.createClient()
+        publisherClient = environment.createClient()
+    })
 
     afterEach(async () => {
         await Promise.allSettled([
             publisherClient?.destroy(),
-            subscriberClient?.destroy(),
-            networkNode?.stop()
+            subscriberClient?.destroy()
         ])
     })
 
@@ -70,27 +53,24 @@ describe('client behaviour on invalid message', () => {
         await wait(PROPAGATION_WAIT_TIME)
         expect(onMessage).not.toHaveBeenCalled()
         expect(onError).not.toHaveBeenCalled()
-    }, TIMEOUT)
+    })
 
     it('invalid messages received by subscriber do not cause unhandled promise rejection (NET-774)', async () => {
+        // the stream doesn't have publish permission for publisherWallet.address
+        // - network node can publish it, but the subscribe sees that it is an invalid message
         await subscriberClient.subscribe(streamId, () => {
             throw new Error('should not get here')
         })
-        networkNode = await createNetworkNode({
-            // TODO better typing for ConfigTest.network.trackers?
-            ...ConfigTest.network as any,
-            id: 'networkNode',
-        })
         const publisherWallet = fastWallet()
+        const networkNode = environment.startNode(publisherWallet.address)
         const msg = new StreamMessage({
             messageId: new MessageID(streamId, 0, Date.now(), 0, publisherWallet.address, ''),
             prevMsgRef: null,
             content: { not: 'allowed' }
         })
-        EncryptionUtil.encryptStreamMessage(msg, GroupKey.generate())
-        msg.signature = await sign(msg.getPayloadToSign(StreamMessage.SIGNATURE_TYPES.ETH), publisherWallet.privateKey.substring(2))
+        msg.signature = sign(msg.getPayloadToSign(StreamMessage.SIGNATURE_TYPES.ETH), publisherWallet.privateKey.substring(2))
         networkNode.publish(msg)
         await wait(PROPAGATION_WAIT_TIME)
         expect(true).toEqual(true) // we never get here if subscriberClient crashes
-    }, TIMEOUT)
+    })
 })
