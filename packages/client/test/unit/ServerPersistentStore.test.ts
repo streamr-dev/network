1c289f70fb1a58411275d8a00ecfa0570fb813f2
Teo Gebhard <teo@streamr.com>
2022-08-26 15:19:45 +0300

    test: add unit test

---
+++ packages/client/test/unit/ServerPersistentStore.test.ts
@@ -0,0 +1,28 @@
+import { toStreamID } from 'streamr-client-protocol'
+import { fastWallet } from 'streamr-test-utils'
+import ServerPersistentStore from '../../src/encryption/ServerPersistentStore'
+import { mockContext } from '../test-utils/utils'
+
+describe('ServerPersistentStore', () => {
+
+    let store: ServerPersistentStore
+
+    beforeEach(async () => {
+        const clientId = fastWallet().address
+        const streamId = toStreamID('0x0000000000000000000000000000000000000001/path')
+        store = new ServerPersistentStore({
+            context: mockContext(),
+            clientId,
+            streamId,
+        })
+    })
+
+    it('happy path', async () => {
+        await store.set('foo', 'bar')
+        expect(await store.get('foo')).toBe('bar')
+    })
+
+    it('no value', async () => {
+        expect(await store.get('non-existing')).toBeUndefined()
+    })
+})
