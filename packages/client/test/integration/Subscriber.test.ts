0ebc9800a3e04bcc0c25dcbd2fe49118440badcf
Teo Gebhard <teo@streamr.com>
2022-08-30 15:09:35 +0300

    test: extract Decrypt unit test from Subscriber integration test

--- packages/client/test/integration/Subscriber.test.ts
+++ packages/client/test/integration/Subscriber.test.ts
@@ -7,7 +7,7 @@ import StreamrClient, { StreamPermission } from '../../src'
 import { createMockMessage } from '../test-utils/utils'
 import { GroupKey } from '../../src/encryption/GroupKey'
 import { nextValue } from '../../src/utils/iterators'
-import { fastWallet, waitForCondition } from 'streamr-test-utils'
+import { fastWallet } from 'streamr-test-utils'
 
 const MOCK_CONTENT = { foo: 'bar' }
 
@@ -70,37 +70,5 @@ describe('Subscriber', () => {
 
         const receivedMessage = await nextValue(sub)
         expect(receivedMessage!.getParsedContent()).toEqual(MOCK_CONTENT)
-    })
-
-    describe.each([
-        [true, /Could not get GroupKey.*GroupKeyErrorResponse/],
-        [false, /Could not get GroupKey.*no permission/]
-    ])('group key not available', (isError: boolean, expectedErrorMessage: RegExp) => {
-        it(`error: ${isError}`, async () => {
-            await stream.grantPermissions({
-                permissions: [StreamPermission.PUBLISH],
-                user: publisherWallet.address
-            })
-            const publisherNode = await startPublisherNode(
-                publisherWallet,
-                [],
-                environment,
-                async () => isError ? 'mock-error-code' : undefined
-            )
-
-            const sub = await subscriber.subscribe(stream.id)
-            const onError = jest.fn()
-            sub.on('error', onError)
-
-            publisherNode.publish(createMockMessage({
-                stream,
-                publisher: publisherWallet,
-                content: MOCK_CONTENT,
-                encryptionKey: GroupKey.generate()
-            }))
-
-            await waitForCondition(() => onError.mock.calls.length > 0)
-            expect(onError.mock.calls[0][0].message).toMatch(expectedErrorMessage)
-        })
     })
 })
