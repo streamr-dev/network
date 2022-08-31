8e1f8e9fcfee7442ca14054224f7239a91655dd7
juslesan <santeri.juslenius@gmail.com>
2022-08-31 16:41:54 +0300

    ci: rework ts-protobuf utilization in monorepo

--- packages/proto-rpc/proto.sh
+++ packages/proto-rpc/proto.sh
@@ -2,3 +2,5 @@ mkdir -p ./src/proto
 npx protoc --ts_out ./src/proto --ts_opt server_generic,generate_dependencies --proto_path protos protos/ProtoRpc.proto
 mkdir -p ./test/proto
 npx protoc --ts_out ./test/proto --ts_opt server_generic,generate_dependencies --proto_path test/protos test/protos/TestProtos.proto
+npx protoc --ts_out ./test/proto --ts_opt server_generic,generate_dependencies --proto_path test/protos test/protos/WakeUpRpc.proto
+npx protoc --ts_out ./test/proto --ts_opt server_generic,generate_dependencies --proto_path test/protos test/protos/HelloRpc.proto
