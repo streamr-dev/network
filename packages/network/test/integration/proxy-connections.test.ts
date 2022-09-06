82e7d6ce8de721a6cbf0dc4bf4335607a3d209a8
juslesan <santeri.juslenius@gmail.com>
2022-09-06 16:06:09 +0300

    test: remove redundant test case

--- packages/network/test/integration/proxy-connections.test.ts
+++ packages/network/test/integration/proxy-connections.test.ts
@@ -307,38 +307,4 @@ describe('Proxy connection tests', () => {
         ])
     }, 20100)
 
-    it('will receive messages after lost connectivity', async () => {
-        let receivedMessages = 0
-        contactNode.on(NodeEvent.MESSAGE_RECEIVED, (_message) => {
-            receivedMessages += 1
-        })
-        await Promise.all([
-            waitForEvent(contactNode, NodeEvent.NODE_UNSUBSCRIBED),
-            contactNode2.unsubscribe(defaultStreamPartId)
-        ])
-        await onewayNode.openProxyConnection(defaultStreamPartId, 'contact-node', ProxyDirection.PUBLISH)
-        
-        await Promise.all([
-            waitForEvent(contactNode, NodeEvent.NODE_CONNECTED, 20000),
-            // @ts-expect-error private
-            contactNode.nodeToNode.disconnectFromNode('publisher', 'testing')
-        ])
-        await Promise.all([
-            waitForEvent(contactNode, NodeEvent.MESSAGE_RECEIVED),
-            onewayNode.publish(new StreamMessage({
-                messageId: new MessageID(toStreamID('stream-0'), 0, 120, 0, 'publisher', 'session'),
-                content: {
-                    hello: 'world 1'
-                },
-            })),
-            onewayNode.publish(new StreamMessage({
-                messageId: new MessageID(toStreamID('stream-0'), 0, 120, 1, 'publisher', 'session'),
-                content: {
-                    hello: 'world 2'
-                },
-            }))
-        ])
-        await waitForCondition(() => receivedMessages === 2, 10000)
-    })
-
 })
