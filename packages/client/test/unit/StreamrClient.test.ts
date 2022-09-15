e93a94fe5c861273bdf243105d1f53898459b9b4
Teo Gebhard <teo@streamr.com>
2022-09-15 17:48:40 +0300

    test: client.id

--- packages/client/test/unit/StreamrClient.test.ts
+++ packages/client/test/unit/StreamrClient.test.ts
@@ -3,21 +3,41 @@ import { container } from 'tsyringe'
 import { merge } from 'lodash'
 import { StreamrClient } from '../../src/StreamrClient'
 import { GroupKey } from '../../src/encryption/GroupKey'
-import { ConfigTest } from '../../src'
+import { ConfigTest, StreamrClientConfig } from '../../src'
+
+const createClient = (opts: StreamrClientConfig = {}) => {
+    return new StreamrClient(merge(
+        {},
+        ConfigTest,
+        {
+            network: {
+                trackers: [] // without this setting NetworkNodeFacade would query the tracker addresses from the contract
+            }
+        },
+        opts
+    ), container)
+}
 
 describe('StreamrClient', () => {
 
+    describe('client id', () => {
+
+        it('default', () => {
+            const client = createClient()
+            expect(client.id).toMatch(/StreamrClient:[-a-z0-9]+/)
+        })
+        
+        it('user defined', () => {
+            const client = createClient({
+                id: 'foobar'
+            })
+            expect(client.id).toBe('foobar')    
+        })
+    })
+
     describe('public API', () => {
 
-        const client = new StreamrClient(merge(
-            {},
-            ConfigTest,
-            {
-                network: {
-                    trackers: [] // without this setting NetworkNodeFacade would query the tracker addresses from the contract
-                }
-            }
-        ), container)
+        const client = createClient()
 
         it('updateEncryptionKey', async () => {
             await expect(() => {
