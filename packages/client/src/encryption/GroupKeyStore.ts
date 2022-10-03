c97e1c6693e303196104eb0919cd517ff05dca3e
Teo Gebhard <teo@streamr.com>
2022-10-03 16:08:54 +0300

    fix(client): Concurrency issue in group key storage (#855)
    Fixed a concurrency issue when group keys are added in parallel for multiple streams. 
    
    As multiple streams initialized a separate `ServerPersistence` instance, each instance called `ServerPersistence#init()` in parallel. Therefore some of the calls could fail with `SQLITE_ERROR: no such table: GroupKeys` error.
    
    ### Refactoring
    
    To fix this issue `GroupKeyStore` uses one `Persistence` instance instead of multiple instances. We give an explicit streamId parameter for `Persistence#get` and `Persistence#set `calls.
    
    There was no real need to have multiple instances as `ServerPersistence` already filtered its data with a `streamId` filter. `BrowserPersistence` implementation used stream-specific stores, and now it hides the store management internally.

--- packages/client/src/encryption/GroupKeyStore.ts
+++ packages/client/src/encryption/GroupKeyStore.ts
@@ -1,15 +1,14 @@
 import { scoped, Lifecycle, inject } from 'tsyringe'
 import { join } from 'path'
 import { instanceId } from '../utils/utils'
-import { CacheAsyncFn } from '../utils/caches'
 import { Context } from '../utils/Context'
-import { ConfigInjectionToken, CacheConfig } from '../Config'
 import { GroupKey, GroupKeyId } from './GroupKey'
 import { StreamID } from 'streamr-client-protocol'
 import { Authentication, AuthenticationInjectionToken } from '../Authentication'
 import { StreamrClientEventEmitter } from '../events'
 import { Persistence } from '../utils/persistence/Persistence'
 import ServerPersistence from '../utils/persistence/ServerPersistence'
+import { pOnce } from '../utils/promises'
 
 // In the client API we use the term EncryptionKey instead of GroupKey.
 // The GroupKey name comes from the protocol. TODO: we could rename all classes
@@ -25,62 +24,43 @@ export interface UpdateEncryptionKeyOptions {
 export class GroupKeyStore implements Context {
     readonly id
     readonly debug
-    private cleanupFns: (() => void)[] = []
-    public getPersistence: ((streamId: StreamID) => Promise<Persistence<GroupKeyId, string>>)
+    private persistence: Persistence<GroupKeyId, string> | undefined
+    private ensureInitialized: () => Promise<void>
 
     constructor(
         context: Context,
         @inject(AuthenticationInjectionToken) private authentication: Authentication,
-        @inject(ConfigInjectionToken.Cache) cacheConfig: CacheConfig,
         @inject(StreamrClientEventEmitter) private eventEmitter: StreamrClientEventEmitter
     ) {
         this.id = instanceId(this)
         this.debug = context.debug.extend(this.id)
-        this.getPersistence = CacheAsyncFn(this.createPersistence.bind(this), {
-            ...cacheConfig,
-            cacheKey([streamId]): StreamID {
-                return streamId
-            }
+        this.ensureInitialized = pOnce(async () => {
+            const clientId = await this.authentication.getAddress()
+            this.persistence = new ServerPersistence({
+                context: this,
+                tableName: 'GroupKeys',
+                valueColumnName: 'groupKey',
+                clientId,
+                migrationsPath: join(__dirname, 'migrations')
+            })
         })
-    }
-
-    private async createPersistence(streamId: StreamID): Promise<Persistence<GroupKeyId, string>> {
-        const clientId = await this.authentication.getAddress()
-        const persistence = new ServerPersistence({
-            context: this,
-            tableName: 'GroupKeys',
-            valueColumnName: 'groupKey',
-            clientId,
-            streamId,
-            migrationsPath: join(__dirname, 'migrations')
-        })
-        this.cleanupFns.push(async () => {
-            try {
-                await persistence.close()
-            } catch (_err) {
-                // whatever
-            }
-        })
-        return persistence
     }
 
     async get(keyId: GroupKeyId, streamId: StreamID): Promise<GroupKey | undefined> {
-        const persistence = await this.getPersistence(streamId)
-        const value = await persistence.get(keyId)
+        await this.ensureInitialized()
+        const value = await this.persistence!.get(keyId, streamId)
         if (value === undefined) { return undefined }
         return new GroupKey(keyId, value)
     }
 
     async add(key: GroupKey, streamId: StreamID): Promise<void> {
-        const persistence = await this.getPersistence(streamId)
+        await this.ensureInitialized()
         this.debug('Add key %s', key.id)
-        await persistence.set(key.id, key.hex)
+        await this.persistence!.set(key.id, key.hex, streamId)
         this.eventEmitter.emit('addGroupKey', key)
     }
 
     async stop(): Promise<void> {
-        const { cleanupFns } = this
-        this.cleanupFns = []
-        await Promise.allSettled(cleanupFns)
+        await this.persistence?.close()
     }
 }
