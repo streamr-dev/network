adc52e0d1a1bd8506fe0eff8b5f7721dc28fdb24
Teo Gebhard <teo@streamr.com>
2022-10-06 12:10:47 +0300

    refactor: Mapping utility

---
+++ packages/client/src/utils/Mapping.ts
@@ -0,0 +1,34 @@
+/*
+ * A map data structure which lazily evaluates values. A factory function
+ * is called to create a value when when an item is queried for the first time.
+ * The map stores the value and any subsequent call to get() returns
+ * the same value.
+ */
+
+// an wrapper object is used so that we can store undefined values
+interface ValueWrapper<V> {
+    value: V
+}
+
+const KEY_SEPARATOR = '|'
+
+export class Mapping<K extends (string | number)[], V> {
+
+    private delegate: Map<string, ValueWrapper<V>> = new Map()
+    private valueFactory: (...args: K) => Promise<V>
+
+    constructor(valueFactory: (...args: K) => Promise<V>) {
+        this.valueFactory = valueFactory
+    }
+
+    async get(...args: K): Promise<V> {
+        const key = args.join(KEY_SEPARATOR)
+        let valueWrapper = this.delegate.get(key)
+        if (valueWrapper === undefined) {
+            const value = await this.valueFactory(...args)
+            valueWrapper = { value }
+            this.delegate.set(key, valueWrapper)
+        }
+        return valueWrapper.value
+    }
+}
