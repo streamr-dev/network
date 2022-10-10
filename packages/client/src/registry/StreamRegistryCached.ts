de93cf6d91e7fc167fa296caf37099fab87d32e2
Teo Gebhard <teo@streamr.com>
2022-10-10 12:22:55 +0300

    refactor: hide cache functions of StreamRegistryCached

--- packages/client/src/registry/StreamRegistryCached.ts
+++ packages/client/src/registry/StreamRegistryCached.ts
@@ -10,6 +10,7 @@ import { Stream } from '../Stream'
 
 const SEPARATOR = '|' // always use SEPARATOR for cache key
 
+/* eslint-disable no-underscore-dangle */
 @scoped(Lifecycle.ContainerScoped)
 export class StreamRegistryCached implements Context {
     readonly id = instanceId(this)
@@ -23,11 +24,13 @@ export class StreamRegistryCached implements Context {
         this.debug = context.debug.extend(this.id)
     }
 
-    private async getStreamPreloaded(streamId: StreamID): Promise<Stream> {
-        return this.streamRegistry.getStream(streamId)
+    getStream(streamId: StreamID): Promise<Stream> {
+        return this._getStream(streamId)
     }
 
-    getStream = CacheAsyncFn(this.getStreamPreloaded.bind(this), {
+    private _getStream = CacheAsyncFn((streamId: StreamID) => {
+        return this.streamRegistry.getStream(streamId)
+    }, {
         ...this.cacheOptions,
         cacheKey: ([streamId]: any) => {
             // see clearStream
@@ -35,37 +38,43 @@ export class StreamRegistryCached implements Context {
         }
     })
 
-    private async isStreamPublisherPreloaded(streamId: StreamID, ethAddress: EthereumAddress): Promise<boolean> {
+    isStreamPublisher(streamId: StreamID, ethAddress: EthereumAddress): Promise<boolean> {
+        return this._isStreamPublisher(streamId, ethAddress)
+    }
+
+    private _isStreamPublisher = CacheAsyncFn((streamId: StreamID, ethAddress: EthereumAddress) => {
         return this.streamRegistry.isStreamPublisher(streamId, ethAddress)
-    }
-
-    isStreamPublisher = CacheAsyncFn(this.isStreamPublisherPreloaded.bind(this), {
+    }, {
         ...this.cacheOptions,
         cacheKey([streamId, ethAddress]: any): string {
             return [streamId, ethAddress.toLowerCase()].join(SEPARATOR)
         }
     })
 
-    private async isStreamSubscriberPreloaded(streamId: StreamID, ethAddress: EthereumAddress): Promise<boolean> {
+    isStreamSubscriber(streamId: StreamID, ethAddress: EthereumAddress): Promise<boolean> {
+        return this._isStreamSubscriber(streamId, ethAddress)
+    }
+
+    private _isStreamSubscriber = CacheAsyncFn((streamId: StreamID, ethAddress: EthereumAddress) => {
         return this.streamRegistry.isStreamSubscriber(streamId, ethAddress)
-    }
-
-    isStreamSubscriber = CacheAsyncFn(this.isStreamSubscriberPreloaded.bind(this), {
+    }, {
         ...this.cacheOptions,
         cacheKey([streamId, ethAddress]: any): string {
             return [streamId, ethAddress.toLowerCase()].join(SEPARATOR)
         }
     })
 
-    private async isPublicSubscriptionStream(streamId: StreamID): Promise<boolean> {
+    async isPublic(streamId: StreamID): Promise<boolean> {
+        return this._isPublic(streamId)
+    }
+
+    private _isPublic = CacheAsyncFn((streamId: StreamID) => {
         return this.streamRegistry.hasPermission({
             streamId,
             public: true,
             permission: StreamPermission.SUBSCRIBE
         })
-    }
-
-    isPublic = CacheAsyncFn(this.isPublicSubscriptionStream.bind(this), {
+    }, {
         ...this.cacheOptions,
         cacheKey([streamId]): any {
             return ['PublicSubscribe', streamId].join(SEPARATOR)
@@ -80,8 +89,8 @@ export class StreamRegistryCached implements Context {
         // include separator so startsWith(streamid) doesn't match streamid-something
         const target = `${streamId}${SEPARATOR}`
         const matchTarget = (s: string) => s.startsWith(target)
-        this.getStream.clearMatching(matchTarget)
-        this.isStreamPublisher.clearMatching(matchTarget)
-        this.isStreamSubscriber.clearMatching(matchTarget)
+        this._getStream.clearMatching(matchTarget)
+        this._isStreamPublisher.clearMatching(matchTarget)
+        this._isStreamSubscriber.clearMatching(matchTarget)
     }
 }
