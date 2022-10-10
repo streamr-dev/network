afd8a3475bd413a768587815496855bbab8d24db
Eric Andrews <harbu88@hotmail.com>
2022-10-10 12:05:09 +0300

    refactor: simplify generateMnemonicFromAddress

--- packages/broker/src/helpers/generateMnemonicFromAddress.ts
+++ packages/broker/src/helpers/generateMnemonicFromAddress.ts
@@ -5,8 +5,7 @@ import { EthereumAddress } from '@streamr/utils'
  * @param address - valid eth address with leading 0x
  */
 export const generateMnemonicFromAddress = (address: EthereumAddress): string => {
-    const prefixedAddress = typeof address === 'string' && !address.startsWith('0x') ? `0x${address}` : address
-    return entropyToMnemonic(prefixedAddress)
+    return entropyToMnemonic(address)
         .split(' ')
         .slice(0, 3)
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
