ade4aacd6f32eb13615b0b580575e90b42396713
Teo Gebhard <teo@streamr.com>
2022-10-13 10:40:35 +0300

    feat: export some helper interfaces/types used by client

--- packages/client/src/index-exports.ts
+++ packages/client/src/index-exports.ts
@@ -58,7 +58,9 @@ export { NotFoundError, ErrorCode } from './HttpUtil'
 export * from './types'
 export { formStorageNodeAssignmentStreamId } from './utils/utils'
 
-export { StreamPartID } from 'streamr-client-protocol'
+export { ProxyDirection, StreamID, StreamPartID } from 'streamr-client-protocol'
+export { BrandedString, EthereumAddress } from '@streamr/utils'
+export { NetworkNodeOptions, Location } from 'streamr-network'
 
 export { BigNumber } from '@ethersproject/bignumber'
 export type { ConnectionInfo } from '@ethersproject/web'
