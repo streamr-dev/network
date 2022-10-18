07c09255755ad3aca2d23b7f7cd08c82f56492f8
Teo Gebhard <teo@streamr.com>
2022-10-18 15:26:03 +0300

    feat: draft

---
+++ packages/client/test/unit/MetricsPublisher.test.ts
@@ -0,0 +1,13 @@
+import { MetricsPublisher } from '../../src/MetricsPublisher'
+
+describe('MetricsPublisher', () => {
+    it('happy path', () => {
+        const metricsPublisher = new MetricsPublisher(
+            undefined as any,
+            undefined as any,
+            eventEmitter ,
+            undefined as any,
+            undefined as any
+        })
+    })
+})
