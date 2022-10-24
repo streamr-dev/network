74b4d9c8207ea081a80e9ba8c8518b05ad147e93
Teo Gebhard <teo@streamr.com>
2022-10-24 11:18:19 +0300

    refactor: require code

--- packages/client/test/test-utils/customMatchers.ts
+++ packages/client/test/test-utils/customMatchers.ts
@@ -2,7 +2,7 @@ import { expect } from '@jest/globals'
 import type { MatcherState } from 'expect'
 import { printExpected, printReceived } from 'jest-matcher-utils'
 import { isFunction } from 'lodash'
-import { StreamrClientError } from './../../src/StreamrClientError'
+import { StreamrClientError, StreamrClientErrorCode } from './../../src/StreamrClientError'
 
 // we could ES2015 module syntax (https://jestjs.io/docs/expect#expectextendmatchers),
 // but the IDE doesn't find custom matchers if we do that
@@ -10,7 +10,7 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace jest {
         interface Matchers<R> {
-            toThrowStreamError(expectedError: Partial<StreamrClientError>): R
+            toThrowStreamError(expectedError: { code: StreamrClientErrorCode, message?: string }): R
         }
     }
 }
@@ -25,7 +25,7 @@ const formError = (description: string, expected: string, actual: string) => {
 const toThrowStreamError = function(
     this: MatcherState,
     actual: unknown, // should be (() => StreamrClientError) | StreamrClientError
-    expectedError: Partial<StreamrClientError>
+    expectedError: { code: StreamrClientErrorCode, message?: string }
 ) {
     let actualError
     if (isFunction(actual)) {
@@ -44,7 +44,7 @@ const toThrowStreamError = function(
     if (!(actualError instanceof StreamrClientError)) {
         return formError('Class name', 'StreamrClientError', (actualError as any).constructor.name)
     }
-    if ((expectedError.code !== undefined) && (actualError.code !== expectedError.code)) {
+    if (actualError.code !== expectedError.code) {
         return formError('StreamrClientError.code', expectedError.code, actualError.code)
     }
     if ((expectedError.message !== undefined) && (actualError.message !== expectedError.message)) {
