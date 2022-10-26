074d31611c94bd69dfe4b5fecd072b80374cc69c
Eric Andrews <eric.andrews@streamr.com>
2022-10-26 13:01:44 +0300

    chore: upgrade to Node.js 18.11.0  (#874)
    Upgrade Node.js to major version 18 which will be the new LTS starting [2022-10-25](https://github.com/nodejs/release#release-schedule).
    
    ## Changes
    - Update `.nvmrc` to be `18.11.0`
    - Update docker images to build against `18.11.0`
    - Apparently behavior of `finished` in `stream/promises` has changed to reject in presence of error. Fixed test in [8267f2c](https://github.com/streamr-dev/network-monorepo/pull/874/commits/8267f2c0cb7bc9846bab5652321787cc52d9f1ed) to accomodate for this.
    - Use `'127.0.0.1'` instead of `'localhost'` in **`dht`** package to avoid binding to IPv6 interface instead of IPv4. Fixed in [f16e721](https://github.com/streamr-dev/network-monorepo/pull/874/commits/f16e721e10ef6afbf24aed2162ea65d3f9ae246d).
    - Update `@types/node`.

--- packages/dht/test/end-to-end/WebSocketConnectionRequest.test.ts
+++ packages/dht/test/end-to-end/WebSocketConnectionRequest.test.ts
@@ -6,7 +6,7 @@ describe('WebSocket IConnection Requests', () => {
     const epPeerDescriptor: PeerDescriptor = {
         peerId: Uint8Array.from([1, 2, 3]),
         type: NodeType.NODEJS,
-        websocket: { ip: 'localhost', port: 10021 }
+        websocket: { ip: '127.0.0.1', port: 10021 }
     }
     let epDhtNode: DhtNode
     let node1: DhtNode
