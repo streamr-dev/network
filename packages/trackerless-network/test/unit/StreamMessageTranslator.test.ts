cd70ed674c062f5cedb939f58a83a742fb83b249
juslesan <santeri.juslenius@gmail.com>
2022-10-27 17:16:54 +0300

    fix: npm run check

--- packages/trackerless-network/test/unit/StreamMessageTranslator.test.ts
+++ packages/trackerless-network/test/unit/StreamMessageTranslator.test.ts
@@ -37,11 +37,11 @@ describe('StreamMessageTranslator', () => {
 
     it('translates old protocol to protobuf', () => {
         const translated = StreamMessageTranslator.toProtobuf(oldProtocolMsg)
-        expect(translated.messageRef.timestamp).toBeGreaterThanOrEqual(0)
-        expect(translated.messageRef.sequenceNumber).toEqual(0)
-        expect(translated.messageRef.streamId).toEqual('TEST')
-        expect(translated.messageRef.streamPartition).toEqual(0)
-        expect(translated.messageRef.publisherId).toEqual('publisher')
+        expect(translated.messageRef!.timestamp).toBeGreaterThanOrEqual(0)
+        expect(translated.messageRef!.sequenceNumber).toEqual(0)
+        expect(translated.messageRef!.streamId).toEqual('TEST')
+        expect(translated.messageRef!.streamPartition).toEqual(0)
+        expect(translated.messageRef!.publisherId).toEqual('publisher')
         expect(translated.previousMessageRef).toEqual(undefined)
         expect(translated.messageType).toEqual(StreamMessageType.MESSAGE)
         expect(translated.groupKeyId).toEqual(undefined)
