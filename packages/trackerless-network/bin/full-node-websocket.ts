577dab50d38c6b2f9a5043d1a3d09a906fabcf62
juslesan <santeri.juslenius@gmail.com>
2022-10-28 12:25:35 +0300

    fix: update bin to latest

--- packages/trackerless-network/bin/full-node-websocket.ts
+++ packages/trackerless-network/bin/full-node-websocket.ts
@@ -1,14 +1,20 @@
-import { ConnectionManager, DhtNode, PeerDescriptor, NodeType, PeerID } from '@streamr/dht'
+import { ConnectionManager, DhtNode, NodeType, PeerDescriptor, PeerID } from '@streamr/dht'
 import { Event as StreamrNodeEvent, StreamrNode } from '../src/logic/StreamrNode'
-import { DataMessage, MessageRef } from '../src/proto/packages/trackerless-network/protos/NetworkRpc'
+import {
+    ContentMessage,
+    MessageRef,
+    StreamMessage,
+    StreamMessageType
+} from '../src/proto/packages/trackerless-network/protos/NetworkRpc'
 import { program } from 'commander'
 
 program
     .option('--id <id>', 'Ethereum address / node id', 'full-node')
     .option('--name <name>', 'Name in published messages', 'full-node')
-    .option('--wsPort <wsPort>', 'port for ws server', '23124')
+    .option('--ip <ip>', 'Ip address to use', '0.0.0.0')
+    .option('--wsPort <port>', 'Name in published messages', '23124')
     .option('--entrypointId <entrypointId>', 'Entrypoints stringId', 'bootstrap')
-    .option('--entrypointIp <entrypointIp>', 'Entrypoints IP address', 'localhost')
+    .option('--entrypointIp <entrypointIp>', 'Entrypoints IP address', '0.0.0.0')
     .option('--streamIds <streamIds>', 'streamId to publish',  (value: string) => value.split(','), ['stream-0'])
     .description('Run full node')
     .parse(process.argv)
@@ -17,6 +23,7 @@ async function run(): Promise<void> {
 
     const streamPartId = 'stream#0'
     const port = parseInt(program.opts().wsPort, 10)
+    const ip = program.opts().ip
 
     const epPeerDescriptor: PeerDescriptor = {
         peerId: PeerID.fromString(program.opts().entrypointId).value,
@@ -24,7 +31,7 @@ async function run(): Promise<void> {
         websocket: { ip: program.opts().entrypointIp, port: 23123 }
     }
 
-    const layer0 = new DhtNode({ webSocketPort: port, webSocketHost: '0.0.0.0', peerIdString: program.opts().id })
+    const layer0 = new DhtNode({ webSocketPort: port, webSocketHost: ip, peerIdString: program.opts().id })
     await layer0.start()
 
     await layer0.joinDht(epPeerDescriptor)
@@ -35,22 +42,30 @@ async function run(): Promise<void> {
 
     streamrNode.subscribeToStream(streamPartId, epPeerDescriptor)
 
-    streamrNode.on(StreamrNodeEvent.NEW_MESSAGE, (msg: DataMessage, _nodeId: string) => {
+    streamrNode.on(StreamrNodeEvent.NEW_MESSAGE, (msg: StreamMessage) => {
         // eslint-disable-next-line no-console
-        console.log(`new message received: ${JSON.parse(msg.content).hello}`)
+        console.log(`new message received: ${JSON.parse(ContentMessage.fromBinary(msg.content).body).hello}`)
     })
 
     let sequenceNumber = 0
     setInterval(() => {
         const messageRef: MessageRef = {
             sequenceNumber,
-            timestamp: BigInt(Date.now())
+            timestamp: BigInt(Date.now()),
+            publisherId: PeerID.fromValue(layer0.getPeerDescriptor().peerId).toString(),
+            streamPartition: 0,
+            streamId: streamPartId,
+            messageChainId: 'network'
         }
-        const message: DataMessage = {
-            content: JSON.stringify({ hello: `from ${program.opts().name }` }),
-            senderId: PeerID.fromValue(layer0.getPeerDescriptor().peerId).toString(),
+
+        const content: ContentMessage = {
+            body: JSON.stringify({ hello: `from ${program.opts().name }` })
+        }
+        const message: StreamMessage = {
+            content: ContentMessage.toBinary(content),
             messageRef,
-            streamPartId
+            messageType: StreamMessageType.MESSAGE,
+            signature: 'signature'
         }
         streamrNode.publishToStream(streamPartId, epPeerDescriptor, message)
         sequenceNumber++
