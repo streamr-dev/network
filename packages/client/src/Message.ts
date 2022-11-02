295844964c1c444c1223d84374c710b9574fa79d
Teo Gebhard <teo@streamr.com>
2022-11-02 13:09:22 +0200

    refactor: update Broker usages

--- packages/client/src/Message.ts
+++ packages/client/src/Message.ts
@@ -1,12 +1,15 @@
 import { EthereumAddress } from '@streamr/utils'
-import { StreamMessage } from 'streamr-client-protocol'
+import { StreamID, StreamMessage } from 'streamr-client-protocol'
 
 export interface Message {
     content: unknown
+    streamId: StreamID
+    streamPartition: number
     timestamp: number
     sequenceNumber: number
     signature: string
     publisherId: EthereumAddress
+    msgChainId: string
     /** @internal */
     streamMessage: StreamMessage // TODO remove this field if possible
 }
@@ -16,10 +19,13 @@ export type MessageMetadata = Omit<Message, 'content'>
 export const convertStreamMessageToMessage = (msg: StreamMessage<any>): Message => {
     return {
         content: msg.getParsedContent(),
+        streamId: msg.getStreamId(),
+        streamPartition: msg.getStreamPartition(),
         timestamp: msg.getTimestamp(),
         sequenceNumber: msg.getSequenceNumber(),
         signature: msg.signature,
         publisherId: msg.getPublisherId(),
+        msgChainId: msg.getMsgChainId(),
         streamMessage: msg
         // TODO add other relevant fields (could update some test assertions to
         // use those keys instead of getting the fields via from streamMessage property)
