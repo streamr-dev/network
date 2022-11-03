streamr-dev/network

commit 5ca2091bf5bebb819be64ac5109b5601fd44f096

refactor(client): Hide ResendSubscription from public API (#912)
Changed the public API of `StreamrClient#subscribe` to return a Subscription instead of `Subscription` | `ResendSubscription`. 

The `ResendSubscription` is a subclass of `Subscription`, and are no extra methods in that class. There is just an extra event type `resendComplete`. Of course non-resend subscription won't emit that message, but in the big picture our public API is more understandable if we don't have a separate class for resends.

Moved all event types to `Subscription` class. Now `error` event type is just one of the event types.

packages/client/src/StreamrClient.ts
@@ -121,18 +121,10 @@ export class StreamrClient {
     /**
      * @category Important
      */
-    subscribe<T>(
-        options: StreamDefinition & { resend: ResendOptions },
-        onMessage?: SubscriptionOnMessage<T>
-    ): Promise<ResendSubscription<T>>
-    subscribe<T>(
-        options: StreamDefinition,
-        onMessage?: SubscriptionOnMessage<T>
-    ): Promise<Subscription<T>>
     async subscribe<T>(
         options: StreamDefinition & { resend?: ResendOptions },
         onMessage?: SubscriptionOnMessage<T>
-    ): Promise<Subscription<T> | ResendSubscription<T>> {
+    ): Promise<Subscription<T>> {
         let result
         if (options.resend !== undefined) {
             result = await this.resendSubscribe(options, options.resend, onMessage)

packages/client/src/subscribe/ResendSubscription.ts
@@ -5,17 +5,11 @@ import { StreamMessage, StreamPartIDUtils } from 'streamr-client-protocol'
 import { ConfigInjectionToken } from '../Config'
 import { OrderMessages } from './OrderMessages'
 import { ResendOptions, Resends } from './Resends'
-import EventEmitter from 'eventemitter3'
 import { DestroySignal } from '../DestroySignal'
 import { LoggerFactory } from '../utils/LoggerFactory'
 
-export interface ResendSubscriptionEvents {
-    resendComplete: () => void
-}
-
 export class ResendSubscription<T> extends Subscription<T> {
     private orderMessages: OrderMessages<T>
-    private eventEmitter: EventEmitter<ResendSubscriptionEvents>
 
     /** @internal */
     constructor(
@@ -25,7 +19,6 @@ export class ResendSubscription<T> extends Subscription<T> {
         container: DependencyContainer
     ) {
         super(subSession, container.resolve(LoggerFactory))
-        this.eventEmitter = new EventEmitter<ResendSubscriptionEvents>()
         this.resendThenRealtime = this.resendThenRealtime.bind(this)
         this.orderMessages = new OrderMessages<T>(
             container.resolve(ConfigInjectionToken.Subscribe),
@@ -57,14 +50,6 @@ export class ResendSubscription<T> extends Subscription<T> {
         })
 
         return resentMsgs
-    }
-
-    once<E extends keyof ResendSubscriptionEvents>(eventName: E, listener: ResendSubscriptionEvents[E]): void {
-        this.eventEmitter.once(eventName, listener as any)
-    }
-
-    off<E extends keyof ResendSubscriptionEvents>(eventName: E, listener: ResendSubscriptionEvents[E]): void {
-        this.eventEmitter.off(eventName, listener as any)
     }
 
     /** @internal */

packages/client/src/subscribe/Subscription.ts
@@ -7,8 +7,14 @@ import { MessageStream, MessageStreamOnMessage } from './MessageStream'
 import { SubscriptionSession } from './SubscriptionSession'
 import { LoggerFactory } from '../utils/LoggerFactory'
 import { Logger } from '@streamr/utils'
+import EventEmitter from 'eventemitter3'
 
 export { MessageStreamOnMessage as SubscriptionOnMessage }
+
+export interface SubscriptionEvents {
+    error: (err: Error) => void
+    resendComplete: () => void
+}
 
 /**
  * @category Important
@@ -19,17 +25,20 @@ export class Subscription<T = unknown> extends MessageStream<T> {
     /** @internal */
     private readonly logger: Logger
     readonly streamPartId: StreamPartID
+    protected eventEmitter: EventEmitter<SubscriptionEvents>
 
     /** @internal */
     constructor(subSession: SubscriptionSession<T>, loggerFactory: LoggerFactory) {
         super()
         this.subSession = subSession
         this.streamPartId = subSession.streamPartId
+        this.eventEmitter = new EventEmitter<SubscriptionEvents>()
         this.logger = loggerFactory.createLogger(module)
         this.onMessage.listen((msg) => {
             this.logger.debug('onMessage %j', msg.serializedContent)
         })
         this.onError.listen((err) => {
+            this.eventEmitter.emit('error', err)
             this.logger.debug('onError %s', err)
         })
     }
@@ -44,7 +53,15 @@ export class Subscription<T = unknown> extends MessageStream<T> {
         return this.subSession.waitForNeighbours(numNeighbours, timeout)
     }
 
-    on(_eventName: 'error', cb: (err: Error) => void): void {
-        this.onError.listen(cb)
+    on<E extends keyof SubscriptionEvents>(eventName: E, listener: SubscriptionEvents[E]): void {
+        this.eventEmitter.on(eventName, listener as any)
+    }
+
+    once<E extends keyof SubscriptionEvents>(eventName: E, listener: SubscriptionEvents[E]): void {
+        this.eventEmitter.once(eventName, listener as any)
+    }
+
+    off<E extends keyof SubscriptionEvents>(eventName: E, listener: SubscriptionEvents[E]): void {
+        this.eventEmitter.off(eventName, listener as any)
     }
 }

packages/client/src/index-exports.ts
@@ -6,10 +6,9 @@ export * from './Stream'
 export { DecryptError } from './encryption/EncryptionUtil'
 export { StreamrClientEvents } from './events'
 export { MessageMetadata } from './publish/Publisher'
-export { Subscription, SubscriptionOnMessage } from './subscribe/Subscription'
+export { Subscription, SubscriptionEvents, SubscriptionOnMessage } from './subscribe/Subscription'
 export { MessageStreamOnMessage } from './subscribe/MessageStream'
 export type { MessageStream } from './subscribe/MessageStream'
-export { ResendSubscription, ResendSubscriptionEvents } from './subscribe/ResendSubscription'
 export { ResendOptions, ResendLastOptions, ResendFromOptions, ResendRangeOptions, ResendRef } from './subscribe/Resends'
 export {
     StreamPermission,

commit f35d2bd22ab3e5560a91d1f1b8567a2ea214b37d

fix(client): Clean up `Subscription` event listeners (#927)
## Summary

Event listeners assigned to `Subscription` were not cleaned up.

The regression was introduced in https://github.com/streamr-dev/network-monorepo/pull/912. In that PR `eventEmitter` was moved from `ResendSubscription` to `Subscription`, but cleanup logic was not moved. Now cleanup is implemented in `Subscription` super class and therefore we cleanup listeners for both `Subscription` and `ResendSubscription`. 

The cleanup is now part of `Subscription#unsubscribe` logic. Therefore it is done:
- when a subscription is unsubscribed manually
- or automatically when `StreamrClient#destroy` calls that method via `Subscriber`

It ok to do it there as unsubscribed subscriptions don't emit any events. 

## Other changes

- `StreamrClient#destroy` calls `Subscriber#unsubscribe` directly
- Removed unused `OrderMessages#loggerFactory` field (the constructor parameter was used, but the field was not)

## Future improvements

We shouldn't divide the unsubscribe logic to both `Subscription#unsubscribe` and `StreamrClient#unsubscribe`: maybe `StreamrClient#unsubscribe` could just call `Subscription#unsubscribe`.
- A unsubscribed subscription is removed from `Subscriber`, because it attached `sub.onBeforeFinally` listener to the subscription. Maybe `StreamrClient#unsubscribe` could call that directly. (Note that the logic is there also because we automatically unsubscribe streams which break out from the iterator loop. If we'd remove the listener, that feature would not work anymore.)

packages/client/src/StreamrClient.ts
@@ -133,7 +133,6 @@ export class StreamrClient {
                 streamPartId,
                 options.resend,
                 this.resends,
-                this.destroySignal,
                 this.loggerFactory,
                 this.config
             )
@@ -343,7 +342,7 @@ export class StreamrClient {
         this.connect.reset() // reset connect (will error on next call)
         const tasks = [
             this.destroySignal.destroy().then(() => undefined),
-            this.subscriber.stop(),
+            this.subscriber.unsubscribe(),
             this.groupKeyStore.stop()
         ]
 

packages/client/src/subscribe/OrderMessages.ts
@@ -34,7 +34,7 @@ export class OrderMessages<T> {
         private options: SubscribeConfig,
         private resends: Resends,
         private readonly streamPartId: StreamPartID,
-        private loggerFactory: LoggerFactory
+        loggerFactory: LoggerFactory
     ) {
         this.logger = loggerFactory.createLogger(module)
         this.stopSignal.listen(() => {

packages/client/src/subscribe/ResendSubscription.ts
@@ -4,7 +4,6 @@ import { StreamMessage, StreamPartID, StreamPartIDUtils } from 'streamr-client-p
 import { ConfigInjectionToken } from '../Config'
 import { OrderMessages } from './OrderMessages'
 import { ResendOptions, Resends } from './Resends'
-import { DestroySignal } from '../DestroySignal'
 import { LoggerFactory } from '../utils/LoggerFactory'
 import { SubscribeConfig } from './../Config'
 import { MessageStream } from './MessageStream'
@@ -17,7 +16,6 @@ export class ResendSubscription<T> extends Subscription<T> {
         streamPartId: StreamPartID,
         private resendOptions: ResendOptions,
         private resends: Resends,
-        destroySignal: DestroySignal,
         loggerFactory: LoggerFactory,
         @inject(ConfigInjectionToken.Subscribe) subscibreConfig: SubscribeConfig
     ) {
@@ -32,9 +30,6 @@ export class ResendSubscription<T> extends Subscription<T> {
         this.pipe(this.orderMessages.transform())
         this.onBeforeFinally.listen(async () => {
             this.orderMessages.stop()
-        })
-        destroySignal.onDestroy.listen(() => {
-            this.eventEmitter.removeAllListeners()
         })
     }
 

packages/client/src/subscribe/Subscriber.ts
@@ -193,8 +193,4 @@ export class Subscriber {
             ...subSession.subscriptions
         ]))
     }
-
-    async stop(): Promise<void> {
-        await this.removeAll()
-    }
 }

packages/client/src/subscribe/Subscription.ts
@@ -39,6 +39,7 @@ export class Subscription<T = unknown> extends MessageStream<T> {
     async unsubscribe(): Promise<void> {
         this.end()
         await this.return()
+        this.eventEmitter.removeAllListeners()
     }
 
     on<E extends keyof SubscriptionEvents>(eventName: E, listener: SubscriptionEvents[E]): void {

packages/client/test/integration/client-destroy.test.ts
@@ -17,6 +17,13 @@ describe('client destroy', () => {
         stream = await createTestStream(client, module)
     })
 
+    it('unsubscribes', async () => {
+        const sub = await client.subscribe(stream.id)
+        jest.spyOn(sub, 'unsubscribe')
+        await client.destroy()
+        expect(sub.unsubscribe).toBeCalled()
+    })
+
     it('ongoing subscribe pipeline ends', async () => {
         const sub = await client.subscribe(stream.id)
         const onError: any = jest.fn()