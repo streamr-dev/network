e8fc8abbc8c4279348b1f8a285732511d30b7577
Eric Andrews <harbu88@hotmail.com>
2022-11-07 13:36:52 +0200

    fix: add comment to test why cast is needed

--- packages/utils/test/composeAbortSignals.test.ts
+++ packages/utils/test/composeAbortSignals.test.ts
@@ -41,7 +41,7 @@ describe(composeAbortSignals, () => {
 
         it('onabort if set is invoked when a signal aborts', () => {
             const listener = jest.fn()
-            ;(composedSignal as any).onabort = listener
+            ;(composedSignal as any).onabort = listener // type cast needed since @types/node missing onabort
 
             expect(listener).toHaveBeenCalledTimes(0)
             controllers[6].abort()
@@ -51,7 +51,7 @@ describe(composeAbortSignals, () => {
 
         it('onabort is invoked max once', () => {
             const listener = jest.fn()
-            ;(composedSignal as any).onabort = listener
+            ;(composedSignal as any).onabort = listener // type cast needed since @types/node missing onabort
             controllers[3].abort()
             controllers[6].abort()
             expect(listener).toHaveBeenCalledTimes(1)
@@ -85,7 +85,7 @@ describe(composeAbortSignals, () => {
 
         it('another signal aborting does not cause onabort to be invoked', () => {
             const listener = jest.fn()
-            ;(composedSignal as any).onabort = listener
+            ;(composedSignal as any).onabort = listener  // type cast needed since @types/node missing onabort
 
             expect(listener).toHaveBeenCalledTimes(0)
             controllers[0].abort()
