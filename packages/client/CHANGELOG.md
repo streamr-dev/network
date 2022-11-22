6f07b019a2421c229d3c8fdbbeb1e53d36f259a1
Teo Gebhard <teo@streamr.com>
2022-11-22 16:16:25 +0200

    refactor: deprecate STREAM_CLIENT_DEFAULTS

--- packages/client/CHANGELOG.md
+++ packages/client/CHANGELOG.md
@@ -12,6 +12,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Deprecated
 
+- Deprecate `STREAM_CLIENT_DEFAULTS` constant
+
 ### Removed
 
 ### Fixed
--- packages/client/test/test-utils/utils.ts
+++ packages/client/test/test-utils/utils.ts
@@ -8,7 +8,7 @@ import { StreamrClient } from '../../src/StreamrClient'
 import { counterId } from '../../src/utils/utils'
 import { Stream, StreamMetadata } from '../../src/Stream'
 import { ConfigTest } from '../../src/ConfigTest'
-import { STREAM_CLIENT_DEFAULTS, StreamrClientConfig } from '../../src/Config'
+import { StreamrClientConfig } from '../../src/Config'
 import { GroupKey } from '../../src/encryption/GroupKey'
 import { addAfterFn } from './jest-utils'
 import { GroupKeyStore } from '../../src/encryption/GroupKeyStore'
@@ -24,7 +24,7 @@ const logger = new Logger(module)
 export function mockLoggerFactory(clientId?: string): LoggerFactory {
     return new LoggerFactory({
         id: clientId ?? counterId('TestCtx'),
-        logLevel: STREAM_CLIENT_DEFAULTS.logLevel
+        logLevel: 'info'
     })
 }
 
