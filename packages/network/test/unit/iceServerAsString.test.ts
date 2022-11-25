streamr-dev/network

commit 7a19d6687e0214716eb945b36bf619be22855ee4

test(network): `IceServer` interface and fix browser turn configuration  (#955)
## Summary

Take in STUN and TURN server configuration as objects instead of strings, and also fix issue in which TURN details were not passed correctly to `RTCPeerConnection` in browser environment.


Example:
```ts
// OLD FORMAT
stunUrls = ['turn://username@passwordturn.server.com:31313']
```

```ts
// NEW FORMAT
iceServers = [{
    url: 'turn://passwordturn.server.com',
    port: 31313,
    username: 'username',
    password: 'password'
}]
```

### Future improvements
- Include `port` in `url`. Find a good parser utility for parsing the different parts of the URI(?)?

packages/network/src/connection/webrtc/NodeWebRtcConnection.ts
@@ -116,7 +116,12 @@ export class NodeWebRtcConnection extends WebRtcConnection {
 
     protected doConnect(): void {
         this.connection = new nodeDataChannel.PeerConnection(this.selfId, {
-            iceServers: [...this.stunUrls],
+            iceServers: this.iceServers.map(({ url, port, username, password }) => ({
+                hostname: url,
+                port,
+                username,
+                password
+            })),
             maxMessageSize: this.maxMessageSize
         })
 

packages/client/src/Config.ts
@@ -12,7 +12,7 @@ import CONFIG_SCHEMA from './config.schema.json'
 import { SmartContractRecord } from 'streamr-client-protocol'
 import { LogLevel } from '@streamr/utils'
 
-import { PRODUCTION_STUN_URLS, NetworkNodeOptions } from 'streamr-network'
+import { NetworkNodeOptions, STREAMR_ICE_SERVERS } from 'streamr-network'
 import type { ConnectionInfo } from '@ethersproject/web'
 import { generateClientId } from './utils/utils'
 
@@ -239,8 +239,8 @@ export const createStrictConfig = (inputOptions: StreamrClientConfig = {}): Stri
         }
     }
 
-    if (options.network.stunUrls === undefined) {
-        options.network.stunUrls = PRODUCTION_STUN_URLS
+    if (options.network.iceServers === undefined) {
+        options.network.iceServers = STREAMR_ICE_SERVERS
     }
 
     return options

packages/client/src/ConfigTest.ts
@@ -39,7 +39,7 @@ export const ConfigTest: StreamrClientConfig = {
             }
         ],
         webrtcDisallowPrivateAddresses: false,
-        stunUrls: []
+        iceServers: []
     },
     mainChainRPCs: {
         name: 'dev_ethereum',

packages/client/test/unit/Config.test.ts
@@ -3,7 +3,7 @@ import { createStrictConfig, STREAM_CLIENT_DEFAULTS } from '../../src/Config'
 import { ConfigTest } from '../../src/ConfigTest'
 import { SmartContractRecord } from 'streamr-client-protocol'
 import { generateEthereumAccount } from '../../src/Ethereum'
-import { PRODUCTION_STUN_URLS } from 'streamr-network'
+import { STREAMR_ICE_SERVERS } from 'streamr-network'
 
 describe('Config', () => {
     describe('validate', () => {
@@ -73,7 +73,7 @@ describe('Config', () => {
 
     it('uses PRODUCTION_STUN_URLS by default', () => {
         const clientDefaults = createStrictConfig()
-        expect(clientDefaults.network.stunUrls).toEqual(PRODUCTION_STUN_URLS)
+        expect(clientDefaults.network.iceServers).toEqual(STREAMR_ICE_SERVERS)
     })
 
     describe('ignorable properties', () => {

packages/network/src/browser.ts
@@ -1,6 +1,8 @@
 import 'setimmediate'
+export { STREAMR_ICE_SERVERS } from './constants'
+export { IceServer } from './connection/webrtc/WebRtcConnection'
 export { NameDirectory } from './NameDirectory'
 export { Logger, MetricsContext } from '@streamr/utils'
 export { Location, AbstractNodeOptions } from './identifiers'
-export { PRODUCTION_STUN_URLS, createNetworkNode, NetworkNodeOptions } from './createNetworkNode'
+export { createNetworkNode, NetworkNodeOptions } from './createNetworkNode'
 export { NetworkNode } from './logic/NetworkNode'

packages/network/src/composition.ts
@@ -11,12 +11,14 @@ export {
 } from './identifiers'
 export {
     COUNTER_UNSUBSCRIBE,
-    DEFAULT_MAX_NEIGHBOR_COUNT
+    DEFAULT_MAX_NEIGHBOR_COUNT,
+    STREAMR_ICE_SERVERS
 } from './constants'
+export { IceServer } from './connection/webrtc/WebRtcConnection'
 export { NetworkNode } from './logic/NetworkNode'
 export { Event as NodeEvent } from './logic/Node'
 export { NameDirectory } from './NameDirectory'
-export { PRODUCTION_STUN_URLS, createNetworkNode, NetworkNodeOptions } from './createNetworkNode'
+export { createNetworkNode, NetworkNodeOptions } from './createNetworkNode'
 export { PeerId, PeerInfo } from './connection/PeerInfo'
 export { decode } from './protocol/utils'
 export { AbstractWsConnection, ReadyState } from './connection/ws/AbstractWsConnection'

packages/network/src/connection/webrtc/BrowserWebRtcConnection.ts
@@ -29,7 +29,11 @@ export class BrowserWebRtcConnection extends WebRtcConnection {
     }
     protected doConnect(): void {
 
-        const urls: RTCIceServer[] = this.stunUrls.map((url) => ({ urls: [url] }))
+        const urls: RTCIceServer[] = this.iceServers.map(({ url, port, username, password }) => ({
+            urls: `${url}:${port}`,
+            username,
+            credential: password
+        }))
         this.peerConnection = new RTCPeerConnection({ iceServers: urls })
 
         this.peerConnection.onicecandidate = (event) => {

packages/network/src/connection/webrtc/WebRtcConnection.ts
@@ -7,11 +7,18 @@ import { MessageQueue, QueueItem } from '../MessageQueue'
 import { NameDirectory } from '../../NameDirectory'
 import crypto from 'crypto'
 
+export interface IceServer {
+    url: string
+    port: number
+    username?: string
+    password?: string
+}
+
 export interface ConstructorOptions {
     selfId: PeerId
     targetPeerId: PeerId
     routerId: string
-    stunUrls: string[]
+    iceServers: ReadonlyArray<IceServer>
     pingInterval: number
     bufferThresholdLow?: number
     bufferThresholdHigh?: number
@@ -106,14 +113,14 @@ export abstract class WebRtcConnection extends ConnectionEmitter {
     protected readonly id: string
     protected readonly maxMessageSize: number
     protected readonly selfId: PeerId
-    protected readonly stunUrls: ReadonlyArray<string>
+    protected readonly iceServers: ReadonlyArray<IceServer>
     protected readonly bufferThresholdHigh: number
     protected readonly bufferThresholdLow: number
 
     constructor({
         selfId,
         targetPeerId,
-        stunUrls,
+        iceServers,
         messageQueue,
         deferredConnectionAttempt,
         pingInterval,
@@ -130,7 +137,7 @@ export abstract class WebRtcConnection extends ConnectionEmitter {
         this.id = `Connection${ID}`
         this.selfId = selfId
         this.peerInfo = PeerInfo.newUnknown(targetPeerId)
-        this.stunUrls = stunUrls
+        this.iceServers = iceServers
         this.bufferThresholdHigh = bufferThresholdHigh
         this.bufferThresholdLow = bufferThresholdLow
         this.maxMessageSize = maxMessageSize

commit 63ea4c574afb5d40691fd1f24b70da227c9f77da

fix(network): fix stun/turn URLs in Node.js (#1019)
Fix issue (introduced in 7a19d6687) in which STUN / TURN URLs were not being accepted by node-datachannel therefore  STUN / TURN was not operational in Node.js environments.

packages/network/src/connection/webrtc/NodeWebRtcConnection.ts
@@ -5,6 +5,7 @@ import { ConstructorOptions, WebRtcConnection } from './WebRtcConnection'
 import { Logger } from "@streamr/utils"
 import { NameDirectory } from "../../NameDirectory"
 import { WebRtcConnectionFactory } from "./WebRtcEndpoint"
+import { iceServerAsString } from './iceServerAsString'
 
 const loggerLevel = process.env.NODE_DATACHANNEL_LOG_LEVEL || 'Fatal'
 nodeDataChannel.initLogger(loggerLevel as LogLevel)
@@ -116,12 +117,7 @@ export class NodeWebRtcConnection extends WebRtcConnection {
 
     protected doConnect(): void {
         this.connection = new nodeDataChannel.PeerConnection(this.selfId, {
-            iceServers: this.iceServers.map(({ url, port, username, password }) => ({
-                hostname: url,
-                port,
-                username,
-                password
-            })),
+            iceServers: this.iceServers.map(iceServerAsString),
             maxMessageSize: this.maxMessageSize
         })
 

packages/network/src/connection/webrtc/iceServerAsString.ts
@@ -1,0 +1,15 @@
+import { IceServer } from './WebRtcConnection'
+
+export function iceServerAsString({ url, port, username, password }: IceServer): string {
+    const [protocol, hostname] = url.split(':')
+    if (hostname === undefined) {
+        throw new Error(`invalid stun/turn format: ${url}`)
+    }
+    if (username === undefined && password === undefined) {
+        return `${protocol}:${hostname}:${port}`
+    }
+    if (username !== undefined && password !== undefined) {
+        return `${protocol}:${username}:${password}@${hostname}:${port}`
+    }
+    throw new Error(`username (${username}) and password (${password}) must be supplied together`)
+}

packages/network/test/unit/iceServerAsString.test.ts
@@ -1,0 +1,50 @@
+import { iceServerAsString } from '../../src/connection/webrtc/iceServerAsString'
+
+describe('iceServerAsString', () => {
+    it('without password and username', () => {
+        expect(iceServerAsString({
+            url: 'stun:stun.streamr.network',
+            port: 5349
+        })).toEqual('stun:stun.streamr.network:5349')
+    })
+
+    it('with password and username', () => {
+        expect(iceServerAsString({
+            url: 'turn:turn.streamr.network',
+            port: 5349,
+            username: 'user',
+            password: 'foobar'
+        })).toEqual('turn:user:foobar@turn.streamr.network:5349')
+    })
+
+    it('throws if given url without protocol', () => {
+        expect(() => {
+            iceServerAsString({
+                url: 'turn.streamr.network',
+                port: 5349,
+                username: 'user',
+                password: 'foobar'
+            })
+        }).toThrowError('invalid stun/turn format: turn.streamr.network')
+    })
+
+    it('throws if given username without password', () => {
+        expect(() => {
+            iceServerAsString({
+                url: 'turn:turn.streamr.network',
+                port: 5349,
+                username: 'user'
+            })
+        }).toThrowError('username (user) and password (undefined) must be supplied together')
+    })
+
+    it('throws if given password without username', () => {
+        expect(() => {
+            iceServerAsString({
+                url: 'turn:turn.streamr.network',
+                port: 5349,
+                password: 'foobar'
+            })
+        }).toThrowError('username (undefined) and password (foobar) must be supplied together')
+    })
+})