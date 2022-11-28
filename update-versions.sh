f89e5a4421deb2fe8a135019df6c9eedba400571
Eric Andrews <harbu88@hotmail.com>
2022-11-28 17:33:50 +0200

    refactor: use Teo's refactored update-versions.sh

--- update-versions.sh
+++ update-versions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+VERSION=$1
+FILE_NAME=package.json
+
 if [[ "$1" == "" ]]; then
     echo 'usage: update-versions.sh <SEMVER>'
     exit 1
@@ -26,23 +30,21 @@ streamr-client
 @streamr/cli-tools
 "
 
-
 # Update package versions
 for pkg in $PKG_DIRS
 do
-    tmp=$(mktemp)
+    FILE_NAME=packages/$pkg/package.json
+    TMP_FILE=$(mktemp)
     if [[ "$pkg" != "broker" ]]; then
-        jq ".version=\"$1\"" packages/$pkg/package.json > $tmp
-    else
-        cat packages/$pkg/package.json > $tmp
+        jq --arg versionNumber $VERSION '.version |= $versionNumber' $FILE_NAME > $TMP_FILE
+        mv $TMP_FILE $FILE_NAME
     fi
-    for dep in $PKG_NAMES
+    for PKG_NAME in $PKG_NAMES
     do
-        tmp2=$(mktemp)
-        jq "if .dependencies.\"$dep\"? then .dependencies.\"$dep\"=\"$1\" else . end" $tmp > $tmp2
-        tmp3=$(mktemp)
-        jq "if .devDependencies.\"$dep\"? then .devDependencies.\"$dep\"=\"$1\" else . end" $tmp2 > $tmp3
-        mv $tmp3 $tmp
+        jq --arg versionNumber $VERSION --arg PKG_NAME $PKG_NAME '
+        if .dependencies[$PKG_NAME]? then .dependencies[$PKG_NAME] |= $versionNumber
+        elif .devDependencies[$PKG_NAME]? then .devDependencies[$PKG_NAME] |= $versionNumber
+        else . end' $FILE_NAME > $TMP_FILE
+        mv $TMP_FILE $FILE_NAME
     done
-    mv $tmp packages/$pkg/package.json
 done
