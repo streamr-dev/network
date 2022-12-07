42abf83099d07eee384484d859f3c3324ffd0799
Teo Gebhard <teo@streamr.com>
2022-12-07 08:47:00 +0200

    refactor: simplify transform

--- bin/run-brokers-and-trackers.sh
+++ bin/run-brokers-and-trackers.sh
@@ -12,12 +12,11 @@ stop_applications(){
 run_application() {
     # use AWK to transform the output:
     # - colorize lines: use line-specific color for errors and warnings, otherwise application-specific color
-    # - abbreviate log level so that are levels are just one character (e.g. "error" is longer string than "info" 
-    #   and therefore log lines wouldn't align without this transformation)
-    (cd ../packages/$2 && STREAMR_APPLICATION_ID=$3 LOG_COLORS=false npm exec $1 -- $5 $6 $7) | awk -v default_color=$4 ' BEGIN { colcode = default_color }
-        /^ERROR/ { colcode = "255;0;0" }
-        /^WARN/ { colcode = "255;255;0" }
-        { print "\033[38;2;" colcode "m" substr($0, 1, 1) substr($0, index($0, " "), length($0)) "\033[39m" ; colcode = default_color }'
+    # - abbreviate log level to one character (otherwise messages don't align as e.g. "error" is a longer word than "info")
+    (cd ../packages/$2 && STREAMR_APPLICATION_ID=$3 LOG_COLORS=false npm exec $1 -- $5 $6 $7) | awk -v default_color=$4 ' BEGIN { color = default_color }
+        /^ERROR/ { color = "255;0;0" }
+        /^WARN/ { color = "255;255;0" }
+        { print "\033[38;2;" color "m" substr($0, 1, 1) substr($0, index($0, " "), length($0)); color = default_color }'
 }
 
 run_application streamr-tracker network-tracker T1 '102;204;102' 0xe5abc5ee43b8830e7b0f98d03efff5d6cae574d52a43204528eab7b52cd6408d T1 --port=30301 &
