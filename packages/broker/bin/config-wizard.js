361792681540b56f934211e936190e4e4d258eda
Eric Andrews <harbu88@hotmail.com>
2022-12-13 12:36:48 +0200

    feat(broker): temp bin/config-wizard.js

---
+++ packages/broker/bin/config-wizard.js
@@ -0,0 +1,4 @@
+// Placeholder script so we don't have to update docs yet
+// https://streamr.network/docs/streamr-network/installing-broker-node.
+// Remove in next version update
+require('child_process').fork('dist/bin/config-wizard.js')
