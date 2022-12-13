98492d4e4915d903ebda985c74002a26dec4031b
Eric Andrews <harbu88@hotmail.com>
2022-12-13 12:48:43 +0200

    fix(broker): fix previous commit

--- packages/broker/bin/config-wizard.js
+++ packages/broker/bin/config-wizard.js
@@ -1,4 +1,5 @@
 // Placeholder script so we don't have to update docs yet
 // https://streamr.network/docs/streamr-network/installing-broker-node.
 // Remove in next version update
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 require('child_process').fork('dist/bin/config-wizard.js')
