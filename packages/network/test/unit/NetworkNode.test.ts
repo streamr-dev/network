be4e88b15580ceec938cbca812a0646c9c982979
Eric Andrews <eric.andrews@streamr.com>
2023-03-29 15:17:11 +0300

    feat: prohibit setProxies when acceptProxyConnections=true (#1156)
    ## Summary
    
    Prevent the use of method `networkNode#setProxies` when accepting proxy connections (i.e. `acceptProxyConnections=true`). Throw a human readable error if such usage is attempted.
    
    ## Limitations and future improvements
    
    The main reason for implementing this restriction is due to incomplete state management in the code when a node
    - wishes to act as a proxy themselves, and
    - wishes to subscribe / publish to a stream using another proxy node.
    
    Our evaluation is that this is a very unlikely scenario in practice. If such behavior is wanted in the future, the restriction imposed in this PR should be removed and the state management logic should be changed to accommodate this scenario.

--- packages/network/test/unit/NetworkNode.test.ts
+++ packages/network/test/unit/NetworkNode.test.ts
@@ -1,19 +1,28 @@
 import { NetworkNode } from '../../src/logic/NetworkNode'
 import { Tracker } from '@streamr/network-tracker'
 import { createTestNetworkNode, startTestTracker } from '../utils'
+import { ProxyDirection, toStreamID, toStreamPartID } from '@streamr/protocol'
+import { randomEthereumAddress } from '@streamr/test-utils'
 
 describe('NetworkNode', () => {
     let tracker: Tracker
     let node: NetworkNode
+    let node2: NetworkNode | undefined
+
     beforeEach(async () => {
         tracker = await startTestTracker({
             port: 30410
         })
-        const trackerInfo = tracker.getConfigRecord()
         node = createTestNetworkNode({
             id: 'node-1',
-            trackers: [trackerInfo]
+            trackers: [tracker.getConfigRecord()]
         })
+    })
+
+    afterEach(async () => {
+        await tracker.stop()
+        await node.stop()
+        await node2?.stop()
     })
 
     it('has id & peerInfo', () => {
@@ -22,8 +31,18 @@ describe('NetworkNode', () => {
         expect(node.peerInfo.isTracker()).toEqual(false)
     })
 
-    afterEach(async () => {
-        await tracker.stop()
-        await node.stop()
+    it('setProxies throws error if acceptProxyConnections=true (NET-950)', async () => {
+        node2 = createTestNetworkNode({
+            id: 'node-1',
+            trackers: [tracker.getConfigRecord()],
+            acceptProxyConnections: true
+        })
+        await expect(() => node2!.setProxies(
+            toStreamPartID(toStreamID('/foobar', randomEthereumAddress()), 0),
+            ['0xa', '0xb'],
+            ProxyDirection.SUBSCRIBE,
+            () => Promise.resolve(''),
+            1
+        )).rejects.toThrow('cannot set proxies when acceptProxyConnections=true')
     })
 })
