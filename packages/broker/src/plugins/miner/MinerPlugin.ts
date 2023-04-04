ea06f5aea95daf77e59dfc7959152d0133dbd957
Eric Andrews <harbu88@hotmail.com>
2023-04-04 16:42:45 +0300

    chore: placeholder implementations of services

--- packages/broker/src/plugins/miner/MinerPlugin.ts
+++ packages/broker/src/plugins/miner/MinerPlugin.ts
@@ -1,17 +1,36 @@
 import { Plugin } from '../../Plugin'
 import PLUGIN_CONFIG_SCHEMA from './config.schema.json'
 import { Schema } from 'ajv'
+import { AnnounceNodeService } from './AnnounceNodeService'
+import { InspectRandomNodeService } from './InspectRandomNodeService'
+import { MaintainOperatorContractService } from './MaintainOperatorContractService'
+import { MaintainTopologyService } from './MaintainTopologyService'
+import { VoteOnSuspectNodeService } from './VoteOnSuspectNodeService'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface MinerPluginConfig {}
 
 export class MinerPlugin extends Plugin<MinerPluginConfig> {
-    // eslint-disable-next-line class-methods-use-this
+    private readonly announceNodeService = new AnnounceNodeService()
+    private readonly inspectRandomNodeService = new InspectRandomNodeService()
+    private readonly maintainOperatorContractService = new MaintainOperatorContractService()
+    private readonly maintainTopologyService = new MaintainTopologyService()
+    private readonly voteOnSuspectNodeService = new VoteOnSuspectNodeService()
+
     async start(): Promise<void> {
+        await this.announceNodeService.start()
+        await this.inspectRandomNodeService.start()
+        await this.maintainOperatorContractService.start()
+        await this.maintainTopologyService.start()
+        await this.voteOnSuspectNodeService.start()
     }
 
-    // eslint-disable-next-line class-methods-use-this
     async stop(): Promise<void> {
+        await this.announceNodeService.stop()
+        await this.inspectRandomNodeService.stop()
+        await this.maintainOperatorContractService.stop()
+        await this.maintainTopologyService.stop()
+        await this.voteOnSuspectNodeService.stop()
     }
 
     // eslint-disable-next-line class-methods-use-this
