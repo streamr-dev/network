44cb7f6df8d93ef723a17a879225288dc8a01f6c
Eric Andrews <harbu88@hotmail.com>
2023-04-06 12:50:04 +0300

    test: fix broken test

--- packages/broker/test/unit/configMigration.test.ts
+++ packages/broker/test/unit/configMigration.test.ts
@@ -9,7 +9,6 @@ const MOCK_PRIVATE_KEY = '0x1111111111111111111111111111111111111111111111111111
 
 const validateTargetConfig = async (config: any): Promise<void> | never => {
     validateConfig(config, BROKER_CONFIG_SCHEMA)
-    validateClientConfig(config.client)
     for (const pluginName of Object.keys(config.plugins)) {
         const pluginConfig = config.plugins[pluginName]
         // validates the config against the schema
