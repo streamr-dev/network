6466a6e4a4c63513a3838d3baef6ad7ffe0b0872
Eric Andrews <harbu88@hotmail.com>
2023-04-06 12:54:28 +0300

    fix test

--- packages/broker/test/unit/configMigration.test.ts
+++ packages/broker/test/unit/configMigration.test.ts
@@ -1,5 +1,4 @@
 import merge from 'lodash/merge'
-import { validateConfig as validateClientConfig } from 'streamr-client'
 import { createMigratedConfig, CURRENT_CONFIGURATION_VERSION, formSchemaUrl, needsMigration } from '../../src/config/migration'
 import BROKER_CONFIG_SCHEMA from '../../src/config/config.schema.json'
 import { validateConfig } from '../../src/config/validateConfig'
