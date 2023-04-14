9f69ac23d64ae1bd4d486e5c091a4fb7e9dba683
Teo Gebhard <teo@streamr.com>
2023-04-14 16:02:22 +0300

    ci: ignore placeholder config wizard script

--- packages/broker/bin/config-wizard.js
+++ packages/broker/bin/config-wizard.js
@@ -1,5 +1,4 @@
 // Placeholder script so we don't have to update docs yet
 // https://streamr.network/docs/streamr-network/installing-broker-node.
 // Remove in next version update
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 require('child_process').fork('dist/bin/config-wizard.js')
