4289f839347838ff6ee29f2a6a5b79c1620b590c
Eric Andrews <harbu88@hotmail.com>
2023-04-20 14:01:54 +0300

    build: add checks to release-git-tags.sh

--- release-git-tags.sh
+++ release-git-tags.sh
@@ -8,6 +8,25 @@ if [[ "$1" == "" ]]; then
     exit 1
 fi
 
+# Exit early if version is wrong
+if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-beta\.[0-9]+)?$ ]]; then
+    echo "Error: Invalid version"
+    exit 1
+fi
+
+CLIENT_TAG=client/v$1
+CLI_TOOLS_TAG=cli-tools/v$1
+
+# Exit early if tags already exist
+if git rev-parse "$CLIENT_TAG" >/dev/null 2>&1; then
+    echo "Error: Tag '$CLIENT_TAG' already exists."
+    exit 1
+fi
+if git rev-parse "$CLI_TOOLS_TAG" >/dev/null 2>&1; then
+    echo "Error: Tag '$CLI_TOOLS_TAG' already exists."
+    exit 1
+fi
+
 git commit -m "release(client, cli-tools): v$1"
 git tag client/v$1
 git tag cli-tools/v$1
