ed3d0990a1febb23a15c9add4b81bd410c610ce8
Eric Andrews <harbu88@hotmail.com>
2023-05-05 16:52:49 +0300

    refactor MaintainTopologyService

--- packages/broker/src/plugins/operator/MaintainTopologyService.ts
+++ packages/broker/src/plugins/operator/MaintainTopologyService.ts
@@ -4,18 +4,28 @@ import StreamrClient, { Stream, Subscription } from 'streamr-client'
 import { StreamID, StreamPartIDUtils, toStreamID } from '@streamr/protocol'
 import { SetMembershipSynchronizer } from '../storage/SetMembershipSynchronizer'
 import pLimit from 'p-limit'
+import { compact } from 'lodash'
 
 const logger = new Logger(module)
 
-function mapOverSet<T, U>(set: Set<T>, transformFn: (t: T) => U): Set<U> {
-    return new Set([...set].map(transformFn))
+function toStreamIDSafe(input: string): StreamID | undefined {
+    try {
+        return toStreamID(input)
+    } catch (err) {
+        logger.warn('Encountered invalid streamId', { input, reason: err?.reason })
+        return undefined
+    }
+}
+
+function singletonSet<T>(element: T): Set<T> {
+    return new Set<T>([element])
 }
 
 export class MaintainTopologyService {
     private readonly streamrClient: StreamrClient
     private readonly operatorClient: OperatorClient
     private readonly subscriptions = new Multimap<StreamID, Subscription>()
-    private readonly setMembershipSynchronizer = new SetMembershipSynchronizer<StreamID>()
+    private readonly synchronizer = new SetMembershipSynchronizer<StreamID>()
     private readonly concurrencyLimit = pLimit(1)
 
     constructor(streamrClient: StreamrClient, operatorClient: OperatorClient) {
@@ -23,29 +33,25 @@ export class MaintainTopologyService {
         this.operatorClient = operatorClient
     }
 
-    // eslint-disable-next-line class-methods-use-this
     async start(): Promise<void> {
-        this.operatorClient.on('addStakedStream', this.onEvent.bind(this, 'addStakedStream'))
-        this.operatorClient.on('removeStakedStream', this.onEvent.bind(this, 'removeStakedStream'))
-        const { streamIds, blockNumber: initialBlockNumber } = await this.operatorClient.getStakedStreams()
-        const { added } = this.setMembershipSynchronizer.ingestSnapshot(mapOverSet(streamIds, toStreamID), initialBlockNumber)
-        const streamParts = await Promise.all([...added].map(async (streamId) => {
-            try {
-                const stream = await this.streamrClient.getStream(streamId)
-                return stream.getStreamParts()
-            } catch (err) {
-                logger.warn('Ignore non-existing stream', { streamId, reason: err?.message })
-                return []
+        this.operatorClient.on('addStakedStream', (streamIdAsStr, blockNumber) => {
+            const streamId = toStreamIDSafe(streamIdAsStr)
+            if (streamId !== undefined) {
+                this.concurrencyLimit(() => this.onAddStakedStream(streamId, blockNumber))
             }
-        }))
-        for (const streamPart of streamParts.flat()) {
-            const id = StreamPartIDUtils.getStreamID(streamPart)
-            const subscription = await this.streamrClient.subscribe({
-                id,
-                partition: StreamPartIDUtils.getStreamPartition(streamPart),
-                raw: true
-            }) // TODO: what if rejects?
-            this.subscriptions.add(id, subscription)
+        })
+        this.operatorClient.on('removeStakedStream', (streamIdAsStr, blockNumber) => {
+            const streamId = toStreamIDSafe(streamIdAsStr)
+            if (streamId !== undefined) {
+                this.concurrencyLimit(() => this.onRemoveStakedStream(streamId, blockNumber))
+            }
+        })
+
+        const { streamIds: rawStreamIds, blockNumber } = await this.operatorClient.getStakedStreams()
+        const streamIds = new Set(compact([...rawStreamIds].map((s) => toStreamIDSafe(s))))
+        const { added } = this.synchronizer.ingestSnapshot(streamIds, blockNumber)
+        for (const streamId of added) {
+            await this.addStream(streamId, blockNumber)
         }
         logger.info('Started')
     }
@@ -55,44 +61,47 @@ export class MaintainTopologyService {
         logger.info('stopped')
     }
 
-    private async onEvent(event: 'addStakedStream' | 'removeStakedStream', streamIdAsStr: string, blockNumber: number) {
-        return this.concurrencyLimit(async () => {
-            if (event === 'addStakedStream') {
-                const { added } = this.setMembershipSynchronizer.ingestPatch(new Set([toStreamID(streamIdAsStr)]), 'added', blockNumber)
-                if (added.length !== 1) {
-                    return
-                }
-                const [streamId] = added
-                if (this.subscriptions.get(streamId).length > 0) {
-                    logger.warn('Ignore already subscribed stream', { streamId })
-                    return
-                }
-                let stream: Stream
-                try {
-                    stream = await this.streamrClient.getStream(streamId)
-                } catch (err) {
-                    logger.warn('Ignore non-existing stream', { streamId, reason: err?.message })
-                    return
-                }
-                for (const streamPart of stream.getStreamParts()) {
-                    const id = StreamPartIDUtils.getStreamID(streamPart)
-                    const subscription = await this.streamrClient.subscribe({
-                        id,
-                        partition: StreamPartIDUtils.getStreamPartition(streamPart),
-                        raw: true
-                    }) // TODO: what if rejects?
-                    this.subscriptions.add(id, subscription)
-                }
-            } else {
-                const { removed } = this.setMembershipSynchronizer.ingestPatch(new Set([toStreamID(streamIdAsStr)]), 'removed', blockNumber)
-                if (removed.length !== 1) {
-                    return
-                }
-                const [streamId] = removed
-                const subscriptions = this.subscriptions.get(streamId)
-                this.subscriptions.removeAll(streamId, subscriptions)
-                await Promise.all(subscriptions.map((sub) => sub.unsubscribe())) // TODO: what if rejects?
-            }
-        })
+    private async onAddStakedStream(streamId: StreamID, blockNumber: number): Promise<void> {
+        const { added } = this.synchronizer.ingestPatch(singletonSet(streamId), 'added', blockNumber)
+        if (added.length !== 1) {
+            logger.warn('Ignore already subscribed stream', { streamId, blockNumber })
+            return
+        }
+        if (this.subscriptions.get(streamId).length > 0) {
+            logger.warn('Ignore already subscribed stream', { streamId, blockNumber })
+            return
+        }
+        await this.addStream(streamId, blockNumber)
+    }
+
+    private async onRemoveStakedStream(streamId: StreamID, blockNumber: number): Promise<void> {
+        const { removed } = this.synchronizer.ingestPatch(singletonSet(streamId), 'removed', blockNumber)
+        if (removed.length !== 1) {
+            logger.warn('Ignore already unsubscribed stream', { streamId, blockNumber })
+            return
+        }
+        const subscriptions = this.subscriptions.get(streamId)
+        this.subscriptions.removeAll(streamId, subscriptions)
+        await Promise.all(subscriptions.map((sub) => sub.unsubscribe())) // TODO: rejects?
+    }
+
+    private async addStream(streamId: StreamID, blockNumber: number): Promise<void> {
+        let stream: Stream
+        try {
+            stream = await this.streamrClient.getStream(streamId)
+        } catch (err) {
+            logger.warn('Ignore non-existing stream', { streamId, reason: err?.message, blockNumber })
+            return
+        }
+        for (const streamPart of stream.getStreamParts()) {
+            const id = StreamPartIDUtils.getStreamID(streamPart)
+            const partition = StreamPartIDUtils.getStreamPartition(streamPart)
+            const subscription = await this.streamrClient.subscribe({
+                id,
+                partition,
+                raw: true
+            }) // TODO: rejects?
+            this.subscriptions.add(id, subscription)
+        }
     }
 }
