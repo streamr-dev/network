9d33a8d995392d7c2194b2c9d40668361d8096fa
Teo Gebhard <teo@streamr.com>
2023-06-19 14:29:15 +0300

    test cases

--- packages/client/test/end-to-end/contract-call-error.test.ts
+++ packages/client/test/end-to-end/contract-call-error.test.ts
@@ -1,6 +1,7 @@
+import { fastWallet, fetchPrivateKeyWithGas } from '@streamr/test-utils'
+import range from 'lodash/range'
 import { CONFIG_TEST } from '../../src/ConfigTest'
 import { StreamrClient } from '../../src/StreamrClient'
-import { fastWallet } from '@streamr/test-utils'
 
 describe('contract call error', () => {
     
@@ -32,6 +33,37 @@ describe('contract call error', () => {
         })
         await expect(() => client.createStream('/path')).rejects.toThrow(
             'Error while executing contract call "streamRegistry.createStream", reason=could not detect network, code=NETWORK_ERROR'
+        )
+    })
+
+    it('concurrent transactions', async () => {
+        const privateKey = await fetchPrivateKeyWithGas()
+        const client = new StreamrClient({
+            ...CONFIG_TEST,
+            auth: {
+                privateKey
+            }
+        })
+        await expect(() => Promise.all([
+            client.createStream('/path1' + Date.now()),
+            client.createStream('/path2' + Date.now())
+            // eslint-disable-next-line max-len
+        ])).rejects.toThrow('Error while executing contract call "streamRegistry.createStream", reason=replacement fee too low, code=REPLACEMENT_UNDERPRICED')
+    })
+
+    it('concurrent transactions with different clients', async () => {
+        const privateKey = await fetchPrivateKeyWithGas()
+        await expect(() => Promise.all(range(2).map((i) => {
+            const client = new StreamrClient({
+                ...CONFIG_TEST,
+                auth: {
+                    privateKey
+                }
+            })
+            return client.createStream(`/path${i}`)
+        }))).rejects.toThrow(
+            // eslint-disable-next-line max-len
+            'Error while executing contract call "streamRegistry.createStream", reason=cannot estimate gas; transaction may fail or may require manual gas limit, code=UNPREDICTABLE_GAS_LIMIT'
         )
     })
 })
