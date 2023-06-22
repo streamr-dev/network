4618dddb73a7d6b52a8fc4eefa4fdd2ff25218f8
Teo Gebhard <teo@streamr.com>
2023-06-22 12:55:39 +0300

    OrderedMessageChain

---
+++ packages/client/src/subscribe/ordering/OrderedMessageChain.ts
@@ -0,0 +1,188 @@
+import { MessageRef, StreamMessage, StreamPartID } from '@streamr/protocol'
+import { Gate, Logger } from '@streamr/utils'
+import EventEmitter from 'eventemitter3'
+import Heap from 'heap'
+
+/*
+ * There are missing messages between these two messages. The "to" message is guaranteed to have prevMsgRef.
+ */
+export interface Gap {
+    from: StreamMessage
+    to: StreamMessage
+}
+
+export interface Events {
+    orderedMessageAdded: (msg: StreamMessage) => void
+    gapFound: (gap: Gap) => void
+    gapResolved: () => void
+}
+
+const logger = new Logger(module)
+
+const areEqualRefs = (ref1: MessageRef, ref2: MessageRef) => {
+    return ref1.compareTo(ref2) === 0
+}
+
+/*
+ * This class represents a chain of messages, in which message are in ascencing order. 
+
+ * There is always one message reference (timestamp + sequence number) to the head of the chain, 
+ * and a message can be immediately added to the chain if the "prevMsgRef" of the new message reference
+ * to the head of the chain. If a message immediately can't be added to the chain, it is stored to 
+ * an internal heap structure (a priority queue which is ordered my message references).
+ *
+ * Messages are added to the chain by calling "addMessage" method. If the new message can be immediately 
+ * added to the chain, the "orderedMessageAdded" event is emitted.
+ * 
+ * If the new message can't be immediately added to the chain, there must be some missing messages
+ * which we need before the message can be processed. In that case we emit the "gapFound" event.
+ * 
+ * Typically the "gapFound" triggers some component to provide us more messages (e.g. fetching
+ * messages from a storage node). Also the same component which produced the original message continues 
+ * to provide more messages. Both of these sources provide messages by calling the "addMessage" method.
+ * 
+ * The sources may provide us messages which can be added be immediately added to the chain. In that case
+ * the messages is added and the "orderedMessageAdded" event is emitted. As the head of the chain changes, 
+ * there may be also some messages in the heap which can also be added now. The "orderedMessageAdded"
+ * is emmitted for those messages, too. 
+ * 
+ * If the sources provide message which can't be immediatelly added to the chain, we store the message
+ * to the internal heap structure normally, but don't emit "gapFound" event as there is already
+ * an existing gap.
+ *
+ * It is very likely that eventually the sources will produce all the missing messages. When that happens
+ * the gap is now filled and the "gapResolved" event is emitted.
+ * 
+ * Alternatively if don't get all the missing messages from the sources, an external component may call
+ * the "resolveMessages" method. It enforces us to ignore the remaining missing messages and allows the
+ * chain to proceed with new messages. In that case we add some of temporarily stored messages to the 
+ * chain, and typically the last of those messages resolves the gap (and therefore "gapResolved" is 
+ * emitted).
+ * 
+ * The consequence from rules above is that we always process on gap at a time. For each "gapFound" event 
+ * there is always a matching "gapResolved" event emitted before new "gapFound" event can be emitted.
+ * 
+ * Implementation:
+ * - The "prevMessageRef" field to a message tells us whether there are missing messages between
+ *   that message and some other messages. Typically all messages contain that field.
+ *   If a message doesn't contain "prevMessageRef" field, we can't know if there are some missing
+ *   messages, and therefore we just add that message to the chain as a latest message (if 
+ *   it is newer than the current latest message).
+ * - There is a check about stale messages in "addMessage". It ensures that we don't re-process 
+ *   any messages which we've already processed. 
+ */
+export class OrderedMessageChain {
+
+    private lastOrderedMsg?: StreamMessage
+    private currentGap?: Gap
+    private readonly pendingMsgs: Heap<StreamMessage>
+    private readonly eventEmitter: EventEmitter<Events>
+    protected readonly streamPartId: StreamPartID
+
+    constructor(streamPartId: StreamPartID) {
+        this.streamPartId = streamPartId
+        this.pendingMsgs = new Heap<StreamMessage>((msg1: StreamMessage, msg2: StreamMessage) => {
+            return msg1.getMessageRef().compareTo(msg2.getMessageRef())
+        })
+        this.eventEmitter = new EventEmitter()
+    }
+
+    addMessage(msg: StreamMessage): void {
+        if (!this.isStaleMessage(msg)) {
+            this.pendingMsgs.push(msg)
+            this.consumePendingOrderedMessages(
+                (msg) => this.isNextOrderedMessage(msg)
+            )
+        }
+    }
+
+    resolveMessages(to: MessageRef): void {
+        if ((this.lastOrderedMsg === undefined) || (this.lastOrderedMsg.getMessageRef().compareTo(to) < 0)) {
+            this.consumePendingOrderedMessages(
+                (msg) => (msg.getMessageRef().compareTo(to) <= 0) ? true : this.isNextOrderedMessage(msg)
+            )
+        }
+    }
+
+    async waitUntilIdle(): Promise<void> {
+        const isIdle = () => this.pendingMsgs.empty()
+        if (!isIdle()) {
+            const gate = new Gate(false)
+            const listener = () => {
+                if (isIdle()) {
+                    gate.open()
+                }
+            }
+            this.on('orderedMessageAdded', listener)
+            await gate.waitUntilOpen()
+            this.off('orderedMessageAdded', listener)
+        }
+    }
+
+    private consumePendingOrderedMessages(isConsumable: (msg: StreamMessage) => boolean) {
+        while (!this.pendingMsgs.empty() && isConsumable(this.pendingMsgs.peek()!)) {
+            const next = this.pendingMsgs.pop()!
+            this.lastOrderedMsg = next
+            this.eventEmitter.emit('orderedMessageAdded', next)
+            this.checkGapResolved()
+        }
+        this.checkGapFound()
+    }
+
+    private checkGapFound() {
+        if (!this.pendingMsgs.empty() && (this.currentGap === undefined)) {
+            this.currentGap = {
+                from: this.lastOrderedMsg!,
+                to: this.pendingMsgs.peek()!
+            }
+            logger.debug('Gap found', {
+                streamPartId: this.streamPartId,
+                from: this.currentGap.from.getMessageRef(),
+                to: this.currentGap.to.getMessageRef()
+            })
+            this.eventEmitter.emit('gapFound', this.currentGap)
+        }
+    }
+
+    private checkGapResolved() {
+        if ((this.currentGap !== undefined) && areEqualRefs(this.lastOrderedMsg!.getMessageRef(), this.currentGap.to.getMessageRef())) {
+            const gap = this.currentGap
+            this.currentGap = undefined
+            logger.debug('Gap resolved', {
+                streamPartId: this.streamPartId,
+                from: gap.from.getMessageRef(),
+                to: gap.to.getMessageRef()
+            })
+            this.eventEmitter.emit('gapResolved')
+        }
+    }
+
+    private isNextOrderedMessage(msg: StreamMessage) {
+        const previousRef = msg.getPreviousMessageRef()
+        return (previousRef !== null)
+            ? (this.lastOrderedMsg === undefined) || areEqualRefs(previousRef, this.lastOrderedMsg.getMessageRef())
+            : true
+    }
+
+    private isStaleMessage(msg: StreamMessage): boolean {
+        return (this.lastOrderedMsg !== undefined 
+            && msg.getMessageRef().compareTo(this.lastOrderedMsg.getMessageRef()) <= 0)
+            || this.pendingMsgs.contains(msg)
+    }
+
+    destroy(): void {
+        this.eventEmitter.removeAllListeners()
+    }
+
+    on<E extends keyof Events>(eventName: E, listener: Events[E]): void {
+        this.eventEmitter.on(eventName, listener as any)
+    }
+
+    once<E extends keyof Events>(eventName: E, listener: Events[E]): void {
+        this.eventEmitter.on(eventName, listener as any)
+    }
+
+    off<E extends keyof Events>(eventName: E, listener: Events[E]): void {
+        this.eventEmitter.off(eventName, listener as any)
+    }
+}
---
+++ packages/client/test/unit/OrderedMessageChain.test.ts
@@ -0,0 +1,192 @@
+import { MessageID, MessageRef, StreamMessage, toStreamID } from '@streamr/protocol'
+import { toEthereumAddress } from '@streamr/utils'
+import { Gap, OrderedMessageChain } from '../../src/subscribe/ordering/OrderedMessageChain'
+
+const STREAM_ID = toStreamID('stream')
+const PUBLISHER_ID = toEthereumAddress('0x0000000000000000000000000000000000000001')
+const MSG_CHAIN_ID = 'msgChainId'
+
+const createMessage = (timestamp: number, hasPrevRef = true) => {
+    return new StreamMessage({
+        messageId: new MessageID(STREAM_ID, 0, timestamp, 0, PUBLISHER_ID, MSG_CHAIN_ID),
+        prevMsgRef: hasPrevRef ? new MessageRef(timestamp - 1, 0) : null,
+        content: '{}',
+        signature: 'signature'
+    })
+}
+
+const createGap = (from: number, to: number): Gap => {
+    return {
+        from: createMessage(from),
+        to: createMessage(to)
+    }
+}
+
+describe('OrderedMessageChain', () => {
+
+    let chain: OrderedMessageChain
+    let onOrderedMessageAdded: jest.Mock<void, [msg: StreamMessage]>
+    let onGapFound: jest.Mock<void, [gap: Gap]>
+    let onGapResolved: jest.Mock<void, []>
+
+    const getOrderedTimestamps = () => {
+        return onOrderedMessageAdded.mock.calls.map((call) => call[0].getTimestamp())
+    }
+
+    const expectGaps = (gaps: Gap[], allResolved = true) => {
+        expect(onGapFound).toBeCalledTimes(gaps.length)
+        for (let i = 0; i < gaps.length; i++) {
+            expect(onGapFound).toHaveBeenNthCalledWith(i + 1, gaps[i])
+        }
+        if (allResolved) {
+            expect(onGapResolved).toBeCalledTimes(gaps.length)
+        }
+    }
+
+    beforeEach(() => {
+        onOrderedMessageAdded = jest.fn()
+        onGapFound = jest.fn()
+        onGapResolved = jest.fn()
+        chain = new OrderedMessageChain(undefined as any)
+        chain.on('orderedMessageAdded', onOrderedMessageAdded)
+        chain.on('gapFound', onGapFound)
+        chain.on('gapResolved', onGapResolved)
+    })
+
+    it('no gaps', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(3))
+        expect(getOrderedTimestamps()).toEqual([1, 2, 3])
+        expectGaps([])
+    })
+
+    it('find gap', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(3))
+        expect(getOrderedTimestamps()).toEqual([1])
+        expectGaps([
+            createGap(1, 3)
+        ], false)
+        expect(onGapResolved).not.toBeCalled()
+    })
+
+    it('debug', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(3))
+        chain.addMessage(createMessage(5))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(4))
+        expect(getOrderedTimestamps()).toEqual([1, 2, 3, 4, 5])
+        expectGaps([
+            createGap(1, 3),
+            createGap(3, 5)
+        ])
+    })
+
+    it('fill single gap', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(3))
+        chain.addMessage(createMessage(9))
+        chain.addMessage(createMessage(7)) 
+        chain.addMessage(createMessage(6))
+        chain.addMessage(createMessage(4))
+        chain.addMessage(createMessage(5))
+        chain.addMessage(createMessage(8))
+        chain.addMessage(createMessage(10))
+        expect(getOrderedTimestamps()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        expectGaps([
+            createGap(3, 9)
+        ])
+    })
+
+    it('fill multiple gaps', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(3))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(5))
+        chain.addMessage(createMessage(4))
+        chain.addMessage(createMessage(6))
+        expect(getOrderedTimestamps()).toEqual([1, 2, 3, 4, 5, 6])
+        expectGaps([
+            createGap(1, 3),
+            createGap(3, 5)
+        ])
+    })
+
+    it('fill nested gap', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(3))
+        chain.addMessage(createMessage(5))
+        chain.addMessage(createMessage(4))
+        chain.addMessage(createMessage(6))
+        chain.addMessage(createMessage(8))
+        chain.addMessage(createMessage(7))
+        chain.addMessage(createMessage(2))
+        expect(getOrderedTimestamps()).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+        expectGaps([
+            createGap(1, 3)
+        ])
+    })
+
+    it('stale message', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(3))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(4))
+        expect(getOrderedTimestamps()).toEqual([1, 2, 3, 4])
+        expectGaps([])
+    })
+
+    it('process pending messages when manually resolving gap', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(5))
+        chain.addMessage(createMessage(3))
+        chain.addMessage(createMessage(6))
+        chain.resolveMessages(new MessageRef(5, 0))
+        expect(getOrderedTimestamps()).toEqual([1, 3, 5, 6])
+        expectGaps([
+            createGap(1, 5)
+        ])
+    })
+
+    it('find pending gap when manually resolving gap', () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(4))
+        chain.addMessage(createMessage(5))
+        chain.addMessage(createMessage(7))
+        chain.resolveMessages(new MessageRef(4, 0))
+        expect(getOrderedTimestamps()).toEqual([1, 2, 4, 5])
+        expectGaps([
+            createGap(2, 4),
+            createGap(5, 7)
+        ], false)
+        expect(onGapResolved).toBeCalledTimes(1)
+    })
+
+    it('without references', () => {
+        chain.addMessage(createMessage(1, false))
+        chain.addMessage(createMessage(3, false))
+        expect(getOrderedTimestamps()).toEqual([1, 3])
+        expectGaps([])
+    })
+
+    it('wait until idle', async () => {
+        chain.addMessage(createMessage(1))
+        chain.addMessage(createMessage(2))
+        chain.addMessage(createMessage(4))
+        const waitUntilIdle = chain.waitUntilIdle()
+        setTimeout(() => {
+            chain.addMessage(createMessage(5))
+            chain.addMessage(createMessage(7))
+            chain.addMessage(createMessage(6))
+            chain.addMessage(createMessage(3))
+        }, 50)
+        await waitUntilIdle
+        expect(getOrderedTimestamps()).toEqual([1, 2, 3, 4, 5, 6, 7])
+    })
+
+})
