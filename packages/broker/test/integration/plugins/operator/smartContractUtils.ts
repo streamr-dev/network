12743f7d84cf81aa86c0d44feb9a221f8cfdb39f
Sami Gabor <45082017+samigabor@users.noreply.github.com>
2023-06-27 12:10:06 +0300

    NET-527 maintain operator value service (#1513)
    Added MaintainOperatorValueService, helper and tests
    
    ---------
    
    Co-authored-by: Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
    Co-authored-by: Eric Andrews <eric.andrews@streamr.com>

--- packages/broker/test/integration/plugins/operator/smartContractUtils.ts
+++ packages/broker/test/integration/plugins/operator/smartContractUtils.ts
@@ -8,7 +8,7 @@ import { deploySponsorship as _deploySponsorship } from './deploySponsorshipCont
 import { deployOperatorContract as _deployOperatorContract } from './deployOperatorContract'
 import { Chains } from '@streamr/config'
 
-const ADMIN_WALLET_PK = '0x4059de411f15511a85ce332e7a428f36492ab4e87c7830099dadbf130f1896ae'
+export const ADMIN_WALLET_PK = '0x4059de411f15511a85ce332e7a428f36492ab4e87c7830099dadbf130f1896ae'
 const CONFIG = Chains.load()["dev1"]
 
 export function getProvider(): Provider {
