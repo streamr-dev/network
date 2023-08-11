3bb36c676d99fbe6ea5b8efc7d2451cc417d1c9e
juslesan <santeri.juslenius@gmail.com>
2023-08-11 14:24:30 +0300

    fix eslint

--- packages/trackerless-network/src/logic/utils.ts
+++ packages/trackerless-network/src/logic/utils.ts
@@ -1,9 +1,13 @@
 import { DuplicateMessageDetector, NumberPair } from "./DuplicateMessageDetector" 
 import { MessageRef } from "../proto/packages/trackerless-network/protos/NetworkRpc"
 
-export const markAndCheckDuplicate = (duplicateDetectors: Map<string, DuplicateMessageDetector>, currentMessageRef: MessageRef, previousMessageRef?: MessageRef): boolean => {
+export const markAndCheckDuplicate = (
+    duplicateDetectors: Map<string, DuplicateMessageDetector>,
+    currentMessageRef: MessageRef, 
+    previousMessageRef?: MessageRef
+): boolean => {
     const previousNumberPair = previousMessageRef ?
-    new NumberPair(Number(previousMessageRef!.timestamp), previousMessageRef!.sequenceNumber) : null
+        new NumberPair(Number(previousMessageRef!.timestamp), previousMessageRef!.sequenceNumber) : null
     const currentNumberPair = new NumberPair(Number(currentMessageRef.timestamp), currentMessageRef.sequenceNumber)
     if (!duplicateDetectors.has(currentMessageRef.messageChainId)) {
         duplicateDetectors.set(currentMessageRef.messageChainId, new DuplicateMessageDetector())
