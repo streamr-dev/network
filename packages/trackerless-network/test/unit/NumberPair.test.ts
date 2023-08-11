cdbf7287f460ea7fa10cc5f064ef461292f2a360
juslesan <santeri.juslenius@gmail.com>
2023-08-11 14:18:13 +0300

    move NumberPair test

--- packages/trackerless-network/test/unit/NumberPair.test.ts
+++ packages/trackerless-network/test/unit/NumberPair.test.ts
@@ -1,4 +1,4 @@
-import { NumberPair } from '../../trackerless-network/src/logic/DuplicateMessageDetector'
+import { NumberPair } from '../../src/logic/DuplicateMessageDetector'
 
 test('equalTo', () => {
     expect(new NumberPair(5, 2).equalTo(new NumberPair(5, 3))).toEqual(false)
