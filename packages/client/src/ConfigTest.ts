442efc1bd9cf35ff58e2246a715801f8016f323f
Teo Gebhard <teo@streamr.com>
2023-08-14 10:54:46 +0300

    update ConfigTest

--- packages/client/src/ConfigTest.ts
+++ packages/client/src/ConfigTest.ts
@@ -1,16 +1,21 @@
 import { toEthereumAddress } from '@streamr/utils'
 import { StreamrClientConfig } from './Config'
 import { MIN_KEY_LENGTH } from './encryption/RSAKeyPair'
+import { Chains } from '@streamr/config'
+
+const MAIN_CHAIN_CONFIG = Chains.load()['dev0']
+const SIDE_CHAIN_CONFIG = Chains.load()['dev1']
 
 function toNumber(value: any): number | undefined {
     return (value !== undefined) ? Number(value) : undefined
 }
 
 const sideChainConfig = {
-    name: 'streamr',
-    chainId: 8997,
+    name: 'streamr', // TODO from config?
+    chainId: SIDE_CHAIN_CONFIG.id,
     rpcs: [{
-        url: process.env.SIDECHAIN_URL || `http://${process.env.STREAMR_DOCKER_DEV_HOST || '127.0.0.1'}:8546`,
+        // TODO do we need "process.env.SIDECHAIN_URL || `http://${process.env.STREAMR_DOCKER_DEV_HOST" support?
+        url: process.env.SIDECHAIN_URL || SIDE_CHAIN_CONFIG.rpcEndpoints[0].url,
         timeout: toNumber(process.env.TEST_TIMEOUT) ?? 30 * 1000,
     }]
 }
@@ -39,14 +44,15 @@ export const CONFIG_TEST: StreamrClientConfig = {
         iceServers: []
     },
     contracts: {
-        streamRegistryChainAddress: '0x6cCdd5d866ea766f6DF5965aA98DeCCD629ff222',
-        streamStorageRegistryChainAddress: '0xd04af489677001444280366Dd0885B03dAaDe71D',
-        storageNodeRegistryChainAddress: '0x231b810D98702782963472e1D60a25496999E75D',    
+        streamRegistryChainAddress: SIDE_CHAIN_CONFIG.contracts.StreamRegistry,
+        streamStorageRegistryChainAddress: SIDE_CHAIN_CONFIG.contracts.StreamStorageRegistry,
+        storageNodeRegistryChainAddress: SIDE_CHAIN_CONFIG.contracts.StorageNodeRegistry,
         mainChainRPCs: {
-            name: 'dev_ethereum',
-            chainId: 8995,
+            name: 'dev_ethereum',  // TODO from config?
+            chainId: MAIN_CHAIN_CONFIG.id,
             rpcs: [{
-                url: process.env.ETHEREUM_SERVER_URL || `http://${process.env.STREAMR_DOCKER_DEV_HOST || '127.0.0.1'}:8545`,
+                // TODO do we need "process.env.ETHEREUM_SERVER_URL || `http://${process.env.STREAMR_DOCKER_DEV_HOST" support?
+                url: process.env.ETHEREUM_SERVER_URL || MAIN_CHAIN_CONFIG.rpcEndpoints[0].url,
                 timeout: toNumber(process.env.TEST_TIMEOUT) ?? 30 * 1000
             }]
         },
