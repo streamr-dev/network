0cb7021c518b368337af629600b31d0866ce5709
Teo Gebhard <teo@streamr.com>
2023-08-14 19:04:55 +0300

    use fastchain admin key

--- packages/broker/test/integration/plugins/operator/VoteOnSuspectNodeService.test.ts
+++ packages/broker/test/integration/plugins/operator/VoteOnSuspectNodeService.test.ts
@@ -15,8 +15,7 @@ import { setupOperatorContract } from './setupOperatorContract'
 const theGraphUrl = `http://${process.env.STREAMR_DOCKER_DEV_HOST ?? '10.200.10.1'}:8800/subgraphs/name/streamr-dev/network-subgraphs`
 
 const TIMEOUT = 1000 * 60 * 10
-const ADMIN_PRIV_KEY = "0x2cd9855d17e01ce041953829398af7e48b24ece04ff9d0e183414de54dc52285" // sidechain
-// const ADMIN_PRIV_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" // fastChain
+const ADMIN_PRIV_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
 const logger = new Logger(module)
 describe('VoteOnSuspectNodeService', () => {
