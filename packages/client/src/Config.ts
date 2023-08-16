3fe10bf44e03388bbf205690dace9b3de129ddfc
juslesan <santeri.juslenius@gmail.com>
2023-08-16 11:23:06 +0300

    update acceptProxyConnections doc

--- packages/client/src/Config.ts
+++ packages/client/src/Config.ts
@@ -111,6 +111,9 @@ export interface NetworkNodeConfig {
     /**
      * Whether to accept proxy connections. Enabling this option allows
      * this network node to act as proxy on behalf of other nodes / clients.
+     * When enabling this option, a WebSocket server should be configured for the client
+     * and the node needs to be in the open internet. The server can be started by setting
+     * the webSocketPort configuration to a free port in the network control layer configuration.
     */
     acceptProxyConnections?: boolean
 }
