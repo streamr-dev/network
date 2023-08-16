d470d7eaa2ed6485906bcfea0da4ae4609382409
Sami Gabor <samoila.gabor@yahoo.com>
2023-08-16 13:28:16 +0300

    refactor: use config contracts instead of deploying a new dev env and new contracts

--- packages/broker/test/integration/plugins/operator/MaintainOperatorValueService.test.ts
+++ packages/broker/test/integration/plugins/operator/MaintainOperatorValueService.test.ts
@@ -1,25 +1,22 @@
+import { Wallet } from "ethers"
 import { Contract } from "@ethersproject/contracts"
 import { Provider, JsonRpcProvider } from "@ethersproject/providers"
-// import { Provider } from "@ethersproject/providers"
 import { parseEther, formatEther } from "@ethersproject/units"
 
-// import { TestToken, Operator, StreamrEnvDeployer } from "@streamr/network-contracts"
-import { tokenABI, TestToken, Operator, StreamrEnvDeployer } from "@streamr/network-contracts"
+import { tokenABI, TestToken, Operator, streamRegistryABI, StreamRegistry } from "@streamr/network-contracts"
 import { Logger, toEthereumAddress, waitForCondition } from '@streamr/utils'
+import { config } from "@streamr/config"
 
 import { deployOperatorContract } from "./deployOperatorContract"
 import { deploySponsorship } from "./deploySponsorshipContract"
 import { ADMIN_WALLET_PK, generateWalletWithGasAndTokens } from "./smartContractUtils"
 
 import { MaintainOperatorValueService } from "../../../../src/plugins/operator/MaintainOperatorValueService"
-
-// TODO: use createClient once configs allow it. For now I'm creating the stream directly using the contract
-// import { createClient } from "../../../utils"
 import { STREAMR_DOCKER_DEV_HOST } from '../../../utils'
 
 const theGraphUrl = `http://${STREAMR_DOCKER_DEV_HOST}:8000/subgraphs/name/streamr-dev/network-subgraphs`
-
 const logger = new Logger(module)
+const fastChainConfig = config.dev2
 
 async function getTotalUnwithdrawnEarnings(operatorContract: Operator): Promise<bigint> {
     const { earnings } = await operatorContract.getEarningsFromSponsorships()
@@ -35,12 +32,11 @@ describe("MaintainOperatorValueService", () => {
     let provider: Provider
     let token: TestToken
     let streamId: string
-    let config: { contracts: Record<string, string> }
 
     const deployNewOperator = async () => {
-        const operatorWallet = await generateWalletWithGasAndTokens(provider, config)
+        const operatorWallet = await generateWalletWithGasAndTokens(provider, fastChainConfig)
         logger.debug("Deploying operator contract")
-        const operatorContract = await deployOperatorContract(config, operatorWallet, { operatorSharePercent: 10 })
+        const operatorContract = await deployOperatorContract(fastChainConfig, operatorWallet, { operatorSharePercent: 10 })
         logger.debug(`Operator deployed at ${operatorContract.address}`)
         const operatorConfig = {
             operatorContractAddress: toEthereumAddress(operatorContract.address),
@@ -54,12 +50,21 @@ describe("MaintainOperatorValueService", () => {
     }
 
     beforeAll(async () => {
-        const streamrEnvDeployer = new StreamrEnvDeployer(ADMIN_WALLET_PK, `http://${STREAMR_DOCKER_DEV_HOST}:8547`)
-        await streamrEnvDeployer.deployEnvironment()
-        const { contracts } = streamrEnvDeployer
-        config = { contracts: streamrEnvDeployer.addresses }
+        provider = new JsonRpcProvider(`http://${STREAMR_DOCKER_DEV_HOST}:8547`)
+        logger.debug("Connected to: ", await provider.getNetwork())
 
+        const adminWallet = new Wallet(ADMIN_WALLET_PK, provider)
+        const streamRegistry = new Contract(fastChainConfig.contracts.StreamRegistry, streamRegistryABI, adminWallet) as unknown as StreamRegistry
+        
         logger.debug("Creating stream for the test")
+        const createStreamReceipt = await (await streamRegistry.createStream(
+            `/operatorvaluewatchertest-${Date.now()}`,
+            '{"partitions":1}"}')
+        ).wait()
+        streamId = createStreamReceipt.events?.find((e) => e.event === "StreamCreated")?.args?.id
+        const streamExists = await streamRegistry.exists(streamId)
+        logger.debug("Stream created:", { streamId, streamExists })
+        // TODO: use createClient once configs allow it. For now I'm creating the stream directly using the contract
         // const client = createClient(ADMIN_WALLET_PK, {
         //     contracts: {
         //         streamRegistryChainAddress: contracts.streamRegistry.address,
@@ -68,32 +73,18 @@ describe("MaintainOperatorValueService", () => {
         // streamId = (await client.createStream(`/operatorvaluewatchertest-${Date.now()}`)).id
         // await client.destroy()
 
-        const createStreamReceipt = await (await contracts.streamRegistry.createStream(
-            `/operatorvaluewatchertest-${Date.now()}`,
-            '{"partitions":1}"}')
-        ).wait()
-        streamId = createStreamReceipt.events?.find((e) => e.event === "StreamCreated")?.args?.id
-
-        const streamExists = await contracts.streamRegistry.exists(streamId)
-        logger.debug("Stream created:", { streamId, streamExists })
-
-        // provider = getProvider()
-        provider = new JsonRpcProvider(`http://${STREAMR_DOCKER_DEV_HOST}:8547`)
-        logger.debug("Connected to: ", await provider.getNetwork())
-
-        token = new Contract(config.contracts.DATA, tokenABI) as unknown as TestToken
-        // token = getTokenContract()
+        token = new Contract(fastChainConfig.contracts.DATA, tokenABI) as unknown as TestToken
     }, 60 * 1000)
 
     it("withdraws sponsorship earnings when earnings are above the safe threshold", async () => {
         const { operatorWallet, operatorContract, operatorConfig } = await deployNewOperator()
 
-        const sponsorship1 = await deploySponsorship(config, operatorWallet, { streamId, earningsPerSecond: parseEther("1") })
+        const sponsorship1 = await deploySponsorship(fastChainConfig, operatorWallet, { streamId, earningsPerSecond: parseEther("1") })
         await (await token.connect(operatorWallet).transferAndCall(sponsorship1.address, parseEther("250"), "0x")).wait()
         await (await token.connect(operatorWallet).transferAndCall(operatorContract.address, parseEther("200"), operatorWallet.address)).wait()
         await (await operatorContract.stake(sponsorship1.address, parseEther("100"))).wait()
 
-        const sponsorship2 = await deploySponsorship(config, operatorWallet, { streamId, earningsPerSecond: parseEther("2") })
+        const sponsorship2 = await deploySponsorship(fastChainConfig, operatorWallet, { streamId, earningsPerSecond: parseEther("2") })
         await (await token.connect(operatorWallet).transferAndCall(sponsorship2.address, parseEther("250"), "0x")).wait()
         await (await operatorContract.stake(sponsorship2.address, parseEther("100"))).wait()
 
