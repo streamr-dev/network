3ba6841f74a07acdc9c4ddd2adf75efd260fc525
Eric Andrews <eric.andrews@streamr.com>
2023-08-18 13:37:12 +0300

    fix address iin OperatorPlugin.test.ts

--- packages/broker/test/integration/plugins/operator/OperatorPlugin.test.ts
+++ packages/broker/test/integration/plugins/operator/OperatorPlugin.test.ts
@@ -23,7 +23,7 @@ describe('OperatorPlugin', () => {
             privateKey: brokerWallet.privateKey,
             extraPlugins: {
                 operator: {
-                    operatorContractAddress: '0x4A5C0EC07F7ddBd4B6050181638e24b0153991b2'
+                    operatorContractAddress: '0x139dfa493a45364b598f2f98e504192819082c85'
                 }
             }
         })
