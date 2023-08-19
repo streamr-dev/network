87730cb8889a42d2d4f64110537b1abf99878d29
Sami Gabor <45082017+samigabor@users.noreply.github.com>
2023-08-19 16:27:32 +0300

    Update packages/broker/src/plugins/operator/OperatorValueBreachWatcher.ts
    Co-authored-by: Eric Andrews <eric.andrews@streamr.com>

--- packages/broker/src/plugins/operator/OperatorValueBreachWatcher.ts
+++ packages/broker/src/plugins/operator/OperatorValueBreachWatcher.ts
@@ -29,7 +29,7 @@ export class OperatorValueBreachWatcher {
         )
     }
 
-    async checkRandomUnwithdrawnEarnings(): Promise<void> {
+    private async checkRandomUnwithdrawnEarnings(): Promise<void> {
         const randomOperatorAddress = await this.helper.getRandomOperator()
         if (randomOperatorAddress === undefined) {
             logger.info('No operators found')
