ea656b523f805a06e042d19d381a66a79d735b24
Eric Andrews <eric.andrews@streamr.com>
2023-08-22 15:39:58 +0300

    refactor: remove unneeded code from protocol (#1761)
    ## Summary
    Continuing from Santeri's work here
    https://github.com/streamr-dev/network/pull/1756 remove some more
    unneeded stuff from protocol package.
    
    *Note*: broker plugins no longer support version parameter for
    outputting raw stream messages.
    
    ---------
    
    Co-authored-by: Teo Gebhard <teo@streamr.com>

--- packages/protocol/src/utils/exports.ts
+++ packages/protocol/src/utils/exports.ts
@@ -1,13 +1,9 @@
-import { createTrackerRegistry, TrackerRegistry, TrackerRegistryRecord } from './TrackerRegistry'
 import { StreamID, toStreamID, StreamIDUtils } from './StreamID'
 import { MAX_PARTITION_COUNT, ensureValidStreamPartitionCount } from './partition'
 import { StreamPartID, toStreamPartID, StreamPartIDUtils } from './StreamPartID'
 import { ProxyDirection } from './types'
 
 export {
-    TrackerRegistryRecord,
-    TrackerRegistry,
-    createTrackerRegistry,
     toStreamID,
     toStreamPartID,
     StreamID,
--- packages/protocol/test/unit/protocol/message_layer/streamMessageSerialization.test.ts
+++ packages/protocol/test/unit/protocol/message_layer/streamMessageSerialization.test.ts
@@ -4,11 +4,9 @@ import ValidationError from '../../../../src/errors/ValidationError'
 import EncryptedGroupKey from '../../../../src/protocol/message_layer/EncryptedGroupKey'
 import MessageID from '../../../../src/protocol/message_layer/MessageID'
 import MessageRef from '../../../../src/protocol/message_layer/MessageRef'
-import StreamMessage, { ContentType, EncryptionType, StreamMessageType } from '../../../../src/protocol/message_layer/StreamMessage'
-import { SIGNATURE_TYPE_ETH } from '../../../../src/protocol/message_layer/StreamMessageSerializerV32'
+import StreamMessage, { ContentType, EncryptionType, StreamMessageType, VERSION } from '../../../../src/protocol/message_layer/StreamMessage'
 import { toStreamID } from '../../../../src/utils/StreamID'
-
-const VERSION = 32
+import { SIGNATURE_TYPE_ETH } from '../../../../src/protocol/message_layer/streamMessageSerialization'
 
 const PUBLISHER_ID = toEthereumAddress('0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
 
@@ -38,7 +36,7 @@ const serializedMessage = JSON.stringify([
     'signature'
 ])
 
-describe('StreamMessageSerializerV32', () => {
+describe('streamMessageSerialization', () => {
 
     describe('deserialize', () => {
 
@@ -67,7 +65,7 @@ describe('StreamMessageSerializerV32', () => {
     describe('serialize', () => {
 
         it('correctly serializes messages', () => {
-            assert.deepStrictEqual(message.serialize(VERSION), serializedMessage)
+            assert.deepStrictEqual(message.serialize(), serializedMessage)
         })
     })
 })
