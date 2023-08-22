2a122dbfc0e9b94ac28f0b7f76e27826bf7c797b
Sami Gabor <samoila.gabor@yahoo.com>
2023-08-22 17:16:00 +0300

    docs: remove comments describing internal details from start

--- packages/broker/test/integration/plugins/operator/MaintainOperatorValueService.test.ts
+++ packages/broker/test/integration/plugins/operator/MaintainOperatorValueService.test.ts
@@ -61,9 +61,6 @@ describe('MaintainOperatorValueService', () => {
 
         const poolValueBeforeWithdraw = await operatorContract.getApproximatePoolValue()
 
-        // 5 DATA = 50% of 10 DATA, 50% is the "safe threshold" (we don't want to wait all the way to the limit, lest we be late)
-        // 10 DATA = 10% of 100 DATA, 10% is the penalty limit (sum of unwithdrawn earnings may not exceed it)
-        // 100 DATA = pool value
         await service.start()
 
         // wait until we see the withdraw happened: first we go above a sum (that must be < safe threshold), then below
