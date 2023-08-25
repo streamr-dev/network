a18894337df890ccfc2b5ed282bffa9c1f4fda13
Teo Gebhard <teo@streamr.com>
2023-08-25 16:30:10 +0300

    rm StreamEntryPoint

--- packages/trackerless-network/src/proto/packages/trackerless-network/protos/NetworkRpc.ts
+++ packages/trackerless-network/src/proto/packages/trackerless-network/protos/NetworkRpc.ts
@@ -241,19 +241,6 @@ export interface NeighborUpdate {
     neighborDescriptors: PeerDescriptor[];
 }
 /**
- * @generated from protobuf message StreamEntryPoint
- */
-export interface StreamEntryPoint {
-    /**
-     * @generated from protobuf field: dht.PeerDescriptor peerDescriptor = 1;
-     */
-    peerDescriptor?: PeerDescriptor;
-    /**
-     * @generated from protobuf field: bool firstToJoin = 2;
-     */
-    firstToJoin: boolean;
-}
-/**
  * @generated from protobuf message ProxyConnectionRequest
  */
 export interface ProxyConnectionRequest {
@@ -547,19 +534,6 @@ class NeighborUpdate$Type extends MessageType<NeighborUpdate> {
  * @generated MessageType for protobuf message NeighborUpdate
  */
 export const NeighborUpdate = new NeighborUpdate$Type();
-// @generated message type with reflection information, may provide speed optimized methods
-class StreamEntryPoint$Type extends MessageType<StreamEntryPoint> {
-    constructor() {
-        super("StreamEntryPoint", [
-            { no: 1, name: "peerDescriptor", kind: "message", T: () => PeerDescriptor },
-            { no: 2, name: "firstToJoin", kind: "scalar", T: 8 /*ScalarType.BOOL*/ }
-        ]);
-    }
-}
-/**
- * @generated MessageType for protobuf message StreamEntryPoint
- */
-export const StreamEntryPoint = new StreamEntryPoint$Type();
 // @generated message type with reflection information, may provide speed optimized methods
 class ProxyConnectionRequest$Type extends MessageType<ProxyConnectionRequest> {
     constructor() {
