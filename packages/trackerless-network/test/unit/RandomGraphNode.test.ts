9f96144a3ace539f9d23ec60dee4fe1a89650f89
Teo Gebhard <teo@streamr.com>
2023-09-05 12:54:07 +0300

    refactor(trackerless-network): Type-safe node ID (#1806)
    We now use `NodeID` annotation instead of `string` in all places where `trackerless-network` operates on node ids. 
    
    Also unified naming: in `trackerless-network` we handle _nodes_, not _peers_ (that is a `dht` level concept).
    - An exception to that rule is the `PeerDescriptor`: didn't rename that to `NodeDescriptor` as it refers to the `dht` level entity
    
    ## Changes
    
    - Introduced `getNodeIdFromPeerDescriptor` helper method which is just a call to `keyFromPeerDescriptor` but casts the return type to `NodeID`
    - Menamed some classes, methods, fields and variables from `peer*` to `node*`, e.g.:
      - class `PeerList` -> `NodeList` (and some methods of the class)
      - methods of `ProxyStreamConnectionClient`, `ProxyStreamConnectionServer` and `TemporaryConnectionServer`
    - Renamed all `stringId` -> `nodeId`
      - remove obsolete `NetworkNode#getNodeStringId `and `StreamrNode#getNodeStringId`
    - Also using `NodeID` annotation in `client` in `broker` (at least most of the places)
     
    ## Notes
    
    - `NodeIDs` are actually byte arrays, but in practice it is convenient that we handle them as strings (e.g. to use as map keys). In that sense it is very similar to e.g. how we handle `EthereumAddress` objects.
    
    ## Future improvements
    
    - Use `bytes` type for node id in the protobuf definitions (as we decided that it is just a hex-representation of byte array)
    - Maybe we don't need `PeerID` class in `trackerless-network` node if get the node id as string in that level
    - The contact term is also from `dht` level, maybe should use `node` or `neighbor` instead?
    - Maybe could simplify some naming, e.g. `targetNeighbor` could be just a `neighbor`? 
    - Maybe could shorten the `randomGraphId` to `graphId` (or maybe a separate term, which tells that this is a the id in the stream delivery network: e.g. `streamrDeliveryId` or `deliveryId`?)

--- packages/trackerless-network/test/unit/RandomGraphNode.test.ts
+++ packages/trackerless-network/test/unit/RandomGraphNode.test.ts
@@ -1,13 +1,14 @@
 import { RandomGraphNode } from '../../src/logic/RandomGraphNode'
-import { keyFromPeerDescriptor, PeerDescriptor, PeerID, peerIdFromPeerDescriptor } from '@streamr/dht'
+import { PeerDescriptor, PeerID, peerIdFromPeerDescriptor } from '@streamr/dht'
 import { MockTransport } from '../utils/mock/Transport'
-import { createMockRemotePeer, mockConnectionLocker } from '../utils/utils'
+import { createMockRemoteNode, mockConnectionLocker } from '../utils/utils'
 import { createRandomGraphNode } from '../../src/logic/createRandomGraphNode'
-import { PeerList } from '../../src/logic/PeerList'
+import { NodeList } from '../../src/logic/NodeList'
 import { MockHandshaker } from '../utils/mock/MockHandshaker'
 import { MockNeighborUpdateManager } from '../utils/mock/MockNeighborUpdateManager'
 import { MockNeighborFinder } from '../utils/mock/MockNeighborFinder'
 import { mockLayer1 } from '../utils/mock/MockLayer1'
+import { getNodeIdFromPeerDescriptor } from '../../src/identifiers'
 
 describe('RandomGraphNode', () => {
 
@@ -17,16 +18,16 @@ describe('RandomGraphNode', () => {
         type: 0
     }
 
-    let targetNeighbors: PeerList
-    let nearbyContactPool: PeerList
-    let randomContactPool: PeerList
+    let targetNeighbors: NodeList
+    let nearbyContactPool: NodeList
+    let randomContactPool: NodeList
 
     beforeEach(async () => {
         const peerId = peerIdFromPeerDescriptor(peerDescriptor)
 
-        targetNeighbors = new PeerList(peerId, 10)
-        randomContactPool = new PeerList(peerId, 10)
-        nearbyContactPool = new PeerList(peerId, 10)
+        targetNeighbors = new NodeList(peerId, 10)
+        randomContactPool = new NodeList(peerId, 10)
+        nearbyContactPool = new NodeList(peerId, 10)
 
         randomGraphNode = createRandomGraphNode({
             targetNeighbors,
@@ -48,26 +49,26 @@ describe('RandomGraphNode', () => {
         await randomGraphNode.stop()
     })
 
-    it('getTargetNeighborStringIds', () => {
-        const mockRemote = createMockRemotePeer()
+    it('getTargetNeighborIds', () => {
+        const mockRemote = createMockRemoteNode()
         targetNeighbors.add(mockRemote)
-        const ids = randomGraphNode.getTargetNeighborStringIds()
-        expect(ids[0]).toEqual(keyFromPeerDescriptor(mockRemote.getPeerDescriptor()))
+        const ids = randomGraphNode.getTargetNeighborIds()
+        expect(ids[0]).toEqual(getNodeIdFromPeerDescriptor(mockRemote.getPeerDescriptor()))
         targetNeighbors.remove(mockRemote.getPeerDescriptor())
     })
 
     it('getNearbyContactPoolIds', () => {
-        const mockRemote = createMockRemotePeer()
+        const mockRemote = createMockRemoteNode()
         nearbyContactPool.add(mockRemote)
         const ids = randomGraphNode.getNearbyContactPoolIds()
-        expect(ids[0]).toEqual(keyFromPeerDescriptor(mockRemote.getPeerDescriptor()))
+        expect(ids[0]).toEqual(getNodeIdFromPeerDescriptor(mockRemote.getPeerDescriptor()))
     })
 
     it('getRandomContactPoolIds', () => {
-        const mockRemote = createMockRemotePeer()
+        const mockRemote = createMockRemoteNode()
         randomContactPool.add(mockRemote)
         const ids = randomGraphNode.getRandomContactPoolIds()
-        expect(ids[0]).toEqual(keyFromPeerDescriptor(mockRemote.getPeerDescriptor()))
+        expect(ids[0]).toEqual(getNodeIdFromPeerDescriptor(mockRemote.getPeerDescriptor()))
     })
 
 })
