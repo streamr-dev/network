73873f34cd6be422e5849dee6c28ea716f29ecc4
juslesan <santeri.juslenius@gmail.com>
2023-09-06 14:24:15 +0300

    signature handling in tests

--- packages/client/test/unit/Authentication.test.ts
+++ packages/client/test/unit/Authentication.test.ts
@@ -1,5 +1,5 @@
 import { createPrivateKeyAuthentication } from '../../src/Authentication'
-import { binaryToHex } from '@streamr/utils'
+import { hexToBinary } from '@streamr/utils'
 
 const PRIVATE_KEY = '0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709'
 
@@ -11,8 +11,8 @@ describe('Authentication', () => {
             const payload = 'data-to-sign'
             const authentication = createPrivateKeyAuthentication(PRIVATE_KEY, undefined as any)
             const signature = await authentication.createMessageSignature(payload)
-            expect(binaryToHex(signature, true)).toEqual('0x084b3ac0f2ad17d387ca5bbf5d72d8f1dfd1b372e399ce6b0bfc60793e'
-                + 'b717d2431e498294f202d8dfd9f56158391d453c018470aea92ed6a80a23c20ab6f7ac1b')
+            expect(signature).toStrictEqual(hexToBinary('0x084b3ac0f2ad17d387ca5bbf5d72d8f1dfd1b372e399ce6b0bfc60793e'
+                + 'b717d2431e498294f202d8dfd9f56158391d453c018470aea92ed6a80a23c20ab6f7ac1b'))
         })
     })
 })
