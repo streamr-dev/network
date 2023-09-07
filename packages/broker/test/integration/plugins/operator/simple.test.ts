65e885a450f9078ed65328dad462ff62fa34c762
Teo Gebhard <teo@streamr.com>
2023-09-07 10:37:29 +0300

    2.7 tokens earned fails

--- packages/broker/test/integration/plugins/operator/simple.test.ts
+++ packages/broker/test/integration/plugins/operator/simple.test.ts
@@ -3,18 +3,16 @@ import { delegate, deploySponsorshipContract, generateWalletWithGasAndTokens, se
 import { createClient, createTestStream } from '../../../utils'
 import { fetchPrivateKeyWithGas } from '@streamr/test-utils'
 import { wait, waitForCondition } from '@streamr/utils'
-import { MaintainOperatorPoolValueHelper } from '../../../../src/plugins/operator/MaintainOperatorPoolValueHelper'
-import { getTotalUnwithdrawnEarnings } from './operatorPoolValueUtils'
 
 const STAKE_AMOUNT = 100
+const MIN_EARNINGS = 2
 const ONE_ETHER = 1e18
-const SAFETY_FRACTION = 0.5
 
 it('simple test', async () => {
     const client = createClient(await fetchPrivateKeyWithGas())
     const streamId = (await createTestStream(client, module)).id
     await client.destroy()
-    const { operatorWallet, operatorContract, operatorServiceConfig, nodeWallets } = await setupOperatorContract({
+    const { operatorWallet, operatorContract, nodeWallets } = await setupOperatorContract({
         nodeCount: 1,
         operatorConfig: {
             operatorsCutPercent: 10
@@ -25,16 +23,12 @@ it('simple test', async () => {
     await sponsor(sponsorer, sponsorship.address, 250)
     await delegate(operatorWallet, operatorContract.address, STAKE_AMOUNT)
     await stake(operatorContract, sponsorship.address, STAKE_AMOUNT)
-    const helper = new MaintainOperatorPoolValueHelper({ ...operatorServiceConfig, signer: nodeWallets[0] })
-    const driftLimitFraction = await helper.getDriftLimitFraction()
-    const driftLimit = STAKE_AMOUNT * Number(driftLimitFraction) / ONE_ETHER
-    const safeDriftLimit = driftLimit * SAFETY_FRACTION
-    console.log('Safety fraction ' + SAFETY_FRACTION)
+    console.log('Min earnings ' + MIN_EARNINGS)
     console.log('Poll for earnings')
     await waitForCondition(async () => {
-        const unwithdrawnEarnings = Number(await getTotalUnwithdrawnEarnings(operatorContract)) / ONE_ETHER
-        console.log('Earnings: ' + unwithdrawnEarnings)
-        return unwithdrawnEarnings > safeDriftLimit
+        const earnings = Number(await operatorContract.getEarningsFromSponsorship(sponsorship.address)) / ONE_ETHER
+        console.log('Earnings: ' + earnings)
+        return earnings >= MIN_EARNINGS
     }, 10000, 1000)
 
     console.log('Withdraw')
