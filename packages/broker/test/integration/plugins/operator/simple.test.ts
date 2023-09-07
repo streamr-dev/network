c30e882dacec3b9439d9b37f9d791a9c0c5f5708
Teo Gebhard <teo@streamr.com>
2023-09-07 11:37:57 +0300

    debug

--- packages/broker/test/integration/plugins/operator/simple.test.ts
+++ packages/broker/test/integration/plugins/operator/simple.test.ts
@@ -1,8 +1,12 @@
+/* eslint-disable */
 import { parseEther } from 'ethers/lib/utils'
 import { delegate, deploySponsorshipContract, generateWalletWithGasAndTokens, setupOperatorContract, sponsor, stake } from './contractUtils'
 import { createClient, createTestStream } from '../../../utils'
 import { fetchPrivateKeyWithGas } from '@streamr/test-utils'
 import { wait, waitForCondition } from '@streamr/utils'
+import { StreamrConfig, streamrConfigABI } from '@streamr/network-contracts'
+import { config as CHAIN_CONFIG } from '@streamr/config'
+import { Contract } from '@ethersproject/contracts'
 
 const STAKE_AMOUNT = 100
 const MIN_EARNINGS = 2
@@ -23,6 +27,13 @@ it('simple test', async () => {
     await sponsor(sponsorer, sponsorship.address, 250)
     await delegate(operatorWallet, operatorContract.address, STAKE_AMOUNT)
     await stake(operatorContract, sponsorship.address, STAKE_AMOUNT)
+
+    const cut = await operatorContract.operatorsCutFraction()
+    const cfg = new Contract(CHAIN_CONFIG.dev2.contracts.StreamrConfig, streamrConfigABI).connect(operatorWallet) as StreamrConfig
+    const penaltyFraction = await cfg.poolValueDriftPenaltyFraction()
+    console.log('Cut: ' + cut)
+    console.log('Penalty fraction: ' + penaltyFraction)
+    
     console.log('Min earnings ' + MIN_EARNINGS)
     console.log('Poll for earnings')
     await waitForCondition(async () => {
