1fd75b48268969da91be72595de878468ad74c80
Teo Gebhard <teo@streamr.com>
2023-09-07 16:30:00 +0300

    refactor(trackerless-network): User id as Ethereum address (#1830)
    Handle the user id field similarly to other id fields: internally it is a string, so that we can use it e.g. as a map key. Before this PR it was stored as a byte array.
    
    The protocol doesn't change: there it is transferred as a byte array, similarly to `NodeID` and others.
    
    The user id is now annotated as `EthereumAddress` as it is that kind of string in practice. It would be possible to have it just as a branded string if we'd want to be more abstract about the content of the strings. (At `trackerless-network` level we don't care what kind of bytes it contains, it is just an ID of a user).

--- packages/client/src/NetworkNodeFacade.ts
+++ packages/client/src/NetworkNodeFacade.ts
@@ -3,12 +3,12 @@
  */
 import { PeerDescriptor } from '@streamr/dht'
 import { StreamMessage, StreamPartID } from '@streamr/protocol'
-import { NetworkNode, NetworkOptions, ProxyDirection, UserID, NodeID } from '@streamr/trackerless-network'
+import { NetworkNode, NetworkOptions, NodeID, ProxyDirection } from '@streamr/trackerless-network'
 import { EthereumAddress, MetricsContext } from '@streamr/utils'
-import { inject, Lifecycle, scoped } from 'tsyringe'
 import EventEmitter from 'eventemitter3'
+import { Lifecycle, inject, scoped } from 'tsyringe'
 import { Authentication, AuthenticationInjectionToken } from './Authentication'
-import { ConfigInjectionToken, StrictStreamrClientConfig, NetworkPeerDescriptor } from './Config'
+import { ConfigInjectionToken, NetworkPeerDescriptor, StrictStreamrClientConfig } from './Config'
 import { DestroySignal } from './DestroySignal'
 import { pOnce } from './utils/promises'
 import { peerDescriptorTranslator } from './utils/utils'
@@ -44,7 +44,7 @@ export interface NetworkNodeStub {
         streamPartId: StreamPartID,
         peerDescriptors: PeerDescriptor[],
         direction: ProxyDirection,
-        userId: UserID,
+        userId: EthereumAddress,
         connectionCount?: number
     ) => Promise<void>
     setStreamPartEntryPoints: (streamPartId: StreamPartID, peerDescriptors: PeerDescriptor[]) => void
@@ -52,10 +52,6 @@ export interface NetworkNodeStub {
 
 export interface Events {
     start: () => void
-}
-
-export const toUserID = (address: EthereumAddress): UserID => {
-    return Buffer.from(address.slice(2), 'hex') as UserID
 }
 
 /**
@@ -224,7 +220,7 @@ export class NetworkNodeFacade {
             streamPartId,
             peerDescriptors,
             direction,
-            toUserID(await this.authentication.getAddress()),
+            await this.authentication.getAddress(),
             connectionCount
         )
     }
