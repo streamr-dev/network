47b726e5c6d47bd3c56d18710612f3b5a208cb4f
Teo Gebhard <teo@streamr.com>
2023-09-07 10:23:05 +0300

    0.6 passes

--- packages/broker/test/integration/plugins/operator/simple.test.ts
+++ packages/broker/test/integration/plugins/operator/simple.test.ts
@@ -8,7 +8,7 @@ import { getTotalUnwithdrawnEarnings } from './operatorPoolValueUtils'
 
 const STAKE_AMOUNT = 100
 const ONE_ETHER = 1e18
-const SAFETY_FRACTION = 0.5
+const SAFETY_FRACTION = 0.6
 
 it('simple test', async () => {
     const client = createClient(await fetchPrivateKeyWithGas())
