24f2e9b5d7bef672ce39b05e9b47cc0ae5e6eb36
Teo Gebhard <teo@streamr.com>
2023-09-13 11:05:21 +0300

    update test

--- packages/broker/test/unit/config.test.ts
+++ packages/broker/test/unit/config.test.ts
@@ -26,7 +26,6 @@ describe('overrideConfigToEnvVarsIfGiven', () => {
         process.env.STREAMR__BROKER__CLIENT__ORDER_MESSAGES = 'true'
         process.env.STREAMR__BROKER__CLIENT__GAP_FILL = 'false'
         process.env.STREAMR__BROKER__CLIENT__NETWORK__CONTROL_LAYER__PEER_DESCRIPTOR__ID = 'kademliaID'
-        process.env.STREAMR__BROKER__CLIENT__NETWORK__CONTROL_LAYER__PEER_DESCRIPTOR__TYPE = '0'
         process.env.STREAMR__BROKER__AUTHENTICATION__KEYS_1 = 'key-1'
         process.env.STREAMR__BROKER__AUTHENTICATION__KEYS_2 = 'key-2'
         overrideConfigToEnvVarsIfGiven(config)
