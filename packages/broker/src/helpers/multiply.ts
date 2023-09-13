409fd4d3ab29e048ae644001f29f7c57b0f150d9
Teo Gebhard <teo@streamr.com>
2023-09-13 09:31:57 +0300

    fix typo

--- packages/broker/src/helpers/multiply.ts
+++ packages/broker/src/helpers/multiply.ts
@@ -1,6 +1,6 @@
-// this precission is big enough as we typically handle bigints which are in weis (the smallest denomination of Ethereum)
-const PRECISSION = 1e18
+// this precision is big enough as we typically handle bigints which are in weis (the smallest denomination of Ethereum)
+const PRECISION = 1e18
 
 export const multiply = (val1: bigint, val2: number): bigint => {
-    return val1 * BigInt(PRECISSION * val2) / BigInt(PRECISSION)
+    return val1 * BigInt(PRECISION * val2) / BigInt(PRECISION)
 }
