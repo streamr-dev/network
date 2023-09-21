f4d32e51b4629147101b0c32cb59a7def8e9fec7
Teo Gebhard <teo@streamr.com>
2023-09-21 14:05:48 +0300

    read peer descriptor from config

--- packages/broker/bin/entry-point.ts
+++ packages/broker/bin/entry-point.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
+import { config as CHAIN_CONFIG } from '@streamr/config'
 import { DhtNode, NodeType } from '@streamr/dht'
 import { hexToBinary } from '@streamr/utils'
+import omit from 'lodash/omit'
 
 const main = async () => {
-    const peerDescriptor = {
-        kademliaId: hexToBinary('eeeeeeeeee'),
-        type: NodeType.NODEJS,
-        websocket: {
-            host: '127.0.0.1',
-            port: 40500,
-            tls: false
+    const peerDescriptor = CHAIN_CONFIG.dev2.entryPoints.map((entryPoint) => {
+        return {
+            kademliaId: hexToBinary(entryPoint.id),
+            type: NodeType.NODEJS,  // TODO remove this when NET-1070 done
+            ...omit(entryPoint, 'id')
         }
-    }
+    })[0]
     const dhtNode = new DhtNode({
         peerDescriptor,
         entryPoints: [peerDescriptor]
