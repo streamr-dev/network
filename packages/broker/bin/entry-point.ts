c1637945ac0684bb23c106cdc9c63d69df59f93c
Teo Gebhard <teo@streamr.com>
2023-09-26 13:51:27 +0300

    rm debug code

--- packages/broker/bin/entry-point.ts
+++ packages/broker/bin/entry-point.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
-//import { config as CHAIN_CONFIG } from '@streamr/config'
 import { DhtNode, NodeType } from '@streamr/dht'
 import { hexToBinary } from '@streamr/utils'
 import { CONFIG_TEST } from 'streamr-client'
 import omit from 'lodash/omit'
 
 const main = async () => {
-    const hostOverride = process.argv[2]
     const entryPoint = CONFIG_TEST.network!.controlLayer!.entryPoints![0]
     const peerDescriptor = {
         ...omit(entryPoint, 'id'),
@@ -14,11 +12,8 @@ const main = async () => {
         type: NodeType.NODEJS,  // TODO remove this when NET-1070 done
         websocket: {
             ...entryPoint.websocket!,
-            host: hostOverride ?? entryPoint.websocket!.host
         }
     }
-    // eslint-disable-next-line no-console
-    console.log('DEBUG: ' + JSON.stringify(peerDescriptor))
     const dhtNode = new DhtNode({
         peerId: entryPoint.id,
         websocketHost: entryPoint.websocket!.host,
