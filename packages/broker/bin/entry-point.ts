d461b712390bd26297c0ae5ac6d13e8b71b8f352
Teo Gebhard <teo@streamr.com>
2023-09-26 13:49:13 +0300

    read entryPoint from CONFIG_TEST

--- packages/broker/bin/entry-point.ts
+++ packages/broker/bin/entry-point.ts
@@ -2,39 +2,29 @@
 //import { config as CHAIN_CONFIG } from '@streamr/config'
 import { DhtNode, NodeType } from '@streamr/dht'
 import { hexToBinary } from '@streamr/utils'
+import { CONFIG_TEST } from 'streamr-client'
 import omit from 'lodash/omit'
 
 const main = async () => {
     const hostOverride = process.argv[2]
-    const tmp = [ // TODO CHAIN_CONFIG.dev2.entryPoints
-        {
-            id: 'eeeeeeeeee',
-            websocket: {
-                host: '10.200.10.1',
-                port: 40500,
-                tls: false
-            }
+    const entryPoint = CONFIG_TEST.network!.controlLayer!.entryPoints![0]
+    const peerDescriptor = {
+        ...omit(entryPoint, 'id'),
+        kademliaId: hexToBinary(entryPoint.id),
+        type: NodeType.NODEJS,  // TODO remove this when NET-1070 done
+        websocket: {
+            ...entryPoint.websocket!,
+            host: hostOverride ?? entryPoint.websocket!.host
         }
-    ]
-    const peerDescriptor = tmp.map((entryPoint) => {
-        return {
-            ...omit(entryPoint, 'id'),
-            kademliaId: hexToBinary(entryPoint.id),
-            type: NodeType.NODEJS,  // TODO remove this when NET-1070 done
-            websocket: {
-                ...entryPoint.websocket,
-                host: hostOverride ?? entryPoint.websocket!.host
-            },
-        }
-    })[0]
+    }
     // eslint-disable-next-line no-console
     console.log('DEBUG: ' + JSON.stringify(peerDescriptor))
     const dhtNode = new DhtNode({
-        peerId: tmp[0].id,
-        websocketHost: tmp[0].websocket.host,
+        peerId: entryPoint.id,
+        websocketHost: entryPoint.websocket!.host,
         websocketPortRange: {
-            min: tmp[0].websocket.port,
-            max: tmp[0].websocket.port
+            min: entryPoint.websocket!.port,
+            max: entryPoint.websocket!.port
         },
         entryPoints: [peerDescriptor]
     })
