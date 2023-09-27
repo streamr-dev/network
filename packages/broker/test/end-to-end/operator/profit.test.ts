d1a576b15cf788a3e764fca5a642e7378298f63a
Teo Gebhard <teo@streamr.com>
2023-09-28 00:46:46 +0300

    update test

--- packages/broker/test/end-to-end/operator/profit.test.ts
+++ packages/broker/test/end-to-end/operator/profit.test.ts
@@ -16,52 +16,68 @@ import {
     undelegate,
     unstake
 } from '../../integration/plugins/operator/contractUtils'
-import { createClient, createTestStream, startBroker } from '../../utils'
+import { createClient, createTestStream } from '../../utils'
 
 /*
  * Given:
  * - one sponsorship
- *   - it generates 20 DATA of earnings per second
+ *   - has 600 DATA sponsored
+ *   - it generates 100 DATA of earnings per second
  * - one operator who mines the sponsorship by running one node
  *   - it has operator’s cut of 10%
  * - one external delegator
  *
  * When:
- * - the operator self-delegates 123 DATA
- * - the external delegator delegates 200 DATA
- * - operator stakes all delegated tokens (i.e. 300 DATA)
- * - operator runs the operator node until the sponsorship no longer funded (~30 seconds)
- * - operator unstakes all delegated tokens
- * - operator and the external delegator undelegate all tokens
- *
- * - protocol fee is 5% of 600 DATA = 30 DATA
- * - operator's cut is 10% of the remaining 570 = 57 DATA
- *   - the operator has automatically self-delegated that amount
- *   - therefore the stakes after that are: operator 123+57=180 DATA, external delegator 200 DATA
- * - their share of the 570 DATA profit is:
- *   - operator: 180/380*570 = 270 DATA
- *   - extrenal delegator: 200/380*570 = 300 DATA
+ * - the operator self-delegates 500 DATA
+ * - the delegator delegates 526 DATA
+ * - operator stakes all delegated tokens (i.e. 1026 DATA)
+ * - operator runs the operator node until the sponsorship no longer funded (~6 seconds)
  *
  * Then:
+ * - the operator and the delegator has earned some profit:
+ *   - all 600 DATA from the sponsorship, excl.protocol fee 30 DATA = 570 DATA
+ *   - operator's cut is 57 DATA
+ *   - operator token's worth is delegations + profit, excl. operator's cut = 500+526+570-57=1539 DATA
+ *     - i.e. the exchange rate is token's worth divide by the delegated DATA = 1539/1026=1.5
+ *   - that worth is allocated to the operator and delegator propotionally to their delegations:
+ *     - operator owns 500*1.5=750 DATA
+ *     - delegator owns 526*1.5=789 DATA
+ *   - operator auto-delegates the operator's cut of 57 DATA by using the current exchange rate of 1.5
+ *     - operator's stake increases by 57/1.5=38 operator tokens
+ *     - operator owns (500+38)*1.5=807 DATA (i.e. 570+57)
+ *
+ * Then:
+ * - the operator unstakes all delegated tokens
+ * - the operator and the external delegator undelegate all tokens
  * - both the operator and the external delegator receive their staked DATA plus profits to their wallets
- *   - operator has 270+123=393 DATA
- *   - external delegator has 300+200=500 DATA
+ *   - operator receive 807 DATA, i.e. profit is 807-500=307 DATA
+ *   - external receive 789 DATA, i.e. profit is 789-526=263 DATA
  * - protocol fee of 3 DATA is transferred to the admin wallet
  */
 
 const SPONSOR_AMOUNT = 600
-const SELF_DELEGATED_AMOUNT = 123
-const EXTERNAL_DELEGATED_AMOUNT = 200
-const EARNINGS_PER_SECOND = 20
+const OPERATOR_DELEGATED_AMOUNT = 500
+const EXTERNAL_DELEGATED_AMOUNT = 526
+const EARNINGS_PER_SECOND = 100
 const OPERATORS_CUT_PERCENTAGE = 10
 const PROTOCOL_FEE_PERCENTAGE = 5 // TODO from config?
+const PROTOCOL_FEE = SPONSOR_AMOUNT * (PROTOCOL_FEE_PERCENTAGE / 100)
+const TOTAL_PROFIT = SPONSOR_AMOUNT - PROTOCOL_FEE
+const OPERATORS_CUT = TOTAL_PROFIT * (OPERATORS_CUT_PERCENTAGE / 100)
+const OPERATOR_TOKEN_WORTH_AFTER_MINING = OPERATOR_DELEGATED_AMOUNT + EXTERNAL_DELEGATED_AMOUNT + TOTAL_PROFIT - OPERATORS_CUT
+const EXCHANGE_RATE_AFTER_MINING = OPERATOR_TOKEN_WORTH_AFTER_MINING / (OPERATOR_DELEGATED_AMOUNT + EXTERNAL_DELEGATED_AMOUNT)
+const OPERATOR_OWNERSHIP_WORTH = OPERATOR_DELEGATED_AMOUNT * EXCHANGE_RATE_AFTER_MINING + OPERATORS_CUT
+const DELEGTOR_OWNERSHIP_WORTH = EXTERNAL_DELEGATED_AMOUNT * EXCHANGE_RATE_AFTER_MINING
+const OPERATOR_PROFIT = OPERATOR_OWNERSHIP_WORTH - OPERATOR_DELEGATED_AMOUNT
+const DELEGATOR_PROFIT = DELEGTOR_OWNERSHIP_WORTH - EXTERNAL_DELEGATED_AMOUNT
 
 describe('profit', () => {
 
     let operatorWallet: Wallet
     let delegatorWallet: Wallet
     let sponsorWallet: Wallet
-    let operatorNodeWallet: Wallet
+    // eslint-disable-next-line no-underscore-dangle
+    let _operatorNodeWallet: Wallet
     let operatorContract: Operator
     let sponsorshipContract: Sponsorship
 
@@ -77,7 +93,7 @@ describe('profit', () => {
             operator: Number(formatEther(await dataToken.balanceOf(operatorWallet.address))),
             delegator: Number(formatEther(await dataToken.balanceOf(delegatorWallet.address))),
             sponsor: Number(formatEther(await dataToken.balanceOf(sponsorWallet.address))),
-            admin: Number(formatEther(await dataToken.balanceOf(adminWallet.address)))
+            admin: Number(formatEther(await dataToken.balanceOf(adminWallet.address))),
         }
     }
 
@@ -88,7 +104,7 @@ describe('profit', () => {
         ;({
             operatorWallet,
             operatorContract,
-            nodeWallets: [operatorNodeWallet]
+            nodeWallets: [_operatorNodeWallet]
         } = await setupOperatorContract({
             nodeCount: 1,
             operatorConfig: {
@@ -107,24 +123,26 @@ describe('profit', () => {
     it('happy path', async () => {
         const beforeBalances = await getBalances()
         await sponsor(sponsorWallet, sponsorshipContract.address, SPONSOR_AMOUNT)
-        await delegate(operatorWallet, operatorContract.address, SELF_DELEGATED_AMOUNT)
+        await delegate(operatorWallet, operatorContract.address, OPERATOR_DELEGATED_AMOUNT)
         await delegate(delegatorWallet, operatorContract.address, EXTERNAL_DELEGATED_AMOUNT)
-        await stake(operatorContract, sponsorshipContract.address, SELF_DELEGATED_AMOUNT + EXTERNAL_DELEGATED_AMOUNT)
+        await stake(operatorContract, sponsorshipContract.address, OPERATOR_DELEGATED_AMOUNT + EXTERNAL_DELEGATED_AMOUNT)
 
+        /* TODO configure the interval of maintainTopologyValue service so that it callx the
+           withdrawMyEarningsFromSponsorships function e.g. 2-3 times during the test run (maybe increase the test time also)
         const broker = await startBroker({
-            privateKey: operatorNodeWallet.privateKey,
+            privateKey: _operatorNodeWallet.privateKey,
             extraPlugins: {
                 operator: {
                     operatorContractAddress: operatorContract.address
                 }
             }
-        })
+        })*/
         await waitForCondition(async () => !(await sponsorshipContract.isFunded()), 60 * 1000)
-        await broker.stop()
+        //await broker.stop()
 
         await unstake(operatorContract, sponsorshipContract.address)
-        await undelegate(delegatorWallet, operatorContract, EXTERNAL_DELEGATED_AMOUNT)
-        await undelegate(operatorWallet, operatorContract, SELF_DELEGATED_AMOUNT)  // TODO does not work before ETH-606
+        await undelegate(delegatorWallet, operatorContract, DELEGTOR_OWNERSHIP_WORTH)
+        await undelegate(operatorWallet, operatorContract, OPERATOR_OWNERSHIP_WORTH)
         const afterBalances = await getBalances()
         const diff = {
             operator: afterBalances.operator - beforeBalances.operator,
@@ -132,15 +150,11 @@ describe('profit', () => {
             sponsor: afterBalances.sponsor - beforeBalances.sponsor,
             admin: afterBalances.admin - beforeBalances.admin,
         }
-        const protocolFee = SPONSOR_AMOUNT * (PROTOCOL_FEE_PERCENTAGE / 100)
-        const totalProfit = SPONSOR_AMOUNT - protocolFee
-        const operatorsCut = totalProfit * (OPERATORS_CUT_PERCENTAGE / 100)
-        const totalDelegatedAmount = SELF_DELEGATED_AMOUNT + EXTERNAL_DELEGATED_AMOUNT + operatorsCut
         expect(diff).toEqual({
-            operator: (totalProfit - operatorsCut) * (SELF_DELEGATED_AMOUNT + operatorsCut / totalDelegatedAmount),
-            delegator: (totalProfit - operatorsCut) * (EXTERNAL_DELEGATED_AMOUNT / totalDelegatedAmount),
+            operator: OPERATOR_PROFIT,
+            delegator: DELEGATOR_PROFIT,
             sponsor: -SPONSOR_AMOUNT,
-            admin: protocolFee
+            admin: PROTOCOL_FEE
         })
-    }, 120 * 1000)
+    }, 30 * 1000)
 })
