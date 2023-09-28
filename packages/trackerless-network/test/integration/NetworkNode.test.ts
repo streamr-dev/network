4d6b94f4b1643d2011d0845bd346e4b3eda8520f
Teo Gebhard <teo@streamr.com>
2023-09-28 17:37:59 +0300

    update test

--- packages/trackerless-network/test/integration/NetworkNode.test.ts
+++ packages/trackerless-network/test/integration/NetworkNode.test.ts
@@ -84,15 +84,12 @@ describe('NetworkNode', () => {
         })
 
         let msgCount = 0
-        // TODO is there a need to join and/or wait here?
-        await node1.joinAndWaitForNeighbors(STREAM_PART_ID, 1)
+        await node1.join(STREAM_PART_ID)
         node1.addMessageListener((msg) => {
             expect(msg.messageId.timestamp).toEqual(666)
             expect(msg.getSequenceNumber()).toEqual(0)
             msgCount += 1
         })
-        // TODO is there a need to join and/or wait here?
-        await node2.joinAndWaitForNeighbors(STREAM_PART_ID, 1)
         await node2.broadcast(streamMessage)
         await waitForCondition(() => msgCount === 1)
     })
