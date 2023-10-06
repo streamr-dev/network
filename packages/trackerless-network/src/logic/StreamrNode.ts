3e531d5115d16d31d29d949e39e95a2591119d81
Teo Gebhard <teo@streamr.com>
2023-10-06 23:54:20 +0300

    fix typo

--- packages/trackerless-network/src/logic/StreamrNode.ts
+++ packages/trackerless-network/src/logic/StreamrNode.ts
@@ -173,7 +173,7 @@ export class StreamrNode extends EventEmitter<Events> {
         logger.debug(`Start layers and join DHT for stream part ${streamPartId}`)
         const stream = this.streams.get(streamPartId)!
         if ((stream === undefined) || (stream.type !== StreamNodeType.RANDOM_GRAPH)) {
-            // leaveStream has been called (or leaveStream called, and then setProxied called)
+            // leaveStream has been called (or leaveStream called, and then setProxies called)
             return
         }
         await stream.layer1!.start()
