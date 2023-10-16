39e9fc5b07436020ff0a475368ab5ad90b551214
Teo Gebhard <teo@streamr.com>
2023-10-16 15:48:00 +0300

    refactor(dht): Simplify `DuplicateDetector` (#1962)
    The `DuplicateDetector` didn't use `senderId` or `message` data in any way. Simplified the class by removing those fields from parameters and data structures. 
    
    Also renamed some fields.

--- packages/dht/test/unit/DuplicateDetector.test.ts
+++ packages/dht/test/unit/DuplicateDetector.test.ts
@@ -3,25 +3,24 @@ import { DuplicateDetector } from '../../src/dht/routing/DuplicateDetector'
 describe('Duplicate Detector', () => {
     let detector: DuplicateDetector
     const maxLimit = 10
-    const senderId = 'sender'
     beforeEach(async () => {
         detector = new DuplicateDetector(maxLimit, 100)
     })
 
     it('detects duplicates', async () => {
-        detector.add('test', senderId)
+        detector.add('test')
         expect(detector.size()).toEqual(1)
         expect(detector.isMostLikelyDuplicate('test')).toEqual(true)
     })
 
     it('resets on resetLimit', () => {
         for (let i = 0; i < maxLimit; i++) {
-            detector.add(`test${i}`, senderId)
+            detector.add(`test${i}`)
         }
         for (let i = 0; i < maxLimit; i++) {
             expect(detector.isMostLikelyDuplicate(`test${i}`)).toEqual(true)
         }
-        detector.add('test10', senderId)
+        detector.add('test10')
         expect(detector.size()).toEqual(10)
         expect(detector.isMostLikelyDuplicate('test0')).toEqual(false)
         expect(detector.isMostLikelyDuplicate('test10')).toEqual(true)
