a80e6e2647f7a773b97a34e84da0256fd79b0280
Teo Gebhard <teo@streamr.com>
2023-10-30 16:49:26 +0200

    increase id length to reduce the likelihood of collisions

--- packages/dht/src/helpers/kademliaId.ts
+++ packages/dht/src/helpers/kademliaId.ts
@@ -1,5 +1,8 @@
 import crypto from 'crypto'
 
+// https://www.scs.stanford.edu/~dm/home/papers/kpos.pdf
+const KADEMLIA_ID_LENGTH_IN_BYTES = 20
+
 export const createRandomKademliaId = () => {
-    return crypto.randomBytes(8)
+    return crypto.randomBytes(KADEMLIA_ID_LENGTH_IN_BYTES)
 }
