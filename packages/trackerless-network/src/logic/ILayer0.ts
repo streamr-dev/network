08f68a600baa7cb7e88c99a41f7f3f453732b72e
Teo Gebhard <teo@streamr.com>
2023-11-01 19:55:29 +0200

    refactor(trackerless-network): Use `ILayer0` and `ILayer1` (#1999)
    Type some fields as `ILayer0` and `ILayer1` instead of `DhtNode`.
    
    ## Future improvements
    
    - Refactor `random-graph-with-real-connections.test.ts` to use normal layering style (i.e. have both layer0 and layer1 e.g. using `NetworkNode`), see comment https://github.com/streamr-dev/network/pull/1999#discussion_r1376607283

--- packages/trackerless-network/src/logic/ILayer0.ts
+++ packages/trackerless-network/src/logic/ILayer0.ts
@@ -2,10 +2,14 @@ import { DataEntry, ITransport, PeerDescriptor } from '@streamr/dht'
 import { Any } from '../proto/google/protobuf/any'
 
 export interface ILayer0 extends ITransport {
+    joinDht(entryPointDescriptors: PeerDescriptor[]): Promise<void>
+    hasJoined(): boolean
     getPeerDescriptor(): PeerDescriptor
     getDataFromDht(key: Uint8Array): Promise<DataEntry[]>
     storeDataToDht(key: Uint8Array, data: Any): Promise<PeerDescriptor[]>
     deleteDataFromDht(key: Uint8Array): Promise<void>
     waitForNetworkConnectivity(): Promise<void>
+    getTransport(): ITransport
+    start(): Promise<void>
     stop(): Promise<void>
 }
