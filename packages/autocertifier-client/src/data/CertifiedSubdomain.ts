9968e7423273a0d5c264e7aabba4f4782339841c
juslesan <santeri.juslenius@gmail.com>
2023-11-07 12:33:59 +0200

    add todo comment

--- packages/autocertifier-client/src/data/CertifiedSubdomain.ts
+++ packages/autocertifier-client/src/data/CertifiedSubdomain.ts
@@ -1,5 +1,6 @@
 import { Certificate } from './Certificate'
 
+// TODO: remove subdomain field? information is already in fqdn
 export interface CertifiedSubdomain {
     subdomain: string
     fqdn: string
