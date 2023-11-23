a990f72a3bf44da46c417d30f4ebd829f457f867
Eric Andrews <harbu88@hotmail.com>
2023-11-23 13:42:31 +0200

    executeSafePromise changes

--- packages/utils/src/executeSafePromise.ts
+++ packages/utils/src/executeSafePromise.ts
@@ -2,11 +2,16 @@ import { Logger } from './Logger'
 
 const logger = new Logger(module)
 
+/**
+ * Execute a promise that should never reject. If it does, log the error and exit the process.
+ * To be used in places where we want to "annotate" that the intention of a promise is never
+ * to reject (unless something is really wrong).
+ */
 export const executeSafePromise = async <T>(createPromise: () => Promise<T>): Promise<T> => {
     try {
         return await createPromise()
     } catch (error: any) {
-        logger.error('Assertion failure!', { message: error?.message, stack: error?.stack })
-        return new Promise(() => {}) as any  // never resolves
+        logger.fatal('Assertion failure!', { message: error?.message, stack: error?.stack })
+        process.exit(1)
     }
 }
