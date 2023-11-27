2e764b527fb4c2a8df23187501913334963c048e
Teo Gebhard <teo@streamr.com>
2023-11-27 23:46:21 +0200

    use @streamr/config in entry-point script

--- packages/broker/bin/entry-point.ts
+++ packages/broker/bin/entry-point.ts
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
+import { config as CHAIN_CONFIG } from '@streamr/config'
 import { DhtNode, NodeType } from '@streamr/dht'
 import { hexToBinary } from '@streamr/utils'
-import { CONFIG_TEST } from 'streamr-client'
 import omit from 'lodash/omit'
 
 const main = async () => {
-    const entryPoint = CONFIG_TEST.network!.controlLayer!.entryPoints![0]
+    const entryPoint = CHAIN_CONFIG.dev2.entryPoints![0]
     const peerDescriptor = {
         ...omit(entryPoint, 'nodeId'),
-        nodeId: hexToBinary(entryPoint.nodeId),
+        nodeId: hexToBinary(entryPoint.id),
         type: NodeType.NODEJS,  // TODO remove this when NET-1070 done
         websocket: {
             ...entryPoint.websocket!,
         }
     }
     const dhtNode = new DhtNode({
-        peerId: entryPoint.nodeId,
+        peerId: entryPoint.id,
         websocketHost: entryPoint.websocket!.host,
         websocketPortRange: {
             min: entryPoint.websocket!.port,
