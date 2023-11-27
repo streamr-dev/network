5d3abb699e55351953f788992adfcc8932213824
Petri Savolainen <petri.savolainen@spaceify.net>
2023-11-27 13:33:59 +0200

    Replicate dns records to AWS Route 53 (#2116)
    ## Summary
    
    * Creates and updates subdomains to AWS Route 53 in addition to the
    local DNS.
    * Creates and deletes acme challenges to AWS Route 53 in addition to the
    local DNS.
    
    ## Limitations and future improvements
    
    * Untested
    * Does not replicate existing entries from local dns to Route 53 upon
    startup yet
    
    ---------
    
    Co-authored-by: juslesan <santeri.juslenius@gmail.com>

---
+++ packages/autocertifier-server/src/Route53Api.ts
@@ -0,0 +1,73 @@
+import {
+    Route53Client,
+    ListResourceRecordSetsCommand,
+    ListResourceRecordSetsCommandOutput,
+    ChangeResourceRecordSetsCommand,
+    ChangeAction,
+    RRType,
+    ChangeResourceRecordSetsCommandOutput
+} from '@aws-sdk/client-route-53'
+
+interface Record {
+    fqdn: string 
+    value: string
+}
+export class Route53Api {
+    
+    private hostedZoneId: string
+    private client: Route53Client
+    
+    constructor(region: string, hostedZoneId: string) {
+        this.hostedZoneId = hostedZoneId
+        this.client = new Route53Client({ region })
+    }
+
+    public async changeRecords(
+        action: ChangeAction,
+        recordType: RRType,
+        records: Record[],
+        ttl: number
+    ): Promise<ChangeResourceRecordSetsCommandOutput> {
+        const input = {
+            HostedZoneId: this.hostedZoneId,
+            ChangeBatch: {
+                Changes: records.map((record) => {
+                    return {
+                        Action: action,
+                        ResourceRecordSet: {
+                            Name: record.fqdn,
+                            Type: recordType,
+                            TTL: ttl,
+                            ResourceRecords: [{
+                                Value: record.value,
+                            }]
+                        }
+                    }
+                })
+            }
+        }
+        const command = new ChangeResourceRecordSetsCommand(input)
+        const response = await this.client.send(command)
+       
+        return response
+    }
+
+    public async upsertRecord(recordType: RRType, fqdn: string, value: string, ttl: number): Promise<ChangeResourceRecordSetsCommandOutput> {
+        return this.changeRecords(ChangeAction.UPSERT, recordType, [ { fqdn, value } ], ttl)
+    }
+
+    public async deleteRecord(recordType: RRType, fqdn: string, value: string, ttl: number): Promise<ChangeResourceRecordSetsCommandOutput> {
+        return this.changeRecords(ChangeAction.DELETE, recordType, [ { fqdn, value } ], ttl)
+    }
+
+    // Debugging tool to list all records in a zone
+    public async listRecords(): Promise<ListResourceRecordSetsCommandOutput> {
+        const input = {
+            HostedZoneId: this.hostedZoneId,
+        }
+    
+        const command = new ListResourceRecordSetsCommand(input)
+        const response = await this.client.send(command)
+        return response
+    }
+}
