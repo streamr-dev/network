cba632b9e7983ad3f9fa33412601a4c3c1cad5a0
Teo Gebhard <teo@streamr.com>
2023-12-02 01:31:37 +0200

    fix entry point initialization

--- packages/dht/test/integration/ReplicateData.test.ts
+++ packages/dht/test/integration/ReplicateData.test.ts
@@ -34,7 +34,6 @@ describe('Replicate data from node to node in DHT', () => {
     let entryPoint: DhtNode
     let nodes: DhtNode[]
     let nodesById: Map<string, DhtNode> = new Map()
-    let entrypointDescriptor: PeerDescriptor
     const simulator = new Simulator(LatencyType.FIXED, 20)
     const NUM_NODES = 100
     const MAX_CONNECTIONS = 80
@@ -47,7 +46,7 @@ describe('Replicate data from node to node in DHT', () => {
         nodes = []
         nodesById.clear()
         for (let i = 0; i < NUM_NODES; i++) {
-            const node = await createMockConnectionDhtNode('dummy', simulator, createRandomNodeId(), K, MAX_CONNECTIONS, undefined, [entrypointDescriptor])
+            const node = await createMockConnectionDhtNode('dummy', simulator, createRandomNodeId(), K, MAX_CONNECTIONS, undefined, [entryPoint.getLocalPeerDescriptor()])
             nodes.push(node)
             nodesById.set(node.getNodeId().toKey(), node)
         }
@@ -101,7 +100,7 @@ describe('Replicate data from node to node in DHT', () => {
         await Promise.all(
             nodes.map(async (node, i) => {
                 if (i !== 0) {
-                    await node.joinDht([entrypointDescriptor])
+                    await node.joinDht([entryPoint.getLocalPeerDescriptor()])
                 }
             })
         )
@@ -129,7 +128,7 @@ describe('Replicate data from node to node in DHT', () => {
 
     it('Data replicates to the last remaining node if all other nodes leave gracefully', async () => {
         logger.info(NUM_NODES + ' nodes joining layer0 DHT')
-        await Promise.all(nodes.map((node) => node.joinDht([entrypointDescriptor])))
+        await Promise.all(nodes.map((node) => node.joinDht([entryPoint.getLocalPeerDescriptor()])))
 
         logger.info('completed ' + NUM_NODES + ' nodes joining layer0 DHT')
 
