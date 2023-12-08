d7b80a02971d5d6a38f16f7605b8b06950e54c0f
Mariusz Roliński <mariusz@mondoreale.pl>
2023-12-08 19:07:10 +0100

    Rearrange

--- packages/broker/src/config/ConfigWizard.ts
+++ packages/broker/src/config/ConfigWizard.ts
@@ -1,10 +1,12 @@
 import { checkbox, confirm, input, password, select } from '@inquirer/prompts'
+import { config as cfg } from '@streamr/config'
 import { toEthereumAddress } from '@streamr/utils'
 import chalk from 'chalk'
 import { Wallet } from 'ethers'
+import { isAddress } from 'ethers/lib/utils'
 import { chmodSync, existsSync, mkdirSync, writeFileSync } from 'fs'
+import { produce } from 'immer'
 import path from 'path'
-import { v4 as uuid } from 'uuid'
 import { z } from 'zod'
 import {
     CURRENT_CONFIGURATION_VERSION,
@@ -16,16 +18,6 @@ import * as WebsocketConfigSchema from '../plugins/websocket/config.schema.json'
 import { ConfigFile, getDefaultFile } from './config'
 import * as BrokerConfigSchema from './config.schema.json'
 
-type Plugin = { port: number }
-
-type PluginKey = 'websocket' | 'mqtt' | 'http'
-
-const DefaultPort: Record<PluginKey, number> = {
-    websocket: WebsocketConfigSchema.properties.port.default,
-    mqtt: MqttConfigSchema.properties.port.default,
-    http: BrokerConfigSchema.properties.httpServer.properties.port.default
-}
-
 export const start = async () => {
     const logger = {
         info: (...args: any[]) => {
@@ -33,120 +25,28 @@ export const start = async () => {
         },
         error: (...args: any[]) => {
             console.error(chalk.bgRed.black('!'), ...args)
+        },
+        success: (tick: boolean, ...args: unknown[]) => {
+            console.info(tick ? chalk.greenBright('✓') : ' ', ...args)
         }
     }
 
     try {
-        const privateKeySource = await select<'Generate' | 'Import'>({
-            message:
-                'Do you want to generate a new Ethereum private key or import an existing one?',
-            choices: [{ value: 'Generate' }, { value: 'Import' }]
-        })
+        const privateKey = await getPrivateKey()
 
-        const privateKey = await (async () => {
-            if (privateKeySource === 'Generate') {
-                return Wallet.createRandom().privateKey
-            }
+        const network = await getNetwork()
 
-            return password({
-                message: 'Please provide the private key to import',
-                validate(value) {
-                    try {
-                        return !!new Wallet(value)
-                    } catch (_) {}
+        const operatorPlugins = await getOperatorPlugins()
 
-                    return 'Invalid private key provided.'
-                }
-            })
-        })()
-
-        if (privateKeySource === 'Generate') {
-            await confirm({
-                message:
-                    'We strongly recommend backing up your private key.\nIt will be written into the config file, but would you also like to see this sensitive information on screen now?',
-                default: false,
-                transformer(value) {
-                    return value
-                        ? `Your node's private key: ${privateKey}`
-                        : 'No'
-                }
-            })
-        }
-
-        const pluginKeys = await checkbox<PluginKey>({
-            message: 'Select the plugins to enable',
-            choices: [
-                { value: 'websocket', name: 'WebSocket' },
-                { value: 'mqtt', name: 'MQTT' },
-                { value: 'http', name: 'HTTP' }
-            ]
-        })
-
-        const enabledPlugins: Partial<Record<PluginKey, Plugin>> = {}
-
-        for (const pluginKey of pluginKeys) {
-            const defaultPort = DefaultPort[pluginKey]
-
-            const port = Number(
-                await input({
-                    message: `Provide a port for the ${pluginKey} plugin`,
-                    default: defaultPort.toString(),
-                    validate(value) {
-                        try {
-                            return !!z.coerce
-                                .number({
-                                    invalid_type_error:
-                                        'Non-numeric value provided'
-                                })
-                                .int('Non-integer value provided')
-                                .min(1024)
-                                .max(49151)
-                                .parse(value)
-                        } catch (e: unknown) {
-                            return (e as z.ZodError).issues
-                                .map(({ message }) => message)
-                                .join(', ')
-                        }
-                    }
-                })
-            )
-
-            if (port !== defaultPort) {
-                enabledPlugins[pluginKey] = { port }
-            }
-        }
-
-        const { http: httpServer, ...plugins } = enabledPlugins
+        const { http: httpServer, ...pubsubPlugins } = await getPubsubPlugins()
 
         if (httpServer) {
-            Object.assign(plugins, {
+            Object.assign(pubsubPlugins, {
                 http: {}
             })
         }
 
-        const storagePath = await (async () => {
-            while (true) {
-                const path = await input({
-                    message: 'Select a path to store the generated config in',
-                    default: getDefaultFile()
-                })
-
-                const proceed =
-                    !existsSync(path) ||
-                    (await confirm({
-                        message: `The selected destination ${path} already exists. Do you want to overwrite it?`,
-                        default: false
-                    }))
-
-                if (proceed) {
-                    return path
-                }
-            }
-        })()
-
-        const apiKey = Buffer.from(uuid().replace(/-/g, ''))
-            .toString('base64')
-            .replace(/[^\da-z]/gi, '')
+        const storagePath = await getStoragePath()
 
         const config: ConfigFile = {
             $schema: formSchemaUrl(CURRENT_CONFIGURATION_VERSION),
@@ -155,34 +55,43 @@ export const start = async () => {
                     privateKey
                 }
             },
-            plugins,
-            apiAuthentication: {
-                keys: [apiKey]
+            plugins: {
+                ...operatorPlugins,
+                ...pubsubPlugins
             },
-            httpServer
+            httpServer: httpServer?.port
+                ? {
+                      port: httpServer.port
+                  }
+                : void 0
         }
 
-        const dirPath = path.dirname(storagePath)
-
-        if (!existsSync(dirPath)) {
-            mkdirSync(dirPath, {
-                recursive: true
-            })
-        }
-
-        writeFileSync(storagePath, JSON.stringify(config, null, 4))
-
-        chmodSync(storagePath, '600')
-
-        logger.info('Welcome to the Streamr Network')
-
-        logger.info(
-            `Your node's generated name is ${getNodeMnemonic(privateKey)}.`
+        persistConfig(
+            storagePath,
+            network === 'polygon' ? config : decorateMumbaiConfig(config)
         )
 
-        logger.info('You can start the broker now with')
+        const lines = [
+            chalk.bold.whiteBright(
+                `Congratulations, you've set up your Streamr Network node!`
+            ),
+            `Your node address is ${chalk.greenBright(
+                new Wallet(privateKey).address
+            )}`,
+            `Your node's generated name is ${chalk.greenBright(
+                getNodeMnemonic(privateKey)
+            )}`,
+            ``,
+            `You can start your Streamr node now with`,
+            chalk.whiteBright(`streamr-broker ${storagePath}`),
+            ``,
+            `For environment specific run instructions, see`,
+            `https://docs.streamr.network/guides/how-to-run-streamr-node`
+        ]
 
-        logger.info(`streamr-broker ${storagePath}`)
+        logger.success(false)
+
+        lines.forEach((line, index) => logger.success(!index, line))
     } catch (e: any) {
         if (/force closed/i.test(e.message)) {
             return
@@ -194,8 +103,245 @@ export const start = async () => {
     }
 }
 
+/**
+ * Generates a mnemonic for a given private key.
+ */
 export const getNodeMnemonic = (privateKey: string): string => {
     return generateMnemonicFromAddress(
         toEthereumAddress(new Wallet(privateKey).address)
     )
+}
+
+/**
+ * Lets the user generate a private key or import an existing private key.
+ */
+async function getPrivateKey() {
+    const privateKeySource = await select<'Generate' | 'Import'>({
+        message:
+            'Do you want to generate a new Ethereum private key or import an existing one?',
+        choices: [{ value: 'Generate' }, { value: 'Import' }]
+    })
+
+    const privateKey = await (async () => {
+        if (privateKeySource === 'Generate') {
+            return Wallet.createRandom().privateKey
+        }
+
+        return password({
+            message: 'Please provide the private key to import',
+            validate(value) {
+                try {
+                    return !!new Wallet(value)
+                } catch (_) {}
+
+                return 'Invalid private key provided.'
+            }
+        })
+    })()
+
+    if (privateKeySource === 'Generate') {
+        await confirm({
+            message:
+                'We strongly recommend backing up your private key. It will be written into the config file, but would you also like to see this sensitive information on screen now?',
+            default: false,
+            transformer(value) {
+                return value ? `Your node's private key: ${privateKey}` : 'no'
+            }
+        })
+    }
+
+    return privateKey
+}
+
+/**
+ * Lets the user decide the desired network for their node.
+ */
+async function getNetwork() {
+    return select<'polygon' | 'mumbai'>({
+        message:
+            'Which network do you want to configure your node to connect to?',
+        choices: [
+            { value: 'polygon', name: 'Streamr 1.0 testnet + Polygon' },
+            {
+                value: 'mumbai',
+                name: 'Streamr 1.0 testing environment + Mumbai'
+            }
+        ],
+        default: 'polygon'
+    })
+}
+
+/**
+ * Lets the user gather and configure desired operator plugins.
+ */
+async function getOperatorPlugins() {
+    const setupOperator = await confirm({
+        message:
+            'Do you wish to participate in earning rewards by staking on stream Sponsorships?',
+        default: true
+    })
+
+    if (!setupOperator) {
+        return {}
+    }
+
+    return {
+        operator: {
+            operatorContractAddress: await input({
+                message: 'Enter your Operator address:',
+                validate(value) {
+                    return isAddress(value) ? true : 'Invalid ethereum address'
+                }
+            })
+        }
+    }
+}
+
+type PubsubPlugin = { port?: number }
+
+type PubsubPluginKey = 'websocket' | 'mqtt' | 'http'
+
+const DefaultPort: Record<PubsubPluginKey, number> = {
+    websocket: WebsocketConfigSchema.properties.port.default,
+    mqtt: MqttConfigSchema.properties.port.default,
+    http: BrokerConfigSchema.properties.httpServer.properties.port.default
+}
+
+/**
+ * Lets the user select and configure desired pub/sub plugins.
+ */
+async function getPubsubPlugins() {
+    const setupPubsub = await confirm({
+        message:
+            'Do you wish to use your node for data publishing/subscribing?',
+        default: true
+    })
+
+    if (!setupPubsub) {
+        return {}
+    }
+
+    const keys = await checkbox<PubsubPluginKey>({
+        message: 'Select the plugins to enable',
+        choices: [
+            { value: 'websocket', name: 'WebSocket' },
+            { value: 'mqtt', name: 'MQTT' },
+            { value: 'http', name: 'HTTP' }
+        ]
+    })
+
+    const pubsubPlugins: Partial<Record<PubsubPluginKey, PubsubPlugin>> = {}
+
+    for (const key of keys) {
+        const defaultPort = DefaultPort[key]
+
+        const port = Number(
+            await input({
+                message: `Provide a port for the ${key} plugin`,
+                default: defaultPort.toString(),
+                validate(value) {
+                    try {
+                        return !!z.coerce
+                            .number({
+                                invalid_type_error: 'Non-numeric value provided'
+                            })
+                            .int('Non-integer value provided')
+                            .min(1024)
+                            .max(49151)
+                            .parse(value)
+                    } catch (e: unknown) {
+                        return (e as z.ZodError).issues
+                            .map(({ message }) => message)
+                            .join(', ')
+                    }
+                }
+            })
+        )
+
+        pubsubPlugins[key] = port !== defaultPort ? { port } : {}
+    }
+
+    return pubsubPlugins
+}
+
+/**
+ * Lets the user decide where to write the config file.
+ */
+async function getStoragePath() {
+    while (true) {
+        const path = await input({
+            message: 'Select a path to store the generated config in',
+            default: getDefaultFile()
+        })
+
+        const proceed =
+            !existsSync(path) ||
+            (await confirm({
+                message: `The selected destination ${path} already exists. Do you want to overwrite it?`,
+                default: false
+            }))
+
+        if (proceed) {
+            return path
+        }
+    }
+}
+
+/**
+ * Writes given config structure into a file.
+ */
+function persistConfig(storagePath: string, config: ConfigFile) {
+    const dirPath = path.dirname(storagePath)
+
+    if (!existsSync(dirPath)) {
+        mkdirSync(dirPath, {
+            recursive: true
+        })
+    }
+
+    writeFileSync(storagePath, JSON.stringify(config, null, 4))
+
+    chmodSync(storagePath, '600')
+}
+
+/**
+ * Adjusts the given config for the Mumbai test environment.
+ */
+function decorateMumbaiConfig(config: ConfigFile): ConfigFile {
+    return produce(config, (draft) => {
+        draft.client || (draft.client = {})
+
+        draft.client.metrics = false
+
+        const {
+            id: chainId,
+            entryPoints,
+            theGraphUrl,
+            rpcEndpoints: rpcs
+        } = cfg.mumbai
+
+        draft.client.network = {
+            controlLayer: {
+                entryPoints
+            }
+        }
+
+        const {
+            StreamRegistry: streamRegistryChainAddress,
+            StreamStorageRegistry: streamStorageRegistryChainAddress,
+            StorageNodeRegistry: storageNodeRegistryChainAddress
+        } = cfg.mumbai.contracts
+
+        draft.client.contracts = {
+            streamRegistryChainAddress,
+            streamStorageRegistryChainAddress,
+            storageNodeRegistryChainAddress,
+            streamRegistryChainRPCs: {
+                name: 'mumbai',
+                chainId,
+                rpcs
+            },
+            theGraphUrl
+        }
+    })
 }
