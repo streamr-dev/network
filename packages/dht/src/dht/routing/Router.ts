d53d60b0a1e042c6e852b296fc3e6cf8c4ab3e98
Teo Gebhard <teo@streamr.com>
2023-12-13 10:45:26 +0200

    add TODO comment

--- packages/dht/src/dht/routing/Router.ts
+++ packages/dht/src/dht/routing/Router.ts
@@ -19,6 +19,7 @@ export interface RouterConfig {
     connections: Map<NodeID, DhtNodeRpcRemote>
     addContact: (contact: PeerDescriptor, setActive?: boolean) => void
     connectionManager?: ConnectionManager
+    // TODO use this or remove the config option?
     rpcRequestTimeout?: number
 }
 
