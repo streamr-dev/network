f447c37e2e854097934ebfb124ae3bb3e1206a13
juslesan <santeri.juslenius@gmail.com>
2023-12-16 17:16:39 +0200

    update

--- packages/client/src/run-subscriber.ts
+++ packages/client/src/run-subscriber.ts
@@ -4,7 +4,10 @@ import { StreamPartIDUtils } from '@streamr/protocol'
 const main = async () => {
     let numOfMessagesPerTenSeconds = 0
     let numOfMessagesPerMinute = 0
-    const client = new StreamrClient({})
+    const client = new StreamrClient({
+        environment: "mumbai",
+        metrics: false
+    })
 
     setInterval(() => {
         console.log('Num of messages in the last ten seconds ' + numOfMessagesPerTenSeconds)
@@ -22,48 +25,55 @@ const main = async () => {
         console.log('total connections (CM): ' + (node.stack.getLayer0Node().getTransport() as ConnectionManager).getAllConnectionPeerDescriptors().length)
         // @ts-expect-error private
         console.log('total connections: (DHTNODE)' + (node.stack.getLayer0Node() as DhtNode).getNumberOfConnections())
-
-        const stream = StreamPartIDUtils.parse('streams.dimo.eth/firehose/weather#0')
-        console.log('total stream neighbors on streams.dimo.eth/firehose/weather: ' + node.getNeighbors(stream).length)
+        const stream = StreamPartIDUtils.parse('0x80da975ba0978d8df26b5ab3c2758a00d7ee298a/operator/coordination#0')
+        console.log('total stream neighbors on 0x80da975ba0978d8df26b5ab3c2758a00d7ee298a/operator/coordination: ' + node.getNeighbors(stream).length)
     }, 10000)
 
+    // await client.subscribe({
+    //     streamId: "streams.dimo.eth/firehose/weather",
+    //     partition: 0
+    // }, () => {
+    //     numOfMessagesPerTenSeconds += 1
+    //     numOfMessagesPerMinute += 1
+    // })
+    // await client.subscribe({
+    //     streamId: "streams.dimo.eth/firehose/weather",
+    //     partition: 1
+    // }, () => {
+    //     numOfMessagesPerTenSeconds += 1
+    //     numOfMessagesPerMinute += 1
+    // })
+    // await client.subscribe({
+    //     streamId: "streams.dimo.eth/firehose/weather",
+    //     partition: 2
+    // }, () => {
+    //     numOfMessagesPerTenSeconds += 1
+    //     numOfMessagesPerMinute += 1
+    // })
+    // await client.subscribe({
+    //     streamId: "eth-watch.eth/ethereum/blocks",
+    //     partition: 0
+    // }, () => {
+    //     numOfMessagesPerTenSeconds += 1
+    //     numOfMessagesPerMinute += 1
+    // })
+    // await client.subscribe({
+    //     streamId: "0xbafb06e3d7546742c6b1f2945b74ce0b3edc201a/nodle",
+    //     partition: 0
+    // }, () => {
+    //     numOfMessagesPerTenSeconds += 1
+    //     numOfMessagesPerMinute += 1
+    // })
+    // await client.subscribe({
+    //     streamId: "streamr.eth/demos/helsinki-trams",
+    //     partition: 0
+    // }, () => {
+    //     numOfMessagesPerTenSeconds += 1
+    //     numOfMessagesPerMinute += 1
+    // })
+
     await client.subscribe({
-        streamId: "streams.dimo.eth/firehose/weather",
-        partition: 0
-    }, () => {
-        numOfMessagesPerTenSeconds += 1
-        numOfMessagesPerMinute += 1
-    })
-    await client.subscribe({
-        streamId: "streams.dimo.eth/firehose/weather",
-        partition: 1
-    }, () => {
-        numOfMessagesPerTenSeconds += 1
-        numOfMessagesPerMinute += 1
-    })
-    await client.subscribe({
-        streamId: "streams.dimo.eth/firehose/weather",
-        partition: 2
-    }, () => {
-        numOfMessagesPerTenSeconds += 1
-        numOfMessagesPerMinute += 1
-    })
-    await client.subscribe({
-        streamId: "eth-watch.eth/ethereum/blocks",
-        partition: 0
-    }, () => {
-        numOfMessagesPerTenSeconds += 1
-        numOfMessagesPerMinute += 1
-    })
-    await client.subscribe({
-        streamId: "0xbafb06e3d7546742c6b1f2945b74ce0b3edc201a/nodle",
-        partition: 0
-    }, () => {
-        numOfMessagesPerTenSeconds += 1
-        numOfMessagesPerMinute += 1
-    })
-    await client.subscribe({
-        streamId: "streamr.eth/demos/helsinki-trams",
+        streamId: "0x80da975ba0978d8df26b5ab3c2758a00d7ee298a/operator/coordination",
         partition: 0
     }, () => {
         numOfMessagesPerTenSeconds += 1
