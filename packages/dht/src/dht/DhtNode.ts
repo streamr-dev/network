57983bb3daa24beecfbe72e441196a1303f631e7
juslesan <santeri.juslenius@gmail.com>
2023-12-18 22:21:04 +0200

    run more redundant getClosetPeers requests to avoid network splits in smaller networks

--- packages/dht/src/dht/DhtNode.ts
+++ packages/dht/src/dht/DhtNode.ts
@@ -155,7 +155,7 @@ export class DhtNode extends EventEmitter<Events> implements ITransport {
             joinParallelism: 3,
             maxNeighborListSize: 200,
             numberOfNodesPerKBucket: 8,
-            joinNoProgressLimit: 5,
+            joinNoProgressLimit: 10,
             dhtJoinTimeout: 60000,
             peerDiscoveryQueryBatchSize: 5,
             maxConnections: 80,
