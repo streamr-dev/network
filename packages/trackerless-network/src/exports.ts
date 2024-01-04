efe84d1336d343da4fdd1dbdef0cad634dc5448e
Eric Andrews <harbu88@hotmail.com>
2024-01-04 12:06:01 +0200

    feat(trackerless-network): add old stream message binary conversion utils

--- packages/trackerless-network/src/exports.ts
+++ packages/trackerless-network/src/exports.ts
@@ -2,3 +2,7 @@ export { NetworkStack, NetworkOptions } from './NetworkStack'
 export { NetworkNode, createNetworkNode } from './NetworkNode'
 export { StreamrNodeConfig } from './logic/StreamrNode'
 export { ProxyDirection } from './proto/packages/trackerless-network/protos/NetworkRpc'
+export {
+    convertStreamMessageToBytes,
+    convertBytesToStreamMessage
+} from './logic/protocol-integration/stream-message/oldStreamMessageBinaryUtils'
