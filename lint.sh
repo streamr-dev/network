31c4406cb93e827a0ca5ad9d43710154a5f1b1ef
Eric Andrews <harbu88@hotmail.com>
2024-01-08 14:23:27 +0200

    remove lint.sh

--- lint.sh
+++
@@ -1,7 +0,0 @@
-# no not merge this script to main
-(cd packages/broker && npm run eslint)
-(cd packages/cli-tools && npm run eslint)
-(cd packages/client && npm run eslint)
-(cd packages/protocol && npm run eslint)
-(cd packages/test-utils && npm run eslint)
-(cd packages/utils && npm run eslint)
