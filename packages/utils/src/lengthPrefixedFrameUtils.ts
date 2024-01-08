ffac86d6819eac6c940fee9af7ba3b44b2d96dce
Eric Andrews <harbu88@hotmail.com>
2024-01-08 16:34:22 +0200

    refactor: replace magic number with constant

--- packages/utils/src/lengthPrefixedFrameUtils.ts
+++ packages/utils/src/lengthPrefixedFrameUtils.ts
@@ -1,11 +1,14 @@
 import { Transform, TransformCallback } from 'stream'
 
+// If you change this you also need to change `writeUint32BE` and `readUInt32BE` in the code below
+const HEADER_LENGTH = 4
+
 /**
  * Utilities to deal with length-prefixed frames, i.e. | length [4 bytes] | payload [variable bytes] |
  */
 
 export const toLengthPrefixedFrame = (payload: Uint8Array): Buffer => {
-    const length = Buffer.alloc(4)
+    const length = Buffer.alloc(HEADER_LENGTH)
     length.writeUint32BE(payload.length)
     return Buffer.concat([length, payload])
 }
@@ -28,13 +31,13 @@ export class LengthPrefixedFrameDecoder extends Transform {
     }
 
     private processStreamData(callback: TransformCallback): void {
-        while (this.buffer.length >= 4) {
+        while (this.buffer.length >= HEADER_LENGTH) {
             const payloadSize = this.buffer.readUInt32BE(0)
 
-            if (this.buffer.length >= payloadSize + 4) {
-                const payload = this.buffer.subarray(4, payloadSize + 4)
+            if (this.buffer.length >= payloadSize + HEADER_LENGTH) {
+                const payload = this.buffer.subarray(HEADER_LENGTH, payloadSize + HEADER_LENGTH)
                 this.push(payload)
-                this.buffer = this.buffer.subarray(payloadSize + 4)
+                this.buffer = this.buffer.subarray(payloadSize + HEADER_LENGTH)
             } else {
                 break
             }
