5b2f247c1550a38228d61bd56731619555b1437a
Eric Andrews <harbu88@hotmail.com>
2024-01-15 12:12:08 +0200

    remove unnecessary runtime validation from EncryptedGroupKey and MessageID

--- packages/protocol/src/protocol/message_layer/EncryptedGroupKey.ts
+++ packages/protocol/src/protocol/message_layer/EncryptedGroupKey.ts
@@ -1,5 +1,3 @@
-import { validateIsType } from '../../utils/validations'
-
 export default class EncryptedGroupKey {
 
     readonly id: string
@@ -7,7 +5,6 @@ export default class EncryptedGroupKey {
 
     constructor(id: string, data: Uint8Array) {
         this.id = id
-        validateIsType('data', data, 'Uint8Array', Uint8Array)
         this.data = data
     }
 }
