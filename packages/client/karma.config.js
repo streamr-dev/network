f250fc63c97b7f5ae3d7cf7abeeeac123b1fbed9
Eric Andrews <harbu88@hotmail.com>
2024-01-16 19:48:46 +0200

    unit tests

--- packages/client/karma.config.js
+++ packages/client/karma.config.js
@@ -3,7 +3,7 @@ const webpackConfig = require('./webpack.config')
 const { createKarmaConfig } = require('@streamr/browser-test-runner')
 
 const TEST_PATHS = [
-    'test/end-to-end/**/*.ts'
+    'test/unit/**/*.ts'
 ]
 
 module.exports = createKarmaConfig(TEST_PATHS, webpackConfig)
