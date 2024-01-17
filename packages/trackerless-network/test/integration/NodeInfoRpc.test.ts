1c7ac4e86a8057d9626fc2ddaa9bc0edabc1c90b
Teo Gebhard <teo@streamr.com>
2024-01-17 12:19:15 +0200

    implement test

--- packages/trackerless-network/test/integration/NodeInfoRpc.test.ts
+++ packages/trackerless-network/test/integration/NodeInfoRpc.test.ts
@@ -1,4 +1,4 @@
-import { Simulator, SimulatorTransport, ListeningRpcCommunicator, areEqualPeerDescriptors } from '@streamr/dht'
+import { Simulator, SimulatorTransport, ListeningRpcCommunicator, areEqualPeerDescriptors, PeerDescriptor } from '@streamr/dht'
 import { NetworkStack } from '../../src/NetworkStack'
 import { createMockPeerDescriptor } from '../utils/utils'
 import { NodeInfoClient } from '../../src/logic/node-info-rpc/NodeInfoClient'
@@ -6,95 +6,110 @@ import { NODE_INFO_RPC_SERVICE_ID } from '../../src/logic/node-info-rpc/NodeInfo
 import { StreamPartIDUtils } from '@streamr/protocol'
 import { waitForCondition } from '@streamr/utils'
 
+const normalizePeerDescriptor = (peerDescriptor: PeerDescriptor) => {
+    return {
+        ...peerDescriptor,
+        nodeId: new Uint8Array(peerDescriptor.nodeId)
+    }
+}
+
 describe('NetworkStack NodeInfoRpc', () => {
 
-    let stack1: NetworkStack
-    let stack2: NetworkStack
+    let requesteStack: NetworkStack
+    let otherStack: NetworkStack
     let nodeInfoClient: NodeInfoClient
-    let transport1: SimulatorTransport
-    let transport2: SimulatorTransport
-    let transport3: SimulatorTransport
+    let requesteeTransport1: SimulatorTransport
+    let otherTransport: SimulatorTransport
+    let requestorTransport: SimulatorTransport
 
     let simulator: Simulator
 
-    const stack1PeerDescriptor = createMockPeerDescriptor()
-    const stack2PeerDescriptor = createMockPeerDescriptor()
-    const stack3PeerDescriptor = createMockPeerDescriptor()
+    const requesteePeerDescriptor = createMockPeerDescriptor()
+    const otherPeerDescriptor = createMockPeerDescriptor()
+    const requestorPeerDescriptor = createMockPeerDescriptor()
 
     beforeEach(async () => {
         simulator = new Simulator()
-        transport1 = new SimulatorTransport(stack1PeerDescriptor, simulator)
-        transport2 = new SimulatorTransport(stack2PeerDescriptor, simulator)
-        transport3 = new SimulatorTransport(stack3PeerDescriptor, simulator)
-        await transport1.start()
-        await transport2.start()
-        await transport3.start()
-        stack1 = new NetworkStack({
+        requesteeTransport1 = new SimulatorTransport(requesteePeerDescriptor, simulator)
+        otherTransport = new SimulatorTransport(otherPeerDescriptor, simulator)
+        requestorTransport = new SimulatorTransport(requestorPeerDescriptor, simulator)
+        await requesteeTransport1.start()
+        await otherTransport.start()
+        await requestorTransport.start()
+        requesteStack = new NetworkStack({
             layer0: {
-                transport: transport1,
-                peerDescriptor: stack1PeerDescriptor,
-                entryPoints: [stack1PeerDescriptor]
+                transport: requesteeTransport1,
+                peerDescriptor: requesteePeerDescriptor,
+                entryPoints: [requesteePeerDescriptor]
             }
         })
-        stack2 = new NetworkStack({
+        otherStack = new NetworkStack({
             layer0: {
-                transport: transport2,
-                peerDescriptor: stack2PeerDescriptor,
-                entryPoints: [stack1PeerDescriptor]
+                transport: otherTransport,
+                peerDescriptor: otherPeerDescriptor,
+                entryPoints: [requesteePeerDescriptor]
             }
         })
-        await stack1.start()
-        await stack2.start()
-        nodeInfoClient = new NodeInfoClient(stack3PeerDescriptor, new ListeningRpcCommunicator(NODE_INFO_RPC_SERVICE_ID, transport3))
+        await requesteStack.start()
+        await otherStack.start()
+        nodeInfoClient = new NodeInfoClient(requestorPeerDescriptor, new ListeningRpcCommunicator(NODE_INFO_RPC_SERVICE_ID, requestorTransport))
     })
 
     afterEach(async () => {
-        await stack1.stop()
-        await stack2.stop()
-        await transport1.stop()
-        await transport2.stop()
-        await transport3.stop()
+        await requesteStack.stop()
+        await otherStack.stop()
+        await requesteeTransport1.stop()
+        await otherTransport.stop()
+        await requestorTransport.stop()
     })
 
     it('NodeInfoClient can query NetworkStacks', async () => {
-        const result = await nodeInfoClient.getInfo(stack1PeerDescriptor)
+        const result = await nodeInfoClient.getInfo(requesteePeerDescriptor)
         expect(result.controlLayer).toBeDefined()
         expect(result.streamPartitions).toBeDefined()
     })
 
     it('NodeInfoClient gets control layer info', async () => {
-        const result = await nodeInfoClient.getInfo(stack1PeerDescriptor)
+        const result = await nodeInfoClient.getInfo(requesteePeerDescriptor)
         expect(result.controlLayer).toBeDefined()
         expect(result.controlLayer!.connections.length).toEqual(2)
         expect(result.controlLayer!.neighbors.length).toEqual(1)
-        expect(areEqualPeerDescriptors(result.controlLayer!.neighbors[0], stack2PeerDescriptor)).toEqual(true)
+        expect(areEqualPeerDescriptors(result.controlLayer!.neighbors[0], otherPeerDescriptor)).toEqual(true)
     })
 
     it('NodeInfoClient gets stream partition info', async () => {
         const streamPartId1 = StreamPartIDUtils.parse('stream1#0')
-        const streamPartId2 = StreamPartIDUtils.parse('stream1#1')
-        stack1.getStreamrNode().joinStreamPart(streamPartId1)
-        stack2.getStreamrNode().joinStreamPart(streamPartId1)
-        stack1.getStreamrNode().joinStreamPart(streamPartId2)
-        stack2.getStreamrNode().joinStreamPart(streamPartId2)
+        const streamPartId2 = StreamPartIDUtils.parse('stream2#0')
+        requesteStack.getStreamrNode().joinStreamPart(streamPartId1)
+        otherStack.getStreamrNode().joinStreamPart(streamPartId1)
+        requesteStack.getStreamrNode().joinStreamPart(streamPartId2)
+        otherStack.getStreamrNode().joinStreamPart(streamPartId2)
         await waitForCondition(() => 
-            stack1.getStreamrNode().getNeighbors(streamPartId1).length === 1 
-            && stack2.getStreamrNode().getNeighbors(streamPartId1).length === 1
-            && stack1.getStreamrNode().getNeighbors(streamPartId2).length === 1
-            && stack2.getStreamrNode().getNeighbors(streamPartId2).length === 1
+            requesteStack.getStreamrNode().getNeighbors(streamPartId1).length === 1 
+            && otherStack.getStreamrNode().getNeighbors(streamPartId1).length === 1
+            && requesteStack.getStreamrNode().getNeighbors(streamPartId2).length === 1
+            && otherStack.getStreamrNode().getNeighbors(streamPartId2).length === 1
         )
-        const result = await nodeInfoClient.getInfo(stack1PeerDescriptor)
-        /*expect(result).toMatchObject({
-            peerDescriptor: {},
-            controlLayer: {},
+        const result = await nodeInfoClient.getInfo(requesteePeerDescriptor)
+        expect(result).toMatchObject({
+            peerDescriptor: normalizePeerDescriptor(requesteePeerDescriptor),
+            controlLayer: {
+                neighbors: [normalizePeerDescriptor(otherPeerDescriptor)],
+                connections: [normalizePeerDescriptor(otherPeerDescriptor), normalizePeerDescriptor(requestorPeerDescriptor)]
+            },
             streamPartitions: [
                 {
-                    id: '',
-                    controlLayerNeighbors: [{}, {}],
-                    deliveryLayerNeighbors: [{}, {}]
+                    id: streamPartId1,
+                    controlLayerNeighbors: [normalizePeerDescriptor(otherPeerDescriptor)],
+                    deliveryLayerNeighbors: [normalizePeerDescriptor(otherPeerDescriptor)]
+                },
+                {
+                    id: streamPartId2,
+                    controlLayerNeighbors: [normalizePeerDescriptor(otherPeerDescriptor)],
+                    deliveryLayerNeighbors: [normalizePeerDescriptor(otherPeerDescriptor)]
                 }
             ]
-        })*/
+        })
         expect(result.streamPartitions.length).toEqual(2)
     })
 
