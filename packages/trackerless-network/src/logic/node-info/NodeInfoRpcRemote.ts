c12134f2a959319064919ad786464f0d3599fe28
Teo Gebhard <teo@streamr.com>
2024-01-17 12:44:52 +0200

    rm comment

--- packages/trackerless-network/src/logic/node-info/NodeInfoRpcRemote.ts
+++ packages/trackerless-network/src/logic/node-info/NodeInfoRpcRemote.ts
@@ -4,7 +4,6 @@ import { RpcRemote } from '@streamr/dht'
 
 export class NodeInfoRpcRemote extends RpcRemote<NodeInfoRpcClient> {
 
-    // streams is a list of streamPartIDs, if the list is empty then info of all streams is returned
     async getInfo(): Promise<NodeInfoResponse> {
         // TODO: Why does TS think this is Promise<void>: https://github.com/streamr-dev/network/pull/2293
         const result = await this.getClient().getInfo({}, this.formDhtRpcOptions())
