a293f38e87607194dff428b58bc545afacdeadb9
Teo Gebhard <teo@streamr.com>
2024-01-18 13:34:16 +0200

    style

--- packages/dht/src/dht/DhtNode.ts
+++ packages/dht/src/dht/DhtNode.ts
@@ -116,45 +116,45 @@ export type Events = TransportEvents & DhtNodeEvents
 
 const calculateNodeIdRaw = (ipAddress: number, privateKey: Uint8Array): DhtAddressRaw => {
     // nodeId is calculated as 
-    // concatenate(get104leastSignificatBits(hash(ipAddress)), 
-    //  get56leastSignificatBits(sign(ipAddress)))
-
+    // concatenate(
+    //   get104leastSignificatBits(hash(ipAddress)), 
+    //   get56leastSignificatBits(sign(ipAddress))
+    // )
     const ipAsBuffer = Buffer.alloc(4)
     ipAsBuffer.writeUInt32BE(ipAddress)
-
     const ipHash = hash(ipAsBuffer)
     const signature = createSignature(ipAsBuffer, privateKey)
-
-    const nodeIdRaw = Buffer.concat([ipHash.slice(ipHash.length - 13, ipHash.length),
-        signature.slice(signature.length - 7, signature.length)])
-
+    const nodeIdRaw = Buffer.concat([
+        ipHash.slice(ipHash.length - 13, ipHash.length),
+        signature.slice(signature.length - 7, signature.length)
+    ])
     return nodeIdRaw
 }
 
 export const createPeerDescriptor = (msg: ConnectivityResponse, nodeId?: DhtAddress): PeerDescriptor => {
-
     const privateKey = crypto.randomBytes(32)
     const publicKey = crypto.randomBytes(20)  // TODO calculate publicKey from privateKey
     let nodeIdRaw: DhtAddressRaw
-    
     // ToDo: add checking that the nodeId is valid
-    
     if (nodeId !== undefined) {
         nodeIdRaw = getRawFromDhtAddress(nodeId)
     } else {
         nodeIdRaw = calculateNodeIdRaw(msg.ipAddress, privateKey)
     }
-
-    const nodeType = isBrowserEnvironment() ? NodeType.BROWSER : NodeType.NODEJS
-
-    const ret: PeerDescriptor = { nodeId: nodeIdRaw, type: nodeType, ipAddress: msg.ipAddress, publicKey }
- 
-    if (msg.websocket) {
-        ret.websocket = { host: msg.websocket.host, port: msg.websocket.port, tls: msg.websocket.tls }
+    const ret: PeerDescriptor = {
+        nodeId: nodeIdRaw,
+        type: isBrowserEnvironment() ? NodeType.BROWSER : NodeType.NODEJS,
+        ipAddress: msg.ipAddress,
+        publicKey 
     }
- 
+    if (msg.websocket) {
+        ret.websocket = {
+            host: msg.websocket.host,
+            port: msg.websocket.port,
+            tls: msg.websocket.tls
+        }
+    }
     ret.signature = createSignature(createPeerDescriptorSignaturePayload(ret), privateKey)
-    
     return ret
 }
 
