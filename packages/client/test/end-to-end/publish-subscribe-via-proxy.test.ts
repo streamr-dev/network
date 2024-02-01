3b84e14686630b2b6d653390676a76a01ded212e
Eric Andrews <harbu88@hotmail.com>
2024-02-01 12:55:44 +0200

    try to remove skip on electron

--- packages/client/test/end-to-end/publish-subscribe-via-proxy.test.ts
+++ packages/client/test/end-to-end/publish-subscribe-via-proxy.test.ts
@@ -11,7 +11,7 @@ const TIMEOUT = 30 * 1000
 const SUBSCRIBE_WAIT_TIME = 2000
 const WEBSOCKET_PORT = 14231
 
-describeOnlyInNodeJs('publish/subscribe via proxy', () => { // Cannot run proxy server in browser
+describe('publish/subscribe via proxy', () => { // Cannot run proxy server in browser
 
     let stream: Stream
     let client: StreamrClient
