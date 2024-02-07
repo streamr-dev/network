d217ff97715e6453e642ef23802aced29b943a9d
Teo Gebhard <teo@streamr.com>
2024-02-07 13:07:51 +0200

    exports

--- packages/trackerless-network/src/exports.ts
+++ packages/trackerless-network/src/exports.ts
@@ -1,7 +1,8 @@
 export { NetworkStack, NetworkOptions } from './NetworkStack'
 export { NetworkNode, createNetworkNode } from './NetworkNode'
 export { StreamrNodeConfig } from './logic/StreamrNode'
-export { ProxyDirection } from './proto/packages/trackerless-network/protos/NetworkRpc'
+export { ProxyDirection, NodeInfoResponse } from './proto/packages/trackerless-network/protos/NetworkRpc'
+export { streamPartIdToDataKey } from './logic/EntryPointDiscovery'
 export {
     convertStreamMessageToBytes,
     convertBytesToStreamMessage
