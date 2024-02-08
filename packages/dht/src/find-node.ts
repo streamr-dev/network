13a58e8a11032b78279d1f7923b6e12f0ef5c243
juslesan <santeri.juslenius@gmail.com>
2024-02-08 15:34:27 +0200

    fix entry points

--- packages/dht/src/find-node.ts
+++ packages/dht/src/find-node.ts
@@ -6,12 +6,12 @@ import { DhtAddress } from "./identifiers"
 // 63876bf3c80c636dec99b016709d0573caaf161c
 // 60e56386b9276f64c820a4a8704e54ad2b5e481c
 const findNode = async () => {
-    const nodeId = '63876bf3c80c636dec99b016709d0573caaf161c'
+    const nodeId = '60e56386b9276f64c820a4a8704e54ad2b5e481c'
     const entryPoints = [{
         nodeId: hexToBinary("e5f87a7ee99b3c91e7b795b70f87ef8ba5497596"),
         type: NodeType.NODEJS,
         websocket: {
-            host: "polygon-entrypoint-1.streamr.network",
+            host: "polygon-entrypoint-3.streamr.network",
             port: 40402,
             tls: true
         }
@@ -20,7 +20,7 @@ const findNode = async () => {
         nodeId: hexToBinary("6f5b53812fd9cc07f225a0b3a6aa5b96672e852e"),
         type: NodeType.NODEJS,
         websocket: {
-            host: "polygon-entrypoint-2.streamr.network",
+            host: "polygon-entrypoint-4.streamr.network",
             port: 40402,
             tls: true
         }
