7daa2b68dde2b7b3030d56c22846cac2b0b3be60
Teo Gebhard <teo@streamr.com>
2024-02-28 18:24:50 +0200

    typo

--- packages/dht/src/helpers/version.ts
+++ packages/dht/src/helpers/version.ts
@@ -1,12 +1,12 @@
 export const LOCAL_PROTOCOL_VERSION = '1.0'
 
 /*
- * When two nodes negotiate whether they are compatible of not, it is up to the
+ * When two nodes negotiate whether they are compatible or not, it is up to the
  * newer version to decide if it supports the old version or not.
  * 
  * The older version assumes optimistically that it may be supported by the newer
  * version. It can't know for sure, but the other node will tell if it is not
- * supported (e.g. rejecting the handshake with UNSUPPORTED_VERSION error)
+ * supported (e.g. rejecting the handshake with UNSUPPORTED_VERSION error).
  */
 export const isMaybeSupportedVersion = (remoteVersion: string): boolean => {
     const localMajor = parseVersion(LOCAL_PROTOCOL_VERSION)!.major
