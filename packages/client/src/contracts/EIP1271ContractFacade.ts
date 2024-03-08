9e71192c35963f20f98181982f147cec56311848
Eric Andrews <harbu88@hotmail.com>
2024-03-08 15:25:15 +0200

    make EIP1271ContractFacade work on multiple contracts

--- packages/client/src/contracts/EIP1271ContractFacade.ts
+++ packages/client/src/contracts/EIP1271ContractFacade.ts
@@ -6,31 +6,31 @@ import ERC1271ContractArtifact from '../ethereumArtifacts/IERC1271Abi.json'
 import type { IERC1271 as ERC1271Contract } from '../ethereumArtifacts/IERC1271'
 import { StrictStreamrClientConfig } from '../Config'
 import { queryAllReadonlyContracts } from '../utils/contract'
+import { Mapping } from '../utils/Mapping'
 
 const SUCCESS_MAGIC_VALUE = '0x1626ba7e' // Magic value for success as defined by ERC-1271
 
 class EIP1271ContractFacade {
-    private readonly contractsReadonly: ERC1271Contract[]
+    private readonly contractsByAddress: Mapping<[EthereumAddress], ERC1271Contract[]>
 
-    constructor(
-        eipContractAddress: EthereumAddress,
-        contractFactory: ContractFactory,
-        config: Pick<StrictStreamrClientConfig, 'contracts'>,
-    ) {
-        this.contractsReadonly = getStreamRegistryChainProviders(config).map((provider: Provider) => {
-            return contractFactory.createReadContract(
-                eipContractAddress,
-                ERC1271ContractArtifact,
-                provider,
-                'erc1271Contract'
-            ) as ERC1271Contract
+    constructor(contractFactory: ContractFactory, config: Pick<StrictStreamrClientConfig, 'contracts'>) {
+        this.contractsByAddress = new Mapping<[EthereumAddress], ERC1271Contract[]>(async (address) => {
+            return getStreamRegistryChainProviders(config).map((provider: Provider) => {
+                return contractFactory.createReadContract(
+                    address,
+                    ERC1271ContractArtifact,
+                    provider,
+                    'erc1271Contract'
+                ) as ERC1271Contract
+            })
         })
     }
 
-    async isValidSignature(hash: Uint8Array, signature: Uint8Array): Promise<boolean> {
+    async isValidSignature(contractAddress: EthereumAddress, hash: Uint8Array, signature: Uint8Array): Promise<boolean> {
+        const contracts = await this.contractsByAddress.get(contractAddress)
         const result = await queryAllReadonlyContracts((contract) => {
             return contract.isValidSignature(hash, signature) // TODO: do we need to convert hash and signature to hex?
-        }, this.contractsReadonly)
+        }, contracts)
         return result === SUCCESS_MAGIC_VALUE
     }
 }
