e16620114b07e32be1395f5f3e719a8ba0c4a37f
Eric Andrews <harbu88@hotmail.com>
2024-03-12 19:07:36 +0200

    wip 2

--- packages/client/test/unit/EIP1271ContractFacade.test.ts
+++ packages/client/test/unit/EIP1271ContractFacade.test.ts
@@ -1,15 +1,38 @@
+import 'reflect-metadata'
 import { EIP1271ContractFacade } from '../../src/contracts/EIP1271ContractFacade'
-import { ContractFactory } from '@ethersproject/contracts'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { StrictStreamrClientConfig } from '../../src'
+import type { IERC1271 as ERC1271Contract } from '../../src/ethereumArtifacts/IERC1271'
+import { randomEthereumAddress } from '@streamr/test-utils'
+import { EthereumAddress } from '@streamr/utils'
+import range from 'lodash/range'
+
+const CONTRACT_ADDRESS = randomEthereumAddress()
 
 describe('EIP1271ContractFacade', () => {
+    let contract: MockProxy<ERC1271Contract>
     let contractFacade: EIP1271ContractFacade
 
     beforeEach(() => {
+        contract = mock<ERC1271Contract>()
         contractFacade = new EIP1271ContractFacade(
             undefined as any,
-            { } as StrictStreamrClientConfig
+            { } as StrictStreamrClientConfig,
+            async (address: EthereumAddress) => {
+                if (address === CONTRACT_ADDRESS) {
+                    return [contract]
+                } else {
+                    return [mock<ERC1271Contract>()]
+                }
+            }
         )
+    })
+
+    it('isValidSignature', async () => {
+        const hash = new Uint8Array([1, 2, 3])
+        const signature = new Uint8Array(range(65))
+        contract.isValidSignature.calledWith(hash, signature).mockResolvedValue('0x1626ba7e')
+        const result = await contractFacade.isValidSignature(CONTRACT_ADDRESS, hash, signature)
+        expect(result).toEqual(true)
     })
 })
