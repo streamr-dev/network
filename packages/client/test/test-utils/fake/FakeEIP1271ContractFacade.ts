a03fa9124ce3979ed3c93497a8b4c45f8b06b4f8
Eric Andrews <harbu88@hotmail.com>
2024-03-14 14:07:02 +0200

    fix FakeEIP1271ContractFacade

--- packages/client/test/test-utils/fake/FakeEIP1271ContractFacade.ts
+++ packages/client/test/test-utils/fake/FakeEIP1271ContractFacade.ts
@@ -1,10 +1,17 @@
 import { Methods } from '@streamr/test-utils'
 import { EIP1271ContractFacade } from '../../../src/contracts/EIP1271ContractFacade'
 import { EthereumAddress } from '@streamr/utils'
+import { Promise } from 'ts-toolbelt/out/Any/Promise'
+import { IERC1271 } from '../../../src/ethereumArtifacts/IERC1271'
 
 export class FakeEIP1271ContractFacade implements Methods<EIP1271ContractFacade> {
     // eslint-disable-next-line class-methods-use-this
-    async isValidSignature(_contractAddress: EthereumAddress, _data: Uint8Array, _signature: Uint8Array): Promise<boolean> {
-        throw new Error('not implemented')
+    async isValidSignature(_contractAddress: EthereumAddress, _payload: Uint8Array, _signature: Uint8Array): Promise<boolean> {
+        throw new Error('Not implemented')
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    setInstantiateContracts(_instantiateContracts: (address: EthereumAddress) => IERC1271[]): void {
+        throw new Error('Not implemented')
     }
 }
