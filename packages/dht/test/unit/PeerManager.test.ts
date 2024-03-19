661d2391a85965dc7767e4c4aca8839a3bff34a2
Teo Gebhard <teo@streamr.com>
2024-03-19 16:03:40 +0200

    feat(dht): Optimize `PeerManager#getClosestContactsTo` and `PeerManager#getClosestNeighborsTo` (#2442)
    Optimized the methods by defining `maxSize` for `SortedContactList` instances.
    
    ## Benchmarks
    
    - 200 contacts in the `PeerManager`
    - find 5 closest  nodes
    - 2 random nodes excluded
    - default settings (`maxNeighborListSize`: 200, `numberOfNodesPerKBucket`: 8)
    - do 1000 queries
    
    `getClosestContactsTo`:
    - before: ~3500 ms
    - after: ~1500 ms
    
    `getClosestNeighborsTo`:
    - before: ~580 ms
    - after: ~280 ms

--- packages/dht/test/unit/PeerManager.test.ts
+++ packages/dht/test/unit/PeerManager.test.ts
@@ -6,29 +6,49 @@ import { DhtNodeRpcRemote } from '../../src/dht/DhtNodeRpcRemote'
 import { MockRpcCommunicator } from '../utils/mock/MockRpcCommunicator'
 import { createMockPeerDescriptor } from '../utils/utils'
 
+const createPeerManager = (nodeIds: DhtAddress[]) => {
+    const peerDescriptor = createMockPeerDescriptor()
+    const manager = new PeerManager({
+        localNodeId: getNodeIdFromPeerDescriptor(peerDescriptor),
+        localPeerDescriptor: peerDescriptor,
+        createDhtNodeRpcRemote: (peerDescriptor: PeerDescriptor) => {
+            return new DhtNodeRpcRemote(undefined as any, peerDescriptor, undefined as any, new MockRpcCommunicator())
+        }
+    } as any)
+    const contacts = nodeIds.map((n) => ({ nodeId: getRawFromDhtAddress(n), type: NodeType.NODEJS }))
+    for (const contact of contacts) {
+        manager.addContact(contact)
+    }
+    return manager
+}
+
 describe('PeerManager', () => {
 
     it('getClosestContactsTo', () => {
         const nodeIds = range(10).map(() => createRandomDhtAddress())
-        const peerDescriptor = createMockPeerDescriptor()
-        const manager = new PeerManager({
-            localNodeId: getNodeIdFromPeerDescriptor(peerDescriptor),
-            localPeerDescriptor: peerDescriptor,
-            createDhtNodeRpcRemote: (peerDescriptor: PeerDescriptor) => {
-                return new DhtNodeRpcRemote(undefined as any, peerDescriptor, undefined as any, new MockRpcCommunicator())
-            }
-        } as any)
-        const contacts = nodeIds.map((n) => ({ nodeId: getRawFromDhtAddress(n), type: NodeType.NODEJS }))
-        for (const contact of contacts) {
-            manager.addContact(contact)
-        }
-
+        const manager = createPeerManager(nodeIds)
         const referenceId = createRandomDhtAddress()
         const excluded = new Set<DhtAddress>(sampleSize(nodeIds, 2))
+
         const actual = manager.getClosestContactsTo(referenceId, 5, excluded)
 
         const expected = sortBy(
             without(nodeIds, ...Array.from(excluded.values())),
+            (n: DhtAddress) => getDistance(getRawFromDhtAddress(n), getRawFromDhtAddress(referenceId))
+        ).slice(0, 5)
+        expect(actual.map((n) => n.getNodeId())).toEqual(expected)
+    })
+
+    it('getClosestNeighborsTo', () => {
+        const nodeIds = range(10).map(() => createRandomDhtAddress())
+        const manager = createPeerManager(nodeIds)
+        const referenceId = createRandomDhtAddress()
+        const excluded = new Set<DhtAddress>(sampleSize(nodeIds, 2))
+
+        const actual = manager.getClosestNeighborsTo(referenceId, 5, excluded)
+
+        const expected = sortBy(
+            without(manager.getNeighbors().map((n) => getNodeIdFromPeerDescriptor(n)), ...Array.from(excluded.values())),
             (n: DhtAddress) => getDistance(getRawFromDhtAddress(n), getRawFromDhtAddress(referenceId))
         ).slice(0, 5)
         expect(actual.map((n) => n.getNodeId())).toEqual(expected)
