e63a2d952d324930bf357ce0feed44180b6f3b8d
Eric Andrews <harbu88@hotmail.com>
2024-03-26 18:55:49 +0200

    test: add end-to-end test

--- packages/client/test/end-to-end/erc1271-publish-subscribe.test.ts
+++ packages/client/test/end-to-end/erc1271-publish-subscribe.test.ts
@@ -11,7 +11,7 @@ import { MessageMetadata } from '../../src'
 const PAYLOAD = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 const TIMEOUT = 30 * 1000
 
-describe('ERC-1271: publish and subscribe', () => {
+describe('ERC-1271: publish', () => {
     let publisherWallet: Wallet
     let subscriberWallet: Wallet
     let erc1271ContractAddress: EthereumAddress
@@ -72,6 +72,68 @@ describe('ERC-1271: publish and subscribe', () => {
             } else {
                 expect(metadatas[0].groupKeyId).toBeString()
             }
+            expect(areEqualBinaries(messages[0] as Uint8Array, PAYLOAD)).toBe(true)
+        }, TIMEOUT)
+    })
+})
+
+describe('ERC-1271: subscribe', () => {
+    let publisherWallet: Wallet
+    let subscriberWallet: Wallet
+    let erc1271ContractAddress: EthereumAddress
+
+    beforeAll(async () => {
+        subscriberWallet = fastWallet()
+        publisherWallet = new Wallet(await fetchPrivateKeyWithGas())
+        erc1271ContractAddress = await deployTestERC1271Contract([toEthereumAddress(subscriberWallet.address)])
+    }, TIMEOUT)
+
+    async function createStream(): Promise<StreamID> {
+        const creator = createTestClient(await fetchPrivateKeyWithGas())
+        const stream = await createTestStream(creator, module)
+        await stream.grantPermissions({
+            permissions: [StreamPermission.PUBLISH],
+            user: publisherWallet.address
+        })
+        await stream.grantPermissions({
+            permissions: [StreamPermission.SUBSCRIBE],
+            user: erc1271ContractAddress
+
+        })
+        await creator.destroy()
+        return stream.id
+    }
+
+    describe('private stream', () => {
+        let publisher: StreamrClient
+        let subscriber: StreamrClient
+        let streamId: StreamID
+
+        beforeEach(async () => {
+            subscriber = createTestClient(subscriberWallet.privateKey)
+            publisher = createTestClient(publisherWallet.privateKey)
+            streamId = await createStream()
+        }, TIMEOUT)
+
+        afterEach(async () => {
+            await subscriber.destroy()
+            await publisher.destroy()
+        })
+
+        it('subscriber configured with ERC-1271 contract can receive messages', async () => {
+            const messages: unknown[] = []
+            const metadatas: MessageMetadata[] = []
+            await subscriber.subscribe({
+                streamId,
+                erc1271Contract: erc1271ContractAddress
+            }, (msg: any, metadata) => {
+                messages.push(msg)
+                metadatas.push(metadata)
+            })
+            await publisher.publish(streamId, PAYLOAD)
+            await waitForCondition(() => messages.length > 0, TIMEOUT)
+            expect(metadatas[0].signatureType).toEqual('SECP256K1')
+            expect(metadatas[0].groupKeyId).toBeString()
             expect(areEqualBinaries(messages[0] as Uint8Array, PAYLOAD)).toBe(true)
         }, TIMEOUT)
     })
