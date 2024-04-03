a8b31236756b34a5bdc8869efb62558aed87c608
Petri Savolainen <petri.savolainen@spaceify.net>
2024-04-03 14:42:16 +0300

    improve concurrency resilence of EsmLoader

--- packages/dht/src/helpers/EsmLoader.ts
+++ packages/dht/src/helpers/EsmLoader.ts
@@ -1,4 +1,5 @@
 import { isBrowserEnvironment } from './browser/isBrowserEnvironment'
+import { waitForCondition } from '@streamr/utils'
 
 declare global {
     // eslint-disable-next-line no-var, no-underscore-dangle
@@ -9,8 +10,8 @@ export type NodeDataChannel = typeof import('node-datachannel', { with: { "resol
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class EsmLoader {
-    private static nodeDataChannelModule: NodeDataChannel | undefined 
-    private static inited = false
+    private static nodeDataChannelModule: NodeDataChannel | undefined
+    private static importStarted = false
 
     static get nodeDataChannel(): NodeDataChannel {
         if (EsmLoader.nodeDataChannelModule === undefined) {
@@ -19,11 +20,18 @@ export class EsmLoader {
         return EsmLoader.nodeDataChannelModule
     }
     static async init(): Promise<void> {
-        if (!EsmLoader.inited && !isBrowserEnvironment() && typeof _streamr_electron_test === 'undefined') {
-            EsmLoader.inited = true
-            // eslint-disable-next-line require-atomic-updates
-            EsmLoader.nodeDataChannelModule = await import('node-datachannel')
-            EsmLoader.nodeDataChannelModule.initLogger('Fatal')
+        if (!isBrowserEnvironment() && typeof _streamr_electron_test === 'undefined') {
+
+            if (!EsmLoader.importStarted) {
+                // if import has not started yet, start it
+                EsmLoader.importStarted = true
+                // eslint-disable-next-line require-atomic-updates
+                EsmLoader.nodeDataChannelModule = await import('node-datachannel')
+                EsmLoader.nodeDataChannelModule.initLogger('Fatal')
+            } else if (EsmLoader.nodeDataChannelModule === undefined) {
+                // if another import has started but not finished yet, wait for it to finish
+                await waitForCondition(() => EsmLoader.nodeDataChannelModule !== undefined)
+            }
         }
     }
 }
