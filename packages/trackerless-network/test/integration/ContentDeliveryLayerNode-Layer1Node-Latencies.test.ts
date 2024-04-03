9c74311e8c873b91c4f311c13d5490caf5f9d31a
Teo Gebhard <teo@streamr.com>
2024-04-03 16:12:30 +0300

    rename variables in tests

--- packages/trackerless-network/test/integration/ContentDeliveryLayerNode-Layer1Node-Latencies.test.ts
+++ packages/trackerless-network/test/integration/ContentDeliveryLayerNode-Layer1Node-Latencies.test.ts
@@ -8,38 +8,38 @@ import { StreamPartIDUtils } from '@streamr/protocol'
 import { Layer1Node } from '../../src/logic/Layer1Node'
 
 describe('ContentDeliveryLayerNode-DhtNode-Latencies', () => {
-    const nodeCount = 64
-    let layer1Nodes: Layer1Node[]
-    let dhtEntryPoint: Layer1Node
+    const otherNodeCount = 64
+    let otherLayer1Nodes: Layer1Node[]
+    let entryPointLayer1Node: Layer1Node
     let entryPointContentDeliveryLayerNode: ContentDeliveryLayerNode
-    let graphNodes: ContentDeliveryLayerNode[]
+    let otherContentDeliveryLayerNodes: ContentDeliveryLayerNode[]
 
     const streamPartId = StreamPartIDUtils.parse('stream#0')
     const entrypointDescriptor = createMockPeerDescriptor()
 
-    const peerDescriptors: PeerDescriptor[] = range(nodeCount).map(() => createMockPeerDescriptor())
+    const peerDescriptors: PeerDescriptor[] = range(otherNodeCount).map(() => createMockPeerDescriptor())
     beforeEach(async () => {
         const simulator = new Simulator(LatencyType.FIXED, 50)
         const entrypointCm = new SimulatorTransport(entrypointDescriptor, simulator)
-        const cms: SimulatorTransport[] = range(nodeCount).map((i) =>
+        const cms: SimulatorTransport[] = range(otherNodeCount).map((i) =>
             new SimulatorTransport(peerDescriptors[i], simulator)
         )
         await entrypointCm.start()
         await Promise.all(cms.map((cm) => cm.start()))
 
-        dhtEntryPoint = new DhtNode({
+        entryPointLayer1Node = new DhtNode({
             transport: entrypointCm,
             peerDescriptor: entrypointDescriptor,
             serviceId: streamPartId
         })
-        layer1Nodes = range(nodeCount).map((i) => new DhtNode({
+        otherLayer1Nodes = range(otherNodeCount).map((i) => new DhtNode({
             transport: cms[i],
             peerDescriptor: peerDescriptors[i],
             serviceId: streamPartId
         }))
-        graphNodes = range(nodeCount).map((i) => createContentDeliveryLayerNode({
+        otherContentDeliveryLayerNodes = range(otherNodeCount).map((i) => createContentDeliveryLayerNode({
             streamPartId,
-            layer1Node: layer1Nodes[i],
+            layer1Node: otherLayer1Nodes[i],
             transport: cms[i],
             connectionLocker: cms[i],
             localPeerDescriptor: peerDescriptors[i],
@@ -47,50 +47,50 @@ describe('ContentDeliveryLayerNode-DhtNode-Latencies', () => {
         }))
         entryPointContentDeliveryLayerNode = createContentDeliveryLayerNode({
             streamPartId,
-            layer1Node: dhtEntryPoint,
+            layer1Node: entryPointLayer1Node,
             transport: entrypointCm,
             connectionLocker: entrypointCm,
             localPeerDescriptor: entrypointDescriptor,
             isLocalNodeEntryPoint: () => false
         })
 
-        await dhtEntryPoint.start()
-        await dhtEntryPoint.joinDht([entrypointDescriptor])
-        await Promise.all(layer1Nodes.map((node) => node.start()))
+        await entryPointLayer1Node.start()
+        await entryPointLayer1Node.joinDht([entrypointDescriptor])
+        await Promise.all(otherLayer1Nodes.map((node) => node.start()))
     })
 
     afterEach(async () => {
-        dhtEntryPoint.stop()
+        entryPointLayer1Node.stop()
         entryPointContentDeliveryLayerNode.stop()
-        await Promise.all(layer1Nodes.map((node) => node.stop()))
-        await Promise.all(graphNodes.map((node) => node.stop()))
+        await Promise.all(otherLayer1Nodes.map((node) => node.stop()))
+        await Promise.all(otherContentDeliveryLayerNodes.map((node) => node.stop()))
     })
 
     it('happy path single node', async () => {
-        await layer1Nodes[0].joinDht([entrypointDescriptor])
+        await otherLayer1Nodes[0].joinDht([entrypointDescriptor])
         entryPointContentDeliveryLayerNode.start()
-        await graphNodes[0].start()
+        await otherContentDeliveryLayerNodes[0].start()
         await Promise.all([
-            waitForCondition(() => graphNodes[0].getNearbyNodeView().getIds().length === 1),
-            waitForCondition(() => graphNodes[0].getNeighbors().length === 1)
+            waitForCondition(() => otherContentDeliveryLayerNodes[0].getNearbyNodeView().getIds().length === 1),
+            waitForCondition(() => otherContentDeliveryLayerNodes[0].getNeighbors().length === 1)
         ])
-        expect(graphNodes[0].getNearbyNodeView().getIds().length).toEqual(1)
-        expect(graphNodes[0].getNeighbors().length).toEqual(1)
+        expect(otherContentDeliveryLayerNodes[0].getNearbyNodeView().getIds().length).toEqual(1)
+        expect(otherContentDeliveryLayerNodes[0].getNeighbors().length).toEqual(1)
     })
 
     it('happy path 5 nodes', async () => {
         entryPointContentDeliveryLayerNode.start()
-        range(4).forEach((i) => graphNodes[i].start())
+        range(4).forEach((i) => otherContentDeliveryLayerNodes[i].start())
         await Promise.all(range(4).map(async (i) => {
-            await layer1Nodes[i].joinDht([entrypointDescriptor])
+            await otherLayer1Nodes[i].joinDht([entrypointDescriptor])
         }))
-        await waitForCondition(() => range(4).every((i) => graphNodes[i].getNeighbors().length >= 4), 15000, 1000)
+        await waitForCondition(() => range(4).every((i) => otherContentDeliveryLayerNodes[i].getNeighbors().length >= 4), 15000, 1000)
         range(4).forEach((i) => {
-            expect(graphNodes[i].getNearbyNodeView().getIds().length).toBeGreaterThanOrEqual(4)
-            expect(graphNodes[i].getNeighbors().length).toBeGreaterThanOrEqual(4)
+            expect(otherContentDeliveryLayerNodes[i].getNearbyNodeView().getIds().length).toBeGreaterThanOrEqual(4)
+            expect(otherContentDeliveryLayerNodes[i].getNeighbors().length).toBeGreaterThanOrEqual(4)
         })
         // Check bidirectionality
-        const allNodes = graphNodes
+        const allNodes = otherContentDeliveryLayerNodes
         allNodes.push(entryPointContentDeliveryLayerNode)
         range(5).forEach((i) => {
             const ownNodeId = allNodes[i].getOwnNodeId()
@@ -105,26 +105,26 @@ describe('ContentDeliveryLayerNode-DhtNode-Latencies', () => {
     }, 60000)
 
     it('happy path 64 nodes', async () => {
-        await Promise.all(range(nodeCount).map((i) => graphNodes[i].start()))
-        await Promise.all(range(nodeCount).map((i) => {
-            layer1Nodes[i].joinDht([entrypointDescriptor])
+        await Promise.all(range(otherNodeCount).map((i) => otherContentDeliveryLayerNodes[i].start()))
+        await Promise.all(range(otherNodeCount).map((i) => {
+            otherLayer1Nodes[i].joinDht([entrypointDescriptor])
         }))
-        await Promise.all(graphNodes.map((node) =>
+        await Promise.all(otherContentDeliveryLayerNodes.map((node) =>
             waitForCondition(() => node.getNeighbors().length >= 4, 10000)
         ))
 
-        await Promise.all(graphNodes.map((node) =>
+        await Promise.all(otherContentDeliveryLayerNodes.map((node) =>
             waitForCondition(() => node.getOutgoingHandshakeCount() === 0)
         ))
 
         await waitForCondition(() => {
             let mismatchCounter = 0
-            graphNodes.forEach((node) => {
+            otherContentDeliveryLayerNodes.forEach((node) => {
                 const nodeId = node.getOwnNodeId()
                 node.getNeighbors().forEach((neighbor) => {
                     const neighborId = getNodeIdFromPeerDescriptor(neighbor)
                     if (neighborId !== entryPointContentDeliveryLayerNode.getOwnNodeId()) {
-                        const neighbor = graphNodes.find((n) => n.getOwnNodeId() === neighborId)
+                        const neighbor = otherContentDeliveryLayerNodes.find((n) => n.getOwnNodeId() === neighborId)
                         const neighborIds = neighbor!.getNeighbors().map((n) => getNodeIdFromPeerDescriptor(n))
                         if (!neighborIds.includes(nodeId)) {
                             mismatchCounter += 1
