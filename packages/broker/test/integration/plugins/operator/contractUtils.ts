0db8680b312126c6bce93171721d40b8d7c194f2
Eric Andrews <harbu88@hotmail.com>
2024-05-16 15:14:58 +0300

    Revert "try batchMaxCount=1"
    This reverts commit 6030a968bac80a7262f7b31fa74eae1c4809c542.

--- packages/broker/test/integration/plugins/operator/contractUtils.ts
+++ packages/broker/test/integration/plugins/operator/contractUtils.ts
@@ -162,7 +162,7 @@ export async function deploySponsorshipContract(opts: DeploySponsorshipContractO
 }
 
 export function getProvider(): Provider {
-    return new JsonRpcProvider(TEST_CHAIN_CONFIG.rpcEndpoints[0].url, undefined, { batchMaxCount: 1 })
+    return new JsonRpcProvider(TEST_CHAIN_CONFIG.rpcEndpoints[0].url)
 }
 
 export function getTokenContract(): TestToken {
