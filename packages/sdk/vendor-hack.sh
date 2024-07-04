10623403c80c270209aaae1849f0e9e972d7d521
Eric Andrews <harbu88@hotmail.com>
2024-07-04 08:56:09 +0200

    chore: rename client directory to sdk

--- packages/sdk/vendor-hack.sh
+++ packages/sdk/vendor-hack.sh
@@ -2,7 +2,7 @@
 
 # A hack to deal with "quick-lru" package being an ESM module. Copies the
 # package from folder "node_modules" into "vendor" leaving out "package.json".
-# TODO: remove when client package converted to ESM package
+# TODO: remove when sdk package converted to ESM package
 
 SRCDIR=`node -p "path.dirname(require.resolve('quick-lru'))"`
 mkdir -p vendor/quick-lru
--- release.sh
+++ release.sh
@@ -9,13 +9,13 @@ fi
 
 npm run build
 
-# Build client's webpack bundle
-cd packages/client || exit
+# Build sdk webpack bundle
+cd packages/sdk || exit
 npm run build-browser
 if [ $? -ne 0 ]
 then
     echo
-    echo 'Client build failed, did not publish all packages!'
+    echo 'SDK build failed, did not publish all packages!'
     echo
     exit 1
 fi
@@ -57,7 +57,7 @@ cd packages/trackerless-network || exit
 npm publish --access public --tag $NPM_TAG
 cd ../..
 
-cd packages/client/dist || exit # Notice: dist folder
+cd packages/sdk/dist || exit # Notice: dist folder
 npm publish --access public --tag $NPM_TAG
 cd ../../..
 
