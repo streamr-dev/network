19107748fa55b82b3f3a861d3f4f872804a2f7e0
juslesan <santeri.juslenius@gmail.com>
2024-07-26 15:30:18 +0300

    register local server methods

---
+++ packages/trackerless-network/src/logic/ExternalNetworkRpc.ts
@@ -0,0 +1,29 @@
+import { IMessageType } from '@protobuf-ts/runtime'
+import { ServerCallContext } from '@protobuf-ts/runtime-rpc'
+import { ITransport, ListeningRpcCommunicator } from '@streamr/dht'
+
+export const SERVICE_ID = 'external-network-service'
+
+export class ExternalNetworkRpc {
+
+    private readonly rpcCommunicator: ListeningRpcCommunicator
+
+    constructor(transport: ITransport) {
+        this.rpcCommunicator = new ListeningRpcCommunicator(SERVICE_ID, transport)
+    }
+
+    registerRpcMethod<
+        RequestClass extends IMessageType<RequestType>,
+        ResponseClass extends IMessageType<ResponseType>,
+        RequestType extends object,
+        ResponseType extends object
+    >(
+        request: RequestClass,
+        response: ResponseClass,
+        name: string, 
+        fn: (req: RequestType, context: ServerCallContext) => Promise<ResponseType>
+    ): void {
+        this.rpcCommunicator.registerRpcMethod(request, response, name, fn)
+    }
+
+}
---
+++ packages/trackerless-network/test/unit/ExternalNetworkRpc.test.ts
@@ -0,0 +1,38 @@
+import { Message } from '@streamr/dht'
+import { ExternalNetworkRpc, SERVICE_ID } from '../../src/logic/ExternalNetworkRpc'
+import { HandshakeRequest, HandshakeResponse } from '../../src/proto/packages/dht/protos/DhtRpc'
+import { MockTransport } from '../utils/mock/MockTransport'
+import { RpcMessage } from '@streamr/proto-rpc/dist/src/proto/ProtoRpc'
+import { Any } from '../../src/proto/google/protobuf/any'
+
+describe('ExternalNetworkRpc', () => {
+
+    let rpc: ExternalNetworkRpc
+    let fn: jest.Mock
+    let transport: MockTransport
+
+    beforeEach(() => {
+        transport = new MockTransport
+        rpc = new ExternalNetworkRpc(transport)
+        fn = jest.fn()
+    })
+
+    it('registers method', async () => {
+        rpc.registerRpcMethod(HandshakeRequest, HandshakeResponse, 'handshake', () => fn())
+        transport.emit('message', Message.create({
+            serviceId: SERVICE_ID,
+            body: {
+                oneofKind: 'rpcMessage',
+                rpcMessage: RpcMessage.create({
+                    header: {
+                        request: 'request',
+                        method: 'handshake'
+                    },
+                    body: Any.pack(HandshakeRequest.create(), HandshakeRequest)
+                })
+            }
+        }))
+        expect(fn).toHaveBeenCalledTimes(1)
+    })
+    
+})
