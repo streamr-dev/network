48fafae0eefcb6f7bff8349c09b6915b6deb82ca
juslesan <santeri.juslenius@gmail.com>
2024-08-02 12:44:25 +0300

    Query operators on a stream from the graph

--- packages/sdk/src/contracts/OperatorRegistry.ts
+++ packages/sdk/src/contracts/OperatorRegistry.ts
@@ -3,8 +3,18 @@ import { shuffle } from 'lodash'
 import { Lifecycle, scoped } from 'tsyringe'
 import { NetworkPeerDescriptor } from '../Config'
 import { LoggerFactory } from '../utils/LoggerFactory'
+import { StreamID } from '../exports'
 
 // TODO maybe this class could be removed/renamed (the method could be extracted to be a function as there is no state)
+
+interface OperatorMetadata {
+    id: string
+    latestHeartbeatMetadata: string
+}
+
+interface Query {
+    query: string
+}
 
 @scoped(Lifecycle.ContainerScoped)
 export class OperatorRegistry {
@@ -24,11 +34,7 @@ export class OperatorRegistry {
         maxQueryResults: number, 
         maxHeartbeatAgeHours: number,
     ): Promise<NetworkPeerDescriptor[]> {
-        interface OperatorMetadata {
-            id: string
-            latestHeartbeatMetadata: string
-        }
-        const createQuery = () => {
+        const createQuery = (): Query => {
             return {
                 query: `{
                     operators(
@@ -46,13 +52,40 @@ export class OperatorRegistry {
                 }`
             }
         }
+        const peerDescriptors = await this.queryPeerDescriptors(createQuery)
+        const picked = shuffle(peerDescriptors).slice(0, maxEntryPoints)
+        this.logger.debug(`Found ${peerDescriptors.length} network entrypoints, picked ${picked.length}`, { picked })
+        return picked
+    }
+
+    async findOperatorsOnStream(streamId: StreamID, maxQueryResults: number): Promise<NetworkPeerDescriptor[]> {
+        const createQuery = (): Query => {
+            return {
+                query: `{
+                    stream(id: ${streamId}) {
+                        sponsorships(where: {isRunning: true}) {
+                            stakes(first: ${maxQueryResults}, orderBy: updateTimestamp, orderDirection: desc) {
+                                operator {
+                                    latestHeartbeatMetadata
+                                    latestHeartbeatTimestamp
+                                }
+                            }
+                        }
+                    }
+                }`
+            }
+        }
+        const peerDescriptors = await this.queryPeerDescriptors(createQuery)
+        return peerDescriptors
+    }
+
+    private async queryPeerDescriptors(createQuery: () => Query): Promise<NetworkPeerDescriptor[]> {
         const operatorMetadatas = this.theGraphClient.queryEntities<OperatorMetadata>(createQuery)
         const peerDescriptors: NetworkPeerDescriptor[] = []
         for await (const operator of operatorMetadatas) {
             peerDescriptors.push(JSON.parse(operator.latestHeartbeatMetadata))
         }
-        const picked = shuffle(peerDescriptors).slice(0, maxEntryPoints)
-        this.logger.debug(`Found ${peerDescriptors.length} network entrypoints, picked ${picked.length}`, { picked })
-        return picked
+        return peerDescriptors
     }
+
 }
