852323d20a05c09381f43ea753be91067a42093b
Eric Andrews <harbu88@hotmail.com>
2024-08-07 16:21:18 +0300

    fatal log level

--- packages/sdk/loop-run.sh
+++ packages/sdk/loop-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {1..100}; do
-    LOG_LEVEL=warn node dist/src/test.js
+    LOG_LEVEL=fatal node dist/src/test.js
     if [[ "$?" != '0' ]]; then
         echo 'Test-fail'
         echo "Exit after $i attempts"
