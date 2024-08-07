d45d7c78bbdc4404a72fbaf1c53a27a8c7bb1db2
Eric Andrews <harbu88@hotmail.com>
2024-08-07 17:59:07 +0300

    test

--- packages/sdk/src/test.ts
+++ packages/sdk/src/test.ts
@@ -1,14 +1,15 @@
 import { StreamrClient } from './StreamrClient'
 import { toStreamID, toStreamPartID } from '@streamr/utils'
-//import { NetworkNodeType } from './Config'
+import { NetworkNodeType } from './Config'
+import { ProxyDirection } from '@streamr/trackerless-network'
 
-const NODE_ID = 'f8619ca67b65ec5310426a87152ed25989514a1b'
+const NODE_ID = 'f8619ca67b65ec5310426a8715345afd6c0bac1c'
 const HOST = 'e12f6842-d716-4379-a1a8-5051ed202d04.streamr-nodes.xyz'
 const PORT = 32200
 
 const targetStreamPartId = toStreamPartID(toStreamID('streamr.eth/demos/video'), 0)
 
-/*const targetPeerDescriptor = {
+const targetPeerDescriptor = {
     nodeId: NODE_ID,
     type: NetworkNodeType.NODEJS,
     websocket: {
@@ -16,19 +17,19 @@ const targetStreamPartId = toStreamPartID(toStreamID('streamr.eth/demos/video'),
         port: PORT,
         tls: true
     }
-}*/
+}
 
 ;(async () => {
-    /*const streamrClient = new StreamrClient({
+    const streamrClient = new StreamrClient({
         network: {
             controlLayer: {
+                websocketPortRange: null,
                 entryPoints: [targetPeerDescriptor]
             }
         }
     })
     await streamrClient.setStreamPartitionEntryPoints(targetStreamPartId, [targetPeerDescriptor])
-     */
-    const streamrClient = new StreamrClient({})
+    //await streamrClient.setProxies(targetStreamPartId, [targetPeerDescriptor], ProxyDirection.SUBSCRIBE)
     const startTime = Date.now()
     await streamrClient.subscribe(targetStreamPartId, async (_message) => {
         const diff = Date.now() - startTime
