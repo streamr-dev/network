7ed4922ce1391589b89d815f99df02fda02b11c2
Eric Andrews <harbu88@hotmail.com>
2024-08-16 12:13:42 +0300

    update test-operator-discovery.ts

--- packages/sdk/src/test-operator-discovery.ts
+++ packages/sdk/src/test-operator-discovery.ts
@@ -1,22 +1,14 @@
 import { StreamrClient } from './StreamrClient'
 import { Logger, toStreamID, toStreamPartID } from '@streamr/utils'
 import { NetworkNodeType } from './Config'
+import { shuffle } from 'lodash'
 
 const STREAM_ID = toStreamID('streamr.eth/demos/video')
 const NODE_ID = 'f8619ca67b65ec5310426a8715d7a6b2253c8d1b'
 const HOST = 'e12f6842-d716-4379-a1a8-5051ed202d04.streamr-nodes.xyz'
 const PORT = 32200
 
-const targetStreamPartId = toStreamPartID(STREAM_ID, 4)
-const targetPeerDescriptor = {
-    nodeId: NODE_ID,
-    type: NetworkNodeType.NODEJS,
-    websocket: {
-        host: HOST,
-        port: PORT,
-        tls: true
-    }
-}
+const targetStreamPartId = toStreamPartID(STREAM_ID, 0)
 
 const streamrClient = new StreamrClient({
     metrics: false,
@@ -30,10 +22,22 @@ const streamrClient = new StreamrClient({
 ;(async () => {
     const node = streamrClient.getNode()
     const startTime = Date.now()
-    const peerDescriptors = await node.discoverOperators(targetPeerDescriptor, toStreamPartID(STREAM_ID, 0))
-    console.log('Discovered', JSON.stringify({
-        peerDescriptors,
-        elapsedTime: Date.now() - startTime
-    }))
-    process.exit(0)
+    const peerDescriptors = await streamrClient.findOperators(STREAM_ID)
+    const diff1 = Date.now() - startTime
+    console.log(`findOperators took ${diff1} ms`)
+    for (const targetPeerDescriptor of shuffle(peerDescriptors)) {
+        try {
+            const startTime = Date.now()
+            const operatorDescriptors = await node.discoverOperators(targetPeerDescriptor, targetStreamPartId)
+            if (operatorDescriptors.length > 0) {
+                const diff2 = Date.now() - startTime
+                console.log(`discoverOperators took ${diff2} ms (${operatorDescriptors.length} results)`)
+                console.log(`total took ${diff1 + diff2} ms`)
+                process.exit(0)
+            }
+        } catch {
+            // noop
+        }
+    }
+
 })()
