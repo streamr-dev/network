545e2a5e5795b6a0c604bfd17270443a3768305b
Teo Gebhard <teo@streamr.com>
2024-09-20 18:56:16 +0300

    update cli-tools test

--- packages/cli-tools/test/stream-permission.test.ts
+++ packages/cli-tools/test/stream-permission.test.ts
@@ -1,6 +1,8 @@
-import { fetchPrivateKeyWithGas, randomEthereumAddress } from '@streamr/test-utils'
-import 'jest-extended'
 import { StreamPermission } from '@streamr/sdk'
+import { fetchPrivateKeyWithGas } from '@streamr/test-utils'
+import { binaryToHex } from '@streamr/utils'
+import { randomBytes } from 'crypto'
+import 'jest-extended'
 import { createTestClient, runCommand } from './utils'
 
 describe('permission', () => {
@@ -9,18 +11,18 @@ describe('permission', () => {
         const privateKey = await fetchPrivateKeyWithGas()
         const client = createTestClient(privateKey)
         const stream = await client.createStream(`/${Date.now()}`)
-        const otherUser = randomEthereumAddress()
+        const otherUser = randomBytes(30)
         const hasPermission = () => client.hasPermission({
             user: otherUser,
             permission: StreamPermission.PUBLISH,
             streamId: stream.id,
             allowPublic: false
         })
-        await runCommand(`stream grant-permission ${stream.id} ${otherUser} publish`, {
+        await runCommand(`stream grant-permission ${stream.id} ${binaryToHex(otherUser, true)} publish`, {
             privateKey
         })
         expect(await hasPermission()).toBeTrue()
-        await runCommand(`stream revoke-permission ${stream.id} ${otherUser} publish`, {
+        await runCommand(`stream revoke-permission ${stream.id} ${binaryToHex(otherUser, true)} publish`, {
             privateKey
         })
         expect(await hasPermission()).toBeFalse()
