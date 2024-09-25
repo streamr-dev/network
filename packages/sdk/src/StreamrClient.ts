b253fcc66120da8d735e166024630fe6caa06328
Teo Gebhard <teo@streamr.com>
2024-09-25 17:19:51 +0300

    StreamrClient#addEncryptionKey argument type

--- packages/sdk/src/StreamrClient.ts
+++ packages/sdk/src/StreamrClient.ts
@@ -186,8 +186,8 @@ export class StreamrClient {
      * @remarks Keys will be added to the store automatically by the client as encountered. This method can be used to
      * manually add some known keys into the store.
      */
-    async addEncryptionKey(key: GroupKey, publisherId: UserID): Promise<void> {
-        await this.localGroupKeyStore.set(key.id, publisherId, key.data)
+    async addEncryptionKey(key: GroupKey, publisherId: string): Promise<void> {
+        await this.localGroupKeyStore.set(key.id, toEthereumAddress(publisherId), key.data)
     }
 
     // --------------------------------------------------------------------------------------------
