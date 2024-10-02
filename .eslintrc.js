5b957d613747ff31e79c5af54ba8972a43b4e8ff
Teo Gebhard <teo@streamr.com>
2024-10-03 02:47:19 +0300

    enable @typescript-eslint/await-thenable

--- .eslintrc.js
+++ .eslintrc.js
@@ -88,11 +88,11 @@ module.exports = {
             allowNever: true,
             allow: [{ from: 'lib', name: ['Error'] }]
         }],
+        '@typescript-eslint/await-thenable': 'error',
         // TODO: in follow up PRs, select which rules we should enable and fix the code. When all recommended rules
         //  have been enabled, consider enabling the "strict" preset.
         '@typescript-eslint/require-await': 'off',
         '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/await-thenable': 'off',
         '@typescript-eslint/no-misused-promises': 'off',
         '@typescript-eslint/no-unsafe-return': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
