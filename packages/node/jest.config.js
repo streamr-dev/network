73b744b02e6dc3784e84f0965b1a9013a11ab9f0
Teo Gebhard <teo@streamr.com>
2024-10-14 13:21:34 +0300

    rm unnecessary custom matcher setup

--- packages/node/jest.config.js
+++ packages/node/jest.config.js
@@ -2,8 +2,5 @@ const rootConfig = require('../../jest.config')
 module.exports = {
     ...rootConfig,
     globalTeardown: './jest.teardown.js',
-    setupFilesAfterEnv: rootConfig.setupFilesAfterEnv.concat(
-        '@streamr/test-utils/setupCustomMatchers'
-    ),
     testTimeout: 10000
 }
--- packages/node/test/types/global.d.ts
+++ packages/node/test/types/global.d.ts
@@ -1,3 +1,1 @@
 import 'jest-extended'
-// eslint-disable-next-line import/no-unresolved
-import '@streamr/test-utils/customMatcherTypes'
