9a6b6340a4f0c1eac4cbc6e691585b3ddf422591
Teo Gebhard <teo@streamr.com>
2024-10-14 13:32:40 +0300

    relative path in createTestStream()

--- packages/node/test/utils.ts
+++ packages/node/test/utils.ts
@@ -1,13 +1,14 @@
-import { EthereumAddress, merge, toEthereumAddress } from '@streamr/utils'
-import padEnd from 'lodash/padEnd'
-import { StreamrClient,
+import {
     CONFIG_TEST,
     NetworkPeerDescriptor,
     Stream,
     StreamMetadata,
     StreamPermission,
+    StreamrClient,
     StreamrClientConfig
 } from '@streamr/sdk'
+import { EthereumAddress, merge, toEthereumAddress } from '@streamr/utils'
+import padEnd from 'lodash/padEnd'
 import { Broker, createBroker } from '../src/broker'
 import { Config } from '../src/config/config'
 
@@ -109,7 +110,7 @@ export const createTestStream = async (
     module: NodeModule,
     props?: Partial<StreamMetadata>
 ): Promise<Stream> => {
-    const id = `${await streamrClient.getAddress()}/test/${getTestName(module)}/${Date.now()}`
+    const id = `/test/${getTestName(module)}/${Date.now()}`
     const stream = await streamrClient.createStream({
         id,
         ...props
