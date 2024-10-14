13f99ee342c3a44966ff1ad48a4d1f8294b16bee
Teo Gebhard <teo@streamr.com>
2024-10-14 18:40:44 +0300

    optimize

--- packages/utils/src/EthereumAddress.ts
+++ packages/utils/src/EthereumAddress.ts
@@ -5,7 +5,7 @@ const REGEX = /^0x[a-fA-F0-9]{40}$/
 export type EthereumAddress = BrandedString<'EthereumAddress'>
 
 export function toEthereumAddress(str: string): EthereumAddress | never {
-    if (str.match(REGEX)) {
+    if (REGEX.test(str)) {
         return str.toLowerCase() as EthereumAddress
     }
     throw new Error(`not a valid Ethereum address: "${str}"`)
