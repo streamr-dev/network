018f7d325c2101984b39a408c3101d243d387257
Teo Gebhard <teo@streamr.com>
2024-10-15 18:51:58 +0300

    refactor(node): Use `UserID` in `Storage` (#2809)
    Use `UserID` in `Storage`-related functionality. Also added query
    parameter validation for `dataQueryEnpoint` queries.

--- packages/utils/src/UserID.ts
+++ packages/utils/src/UserID.ts
@@ -2,6 +2,12 @@ import { EthereumAddress, toEthereumAddress } from './EthereumAddress'
 
 export type UserID = EthereumAddress
 
-export const toUserId = (value: string): UserID => {
-    return toEthereumAddress(value)
+const REGEX = /^0x[a-fA-F0-9]+$/
+
+export const toUserId = (input: string): UserID => {
+    return toEthereumAddress(input)
+}
+
+export const isValidUserId = (input: string): boolean => {
+    return REGEX.test(input)
 }
