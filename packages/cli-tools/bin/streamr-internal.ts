e26321f1d1fa4b332e78abafca24ee89ab54479e
Teo Gebhard <teo@streamr.com>
2024-10-24 02:59:08 +0300

    New internal `node-info` command (#2796)
    ## Changes
    
    Added new internal `node-info` command. It shows neighbors and
    connections for given node.
    
    ## Example
    
    ```
    streamr internal node-info 917accd69d7f30f997c57db9a0b7d4f86111c71b --env dev2
    ```
    
    ```
    {
        "peerDescriptor": {
            "nodeId": "917accd69d7f30f997c57db9a0f44af72304de1b",
            "type": 0,
            "websocket": {
                "port": 32406,
                "host": "10.200.10.1",
                "tls": false
            },
            "region": 8500,
            "ipAddress": 180881921,
            "publicKey": "68b06da79558a4d57d487f2faf2b3dab696eef72",
            "signature": "46c3c85463e64811d7e0f758f65a822c23fcc757f1aa18a6bd3b49fd08d1a41458d768432437429230729b4be90767b2bd1d3c42a32cac6acfdce5a5cd70c6331b"
        },
        "controlLayer": {
            "neighbors": [
                "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
                "917accd69d7f30f997c57db9a02cdd40d5a4741c"
            ],
            "connections": [
                "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
                "917accd69d7f30f997c57db9a02cdd40d5a4741c"
            ]
        },
        "streamPartitions": [
            {
                "id": "0x7e5f4552091a69125d5dfcb7b8c2659029395bdf/foobar#0",
                "controlLayerNeighbors": [],
                "contentDeliveryLayerNeighbors": []
            }
        ],
        "version": "101.1.2"
    }
    ```

--- packages/cli-tools/bin/streamr-internal.ts
+++ packages/cli-tools/bin/streamr-internal.ts
@@ -6,6 +6,7 @@ program
     .version(pkg.version)
     .usage('<command> [<args>]')
     .description('subcommands for internal use, the API of the commands may change')
+    .command('node-info', 'info about a node')
     .command('visualize-topology', 'visualize network topology')
     .command('show-sdk-config', 'show config used by internal StreamrClient')
     .parse()
