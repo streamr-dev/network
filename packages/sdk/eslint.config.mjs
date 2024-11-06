92da701e13752a55fa2dd2e0e2579647d46bd190
Teo Gebhard <teo@streamr.com>
2024-11-06 15:42:39 +0200

    Revert "temporarily ignore browser classes"
    This reverts commit 876423f8ceb61fe513765d5ca2b9aa1fa6c6ae46.

--- packages/sdk/eslint.config.mjs
+++ packages/sdk/eslint.config.mjs
@@ -7,9 +7,7 @@ export default [
             'vendor/**',
             'test/exports/**',
             'test/benchmarks/**',
-            'test/memory/*',
-            // TODO remove when https://github.com/streamr-dev/network/pull/2848 merged to main
-            'src/utils/persistence/BrowserPersistence.ts'
+            'test/memory/*'
         ]
     },
     ...baseConfig
