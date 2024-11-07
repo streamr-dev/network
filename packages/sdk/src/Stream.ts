7def4c2dd3ad1c5e14ca60b87798152c87fea1aa
Teo Gebhard <teo@streamr.com>
2024-11-07 23:30:42 +0200

    rm Stream#detectFields()

--- packages/sdk/src/Stream.ts
+++ packages/sdk/src/Stream.ts
@@ -2,7 +2,6 @@ import {
     HexString,
     StreamID,
     StreamPartID,
-    collect,
     toEthereumAddress,
     toStreamPartID
 } from '@streamr/utils'
@@ -10,7 +9,6 @@ import { isNumber, isString } from 'lodash'
 import range from 'lodash/range'
 import { PublishMetadata } from '../src/publish/Publisher'
 import { Message } from './Message'
-import { DEFAULT_PARTITION } from './StreamIDBuilder'
 import { StreamMetadata, getPartitionCount } from './StreamMetadata'
 import { StreamrClient } from './StreamrClient'
 import {
@@ -20,46 +18,6 @@ import {
     toInternalPermissionAssignment,
     toInternalPermissionQuery
 } from './permission'
-
-const VALID_FIELD_TYPES = ['number', 'string', 'boolean', 'list', 'map'] as const
-
-interface Field {
-    name: string
-    type: typeof VALID_FIELD_TYPES[number]
-}
-
-function getFieldType(value: any): (Field['type'] | undefined) {
-    const type = typeof value
-    switch (true) {
-        case Array.isArray(value): {
-            return 'list'
-        }
-        case type === 'object': {
-            return 'map'
-        }
-        case (VALID_FIELD_TYPES as readonly string[]).includes(type): {
-            // see https://github.com/microsoft/TypeScript/issues/36275
-            return type as Field['type']
-        }
-        default: {
-            return undefined
-        }
-    }
-}
-
-export const flatMerge = <TTarget>(...sources: (Partial<TTarget> | undefined)[]): TTarget => {
-    const result: Record<string, unknown> = {}
-    for (const source of sources) {
-        if (source !== undefined) {
-            for (const [key, value] of Object.entries(source)) {
-                if (value !== undefined) {
-                    result[key] = value
-                }
-            }
-        }
-    }
-    return result as TTarget
-}
 
 /**
  * A convenience API for managing and accessing an individual stream.
@@ -168,46 +126,6 @@ export class Stream {
      */
     async getStorageNodes(): Promise<HexString[]> {
         return this.client.getStorageNodes(this.id)
-    }
-
-    /**
-     * Attempts to detect and update the {@link StreamMetadata.config} metadata of the stream by performing a resend.
-     *
-     * @remarks Only works on stored streams.
-     *
-     * @returns be mindful that in the case of there being zero messages stored, the returned promise will resolve even
-     * though fields were not updated
-     */
-    async detectFields(): Promise<void> {
-        // Get last message of the stream to be used for field detecting
-        const sub = await this.client.resend(
-            toStreamPartID(this.id, DEFAULT_PARTITION),
-            {
-                last: 1
-            }
-        )
-
-        const receivedMsgs = await collect(sub)
-
-        if (!receivedMsgs.length) { return }
-
-        const lastMessage = receivedMsgs[0].content
-
-        const fields = Object.entries(lastMessage as any).map(([name, value]) => {
-            const type = getFieldType(value)
-            return !!type && {
-                name,
-                type,
-            }
-        }).filter(Boolean) as Field[] // see https://github.com/microsoft/TypeScript/issues/30621
-
-        // Save field config back to the stream
-        const merged = flatMerge(this.getMetadata(), {
-            config: {
-                fields
-            }
-        })
-        await this.update(merged)
     }
 
     /**
