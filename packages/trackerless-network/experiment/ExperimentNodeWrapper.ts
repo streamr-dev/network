0c5f4afeef94bd1cf9910babbf88ea0f59d1a9f6
juslesan <santeri.juslenius@gmail.com>
2024-11-14 12:17:22 +0200

    set routing batch size to 1

--- packages/trackerless-network/experiment/ExperimentNodeWrapper.ts
+++ packages/trackerless-network/experiment/ExperimentNodeWrapper.ts
@@ -191,7 +191,7 @@ export class ExperimentNodeWrapper {
         logger.info('running routing experiment')
         const client = this.node!.createExternalRoutingRpcClient(RoutingExperimentRpcClient)
         const results: any = []
-        const batches = chunk(nodes, 2)
+        const batches = chunk(nodes, 1)
         for (const batch of batches) {
             await Promise.all(batch.map(async (node) => {
                 try {
