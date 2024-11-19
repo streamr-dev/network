e471831921fbae59ec32bb293fae184e1bb41820
juslesan <santeri.juslenius@gmail.com>
2024-11-19 16:27:39 +0200

    increase wait time

--- packages/trackerless-network/experiment/ExperimentController.ts
+++ packages/trackerless-network/experiment/ExperimentController.ts
@@ -90,7 +90,7 @@ export class ExperimentController {
                 onConditionFn()
             }
             return this.clients.size === this.nodeCount 
-        }, 10 * 60 * 1000, 10000)
+        }, 20 * 60 * 1000, 10000)
         
     }
 
