2665287ef8bf725690e7691cfee6cda00be8a762
juslesan <santeri.juslenius@gmail.com>
2024-11-19 14:48:31 +0200

    add more regions

--- packages/trackerless-network/experiment/run.ts
+++ packages/trackerless-network/experiment/run.ts
@@ -12,7 +12,25 @@ const experiment = process.argv[2]
 const env = process.argv[3]
 const nodeCountPerRegion = parseInt(process.argv[4])
 
-const REGIONS = [ 'eu-north-1', 'eu-central-1' ]
+const REGIONS = [
+    'eu-north-1',
+    'eu-central-1',
+    // 'eu-west-1',
+    'eu-west-2',
+    'eu-west-3',
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2',
+    'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-northeast-3',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'ap-south-1',
+    'sa-east-1',
+    'ca-central-1',
+]
 const AUTO_SCALING_GROUP_NAME = 'network-experiment-tf-test'
 
 if (!modes.includes(experiment)) {
