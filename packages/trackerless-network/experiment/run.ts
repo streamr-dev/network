af450808798d9151331057e4ea6f166f717fca17
juslesan <santeri.juslenius@gmail.com>
2024-11-20 11:57:07 +0200

    Revert "cpu profiling"
    This reverts commit 8722cf035b45835684acb4c38e24cb383068ef77.

--- packages/trackerless-network/experiment/run.ts
+++ packages/trackerless-network/experiment/run.ts
@@ -14,24 +14,24 @@ const nodeCountPerRegion = parseInt(process.argv[4])
 
 const REGIONS = [
     'eu-north-1',
-    // 'eu-central-1',
+    'eu-central-1',
     // 'eu-west-1',
-    // 'eu-west-2',
-    // 'eu-west-3',
-    // 'us-east-1',
-    // 'us-east-2',
-    // 'us-west-1',
-    // 'us-west-2',
-    // 'ap-northeast-1',
-    // 'ap-northeast-2',
-    // 'ap-northeast-3',
-    // 'ap-southeast-1',
-    // 'ap-southeast-2',
-    // 'ap-south-1',
-    // 'sa-east-1',
-    // 'ca-central-1',
+    'eu-west-2',
+    'eu-west-3',
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2',
+    'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-northeast-3',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'ap-south-1',
+    'sa-east-1',
+    'ca-central-1',
 ]
-const AUTO_SCALING_GROUP_NAME = 'network-experiment'
+const AUTO_SCALING_GROUP_NAME = 'network-experiment-tf-test'
 
 if (!modes.includes(experiment)) {
     throw new Error('unknown experiment ' + experiment)
