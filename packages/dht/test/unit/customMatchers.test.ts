5c639daf8b4b67b3e794975a66ab1b470a538149
Teo Gebhard <teo@streamr.com>
2024-11-21 14:57:44 +0200

    test(dht): Fix `toEqualPeerDescriptor` inverse custom matcher (#2892)
    Fixed error message for `toBeEqualPeerDescriptor` for a use case where
    it is used like this:
    ```ts
    expect(foo).not.toEqualPeerDescriptor(foo)
    ```
    
    ## Other changes
    
    Also fixed `tcp` field name in error messages. The custom matcher
    implementation now uses stricter types.
    
    Also improved error message for normal use case. Now the error message
    mentions that we are comparing `PeerDescriptor` instances. Typical error
    message is like this:
    ```
    PeerDescriptor nodeId values don't match:
    Expected: "5d7e7be5ba676dda8a390d557e345c85e19458b4"
    Received: "61c7c821496d1d151ea1932655c40140f6cd44d2"
    ```

--- packages/dht/test/unit/customMatchers.test.ts
+++ packages/dht/test/unit/customMatchers.test.ts
@@ -13,4 +13,22 @@ describe('custom matchers', () => {
     it('no match', () => {
         expect(createMockPeerDescriptor()).not.toEqualPeerDescriptor(createMockPeerDescriptor())
     })
+
+    describe('error message', () => {
+
+        it('normal', () => {
+            const actual = createMockPeerDescriptor()
+            const expected = createMockPeerDescriptor()
+            expect(() => {
+                expect(actual).toEqualPeerDescriptor(expected)
+            }).toThrow('PeerDescriptor nodeId values don\'t match')
+        })
+
+        it('inverse', () => {
+            const peerDescriptor = createMockPeerDescriptor()
+            expect(() => {
+                expect(peerDescriptor).not.toEqualPeerDescriptor(peerDescriptor)
+            }).toThrow('PeerDescriptors are equal')
+        })
+    })
 })
