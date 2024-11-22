2dae8b0a3deac11546a50473d31a270b21d8ba7e
Teo Gebhard <teo@streamr.com>
2024-11-22 14:39:06 +0200

    feat(sdk): `Mapping` concurrency (#2899)
    Improved concurrency for `Mapping` class. Now concurrent equal `get()`
    calls share the same promise, i.e. they evaluate the `valueFactory` only
    once.
    
    ## Example
    
    ```ts
    const valueFactory = async () => ...
    const mapping = new Mapping(valueFactory)
    await Promise.all([
        mapping.get('foo'),
        mapping.get('bar'),
        mapping.get('bar'),
        mapping.get('foo'),
        mapping.get('foo')
    ])
    ```
    
    Triggers two calls to `valueFactory` (one for `foo` and another for
    `bar`). Before this PR it triggered 5 calls.
    
    ## Small refactoring
    
    - Added `readonly` field annotations
    - Updated deprecated `jest` method call

--- packages/sdk/test/unit/Mapping.test.ts
+++ packages/sdk/test/unit/Mapping.test.ts
@@ -1,3 +1,4 @@
+import { wait } from '@streamr/utils'
 import { Mapping } from '../../src/utils/Mapping'
 
 describe('Mapping', () => {
@@ -27,7 +28,49 @@ describe('Mapping', () => {
         const mapping = new Mapping(valueFactory)
         expect(await mapping.get('foo')).toBe(undefined)
         expect(await mapping.get('foo')).toBe(undefined)
-        expect(valueFactory).toBeCalledTimes(1)
+        expect(valueFactory).toHaveBeenCalledTimes(1)
     })
 
+    it('rejections are not cached', async () => {
+        const valueFactory = jest.fn().mockImplementation(async (p1: string, p2: number) => {
+            throw new Error(`error ${p1}-${p2}`)
+        })
+        const mapping = new Mapping(valueFactory)
+        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
+        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
+        expect(valueFactory).toHaveBeenCalledTimes(2)
+    })
+
+    it('throws are not cached', async () => {
+        const valueFactory = jest.fn().mockImplementation((p1: string, p2: number) => {
+            throw new Error(`error ${p1}-${p2}`)
+        })
+        const mapping = new Mapping(valueFactory)
+        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
+        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
+        expect(valueFactory).toHaveBeenCalledTimes(2)
+    })
+
+    it('concurrency', async () => {
+        const valueFactory = jest.fn().mockImplementation(async (p1: string, p2: number) => {
+            await wait(50)
+            return `${p1}${p2}`
+        })
+        const mapping = new Mapping(valueFactory)
+        const results = await Promise.all([
+            mapping.get('foo', 1),
+            mapping.get('foo', 2),
+            mapping.get('foo', 2),
+            mapping.get('foo', 1),
+            mapping.get('foo', 1)
+        ])
+        expect(valueFactory).toHaveBeenCalledTimes(2)
+        expect(results).toEqual([
+            'foo1',
+            'foo2',
+            'foo2',
+            'foo1',
+            'foo1'
+        ])
+    })
 })
