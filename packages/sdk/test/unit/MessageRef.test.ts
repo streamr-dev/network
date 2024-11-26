f997668579f3a7e1d175540c920bdabf27926899
Teo Gebhard <teo@streamr.com>
2024-11-26 13:13:48 +0200

    jest/no-identical-title

--- packages/sdk/test/unit/MessageRef.test.ts
+++ packages/sdk/test/unit/MessageRef.test.ts
@@ -18,12 +18,12 @@ describe('MessageRef', () => {
             const mr2 = new MessageRef(2018043150, 6)
             assert.equal(mr1.compareTo(mr2), 1)
         })
-        it('should be less than', () => {
+        it('same timestamp, different sequenceNumber: should be less than', () => {
             const mr1 = new MessageRef(2018043150, 4)
             const mr2 = new MessageRef(2018043150, 8)
             assert.equal(mr1.compareTo(mr2), -1)
         })
-        it('should be greater than', () => {
+        it('same timestamp, different sequenceNumber: should be greater than', () => {
             const mr1 = new MessageRef(2018043150, 5)
             const mr2 = new MessageRef(2018043150, 2)
             assert.equal(mr1.compareTo(mr2), 1)
