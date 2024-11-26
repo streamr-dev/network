aa406f781cfd9e47a110dff6174eb6b40bacf48f
Teo Gebhard <teo@streamr.com>
2024-11-26 14:17:23 +0200

    test(sdk): Unify `toThrowStreamrError()` (#2894)
    The output is now similar to `toEqualPeerDescriptor()` (see
    https://github.com/streamr-dev/network/pull/2892).
    
    The custom matcher implementation now uses stricter types. Also improved
    output when the error thrown is a primitive value.
    
    ---------
    
    Co-authored-by: Santeri Juslenius <santeri.juslenius@gmail.com>

---
+++ packages/sdk/test/unit/customMatchers.test.ts
@@ -0,0 +1,59 @@
+import { StreamrClientError } from '../../src/StreamrClientError'
+
+describe('custom matchers', () => {
+
+    describe('toThrowStreamrError', () => {
+
+        it('happy path', () => {
+            const error = new StreamrClientError('Foobar', 'UNKNOWN_ERROR')
+            expect(() => {
+                throw error
+            }).toThrowStreamrError({
+                code: error.code,
+                message: error.message
+            })
+        })
+
+        describe('error message', () => {
+
+            it('field', () => {
+                const actual = new StreamrClientError('Foobar', 'UNKNOWN_ERROR')
+                expect(() => {
+                    expect(() => { throw actual }).toThrowStreamrError({
+                        message: 'Foobar',
+                        code: 'UNSUPPORTED_OPERATION'
+                    })
+                }).toThrow('StreamrClientError code values don\'t match')
+            })
+
+            it('unexpected class', () => {
+                // eslint-disable-next-line @typescript-eslint/no-extraneous-class
+                class TestClass {}
+                expect(() => {
+                    // eslint-disable-next-line @typescript-eslint/only-throw-error
+                    expect(() => { throw new TestClass() }).toThrowStreamrError({
+                        message: 'Foobar',
+                        code: 'UNSUPPORTED_OPERATION'
+                    })
+                }).toThrow('Not an instance of StreamrClientError:\nReceived: "TestClass"')
+            })
+
+            it('unexpected primitive', () => {
+                expect(() => {
+                    // eslint-disable-next-line @typescript-eslint/only-throw-error
+                    expect(() => { throw 'mock-error' }).toThrowStreamrError({
+                        message: 'Foobar',
+                        code: 'UNSUPPORTED_OPERATION'
+                    })
+                }).toThrow('Not an instance of StreamrClientError:\nReceived: "mock-error')
+            })
+
+            it('inverse', () => {
+                const actual = new StreamrClientError('Foobar', 'UNKNOWN_ERROR')
+                expect(() => {
+                    expect(() => { throw actual }).not.toThrowStreamrError(actual)
+                }).toThrow('Expected not to throw StreamrClientError')
+            })
+        })
+    })
+})
