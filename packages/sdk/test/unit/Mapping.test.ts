9a312608643155051c2b3af09229db3dd52b9f6e
Teo Gebhard <teo@streamr.com>
2024-12-04 14:55:41 +0200

    refactor(sdk): Use `Mapping` instead of `CachingMap` (#2903)
    Use `Mapping` cache instead of `CachingMap` in `StreamRegistry` and
    `StreamStorageRegistry`. After these modifications there are no usages
    for `CacheMap` in `sdk`, and we can remove it (in a follow-up PR).
    
    ## Testing
    
    The implementations of `Mapping` and `CachingMap` are very different,
    but the functionality should be almost/exactly same. Double-check that
    there are no corner case in which behavior is significantly different.
    Checked that CachingMap tests pass also with the new implementation (see
    temporary commit
    [e5bfdcf](https://github.com/streamr-dev/network/pull/2903/commits/e5bfdcfc94db2af5c0fc46c22261f4022a8473db)).
    The same functionality was already covered by `Mapping.test.ts`, so
    there was no need keep any `CachingMap.test.ts` test cases for the
    `Mapping` class.
    
    ## Other changes
    
    - Changed argument type of `Mapping#get()` and `Mapping#valueFactory()`,
    and return type of `Mapping#values()`
    
    ## Future improvements
    
    - Remove `CachingMap`

--- packages/sdk/test/unit/Mapping.test.ts
+++ packages/sdk/test/unit/Mapping.test.ts
@@ -5,17 +5,17 @@ import { range } from 'lodash'
 describe('Mapping', () => {
 
     it('create', async () => {
-        const mapping = createLazyMap({
-            valueFactory: async (p1: string, p2: number) => `${p1}${p2}`
+        const mapping = createLazyMap<[string, number], string>({
+            valueFactory: async ([p1, p2]: [string, number]) => `${p1}${p2}`
         })
-        expect(await mapping.get('foo', 1)).toBe('foo1')
-        expect(await mapping.get('bar', 2)).toBe('bar2')
+        expect(await mapping.get(['foo', 1])).toBe('foo1')
+        expect(await mapping.get(['bar', 2])).toBe('bar2')
     })
 
     it('memorize', async () => {
         let evaluationIndex = 0
-        const mapping = createLazyMap({
-            valueFactory: async (_p: string) => {
+        const mapping = createLazyMap<string, number>({
+            valueFactory: async (_p) => {
                 const result = evaluationIndex
                 evaluationIndex++
                 return result
@@ -31,60 +31,60 @@ describe('Mapping', () => {
     it('undefined', async () => {
         const valueFactory = jest.fn().mockResolvedValue(undefined)
         const mapping = createLazyMap({ valueFactory })
-        expect(await mapping.get('foo')).toBe(undefined)
-        expect(await mapping.get('foo')).toBe(undefined)
+        expect(await mapping.get(['foo'])).toBe(undefined)
+        expect(await mapping.get(['foo'])).toBe(undefined)
         expect(valueFactory).toHaveBeenCalledTimes(1)
     })
 
     it('rejections are not cached', async () => {
-        const valueFactory = jest.fn().mockImplementation(async (p1: string, p2: number) => {
+        const valueFactory = jest.fn().mockImplementation(async ([p1, p2]: [string, number]) => {
             throw new Error(`error ${p1}-${p2}`)
         })
         const mapping = createLazyMap({ valueFactory })
-        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
-        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
+        await expect(mapping.get(['foo', 1])).rejects.toEqual(new Error('error foo-1'))
+        await expect(mapping.get(['foo', 1])).rejects.toEqual(new Error('error foo-1'))
         expect(valueFactory).toHaveBeenCalledTimes(2)
     })
 
     it('throws are not cached', async () => {
-        const valueFactory = jest.fn().mockImplementation((p1: string, p2: number) => {
+        const valueFactory = jest.fn().mockImplementation(([p1, p2]: [string, number]) => {
             throw new Error(`error ${p1}-${p2}`)
         })
         const mapping = createLazyMap({ valueFactory })
-        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
-        await expect(mapping.get('foo', 1)).rejects.toEqual(new Error('error foo-1'))
+        await expect(mapping.get(['foo', 1])).rejects.toEqual(new Error('error foo-1'))
+        await expect(mapping.get(['foo', 1])).rejects.toEqual(new Error('error foo-1'))
         expect(valueFactory).toHaveBeenCalledTimes(2)
     })
 
     it('isCacheableValue', async () => {
-        const valueFactory = jest.fn().mockImplementation(async (p1: string, p2: number) => {
+        const valueFactory = jest.fn().mockImplementation(async ([p1, p2]: [string, number]) => {
             return `${p1}${p2}`
         })
         const mapping = createLazyMap({ valueFactory, isCacheableValue: (value: string) => value === 'foo1' })
-        const result1 = await mapping.get('foo', 1)
-        const result2 = await mapping.get('foo', 1)
+        const result1 = await mapping.get(['foo', 1])
+        const result2 = await mapping.get(['foo', 1])
         expect(result1).toBe('foo1')
         expect(result2).toBe('foo1')
         expect(valueFactory).toHaveBeenCalledTimes(1)
-        const result3 = await mapping.get('foo', 2)
-        const result4 = await mapping.get('foo', 2)
+        const result3 = await mapping.get(['foo', 2])
+        const result4 = await mapping.get(['foo', 2])
         expect(result3).toBe('foo2')
         expect(result4).toBe('foo2')
         expect(valueFactory).toHaveBeenCalledTimes(1 + 2)  // two additional calls as neither of the new calls was cached
     })
     
     it('concurrency', async () => {
-        const valueFactory = jest.fn().mockImplementation(async (p1: string, p2: number) => {
+        const valueFactory = jest.fn().mockImplementation(async ([p1, p2]: [string, number]) => {
             await wait(50)
             return `${p1}${p2}`
         })
         const mapping = createLazyMap({ valueFactory })
         const results = await Promise.all([
-            mapping.get('foo', 1),
-            mapping.get('foo', 2),
-            mapping.get('foo', 2),
-            mapping.get('foo', 1),
-            mapping.get('foo', 1)
+            mapping.get(['foo', 1]),
+            mapping.get(['foo', 2]),
+            mapping.get(['foo', 2]),
+            mapping.get(['foo', 1]),
+            mapping.get(['foo', 1])
         ])
         expect(valueFactory).toHaveBeenCalledTimes(2)
         expect(results).toEqual([
@@ -98,22 +98,22 @@ describe('Mapping', () => {
 
     it('max size', async () => {
         const SIZE = 3
-        const valueFactory = jest.fn().mockImplementation(async (p1: string, p2: number) => {
+        const valueFactory = jest.fn().mockImplementation(async ([p1, p2]: [string, number]) => {
             return `${p1}${p2}`
         })
         const mapping = createCacheMap({ valueFactory, maxSize: 3 })
         const ids = range(SIZE)
         for (const id of ids) {
-            await mapping.get('foo', id)
+            await mapping.get(['foo', id])
         }
         expect(valueFactory).toHaveBeenCalledTimes(3)
         // add a value which is not in cache
-        await mapping.get('foo', -1)
+        await mapping.get(['foo', -1])
         expect(valueFactory).toHaveBeenCalledTimes(4)
         // one of the items was removed from cache when -1 was added, now we is re-add that
         // (we don't know which item it was)
         for (const id of ids) {
-            await mapping.get('foo', id)
+            await mapping.get(['foo', id])
         }
         expect(valueFactory).toHaveBeenCalledTimes(5)
     })
@@ -121,13 +121,26 @@ describe('Mapping', () => {
     it('max age', async () => {
         const MAX_AGE = 100
         const JITTER = 50
-        const valueFactory = jest.fn().mockImplementation(async (p1: string, p2: number) => {
+        const valueFactory = jest.fn().mockImplementation(async ([p1, p2]: [string, number]) => {
             return `${p1}${p2}`
         })
         const mapping = createCacheMap({ valueFactory, maxSize: 999999, maxAge: MAX_AGE })
-        await mapping.get('foo', 1)
+        await mapping.get(['foo', 1])
         await wait(MAX_AGE + JITTER)
-        await mapping.get('foo', 1)
+        await mapping.get(['foo', 1])
         expect(valueFactory).toHaveBeenCalledTimes(2)
+    })
+
+    it('invalidate', async () => {
+        const mapping = createLazyMap<[string, number], string>({
+            valueFactory: async ([p1, p2]: [string, number]) => `${p1}${p2}`,
+        })
+        mapping.set(['foo', 1], 'foo1')
+        mapping.set(['bar', 1], 'bar1')
+        await mapping.get(['foo', 2])
+        await mapping.get(['bar', 2])
+        expect([...mapping.values()]).toIncludeSameMembers(['foo1', 'bar1', 'foo2', 'bar2'])
+        mapping.invalidate(([p1]) => (p1 === 'bar'))
+        expect([...mapping.values()]).toIncludeSameMembers(['foo1', 'foo2'])
     })
 })
