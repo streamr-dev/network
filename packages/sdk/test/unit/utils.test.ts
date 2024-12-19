989c1f4889863c77c7311cf0182ba33e650b49d0
Mariusz Roliński <mariusz@mondoreale.pl>
2024-12-19 18:25:56 +0100

    deps: Swap `node-fetch` library with a having-it-all native `fetch` (#2943)
    ## Summary
    
    I replace ES-only `node-fetch` with the native
    [`fetch`](https://nodejs.org/dist/v18.20.5/docs/api/globals.html). ~~a
    wrapper module that deals with CJS, ESM, and uses `window.fetch` within
    a browser environment.~~
    
    ~~`@streamr/fetch` wraps `node-fetch` v3 (most recent) and some
    signatures within it have been changed. This PR addresses it, too.~~
    
    ## Changes
    
    - Replace `node-fetch` with `fetch`.
    - Remove shims, tricks and fixes for `node-fetch` for the browser
    (mostly SDK).
    - `composeAbortSignals` can take an empty list of arguments and
    arguments can be either instances of `AbortSignal` or `undefined` –
    makes the utility a bit more flexible, and way more convenient.
    
    ## Limitations and future improvements
    
    - n/a
    
    ## Checklist before requesting a review
    
    - [x] Is this a breaking change? If it is, be clear in summary.
    - [x] Read through code myself one more time.
    - [x] Make sure any and all `TODO` comments left behind are meant to be
    left in.
    - [x] Has reasonable passing test coverage?
    - [x] ~~Updated changelog if applicable.~~
    - [x] ~~Updated documentation if applicable.~~

--- packages/sdk/test/unit/utils.test.ts
+++ packages/sdk/test/unit/utils.test.ts
@@ -1,6 +1,6 @@
 import { isRunningInElectron, startTestServer, testOnlyInNodeJs } from '@streamr/test-utils'
 import { collect, toLengthPrefixedFrame, until } from '@streamr/utils'
-import { Request, Response } from 'express'
+import express from 'express'
 import range from 'lodash/range'
 import { FetchHttpStreamResponseError, createQueryString, fetchLengthPrefixedFrameHttpBinaryStream, getEndpointUrl } from '../../src/utils/utils'
 import { nextValue } from './../../src/utils/iterators'
@@ -30,7 +30,7 @@ describe('utils', () => {
 
         it('happy path', async () => {
             const LINE_COUNT = 5
-            const server = await startTestServer('/', async (_req: Request, res: Response) => {
+            const server = await startTestServer('/', async (_req: express.Request, res: express.Response) => {
                 for (const i of range(LINE_COUNT)) {
                     res.write(toLengthPrefixedFrame(Buffer.from(`${i}`)))
                 }
@@ -43,7 +43,7 @@ describe('utils', () => {
 
         testOnlyInNodeJs('abort', async () => {
             let serverResponseClosed = false
-            const server = await startTestServer('/', async (_req: Request, res: Response) => {
+            const server = await startTestServer('/', async (_req: express.Request, res: express.Response) => {
                 res.on('close', () => {
                     serverResponseClosed = true
                 })
@@ -74,7 +74,7 @@ describe('utils', () => {
 
         it('invalid host', async () => {
             const iterator = fetchLengthPrefixedFrameHttpBinaryStream('http://mock.test')[Symbol.asyncIterator]()
-            await expect(() => nextValue(iterator)).rejects.toThrow(isRunningInElectron() ? /Failed to fetch/ : /getaddrinfo ENOTFOUND/)
+            await expect(() => nextValue(iterator)).rejects.toThrow(isRunningInElectron() ? /failed to fetch/i : /fetch failed/i)
         })
     })
 })
