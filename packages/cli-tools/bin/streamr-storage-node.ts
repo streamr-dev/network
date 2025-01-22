211294e798acdf13ea760b4dd39b2b039b05117b
Eric Andrews <harbu88@hotmail.com>
2025-01-22 10:28:35 +0200

    feat(cli-tools): show info about storage node

--- packages/cli-tools/bin/streamr-storage-node.ts
+++ packages/cli-tools/bin/streamr-storage-node.ts
@@ -7,6 +7,7 @@ program
     .usage('<command> [<args>]')
     .description('storage node subcommands')
     .command('list', 'list storage nodes')
+    .command('show', 'show information about a storage node')
     .command('register', 'register a storage node')
     .command('add-stream', 'add stream')
     .command('remove-stream', 'remove stream')
