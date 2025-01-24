181a740054b5c3a11e91f26df1441394e62ce731
Eric Andrews <harbu88@hotmail.com>
2025-01-24 11:39:30 +0200

    test: add test case for new commands

--- packages/cli-tools/test/storage-node.test.ts
+++ packages/cli-tools/test/storage-node.test.ts
@@ -3,6 +3,7 @@ import { createTestPrivateKey } from '@streamr/test-utils'
 import { until } from '@streamr/utils'
 import 'jest-extended'
 import { DOCKER_DEV_STORAGE_NODE, createTestClient, runCommand } from './utils'
+import { Wallet } from 'ethers'
 
 const isStored = async (streamId: StreamID): Promise<boolean> => {
     const output = await runCommand(`storage-node list-streams ${DOCKER_DEV_STORAGE_NODE}`)
@@ -30,4 +31,24 @@ describe('storage node', () => {
         const outputLines = await runCommand('storage-node list')
         expect(outputLines.join()).toMatch(DOCKER_DEV_STORAGE_NODE.toLowerCase())
     })
+
+    it('register storage node, show info, and finally unregister', async () => {
+        const privateKey = await createTestPrivateKey({ gas: true })
+        const publicAddress = new Wallet(privateKey).address
+
+        const urls = 'http://foobar.com,http://foobar.org'
+        await runCommand(`storage-node register ${urls}`, {
+            privateKey
+        })
+
+        const outputLines = await runCommand(`storage-node show ${publicAddress}`)
+        expect(outputLines.join()).toContain('http://foobar.com')
+        expect(outputLines.join()).toContain('http://foobar.org')
+
+        await runCommand('storage-node unregister', {
+            privateKey
+        })
+        const outputLines2 = await runCommand('storage-node list')
+        expect(outputLines2.join()).not.toContain(publicAddress.toLowerCase())
+    }, 80 * 1000)
 })
