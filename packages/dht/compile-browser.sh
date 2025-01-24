5ddcf9d150791d0a5d27910f0e9c505c808e4c58
Petri Savolainen <petri.savolainen@spaceify.net>
2025-01-24 06:17:29 +0200

    do not delete Node-version of connections in compile-browser

--- packages/dht/compile-browser.sh
+++ packages/dht/compile-browser.sh
@@ -2,9 +2,3 @@
 
 # Compile the browser version of the source code for webpacking as a part of a larger build process
 npx tsc -b tsconfig.compile-browser.json             
-
-# Remove the NodeWebsocketClientConnection and NodeWebrtcConnection files from dist
-# as they crash the browser when loading the webpacked bundle
-
-rm dist/src/connection/websocket/NodeWebsocketClientConnection.*
-rm dist/src/connection/webrtc/NodeWebrtcConnection.*
