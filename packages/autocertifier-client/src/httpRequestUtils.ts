39eb684bdc1fa096581eac00d7c035e88ba9971a
Eric Andrews <harbu88@hotmail.com>
2025-01-31 11:20:10 +0200

    fix test

--- packages/autocertifier-client/src/httpRequestUtils.ts
+++ packages/autocertifier-client/src/httpRequestUtils.ts
@@ -13,12 +13,12 @@ export async function request<T>(method: string, url: string, body: object, time
             let data = ''
             res.on('data', (chunk) => data += chunk)
             res.on('end', () => {
-                const responseBody = JSON.parse(data) as T
+                const responseBody = JSON.parse(data)
 
                 if (res.statusCode && res.statusCode >= 200 && res.statusCode < 300) {
                     resolve(responseBody)
                 } else {
-                    reject(new ServerError(new Err(ErrorCode.SERVER_ERROR, res.statusCode ?? 500, data)))
+                    reject(new ServerError(new Err(responseBody.code ?? ErrorCode.SERVER_ERROR, res.statusCode ?? 500, data)))
                 }
             })
         })
