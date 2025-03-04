a078bc0ff8aad2226971d3cb6b8fe55f1a536f69
Mariusz Roliński <mariusz@mondoreale.pl>
2025-03-04 16:44:39 +0100

    Again, fix esm exports
    Mods commit 086c8cc8c2301e7ff8207f307248bbbc8c443b17.

--- packages/sdk/src/exports-esm.mjs
+++ packages/sdk/src/exports-esm.mjs
@@ -1,7 +1,5 @@
 // ESM EntryPoint
-import { StreamrClient } from './exports.js'
-
-export * from './exports.js'
-
-export default StreamrClient
+export * from './index.js'
+// required to get import StreamrClient from '@streamr/sdk' to work
+export { StreamrClient as default } from './index.js'
 // note this file is manually copied as-is into dist/src since we don't want tsc to compile it to commonjs
