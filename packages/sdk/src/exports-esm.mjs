086c8cc8c2301e7ff8207f307248bbbc8c443b17
Mariusz Roliński <mariusz@mondoreale.pl>
2025-03-04 16:28:28 +0100

    SDK: Fix esm export
    Looks like it didn't fly far with the way it was exported. I've been getting `StreamrClient is not a constructor` errors.

--- packages/sdk/src/exports-esm.mjs
+++ packages/sdk/src/exports-esm.mjs
@@ -1,6 +1,7 @@
 // ESM EntryPoint
-import StreamrClient from './index.js'
-export * from './index.js'
-// required to get import StreamrClient from '@streamr/sdk' to work
-export default StreamrClient.default
+import { StreamrClient } from './exports.js'
+
+export * from './exports.js'
+
+export default StreamrClient
 // note this file is manually copied as-is into dist/src since we don't want tsc to compile it to commonjs
