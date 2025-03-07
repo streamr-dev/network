67c7f6728df487ee1d8f16df300cf33c6cae944c
juslesan <santeri.juslenius@gmail.com>
2025-03-07 11:13:44 +0200

    routing table size 20

--- packages/dht/src/dht/routing/RoutingSession.ts
+++ packages/dht/src/dht/routing/RoutingSession.ts
@@ -16,7 +16,7 @@ import { RoutingTable, RoutingTablesCache } from './RoutingTablesCache'
 const logger = new Logger(module)
 
 const MAX_FAILED_HOPS = 2
-const ROUTING_TABLE_MAX_SIZE = 10
+const ROUTING_TABLE_MAX_SIZE = 20
 
 export class RoutingRemoteContact extends Contact {
 
