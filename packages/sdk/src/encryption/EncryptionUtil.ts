d10fff89b4ca5cbb5cb60be38d1dac8b25bd863a
Henri Pihkala <henri@streamr.network>
2025-03-25 20:39:30 +0100

    EncryptionUtil supports ML-KEM

--- packages/sdk/src/encryption/EncryptionUtil.ts
+++ packages/sdk/src/encryption/EncryptionUtil.ts
@@ -1,9 +1,15 @@
 import crypto, { CipherKey } from 'crypto'
+import { ml_kem1024 } from '@noble/post-quantum/ml-kem';
+import { randomBytes } from '@noble/post-quantum/utils';
 import { StreamMessageAESEncrypted } from '../protocol/StreamMessage'
 import { StreamrClientError } from '../StreamrClientError'
 import { GroupKey } from './GroupKey'
 
 export const INITIALIZATION_VECTOR_LENGTH = 16
+
+const INFO = Buffer.from('streamr-key-exchange')
+const KEM_CIPHER_LENGTH_BYTES = 1568
+const KDF_SALT_LENGTH_BYTES = 64
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class EncryptionUtil {
@@ -23,6 +29,46 @@ export class EncryptionUtil {
 
     static decryptWithRSAPrivateKey(ciphertext: Uint8Array, privateKey: crypto.KeyLike): Buffer {
         return crypto.privateDecrypt(privateKey, ciphertext)
+    }
+
+    static encryptWithMLKEMPublicKey(plaintextBuffer: Uint8Array, publicKey: Uint8Array): Buffer {
+        // Encapsulate to get kemCipher and shared secret
+        // The recipient will be able to derive sharedSecret using privateKey and kemCipher
+        const { cipherText: kemCipher, sharedSecret } = ml_kem1024.encapsulate(publicKey);
+
+        if (kemCipher.length !== KEM_CIPHER_LENGTH_BYTES) {
+            throw new Error(`Expected KEM cipher to be ${KEM_CIPHER_LENGTH_BYTES}, but it was ${kemCipher.length} bytes`)
+        }
+
+        // Derive an AES wrapping key from the shared secret using HKDF
+        // The recipient will be able to repeat this computation to derive the same key
+        const kdfSalt = randomBytes(KDF_SALT_LENGTH_BYTES)
+        const wrappingAESKey = crypto.hkdfSync('sha512', sharedSecret, kdfSalt, INFO, 32);
+        
+        // Encrypt plaintext with the AES wrapping key
+        const aesEncryptedPlaintext = this.encryptWithAES(plaintextBuffer, Buffer.from(wrappingAESKey))
+
+        // Concatenate the deliverables into a binary package
+        return Buffer.concat([kemCipher, kdfSalt, aesEncryptedPlaintext]);
+    }
+
+    static decryptWithMLKEMPrivateKey(cipherPackage: Uint8Array, privateKey: Uint8Array): Buffer {
+        // Split the cipherPackage, see encryptWithMLKEMPublicKey how it's constructed
+        let pos = 0
+        const kemCipher = cipherPackage.slice(0, KEM_CIPHER_LENGTH_BYTES);
+        pos += KEM_CIPHER_LENGTH_BYTES
+        const kdfSalt = cipherPackage.slice(pos, pos + KDF_SALT_LENGTH_BYTES);
+        pos += KDF_SALT_LENGTH_BYTES
+        const aesEncryptedPlaintext = cipherPackage.slice(pos);
+
+        // Derive the shared secret using the private key and kemCipher
+        const sharedSecret = ml_kem1024.decapsulate(kemCipher, privateKey);
+
+        // Derive the wrappingAESKey
+        const wrappingAESKey = crypto.hkdfSync('sha512', sharedSecret, kdfSalt, INFO, 32);
+
+        // Decrypt the aesEncryptedPlaintext
+        return this.decryptWithAES(aesEncryptedPlaintext, Buffer.from(wrappingAESKey))
     }
 
     /*
