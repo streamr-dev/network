c12f400c299ae305e6f953bd7cc6cddedc72bad9
Henri Pihkala <henri@streamr.network>
2025-04-29 20:46:45 +0200

    fix cli-tools

--- packages/cli-tools/bin/streamr-identity-generate.ts
+++ packages/cli-tools/bin/streamr-identity-generate.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import '../src/logLevel'
-import { KEY_TYPES, SigningUtil } from '@streamr/utils'
+import { binaryToHex, KEY_TYPES, SigningUtil, toUserId } from '@streamr/utils'
 import { createCommand, Options } from '../src/command'
 import { formEnumArgValueDescription, createFnParseEnum } from '../src/common'
 
@@ -11,6 +11,9 @@ createCommand()
     .action(async (options: Options) => {
         const signingUtil = SigningUtil.getInstance(options.keyType!) // required option
         const keyPair = signingUtil.generateKeyPair()
-        console.info(JSON.stringify(keyPair, null, 4))
+        console.info(JSON.stringify({
+            publicKey: toUserId(keyPair.publicKey),
+            privateKey: binaryToHex(keyPair.privateKey),
+        }, null, 4))
     })
     .parse()
