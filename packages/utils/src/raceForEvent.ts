f9e76cbea8adfa0913687c03505fc8b0a301c484
Teo Gebhard <teo@streamr.com>
2025-05-20 14:29:56 +0300

    style

--- packages/utils/src/raceForEvent.ts
+++ packages/utils/src/raceForEvent.ts
@@ -21,8 +21,8 @@ export async function raceForEvent<TEvents extends Record<string, (...args: any[
         result = await Promise.race(promises)
     } finally {
         // Call raceAbortController.abort() to remove the event listeners. Note that this not strictly needed when Promise.race(promises) rejects.
-        // The race can reject only if `withTimeout()` timeouts, and as all timeouts happen at the same time, the event listeners are cleaned
-        // up immediately. In that sense this could be moved out from the `finally` block. But it makes sense to keep it here so that the function
+        // The race can reject only if withTimeout() timeouts, and as all timeouts happen at the same time, the event listeners are cleaned
+        // up immediately. In that sense this could be moved out from the finally block. But it makes sense to keep it here so that the function
         // can be seen as an atomic operation: all cleanup happens _before_ the function returns, not immediately _after_ it returns.
         // The Promise.allSettled() call implements that atomicity by waiting the cleanups initiated by raceAbortController.abort() to complete.
         raceAbortController.abort()
