ffdebfb61742eb73194012d0dc2874657417ecef
Teo Gebhard <teo@streamr.com>
2025-06-02 17:15:03 +0300

    feat(utils): `scheduleAtApproximateInterval()` (#3136)
    Added a scheduler which runs a task at approximate intervals, e.g. about
    once per hour. There is a configurable _drift multiplier_ which defines
    how much the wait time can vary: e.g. if the interval is 60 minutes and
    the drift is 0.1, the delay between invocations will range from 54 to 66
    minutes.
    
    ## Other changes
    
    Removed unnecessary optional chaining in `scheduleAtInterval.ts ` and
    improved test name in `scheduleAtInterval.test.ts`.
    
    ---------
    
    Co-authored-by: Eric Andrews <eric.andrews@streamr.com>

---
+++ packages/utils/src/scheduleAtApproximateInterval.ts
@@ -0,0 +1,32 @@
+import { scheduleAtInterval } from './scheduleAtInterval'
+import { wait } from './wait'
+
+/**
+ * @param {number} approximateIntervalInMs - approximate time (in milliseconds) to wait after a task is completed
+ * @param {number} driftMultiplier how much the wait time can vary: e.g. if the interval is 60 minutes and the drift is 0.1,
+ * the delay between invocations will range from 54 to 66 minutes
+ */
+export const scheduleAtApproximateInterval = async (
+    task: () => Promise<void>,
+    approximateIntervalInMs: number,
+    driftMultiplier: number,
+    executeAtStart: boolean,
+    abortSignal: AbortSignal
+): Promise<void> => {
+    if (abortSignal.aborted) {
+        return
+    }
+    if (executeAtStart) {
+        await task()
+    }
+    return scheduleAtInterval(async () => {
+        try {
+            await wait(Math.round(Math.random() * approximateIntervalInMs * 2 * driftMultiplier), abortSignal)
+        } catch {
+            // the abort signal timeouted, ignore
+        }
+        if (!abortSignal.aborted) {
+            await task()
+        }
+    }, approximateIntervalInMs * (1 - driftMultiplier), false, abortSignal)
+}
--- packages/utils/test/scheduleAtApproximateInterval.test.ts
+++ packages/utils/test/scheduleAtApproximateInterval.test.ts
@@ -1,11 +1,12 @@
-import { scheduleAtInterval } from '../src/scheduleAtInterval'
+import { scheduleAtApproximateInterval } from '../src/scheduleAtApproximateInterval'
 import { wait } from '../src/wait'
 
-const INTERVAL = 50
-const JITTER = INTERVAL * 2
-const AT_LEAST_FIVE_REPEATS_TIME = INTERVAL * 5 + JITTER
+const INTERVAL = 100
+const JITTER = INTERVAL / 4
+const DRIFT_MULTIPLIER = 0.1
+const AT_LEAST_FIVE_REPEATS_TIME = ((INTERVAL * (1 + DRIFT_MULTIPLIER)) + JITTER) * 5
 
-describe('scheduleAtInterval', () => {
+describe('scheduleAtApproximateInterval', () => {
     let task: jest.Mock<Promise<void>, []>
     let abortController: AbortController
 
@@ -19,31 +20,31 @@ describe('scheduleAtInterval', () => {
     })
 
     it('execute at start enabled', async () => {
-        await scheduleAtInterval(task, INTERVAL, true, abortController.signal)
+        await scheduleAtApproximateInterval(task, INTERVAL, DRIFT_MULTIPLIER, true, abortController.signal)
         expect(task).toHaveBeenCalledTimes(1)
     })
 
     it('execute at start disabled', async () => {
-        await scheduleAtInterval(task, INTERVAL, false, abortController.signal)
+        await scheduleAtApproximateInterval(task, INTERVAL, DRIFT_MULTIPLIER, false, abortController.signal)
         expect(task).toHaveBeenCalledTimes(0)
     })
 
     it('repeats every `interval`', async () => {
-        await scheduleAtInterval(task, INTERVAL, false, abortController.signal)
+        await scheduleAtApproximateInterval(task, INTERVAL, DRIFT_MULTIPLIER, false, abortController.signal)
         await wait(AT_LEAST_FIVE_REPEATS_TIME)
         expect(task.mock.calls.length).toBeGreaterThanOrEqual(5)
     })
 
     it('there is no special handling for slow tasks', async () => {
         task.mockImplementation(() => wait(INTERVAL))
-        await scheduleAtInterval(task, INTERVAL, false, abortController.signal)
+        await scheduleAtApproximateInterval(task, INTERVAL, DRIFT_MULTIPLIER, false, abortController.signal)
         await wait(AT_LEAST_FIVE_REPEATS_TIME)
         expect(task.mock.calls.length).toBeLessThan(5)
     })
 
     it('task never invoked if initially aborted', async () => {
         abortController.abort()
-        await scheduleAtInterval(task, INTERVAL, true, abortController.signal)
+        await scheduleAtApproximateInterval(task, INTERVAL, DRIFT_MULTIPLIER, true, abortController.signal)
         await wait(AT_LEAST_FIVE_REPEATS_TIME)
         expect(task).not.toHaveBeenCalled()
     })
