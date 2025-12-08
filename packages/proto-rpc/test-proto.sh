be2f35f730257f6415bae4cae66a34fbfb21919e
Mariusz Roliński <mariusz@mondoreale.pl>
2025-12-08 17:18:06 +0100

    Clean-up tsconfigs for the `proto-rpc` package

--- packages/proto-rpc/test-proto.sh
+++ packages/proto-rpc/test-proto.sh
@@ -1,2 +1,9 @@
-mkdir -p ./test/proto
-npx protoc --ts_out ./test/proto --ts_opt server_generic,generate_dependencies --proto_path test/protos test/protos/*.proto --experimental_allow_proto3_optional
+# Run protoc only when test/protos exists (it's absent in Docker builds)
+if [ -d ./test/protos ]; then
+  mkdir -p ./test/proto
+  npx protoc \
+    --ts_out ./test/proto \
+    --ts_opt server_generic,generate_dependencies \
+    --proto_path test/protos test/protos/*.proto \
+    --experimental_allow_proto3_optional
+fi
