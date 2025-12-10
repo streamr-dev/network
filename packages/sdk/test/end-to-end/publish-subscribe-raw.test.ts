8c5f0cd9b2968770f08327c85a1de48a6d5824a2
Teo Gebhard <teo@streamr.com>
2025-12-11 01:34:01 +0200

    feat(sdk): Raw publish (#3280)
    Added the `StreamrClient#publishRaw()` method.
    
    It is the counterpart to the existing `StreamrClient#subscribe({ raw:
    true }`) method. In raw publish/subscribe mode, the SDK’s message
    pipeline is bypassed: e.g. validation, decryption, and message ordering
    are skipped.

---
+++ packages/sdk/test/end-to-end/publish-subscribe-raw.test.ts
@@ -0,0 +1,52 @@
+import { StreamID, toUserId } from '@streamr/utils'
+import { createTestClient, createTestStream } from '../test-utils/utils'
+import { nextValue } from '../../src/utils/iterators'
+import { EthereumKeyPairIdentity } from '../../src/identity/EthereumKeyPairIdentity'
+import { MessageSigner } from '../../src/signature/MessageSigner'
+import { Wallet } from 'ethers'
+import { MessageID } from '../../src/protocol/MessageID'
+import { ContentType, EncryptionType, SignatureType } from '@streamr/trackerless-network'
+import { StreamMessageType, StreamPermission } from '../../src'
+import { createTestWallet } from '@streamr/test-utils'
+
+describe('publish-subscribe-raw', () => {
+
+    let streamId: StreamID
+    let publisherWallet: Wallet
+
+    beforeEach(async () => {
+        const creatorWallet = await createTestWallet({ gas: true })
+        const creatorClient = createTestClient(creatorWallet.privateKey)
+        const stream = await createTestStream(creatorClient, module)
+        streamId = stream.id
+        publisherWallet = await createTestWallet()
+        stream.grantPermissions({
+            userId: publisherWallet.address.toLowerCase(),
+            permissions: [StreamPermission.PUBLISH]
+        })
+        await creatorClient.destroy()
+    })
+
+    async function createTestMessage() {
+        const messageSigner = new MessageSigner(EthereumKeyPairIdentity.fromPrivateKey(publisherWallet.privateKey))
+        return await messageSigner.createSignedMessage({
+            messageId: new MessageID(streamId, 0, 123456789, 0, toUserId(publisherWallet.address), 'mock-msgChainId'),
+            content: new Uint8Array([1, 2, 3]),
+            contentType: ContentType.BINARY,
+            encryptionType: EncryptionType.NONE,
+            messageType: StreamMessageType.MESSAGE
+        }, SignatureType.ECDSA_SECP256K1_EVM)
+    }
+
+    it('happy path', async () => {
+        const publisher = createTestClient()
+        const subscriber = createTestClient()
+        const subcription = await subscriber.subscribe({ streamId, raw: true })
+        const sentMessage = await createTestMessage()
+        await publisher.publishRaw(sentMessage)
+        const receivedMessage = await nextValue(subcription[Symbol.asyncIterator]())
+        expect(receivedMessage! .streamMessage).toEqual(sentMessage)
+        await publisher.destroy()
+        await subscriber.destroy()
+    })
+})
