55315af1f4d836ef4383c5d515834ca4e9f0ad5c
juslesan <santeri.juslenius@gmail.com>
2025-12-16 13:16:10 +0200

    test

---
+++ packages/sdk/test/integration/disable-contract-validations.test.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata'
+
+import { StreamPartIDUtils } from "@streamr/utils"
+import { FakeEnvironment } from "../test-utils/fake/FakeEnvironment"
+import { nextValue } from "../../src/utils/iterators"
+
+describe('disable contract validations', () => {
+
+    it('happy path', async () => {
+        const environment = new FakeEnvironment()
+        const config = {
+            validation: {
+                permissions: false,
+                partitions: false
+            }
+        }
+        const publisher = environment.createClient(config)
+        const subscriber = environment.createClient(config)
+        const streamPartId = StreamPartIDUtils.parse('test.eth/foo/bar#4')
+        const subscription = await subscriber.subscribe(streamPartId)
+        await publisher.publish(streamPartId, {
+            message: 'hello'
+        })
+        const message = await nextValue(subscription[Symbol.asyncIterator]())
+        expect(message!.content).toEqual({
+            message: 'hello'
+        })
+        await publisher.destroy()
+        await subscriber.destroy()
+        await environment.destroy()
+    })
+})
