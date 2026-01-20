496ce660cd12c01dd875c5829cd7bf113ecc8e9a
Mariusz Roliński <mariusz@mondoreale.pl>
2026-01-21 00:17:05 +0100

    Use consistent DI resolution logic

--- packages/sdk/src/StreamrClient.ts
+++ packages/sdk/src/StreamrClient.ts
@@ -65,6 +65,7 @@ import { addStreamToStorageNode } from './utils/addStreamToStorageNode'
 import { assertCompliantIdentity } from './utils/encryptionCompliance'
 import { pOnce } from './utils/promises'
 import { convertPeerDescriptorToNetworkPeerDescriptor, createTheGraphClient } from './utils/utils'
+import { Tokens } from './tokens'
 
 // TODO: this type only exists to enable tsdoc to generate proper documentation
 export type SubscribeOptions = StreamDefinition & ExtraSubscribeOptions
@@ -137,8 +138,8 @@ export class StreamrClient {
         this.identity = identity
         this.theGraphClient = theGraphClient
         this.publisher = container.resolve<Publisher>(Publisher)
-        this.subscriber = container.resolve<Subscriber>(Subscriber)
-        this.resends = container.resolve<Resends>(Resends)
+        this.subscriber = container.resolve<Subscriber>(Tokens.Subscriber)
+        this.resends = container.resolve<Resends>(Tokens.Resends)
         this.node = container.resolve<NetworkNodeFacade>(NetworkNodeFacade)
         this.rpcProviderSource = container.resolve(RpcProviderSource)
         this.streamRegistry = container.resolve<StreamRegistry>(StreamRegistry)
