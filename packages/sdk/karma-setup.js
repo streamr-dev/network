0e09773600c9258cf4d37a9c89d68187e731f4ff
Mariusz Roliński <mariusz@mondoreale.pl>
2026-01-20 23:03:36 +0100

    Fix sdk's karma build

--- packages/sdk/karma-setup.js
+++ packages/sdk/karma-setup.js
@@ -2,7 +2,7 @@
  * This setup script is executed indirectly by Karma via the `browser-test-runner`
  * package. See karma-config.js in that package for more details.
  */
-
+import './src/setupTsyringe.ts'
 import './test/test-utils/customMatchers'
 import { customMatchers } from '@streamr/test-utils'
 
