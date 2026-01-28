a6798fb6d50521f23adced7bfd1cb09c51948edf
Mariusz Roliński <mariusz@mondoreale.pl>
2026-01-28 12:52:47 +0100

    Stick to "allowed" syntax

--- packages/sdk/test/test-utils/importMetaTransformer.js
+++ packages/sdk/test/test-utils/importMetaTransformer.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { TsJestTransformer } = require('ts-jest')
+import { TsJestTransformer } from 'ts-jest'
 
 class ImportMetaTransformer extends TsJestTransformer {
     process(sourceText, sourcePath, options) {
@@ -20,7 +20,8 @@ class ImportMetaTransformer extends TsJestTransformer {
     }
 }
 
-module.exports = {
+export default {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     createTransformer(tsJestConfig) {
         return new ImportMetaTransformer(tsJestConfig)
     },
